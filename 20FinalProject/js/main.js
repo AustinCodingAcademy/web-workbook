5cb7a46dcdad8db2032d2ec7d0c0a677bde010af
nicolemichelle88 <daugereaux@gmail.com>
2016-07-10 16:34:10 -0500

    Final Project in progress

---
+++ 20FinalProject/js/main.js
@@ -0,0 +1,21 @@
+// smooth scrolling jQuery
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// Owl Carousel
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
