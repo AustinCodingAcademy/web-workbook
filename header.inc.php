f6664dc10fd1c6e76da4d166ff72865d42bf518c
Salvador <salvadorrod13@gmail.com>
2016-07-11 16:31:28 -0500

    My PortFolio, & Final Project V2

---
+++ header.inc.php
@@ -0,0 +1,79 @@
+
+<!--   HEADER -->
+<header>
+  <!-- collapse menu -->
+  <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="navbar-header">
+          <button class="collapsed navbar-toggle" data-toggle="collapse" data-target="#nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <a href=".//index.html" class="navbar-brand">Salvador Rodriguez, (512) 652 6833</a>
+          <!-- <div class="logo">
+            <a href="../index.html">
+              <img alt="Studio" src="../img/logo-dark.png">
+            </a>
+          </div> -->
+
+        </div>
+        <div class="collapse navbar-collapse" id="nav-collapse">
+          <ul class="nav navbar-nav navbar-right">
+
+            <!-- Search in menu -->
+            <div class="search right">
+              <form action="#" method="get">
+                <input type="text" name="search" placeholder="Search">
+                <button>
+                  <i class="fa fa-search" aria-hidden="true"></i>
+                </button>
+              </form>
+            </div>
+
+            <li class="dropdown">
+              <a href="#portfolio" data-toggle="dropdown">PORTFOLIO<span class="caret"</span>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="#webdesign-portfolio">WEB DESIGN</a></li>
+                <li><a href="#powerbuilder-portfolio">POWER BUILDER</a></li>
+                <li><a href="#">DATA BASE DESIGN</a></li>
+                <li class="divider" role="separator"> header here</li>
+                <li><a href="#">ART WORK</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" data-toggle="dropdown">WORKBOOK<span class="caret"</span></a>
+              <ul class="dropdown-menu">
+                <!-- PORTAFOLIO menu with sub-menu -->
+                <li>
+                  <a class="test" href="#"  >INTRO WEB-WORKBOOK
+                    <i class="fa fa-caret-right" aria-hidden="true"></i>
+                  </a>
+                  <ul class="dropdown-menu sub-menu">
+                    <li><a href="./20FinalProject/">Final Project</a></li>
+                    <li><a href="./19LessonNineteen/">Lesson19</a></li>
+                    <li><a href="./18LessonEighteen/">Lesson18</a></li>
+                    <li><a href="./17LessonSeventeen/">Lesson17</a></li>
+                    <li><a href="./16LessonSixteen/">Lesson16</a></li>
+                    <li><a href="./15LessonFifteen/">Lesson15</a></li>
+                    <li><a href="./14LessonFourteen/">Lesson14</a></li>
+                    <li><a href="./13LessonThirteen/">Lesson13</a></li>
+                    <li><a href="./12LessonTwelve/">Lesson12</a></li>
+                    <li><a href="./10LessonEleven">Lesson11</a></li>
+                    <li><a href="./10LessonTen/">Lesson10</a></li>
+                  </ul>
+                </li>
+
+                <li><a href="#">PHP-WORKBOOK</a></li>
+                <li><a href="#">AJAX-WORKBOOK</a></li>
+                <li><a href="#">JAVA-WORKBOOK</a></li>
+              </ul>
+            </li>
+            <li><a href=".//reference.html">CLIENTS</a> </li>
+            <li><a href="#">ABOUT</a> </li>
+            <li><a href=".//contact.html">CONTACT</a> </li>
+          </ul>
+        </div>
+  </nav>
+</header> <!-- End of header -->
