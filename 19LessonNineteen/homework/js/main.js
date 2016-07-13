5b5d429c1205f8c4255ac6ec0b17f7344f6163ac
hcgrace05 <hcgrace.05@gmail.com>
2016-07-13 18:59:19 -0500

    final project

---
+++ 19LessonNineteen/homework/js/main.js
@@ -0,0 +1,143 @@
+jQuery(document).ready(function($) {
+
+
+/*
+|--------------------------------------------------------------------------
+| Global myTheme Obj / Variable Declaration
+|--------------------------------------------------------------------------
+|
+|
+|
+*/
+
+	var myTheme = window.myTheme || {},
+    $win = $( window );
+	
+
+
+/*
+|--------------------------------------------------------------------------
+| isotope
+|--------------------------------------------------------------------------
+|
+|
+|
+*/		
+
+	myTheme.Isotope = function () {
+	
+		// 4 column layout
+		var isotopeContainer = $('.isotopeContainer');
+		if( !isotopeContainer.length || !jQuery().isotope ) return;
+		$win.load(function(){
+			isotopeContainer.isotope({
+				itemSelector: '.isotopeSelector'
+			});
+		$('.isotopeFilters').on( 'click', 'a', function(e) {
+				$('.isotopeFilters').find('.active').removeClass('active');
+				$(this).parent().addClass('active');
+				var filterValue = $(this).attr('data-filter');
+				isotopeContainer.isotope({ filter: filterValue });
+				e.preventDefault();
+			});
+		});
+		
+		// 3 column layout
+		var isotopeContainer2 = $('.isotopeContainer2');
+		if( !isotopeContainer2.length || !jQuery().isotope ) return;
+		$win.load(function(){
+			isotopeContainer2.isotope({
+				itemSelector: '.isotopeSelector'
+			});
+		$('.isotopeFilters2').on( 'click', 'a', function(e) {
+				$('.isotopeFilters2').find('.active').removeClass('active');
+				$(this).parent().addClass('active');
+				var filterValue = $(this).attr('data-filter');
+				isotopeContainer2.isotope({ filter: filterValue });
+				e.preventDefault();
+			});
+		});
+		
+		// 2 column layout
+		var isotopeContainer3 = $('.isotopeContainer3');
+		if( !isotopeContainer3.length || !jQuery().isotope ) return;
+		$win.load(function(){
+			isotopeContainer3.isotope({
+				itemSelector: '.isotopeSelector'
+			});
+		$('.isotopeFilters3').on( 'click', 'a', function(e) {
+				$('.isotopeFilters3').find('.active').removeClass('active');
+				$(this).parent().addClass('active');
+				var filterValue = $(this).attr('data-filter');
+				isotopeContainer3.isotope({ filter: filterValue });
+				e.preventDefault();
+			});
+		});
+		
+		// 1 column layout
+		var isotopeContainer4 = $('.isotopeContainer4');
+		if( !isotopeContainer4.length || !jQuery().isotope ) return;
+		$win.load(function(){
+			isotopeContainer4.isotope({
+				itemSelector: '.isotopeSelector'
+			});
+		$('.isotopeFilters4').on( 'click', 'a', function(e) {
+				$('.isotopeFilters4').find('.active').removeClass('active');
+				$(this).parent().addClass('active');
+				var filterValue = $(this).attr('data-filter');
+				isotopeContainer4.isotope({ filter: filterValue });
+				e.preventDefault();
+			});
+		});
+	
+	};
+	
+	
+
+/*
+|--------------------------------------------------------------------------
+| Fancybox
+|--------------------------------------------------------------------------
+|
+|
+|
+*/		
+
+	myTheme.Fancybox = function () {
+		
+		$(".fancybox-pop").fancybox({
+			maxWidth	: 900,
+			maxHeight	: 700,
+			fitToView	: true,
+			width		: '80%',
+			height		: '80%',
+			autoSize	: false,
+			closeClick	: false,
+			openEffect	: 'elastic',
+			closeEffect	: 'none'
+		});
+	
+	};
+	
+		
+	
+	
+/*
+|--------------------------------------------------------------------------
+| Functions Initializers
+|--------------------------------------------------------------------------
+|
+|
+|
+*/
+	
+	myTheme.Isotope();
+	myTheme.Fancybox();
+	
+	
+
+});
+
+
+
+
