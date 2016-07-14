e0472cf545cf39389b8bca1a98f30c599343c727
nina2cool <christinalmcmillan@gmail.com>
2016-07-14 16:53:58 -0500

    final project commit

---
+++ 20FinalProject/js/scroll.js
@@ -0,0 +1,16 @@
+// Javascript for the scroll functionality
+
+$('a[href*=#]:not([href=#myCarousel]').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') ||
+        location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
