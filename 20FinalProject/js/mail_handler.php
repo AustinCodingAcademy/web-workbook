8712c4ae9a9a02d0f709625fd3b5a5cab53ca8c6
nicolemichelle88 <daugereaux@gmail.com>
2016-07-14 19:03:58 -0500

    .

--- 20FinalProject/js/mail_handler.php
+++ 20FinalProject/js/mail_handler.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])){
-    $to = "[email protected]"; // this is your Email address
+    $to = "[daugereaux@gmail.com]"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
