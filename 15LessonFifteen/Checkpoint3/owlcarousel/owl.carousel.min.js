a2a1496502d40458a500cf5c069794873403249a
PAJARO1 <larrygutierrez@Larrys-MacBook-Pro.local>
2016-09-11 22:44:48 -0500

    not done yet lesson 15

---
+++ 15LessonFifteen/Checkpoint3/owlcarousel/owl.carousel.min.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    margin: 5,
+    items: 1,
+    nav: true,
+    responsive: {
+      480: {
+        items: 2
+      },
+      620: {
+        items: 3
+      },
+      810: {
+        items: 4
+      },
+      1400: {
+        items: 6
+      }
+    }
+  });
+})
