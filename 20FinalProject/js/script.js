16f8cdd38d008cf62d54fc8e66c6f76076523944
Stacihs <stacihs@hotmail.com>
2016-09-20 19:44:23 -0500

    20FinalProject

---
+++ 20FinalProject/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:0,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:3
+          }
+      }
+  })
+});
