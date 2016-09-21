b78c19c91bc57794122300f7c32ef42771f285b6
khadijah17 <khadijah17@yahoo.com>
2016-09-20 19:00:25 -0500

    changes to final project

---
+++ 20FinalProject/js/script.js
@@ -0,0 +1,31 @@
+carousel = (function(){
+  var box = document.querySelector('.carouselbox');
+  var next = box.querySelector('.next');
+  var prev = box.querySelector('.prev');
+  var items = box.querySelectorAll('.content li');
+  var counter = 0;
+  var amount = items.length;
+  var current = items[0];
+  box.classList.add('active');
+  function navigate(direction) {
+    current.classList.remove('current');
+    counter = counter + direction;
+    if (direction === -1 &&
+        counter < 0) {
+      counter = amount - 1;
+    }
+    if (direction === 1 &&
+        !items[counter]) {
+      counter = 0;
+    }
+    current = items[counter];
+    current.classList.add('current');
+  }
+  next.addEventListener('click', function(ev) {
+    navigate(1);
+  });
+  prev.addEventListener('click', function(ev) {
+    navigate(-1);
+  });
+  navigate(0);
+})();
