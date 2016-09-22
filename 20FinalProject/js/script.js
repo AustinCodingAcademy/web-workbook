6f72b0e219eb920881f6f00c8a9eddec071ba970
Stacihs <stacihs@hotmail.com>
2016-09-22 18:30:31 -0500

    20Final
    owl carousel fix

--- 20FinalProject/js/script.js
+++ 20FinalProject/js/script.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:2
-          },
-          1000:{
-              items:3
-          }
+  $(".owl-carousel").owlCarousel({
+    margin: 5,
+    items: 1,
+    nav: true,
+    responsive: {
+      480: {
+        items: 1
+      },
+      620: {
+        items: 2
+      },
+      810: {
+        items: 3
+      },
+      1400: {
+        items: 4
       }
-  })
-});
+    }
+  });
+})
