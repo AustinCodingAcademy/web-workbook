3c7bb29ced92c5c6202f08fff6676bf58cfeb1a2
khadijah17 <khadijah17@yahoo.com>
2016-09-22 19:30:48 -0500

    20
    20

--- 20FinalProject/js/script.js
+++ 20FinalProject/js/script.js
@@ -1,31 +1,21 @@
-carousel = (function(){
-  var box = document.querySelector('.carouselbox');
-  var next = box.querySelector('.next');
-  var prev = box.querySelector('.prev');
-  var items = box.querySelectorAll('.content li');
-  var counter = 0;
-  var amount = items.length;
-  var current = items[0];
-  box.classList.add('active');
-  function navigate(direction) {
-    current.classList.remove('current');
-    counter = counter + direction;
-    if (direction === -1 &&
-        counter < 0) {
-      counter = amount - 1;
-    }
-    if (direction === 1 &&
-        !items[counter]) {
-      counter = 0;
-    }
-    current = items[counter];
-    current.classList.add('current');
-  }
-  next.addEventListener('click', function(ev) {
-    navigate(1);
-  });
-  prev.addEventListener('click', function(ev) {
-    navigate(-1);
-  });
-  navigate(0);
-})();
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        margin: 5,
+        items: 4,
+        nav: true,
+        responsive: {
+            480: {
+                items: 2
+            },
+            620: {
+                items: 3
+            },
+            810: {
+                items: 4
+            },
+            1400: {
+                items: 6
+            }
+        }
+    });
+});
