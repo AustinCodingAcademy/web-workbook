50b75fc8443f62572c4edf5073093ade40124c65
Austin Shipp <austinshipp@yahoo.com>
2016-09-25 01:23:42 -0500

    Fixed parallax width bug causing horizontal margin

--- 20FinalProject/js/script.js
+++ 20FinalProject/js/script.js
@@ -2,13 +2,6 @@ $(function() {
     $('.carousel').carousel({
         interval: 3000
     })
-    $('.click-animate').click(function() {
-        var heading = $(this)
-        heading.removeClass('flip');
-        setTimeout(function() {
-            heading.addClass('flip click-animate-activate');
-        }, 1);
-    })
     $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
