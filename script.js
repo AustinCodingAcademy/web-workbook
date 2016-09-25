221b9aab7b7e3b289484d541bf4434646f881bde
soapej <jonathansoape@gmail.com>
2016-09-25 13:00:48 -0500

    Intro Workbook Homepage
    Added functions to clear headers on hover. Replaced some images

---
+++ script.js
@@ -0,0 +1,528 @@
+$(document).ready(function() {
+
+  /*   1   */
+  $('.hw1').hover(function() {
+    $('.one').css({
+      "background-image": "url(img/screen/hw1.png)"
+    });
+    $('.one > .links').css({
+      "margin-top": "225px"
+    });
+    $('.one > h3').hide();
+  });
+
+  $('.hw1').mouseout(function() {
+    $('.one').css({
+      "background-image": "url(img/wiley.jpg)"
+    });
+    $('.one > .links').css({
+      "margin-top": "150px"
+    });
+    $('.one > h3').show();
+  });
+
+  /*  2  */
+  $('.pr2').hover(function() {
+    $('.two').css({
+      "background-image": "url(img/screen/pr2.png)"
+    });
+    $('.two > .links').css({
+      "margin-top": "225px"
+    });
+    $('.two > h3').hide();
+  });
+
+  $('.pr2').mouseout(function() {
+    $('.two').css({
+      "background-image": "url(img/mickeyPeril.jpg)"
+    });
+    $('.two > .links').css({
+      "margin-top": "150px"
+    });
+    $('.two > h3').show();
+  });
+
+  $('.hw2').hover(function() {
+    $('.two').css({
+      "background-image": "url(img/screen/hw2.png)"
+    });
+    $('.two > .links').css({
+      "margin-top": "225px"
+    });
+    $('.two > h3').hide();
+  });
+
+  $('.hw2').mouseout(function() {
+    $('.two').css({
+      "background-image": "url(img/mickeyPeril.jpg)"
+    });
+    $('.two > .links').css({
+      "margin-top": "150px"
+    });
+    $('.two > h3').show();
+  });
+
+  // /*    3     */
+  $('.pr3').hover(function() {
+    $('.three').css({
+      "background-image": "url(img/screen/pr3.png)"
+    });
+    $('.three > .links').css({
+      "margin-top": "225px"
+    });
+    $('.three > h3').hide();
+  });
+
+  $('.pr3').mouseout(function() {
+    $('.three').css({
+      "background-image": "url(img/porkyHunt.jpg)"
+    });
+    $('.three > .links').css({
+      "margin-top": "150px"
+    });
+    $('.three > h3').show();
+  });
+
+  $('.hw3').hover(function() {
+    $('.three').css({
+      "background-image": "url(img/screen/hw3.png)"
+    });
+    $('.three > .links').css({
+      "margin-top": "225px"
+    });
+    $('.three > h3').hide();
+  });
+
+  $('.hw3').mouseout(function() {
+    $('.three').css({
+      "background-image": "url(img/porkyHunt.jpg)"
+    });
+    $('.three > .links').css({
+      "margin-top": "150px"
+    });
+    $('.three > h3').show();
+  });
+
+  // /*    4     */
+  $('.pr4').hover(function() {
+    $('.four').css({
+      "background-image": "url(img/screen/pr4.png)"
+    });
+    $('.four > .links').css({
+      "margin-top": "225px"
+    });
+    $('.four > h3').hide();
+  });
+
+  $('.pr4').mouseout(function() {
+    $('.four').css({
+      "background-image": "url(img/hippo.png)"
+    });
+    $('.four > .links').css({
+      "margin-top": "150px"
+    });
+    $('.four > h3').show();
+  });
+
+  // /*    5     */
+  $('.ch1').hover(function() {
+    $('.five').css({
+      "background-image": "url(img/screen/ch1.png)"
+    });
+    $('.five > .links').css({
+      "margin-top": "225px"
+    });
+    $('.five > h3').hide();
+  });
+
+  $('.ch1').mouseout(function() {
+    $('.five').css({
+      "background-image": "url(img/mickeyBeer.jpg)"
+    });
+    $('.five > .links').css({
+      "margin-top": "150px"
+    });
+    $('.five > h3').show();
+  });
+
+
+  // /*    6     */
+  $('.pr6').hover(function() {
+    $('.six').css({
+      "background-image": "url(img/screen/pr6.png)"
+    });
+    $('.six > .links').css({
+      "margin-top": "225px"
+    });
+    $('.six > h3').hide();
+  });
+
+  $('.pr6').mouseout(function() {
+    $('.six').css({
+      "background-image": "url(img/minnie.jpg)"
+    });
+    $('.six > .links').css({
+      "margin-top": "150px"
+    });
+    $('.six > h3').show();
+  });
+
+
+  // /*    7     */
+  $('.pr7').hover(function() {
+    $('.seven').css({
+      "background-image": "url(img/screen/pr7.png)"
+    });
+    $('.seven > .links').css({
+      "margin-top": "225px"
+    });
+    $('.seven > h3').hide();
+  });
+
+  $('.pr7').mouseout(function() {
+    $('.seven').css({
+      "background-image": "url(img/streetFighter.jpg)"
+    });
+    $('.seven > .links').css({
+      "margin-top": "150px"
+    });
+    $('.seven > h3').show();
+  });
+
+  $('.hw7').hover(function() {
+    $('.seven').css({
+      "background-image": "url(img/screen/hw7.png)"
+    });
+    $('.seven > .links').css({
+      "margin-top": "225px"
+    });
+    $('.seven > h3').hide();
+  });
+
+  $('.hw7').mouseout(function() {
+    $('.seven').css({
+      "background-image": "url(img/streetFighter.jpg)"
+    });
+    $('.seven > .links').css({
+      "margin-top": "150px"
+    });
+    $('.seven > h3').show();
+  });
+
+  // /*    8     */
+  $('.pr8').hover(function() {
+    $('.eight').css({
+      "background-image": "url(img/screen/pr8.png)"
+    });
+    $('.eight > .links').css({
+      "margin-top": "225px"
+    });
+    $('.eight > h3').hide();
+  });
+
+  $('.pr8').mouseout(function() {
+    $('.eight').css({
+      "background-image": "url(img/dogs.jpg)"
+    });
+    $('.eight > .links').css({
+      "margin-top": "150px"
+    });
+    $('.eight > h3').show();
+  });
+
+
+
+  // /*    9     */
+  $('.pr9').hover(function() {
+    $('.nine').css({
+      "background-image": "url(img/screen/pr9.png)"
+    });
+    $('.nine > .links').css({
+      "margin-top": "225px"
+    });
+    $('.nine > h3').hide();
+  });
+
+  $('.pr9').mouseout(function() {
+    $('.nine').css({
+      "background-image": "url(img/donaldMickey.jpg)"
+    });
+    $('.nine > .links').css({
+      "margin-top": "150px"
+    });
+    $('.nine > h3').show();
+  });
+
+  //
+  // /*    10     */
+  $('.ch2').hover(function() {
+    $('.ten').css({
+      "background-image": "url(img/screen/ch2.png)"
+    });
+    $('.ten > .links').css({
+      "margin-top": "225px"
+    });
+    $('.ten > h3').hide();
+  });
+
+  $('.ch2').mouseout(function() {
+    $('.ten').css({
+      "background-image": "url(img/goofy.jpg)"
+    });
+    $('.ten > .links').css({
+      "margin-top": "150px"
+    });
+    $('.ten > h3').show();
+  });
+
+  $('.optional').hover(function() {
+    $('.ten').css({
+      "background-image": "url(img/screen/opt.png)"
+    });
+    $('.ten > .links').css({
+      "margin-top": "225px"
+    });
+    $('.ten > h3').hide();
+  });
+
+  $('.optional').mouseout(function() {
+    $('.ten').css({
+      "background-image": "url(img/goofy.jpg)"
+    });
+    $('.ten > .links').css({
+      "margin-top": "150px"
+    });
+    $('.ten > h3').show();
+  });
+
+  // /*    11     */
+  $('.pr11').hover(function() {
+    $('.eleven').css({
+      "background-image": "url(img/screen/pr11.png)"
+    });
+    $('.eleven > .links').css({
+      "margin-top": "225px"
+    });
+    $('.eleven > h3').hide();
+  });
+
+  $('.pr11').mouseout(function() {
+    $('.eleven').css({
+      "background-image": "url(img/pluto.jpg)"
+    });
+    $('.eleven > .links').css({
+      "margin-top": "150px"
+    });
+    $('.eleven > h3').show();
+  });
+
+  $('.hw11').hover(function() {
+    $('.eleven').css({
+      "background-image": "url(img/screen/hw11.png)"
+    });
+    $('.eleven > .links').css({
+      "margin-top": "225px"
+    });
+    $('.eleven > h3').hide();
+  });
+
+  $('.hw11').mouseout(function() {
+    $('.eleven').css({
+      "background-image": "url(img/pluto.jpg)"
+    });
+    $('.eleven > .links').css({
+      "margin-top": "150px"
+    });
+    $('.eleven > h3').show();
+  });
+
+  // /*    12     */
+  $('.pr12').hover(function() {
+    $('.twelve').css({
+      "background-image": "url(img/screen/pr12.png)"
+    });
+    $('.twelve > .links').css({
+      "margin-top": "225px"
+    });
+    $('.twelve > h3').hide();
+  });
+
+  $('.pr12').mouseout(function() {
+    $('.twelve').css({
+      "background-image": "url(img/daffy.png)"
+    });
+    $('.twelve > .links').css({
+      "margin-top": "150px"
+    });
+    $('.twelve > h3').show();
+  });
+
+  $('.hw12').hover(function() {
+    $('.twelve').css({
+      "background-image": "url(img/screen/hw12.png)"
+    });
+    $('.twelve > .links').css({
+      "margin-top": "225px"
+    });
+    $('.twelve > h3').hide();
+  });
+
+  $('.hw12').mouseout(function() {
+    $('.twelve').css({
+      "background-image": "url(img/daffy.png)"
+    });
+    $('.twelve > .links').css({
+      "margin-top": "150px"
+    });
+    $('.twelve > h3').show();
+  });
+
+  // /*    13     */
+  $('.pr13').hover(function() {
+    $('.thirteen').css({
+      "background-image": "url(img/screen/pr13.png)"
+    });
+    $('.thirteen > .links').css({
+      "margin-top": "225px"
+    });
+    $('.thirteen > h3').hide();
+  });
+
+  $('.pr13').mouseout(function() {
+    $('.thirteen').css({
+      "background-image": "url(img/scientist.jpg)"
+    });
+    $('.thirteen > .links').css({
+      "margin-top": "150px"
+    });
+    $('.thirteen > h3').show();
+  });
+
+  $('.hw13').hover(function() {
+    $('.thirteen').css({
+      "background-image": "url(img/screen/hw13.png)"
+    });
+    $('.thirteen > .links').css({
+      "margin-top": "225px"
+    });
+    $('.thirteen > h3').hide();
+  });
+
+  $('.hw13').mouseout(function() {
+    $('.thirteen').css({
+      "background-image": "url(img/scientist.jpg)"
+    });
+    $('.thirteen > .links').css({
+      "margin-top": "150px"
+    });
+    $('.thirteen > h3').show();
+  });
+
+  // /*    14     */
+  $('.pr14').hover(function() {
+    $('.fourteen').css({
+      "background-image": "url(img/screen/pr14.png)"
+    });
+    $('.fourteen > .links').css({
+      "margin-top": "225px"
+    });
+    $('.fourteen > h3').hide();
+  });
+
+  $('.pr14').mouseout(function() {
+    $('.fourteen').css({
+      "background-image": "url(img/plutoAngel.png)"
+    });
+    $('.fourteen > .links').css({
+      "margin-top": "150px"
+    });
+    $('.fourteen > h3').show();
+  });
+
+  // /*    15     */
+  $('.ch3').hover(function() {
+    $('.fifteen').css({
+      "background-image": "url(img/screen/ch3.png)"
+    });
+    $('.fifteen > .links').css({
+      "margin-top": "225px"
+    });
+    $('.fifteen > h3').hide();
+  });
+
+  $('.ch3').mouseout(function() {
+    $('.fifteen').css({
+      "background-image": "url(img/kid.png)"
+    });
+    $('.fifteen > .links').css({
+      "margin-top": "150px"
+    });
+    $('.fifteen > h3').show();
+  });
+
+  // /*    16     */
+  $('.pr16').hover(function() {
+    $('.sixteen').css({
+      "background-image": "url(img/screen/pr16.png)"
+    });
+    $('.sixteen > .links').css({
+      "margin-top": "225px"
+    });
+    $('.sixteen > h3').hide();
+  });
+
+  $('.pr16').mouseout(function() {
+    $('.sixteen').css({
+      "background-image": "url(img/scientist2.jpg)"
+    });
+    $('.sixteen > .links').css({
+      "margin-top": "150px"
+    });
+    $('.sixteen > h3').show();
+  });
+
+
+
+
+
+  /*    17     */
+  $('.pr17').hover(function() {
+    $('.seventeen').css({
+      "background-image": "url(img/screen/pr17.png)"
+    });
+    $('.seventeen > .links').css({
+      "margin-top": "225px"
+    });
+    $('.seventeen > h3').hide();
+  });
+
+  $('.pr17').mouseout(function() {
+    $('.seventeen').css({
+      "background-image": "url(img/plutoAngel.png)"
+    });
+    $('.seventeen > .links').css({
+      "margin-top": "150px"
+    });
+    $('.seventeen > h3').show();
+  });
+
+  /*    18     */
+
+
+  /*    19     */
+
+
+  /*    20     */
+  // $('.final').hover(function() {
+  //   $('.fifteen').css({
+  //     "background-image": "url(img/screen/ch3.png)"
+  //   });
+  // });
+  //
+  // $('.ch3').mouseout(function() {
+  //   $('.fifteen').css({
+  //     "background-image": "url(img/kid.png)"
+  //   });
+  // });
+
+});
