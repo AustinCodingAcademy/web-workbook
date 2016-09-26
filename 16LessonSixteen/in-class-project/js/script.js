03b2fa9753719e64690672d4867407bda8dde032
Austin Shipp <austinshipp@yahoo.com>
2016-09-26 14:13:56 -0500

    Added Modal Enabled Application Form on Lesson 16 In Class

--- 16LessonSixteen/in-class-project/js/script.js
+++ 16LessonSixteen/in-class-project/js/script.js
@@ -1,7 +1,9 @@
-$('.animate').hover(function() {
-  var heading =  $(this);
-  heading.removeClass('rubberBand');
-  setTimeout(function() {
-    heading.addClass('rubberBand');
-  }, 10);
+$(function() {
+    $('.animate').hover(function() {
+        var heading = $(this);
+        heading.removeClass('rubberBand');
+        setTimeout(function() {
+            heading.addClass('rubberBand');
+        }, 10);
+    })
 });
--- 20FinalProject/js/script.js
+++ 20FinalProject/js/script.js
@@ -1,20 +1,31 @@
 $(function() {
+    // CAROUSEL SPEED
     $('.carousel').carousel({
-        interval: 3000
-    })
+            interval: 3000
+        })
+        // SMOOTH SCROLLING
     $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
             }
-        }
-    })
+        })
+        // MODAL PLUGIN
     $('#myModal').on('shown.bs.modal', function() {
-        $('#myInput').focus()
-    })
+            $('#myInput').focus()
+        })
+        // "Hello!" ANIMATION SETTINGS
+    $('.animated').click(function() {
+        var heading = $(this)
+        heading.removeClass('rubberBand');
+        setTimeout(function() {
+            heading.addClass('rubberBand');
+        }, .1);
+    });
 });
