b83265834fcb5781ba2e71a0b6721fb518d9eea3
Clint Lucio <clintlucio@gmail.com>
2016-09-27 15:39:10 -0500

    Lesson 17 homework
    Still having sizing issues with tablet view. Any changes to tablet
    effects laptop view.

---
+++ 17LessonSeventeen/homework/js/script.js
@@ -0,0 +1,38 @@
+<script type="text/javascript" src="js/script.js"></script>
+$('#slider').slideReveal({
+  trigger: $("#trigger")
+});
+$(document).ready(function() {
+  $('.box').hover(function(){
+      $(this).find('.caption').slideDown();
+    },
+    function(){
+      $(this).find('.caption').slideUp();
+    }
+  );
+  });
+  $(function() {
+    $("img.scale").imageScale();
+  });
+  <script>
+      var slideIndex = 1;
+      showDivs(slideIndex);
+
+      function plusDivs(n) {
+          showDivs(slideIndex += n);
+      }
+
+      function showDivs(n) {
+          var i;
+          var x = document.getElementsByClassName("mySlides");
+          if (n > x.length) {
+              slideIndex = 1
+          }
+          if (n < 1) {
+              slideIndex = x.length
+          }
+          for (i = 0; i < x.length; i++) {
+              x[i].style.display = "none";
+          }
+          x[slideIndex - 1].style.display = "block";
+      }
