56a7ce3f8b4241c4298ee34f8df30aa18ae3f4f6
crackoner <crackoner@hotmail.com>
2016-09-29 16:25:38 -0500

    Lesson 20 - Final Project

---
+++ 20FinalProject/js/smooth_scroll.js
@@ -0,0 +1,19 @@
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $(id).offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+});
