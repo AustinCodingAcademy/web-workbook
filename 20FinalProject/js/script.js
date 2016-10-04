2617933ce0fd3515b64851aa0211a04e9cae784b
Mona0310 <mengxun0310@gmail.com>
2016-10-04 00:46:09 -0500

    Final Project - Xun Meng

---
+++ 20FinalProject/js/script.js
@@ -0,0 +1,34 @@
+// function nav () {
+// if('.nav>ul>li'). hasClass('selected'){
+// ('.selected').addClass('active');
+// var currentleft=$('.selected').position().left+"px";
+// var currentwidth=$('.selected').css('width');
+// $('.lamp').css({"left":currentleft,"width":currentwidth});
+// }
+// else{
+//   ('.nav>ul>li').first().addClass('active');
+//   var currentleft=$('.active').position().left+"px";
+// var currentwidth=$('.active').css('width');
+// ('.lamp').css({"left":currentleft,"width":currentwidth});
+// }
+// ('.nav>ul>li').hover(function(){
+//   ('.nav ul li').removeClass('active');
+//   (this).addClass('active');
+// var currentleft=('.active').position().left+"px";
+// var currentwidth=('.active').css('width');
+// ('.lamp').css({"left":currentleft,"width":currentwidth});
+// },function(){
+// if('.nav>ul>li').hasClass('selected'){
+// ('.selected').addClass('active');
+// var currentleft=('.selected').position().left+"px";
+// var currentwidth=('.selected').css('width');
+// ('.lamp').css({"left":currentleft,"width":currentwidth});
+// }
+// else{
+//   ('.nav>ul>li').first().addClass('active');
+//   var currentleft=('.active').position().left+"px";
+// var currentwidth=('.active').css('width');
+// ('.lamp').css({"left":currentleft,"width":currentwidth});
+// }
+// });
+// });
