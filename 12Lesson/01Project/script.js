f383244881ec7776bdc95c9e5f4ce463f637d3a8
lfilipos <luke@austincodingacademy.com>
2016-11-30 21:05:15 -0600

    12lesson01project initial try

---
+++ 12Lesson/01Project/script.js
@@ -0,0 +1,8 @@
+// 
+// $('.slide-img').hover(function() {
+//   var heading =  $(this)
+//   heading.removeClass('animated slideOutUp');
+//   setTimeout(function() {
+//     heading.addClass('animated slideOutUp');
+//   }, 10);
+// });
