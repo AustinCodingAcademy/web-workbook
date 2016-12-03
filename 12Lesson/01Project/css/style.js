6d7e6a6748b5718ee267fe72045faa674dcf9711
tatianagwatson <tatianagwatson@gmail.com>
2016-12-02 22:29:58 -0600

    12Lesson/01Project

--- 12Lesson/01Project/css/style.js
+++ 12Lesson/01Project/css/style.js
@@ -1,7 +1,15 @@
-$('.animated').click(function() {
+$('.animated').hover(function() {
   var heading =  $(this)
   heading.removeClass('bounce');
   setTimeout(function() {
     heading.addClass('bounce');
+  }, 10);
+});
+
+$('.animated').hover(function() {
+  var heading =  $('tada')
+  heading.removeClass('tada');
+  setTimeout(function() {
+    heading.addClass('tada');
   }, 10);
 });
