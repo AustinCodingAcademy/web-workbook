48edf67b55c9bbb21b81309ac27dd31c127d4550
Tony <arinaldi@vt.edu>
2016-12-09 11:18:39 -0600

    Lesson 11 - Homework 1
    Add Font Awesome and Owl image carousel to “Order Food” site

---
+++ 11Lesson/01Homework/js/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:4
+        }
+    }
+});
