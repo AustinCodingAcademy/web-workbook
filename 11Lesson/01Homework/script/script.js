22befad130a1eb89d36ef206cacac5d06dcf4fb8
UnimportantWank <Xeromancer@gmail.com>
2016-12-13 18:31:49 -0600

    moves the js to a separate file

--- 11Lesson/01Homework/script/script.js
+++ 11Lesson/01Homework/script/script.js
@@ -1,4 +1,43 @@
 $(document).ready(function() {
 
-  
+  var owl = $("#owl-demo");
+
+  owl.owlCarousel({
+      items : 4, //10 items above 1000px browser width
+      itemsDesktop : [1000,5], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,3], // betweem 900px and 601px
+      itemsTablet: [600,2], //2 items between 600 and 0
+      itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+      nav : true,
+      responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:true
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+  });
+
+  // Custom Navigation Events
+  $(".next").click(function(){
+    owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+    owl.trigger('owl.prev');
+  })
+  $(".play").click(function(){
+    owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+  })
+  $(".stop").click(function(){
+    owl.trigger('owl.stop');
+  })
 });
