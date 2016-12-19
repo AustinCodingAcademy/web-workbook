f2806e72d11a361ed691b99ad34e0f6b8748ae82
MicahMonfrini <micah.monfrini@PETUNIA-MMO.local>
2016-12-18 19:27:25 -0600

    Final - added smoothscroll file

---
+++ 14Lesson/js/smoothscroll.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+$('.nav li a').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+  && location.hostname == this.hostname) {
+    var $target = $(this.hash);
+    $target = $target.length && $target
+    || $('[name=' + this.hash.slice(1) +']');
+    if ($target.length) {
+      var targetOffset = $target.offset().top;
+      $('html,body')
+      .animate({scrollTop: targetOffset}, 1000);
+     return false;
+    }
+  }
+});
+});
+
+$(document).ready(function(){
+$('.jumbotron .container p a').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+  && location.hostname == this.hostname) {
+    var $target = $(this.hash);
+    $target = $target.length && $target
+    || $('[name=' + this.hash.slice(1) +']');
+    if ($target.length) {
+      var targetOffset = $target.offset().top;
+      $('html,body')
+      .animate({scrollTop: targetOffset}, 1000);
+     return false;
+    }
+  }
+});
+});
