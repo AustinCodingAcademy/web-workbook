897ee811c0d6df1f18bb87d8584a969ac45832ff
tatianagwatson <tatianagwatson@gmail.com>
2016-12-20 13:07:37 -0600

    save

---
+++ 15Lesson/01Portfolio/css/style.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+//     $('.parallax').parallax();
+// });
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
