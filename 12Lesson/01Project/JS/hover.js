023e8835e46b2cddc98f26b1f9ca1a2db45c16ba
Arnold73 <acpwells@gmail.com>
2016-12-21 16:40:55 -0600

    first draft final
    # Conflicts:
    #	07Lesson/01Homework/img/wine-bg.jpg
    #	07Lesson/01Homework/img/wine13.jpg
    #	07Lesson/01Homework/img/wine16.jpg
    #	07Lesson/01Homework/img/wine3.jpg
    #	07Lesson/01Homework/img/wine6.jpg
    #	07Lesson/01Homework/img/wine7.jpg

---
+++ 12Lesson/01Project/JS/hover.js
@@ -0,0 +1,26 @@
+function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);
+        });
+}
+
+function animationClick(element, animation){
+    element = $(element);
+    element.click(
+        function() {
+            element.addClass('animated ' + animation);
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);
+
+        });
+}
