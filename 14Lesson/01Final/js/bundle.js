4eb18e40b0a0f42a5b29683b8c142edc77cc34fb
Morgakd <morgakd@sbcglobal.net>
2016-12-21 16:31:50 -0600

    Lesson 15 - Portfolio - Morgan Dean
    update script

---
+++ 14Lesson/01Final/js/bundle.js
@@ -0,0 +1,361 @@
+.project - three - info - div, .project - title {
+    -webkit - user - select: none; - moz - user - select: none; - ms - user - select: none
+}.css3dDom, .info - ui, .project - title {
+    text - rendering: geometricPrecision
+}.counter - project li, .timeline - trail, .trail {
+    background - color: #fff
+}.about a, .project - info - text a {
+    border - bottom: 1 px solid# fff; - webkit - transition: opacity .3 s
+}.overflow, body, html {
+    overflow: hidden
+}.css3dDom {
+    position: absolute;left: 0;top: 0;z - index: 10
+}.project - three - div {
+    color: #fff;width: 540 px;cursor: default;pointer - events: none
+}.phone.project - three - div, .tablet.project - three - div {
+    width: 580 px
+}.phone.info - project, .tablet.info - project {
+    display: none
+}.info - ui, .project - three - info - div.overflow {
+    display: inline - block;vertical - align: middle
+}.project - three - info - div {
+    color: #fff;cursor: pointer;user - select: none;z - index: 10
+}.info - ui {
+    font - size: 16 px;
+    letter - spacing: 1 px;
+    color: #fff
+}.trail {
+    width: 70 px;height: 1 px;opacity: .4;vertical - align: middle
+}.project - title {
+    font - family: Gza;
+    font - size: 110 px;
+    cursor: default;
+    user - select: none;
+    line - height: 110 px;
+    padding - bottom: 11 px
+}.project - info - title, .project - subtitle {
+    -webkit - user - select: none; - moz - user - select: none; - ms - user - select: none;
+    text - rendering: geometricPrecision
+}.project - subtitle {
+    font - family: Relative;
+    font - size: 13 px;
+    text - transform: uppercase;
+    letter - spacing: 2 px;
+    user - select: none;
+    line - height: 22 px;
+    padding - top: 15 px
+}.project - three - div - infoimage {
+    width: 480 px;height: auto
+}.project - info {
+    width: 640 px;pointer - events: none
+}.project - info - title {
+    letter - spacing: 5 px;
+    line - height: 124 px;
+    font - family: Gza;
+    font - size: 94 px;
+    cursor: default;
+    user - select: none;
+    color: #fff;
+    white - space: nowrap
+}.counter - project, .project - info - subtitle {
+    -webkit - user - select: none; - moz - user - select: none; - ms - user - select: none
+}.project - info - subtitle {
+    color: #fff;white - space: nowrap;font - family: Relative;text - transform: uppercase;text - rendering: geometricPrecision;cursor: default;user - select: none;letter - spacing: 3 px;font - size: 15 px;line - height: 35 px
+}.about - links li, .project - info - text a {
+    padding - bottom: 2 px;
+    cursor: pointer!important
+}.project - info - colored {
+    font - size: 16 px;
+    font - family: Relative;
+    margin - left: 16 px
+}.project - info - description {
+    display: block;margin - bottom: 30 px;line - height: 28 px
+}.project - info - text {
+    width: 71 % ;display: block;line - height: 21 px;text - rendering: geometricPrecision
+}.project - info - text a {
+    color: #fff;line - height: 20 px;pointer - events: auto;text - decoration: none;transition: opacity .3 s
+}.project - info - text a: hover {
+    opacity: .5
+}.counter - project {
+    pointer - events: none;
+    user - select: none
+}.counter - project - title.overflow, .grabui - container div {
+    -webkit - user - select: none; - moz - user - select: none; - ms - user - select: none
+}.counter - project ul {
+    -webkit - transform: translateX(50 % ) rotate(180 deg); - ms - transform: translateX(50 % ) rotate(180 deg);
+    transform: translateX(50 % ) rotate(180 deg)
+}.counter - project li {
+    display: inline - block;height: 10 px;width: 2 px;margin - left: 6 px;opacity: .2; - webkit - transform - origin: 50 % 0; - ms - transform - origin: 50 % 0;transform - origin: 50 % 0
+}.counter - project.active {
+    opacity: 1
+}.counter - project - title {
+    color: #fff;font - size: 24 px;letter - spacing: 1 px;font - family: Gza;line - height: 32 px
+}.counter - project - title.overflow {
+    pointer - events: none;
+    user - select: none; - webkit - transform: translateX(50 % ) translateY(50 % ); - ms - transform: translateX(50 % ) translateY(50 % );
+    transform: translateX(50 % ) translateY(50 % )
+}.project - info - container {
+    -webkit - transform: translateY(50 % ); - ms - transform: translateY(50 % );
+    transform: translateY(50 % )
+}.grabui - container {
+    height: 80 % ;width: 90 px;position: absolute;top: 10 % ;right: 6 % ;z - index: 100;cursor: move;cursor: -webkit - grab;cursor: grab;opacity: 0;visibility: hidden; - webkit - transform: translateZ(0);transform: translateZ(0)
+}.grabui - container div {
+    user - select: none
+}.nav, .text {
+    -webkit - user - select: none; - moz - user - select: none; - ms - user - select: none
+}.grabui - container: active {
+    cursor: -webkit - grabbing;cursor: grabbing
+}.grabui - container: active.number {
+    color: #3f3f3f}.cursor-container{width:100%;height:88px;position:relative}.cursor,.cursor .arrow{position:absolute;margin:auto;bottom:0;top:0}.cursor{width:100%;height:18px;text-align:center;-webkit-transform:translateY(-50%);-ms-transform:translateY(-50%);transform:translateY(-50%)}.cursor .inline{display:inline-block;vertical-align:middle}.cursor .number{width:32px;height:20px;color:# fff; - webkit - transition: color .25 s;transition: color .25 s
+}.cursor.arrow {
+    width: 8 px;height: 4 px;background - image: url(.. / media / assets / arrow.svg);backround - repeat: no - repeat;background - size: 100 % ;left: 0;right: 0
+}.grabui - burg {
+    position: relative;width: 16 px;height: 1 px
+}.grabui - burg.burg - trail {
+    position: absolute;width: 100 % ;height: 100 % ;background - color: #fff
+}.timeline {
+    position: absolute;height: 100 % ;width: 1 px;left: 50 % ;top: 0; - webkit - transform: translateX(-50 % ); - ms - transform: translateX(-50 % );transform: translateX(-50 % )
+}.about, .timeline - trail {
+    width: 100 % ;position: absolute;height: 100 %
+}.timeline - trail {
+    opacity: .1
+}.about {
+    z - index: 100000;
+    top: 0;
+    left: 0;
+    background - color: rgba(0, 0, 0, .8);
+    visibility: hidden;
+    opacity: 0
+}.about::-moz - selection, .about p::-moz - selection {
+    color: #000;background-color:# fff
+}.about::-moz - selection, .about p::-moz - selection, .about p::selection {
+    color: #000;background-color:# fff
+}.about::-moz - selection {
+    color: #000;background-color:# fff
+}.about::selection {
+    color: #000;background-color:# fff
+}.about a {
+    text - decoration: none!important;
+    line - height: 16 px;
+    opacity: .3;
+    transition: opacity .3 s
+}.about a: hover {
+    opacity: 1
+}.about.description {
+    font - size: 14 px;
+    letter - spacing: .5 px
+}.tablet.about - wrapper {
+    -webkit - transform: scale(.85) translateY(-50 % ); - ms - transform: scale(.85) translateY(-50 % );
+    transform: scale(.85) translateY(-50 % ); - webkit - transform - origin: 0 50 % ; - ms - transform - origin: 0 50 % ;
+    transform - origin: 0 50 %
+}.phone.about - wrapper {
+    -webkit - transform: scale(.8) translateY(-50 % ); - ms - transform: scale(.8) translateY(-50 % );
+    transform: scale(.8) translateY(-50 % );
+    margin - left: 10 px; - webkit - transform - origin: 0 50 % ; - ms - transform - origin: 0 50 % ;
+    transform - origin: 0 50 %
+}.about - wrapper {
+    color: #fff;position: absolute;top: 50 % ;left: 10 % ; - webkit - transform: translateY(-50 % ); - ms - transform: translateY(-50 % );transform: translateY(-50 % );width: 805 px
+}.about - wrapper p {
+    line - height: 10 px;
+    font - size: 14 px;
+    letter - spacing: .5 px
+}.head {
+    font - family: Gza
+}.about - tables {
+    min - height: 160 px;
+    margin - top: 46 px;
+    margin - bottom: 2 px
+}.about - tables li {
+    font - size: 14 px;
+    line - height: 20 px;
+    letter - spacing: .5 px;
+    font - family: Relative;
+    color: #6a6a6a;text-rendering:geometricPrecision}.about-table{display:inline-block;vertical-align:top}.about-table.left{width:36%}.about-table.right{width:63%}.about-table h3{line-height:40px;letter-spacing:2px;font-size:13.5px}.about-links li{display:inline-block;font-family:Relative;margin-right:26px;line-height:37px;font-size:14px;letter-spacing:.5px;pointer-events:auto;text-decoration:none;-webkit-transition:opacity .3s;transition:opacity .3s}.greeting a{color:# fff;
+    opacity: .4!important
+}.greeting a: hover {
+    opacity: 1!important
+}
+@font - face {
+    font - family: Relative;
+    font - style: normal;
+    src: url(.. / media / fonts / Relative - Book.ttf)
+}
+@font - face {
+    font - family: Gza;
+    font - style: normal;
+    src: url(.. / media / fonts / gza - seminegra - webfont.ttf)
+}.nav {
+    z - index: 1000001;
+    user - select: none
+}.nav a {
+    text - decoration: none!important
+}.nav - transform {
+    z - index: 1000001;
+    position: absolute; - webkit - transform: rotateZ(-90 deg) translateX(-50 % );
+    transform: rotateZ(-90 deg) translateX(-50 % ); - webkit - transform - origin: 0 50 % ; - ms - transform - origin: 0 50 % ;
+    transform - origin: 0 50 % ;
+    top: 50 %
+}.nav - transform - right {
+    z - index: 1000001;
+    position: absolute; - webkit - transform: rotateZ(-90 deg) translateX(50 % );
+    transform: rotateZ(-90 deg) translateX(50 % ); - webkit - transform - origin: 100 % 50 % ; - ms - transform - origin: 100 % 50 % ;
+    transform - origin: 100 % 50 % ;
+    top: 50 %
+}.nav - left {
+    left: 40 px
+}.nav - right {
+    right: 40 px
+}.nav - about - wrapper, .nav - mail - wrapper, .nav - works - wrapper {
+    display: inline - block;padding: 10 px
+}.nav - about, .nav - mail, .nav - works {
+    color: #fff;opacity: .5;white - space: nowrap;cursor: pointer; - webkit - transition: opacity .3 s;transition: opacity .3 s
+}.nav - about: hover, .nav - mail: hover, .nav - works: hover {
+    opacity: 1
+}.nav - about - wrapper.active.nav - about {
+    opacity: 1!important
+}.text# name, .text# namesub, .wrapper canvas {
+    opacity: 0;visibility: hidden
+}
+html {
+    box - sizing: border - box;
+    border - collapse: collapse;
+    line - height: 1;
+    font - family: sans - serif
+} * {
+    margin: 0;padding: 0;border - collapse: inherit; - webkit - tap - highlight - color: transparent
+} * ,: after,: before {
+    box - sizing: inherit
+}
+body {
+    position: relative
+}.overflow - topname, .text {
+    position: absolute;left: 50 % ;text - align: center
+}[hidden] {
+    display: none!important
+}
+menu, ol, ul {
+    list - style: none
+}
+a {
+    color: inherit
+}
+fieldset {
+    border: none
+}.wrapper {
+    width: 100 % ;height: 100 %
+}
+body, html {
+    width: 100 % ;height: 100 % ;margin: 0;background - color: #000;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}body .infoopen,html .infoopen{cursor:s-resize!important}.text{-webkit-transform:translate(-50%,-50%);-ms-transform:translate(-50%,-50%);transform:translate(-50%,-50%);top:50%;color:# fff;text - rendering: geometricPrecision;user - select: none;cursor: default;white - space: nowrap
+}.text# name {
+    font - family: Gza;
+    line - height: 68 px;
+    font - size: 55 px;
+    letter - spacing: 0
+}.text# namesub {
+    font - family: Relative;
+    letter - spacing: 2 px;
+    font - size: 12 px;
+    text - transform: uppercase
+}.overflow - topname {
+    font - family: Gza;
+    top: 5 % ; - webkit - transform: translateX(-50 % ); - ms - transform: translateX(-50 % );
+    transform: translateX(-50 % );
+    z - index: 50000;
+    overflow: hidden; - webkit - user - select: none; - moz - user - select: none; - ms - user - select: none;
+    user - select: none;
+    line - height: 23 px
+}#
+fpscount, .dg {
+    z - index: 50000!important
+}.overflow - topname div {
+    color: #fff;cursor: pointer;opacity: .5; - webkit - transition: opacity .3 s;transition: opacity .3 s;display: none
+}.overflow - topname div: hover {
+    opacity: 1
+}
+h1, h2 {
+    font - family: Gza;
+    letter - spacing: 4.1 px;
+    text - rendering: geometricPrecision
+}
+h3, p {
+    font - family: Relative;
+    text - rendering: geometricPrecision
+}
+h1 {
+    font - size: 48 px;
+    line - height: 21 px
+}
+h2 {
+    line - height: 81 px;
+    text - rendering: geometricPrecision;
+    font - size: 28 px
+}
+h3 {
+    letter - spacing: 1.5 px;
+    font - size: 11 px
+}
+p {
+    font - size: 15 px;
+    line - height: 20 px;
+    letter - spacing: 1 px;
+    color: #6a6a6a}.underbar{width:100%;height:2px;background-color:# fff; - webkit - transform - origin: 0 50 % ; - ms - transform - origin: 0 50 % ;
+    transform - origin: 0 50 % ;
+    margin - top: 5 px
+}#
+orientation - fallback, #orientationside {
+    width: 100 % ;background - size: cover;color: #fff;font - family: Gza;height: 100 % ;display: none;position: absolute
+}#
+fpscount {
+    position: absolute;top: 10;left: 10;color: #fff
+}#
+orientationside {
+    background - image: url(.. / media / assets / bg_ipad.jpg);
+    top: 0;
+    left: 0;
+    z - index: 10000000
+}.phone# orientation - fallback, .tablet# orientation - fallback {
+    background - image: url(.. / media / assets / bg_ipad.jpg) !important
+}.orientation - text {
+    text - align: center;
+    top: 50 % ;
+    left: 50 % ;
+    position: absolute; - webkit - transform: translateY(-50 % ) translateX(-50 % ); - ms - transform: translateY(-50 % ) translateX(-50 % );
+    transform: translateY(-50 % ) translateX(-50 % )
+}.phone.orientation - text {
+    font - size: 20 px;
+    line - height: 30 px
+}.tablet.orientation - text {
+    font - size: 40 px;
+    line - height: 40 px
+}#
+orientation - fallback {
+    background - image: url(.. / media / assets / fullback.jpg);
+    top: 0;
+    left: 0;
+    z - index: 99999
+}.loader {
+    background - image: url(.. / media / assets / loader.gif);
+    width: 50 px;
+    height: 50 px;
+    background - size: 100 % ;
+    position: absolute;
+    z - index: 999999;
+    top: 50 % ;
+    left: 50 % ; - webkit - transform: translateX(-50 % ) translateY(-50 % ); - ms - transform: translateX(-50 % ) translateY(-50 % );
+    transform: translateX(-50 % ) translateY(-50 % )
+}#
+arrowdown {
+    position: absolute;bottom: 30 px;right: 0;width: 44 px;height: 80 px;left: 50 % ; - webkit - transform: translateX(-50 % ) scaleX(.5) scaleY(.5); - ms - transform: translateX(-50 % ) scaleX(.5) scaleY(.5);transform: translateX(-50 % ) scaleX(.5) scaleY(.5);cursor: pointer;z - index: 99999;display: none
+}#
+arrowdown# arrowdown - trail {
+    height: 80 px;width: 1 px;text - align: center;background - color: #aaa;position: absolute; - webkit - transform - origin: 50 % 0; - ms - transform - origin: 50 % 0;transform - origin: 50 % 0;margin - left: 22 px;top: 0
+}#
+arrowdown.arrowdown - caps - one, #arrowdown.arrowdown - caps - two {
+    position: absolute;bottom: 0;width: 1 px;height: 16 px; - webkit - transform - origin: 50 % 100 % ; - ms - transform - origin: 50 % 100 % ;transform - origin: 50 % 100 % ;background - color: #aaa
+}#
+arrowdown.rotateLeft {
+    position: absolute;bottom: 0; - webkit - transform: rotate(45 deg); - ms - transform: rotate(45 deg);transform: rotate(45 deg);margin - left: 22 px
+}#
+arrowdown.rotateRight {
+    position: absolute;bottom: 0; - webkit - transform: rotate(-45 deg); - ms - transform: rotate(-45 deg);transform: rotate(-45 deg);margin - left: 22 px
+}
