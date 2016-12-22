9fb204f73ffad8b36e9c4f17869ef75c4d0936c5
jeffgojeff <jeffgojeff@gmail.com>
2016-12-21 19:55:01 -0600

    Quick

--- 14Lesson/01Final/bootstrap/js/slideshow.js
+++ 14Lesson/01Final/bootstrap/js/slideshow.js
@@ -10,5 +10,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 10000); // Change image every 2 seconds
+    setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
