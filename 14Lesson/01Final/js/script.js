fac22bcdc12b6b6cb8475a12622892d87a50d958
UnimportantWank <Xeromancer@gmail.com>
2016-12-22 18:26:09 -0600

    positioning of the about to make the links work and fixed the fade animations/JS

--- 14Lesson/01Final/js/script.js
+++ 14Lesson/01Final/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         $(".top").css("opacity", 1.1 - $(window).scrollTop() / 250);
     });
     $(window).scroll(function() {
-        $("#about").css("opacity",2.85 - $(window).scrollTop() / 360);
+        $("#about").css("opacity",2.65 - $(window).scrollTop() / 360);
     });
 
     // ~~sparkles!!~~
