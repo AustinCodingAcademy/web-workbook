923dc99b5dbd5354f572bd519599842c92534e57
Greg Devany <greg.devany@yahoo.com>
2017-03-10 01:30:36 -0600

    Created JQuery tictactoe

---
+++ 09week/tictactoe/tttscript.js
@@ -0,0 +1,59 @@
+$(function() {
+  let playerTurn = 'X';
+  $('[data-cell]').click(function() {
+    $(this).text(playerTurn);
+// Check to see if there's a winner
+    if (checkWin()) {
+      $('#announce-winner').text(playerTurn + " wins!");
+    }
+// Clears the board and fades a clean board back in
+    $("button").click(function() {
+      $("div[data-cell]").fadeOut(1000,function(){
+        $("div[data-cell]").empty();
+        $('#announce-winner').empty();
+        $("div").fadeIn(1000);
+        playerTurn = 'X';
+      });
+    })
+// Switches from X to O and visa versa
+      if (playerTurn === 'X') {
+        playerTurn = 'O';
+      } else {
+        playerTurn = 'X'
+      }
+  })
+// Check to see if there's a winner function
+  function checkWin() {
+    if(($('[data-cell="0"]').text() === playerTurn &&
+       $('[data-cell="3"]').text() === playerTurn &&
+       $('[data-cell="6"]').text() === playerTurn
+     ) || ($('[data-cell="1"]').text() === playerTurn &&
+          $('[data-cell="4"]').text() === playerTurn &&
+          $('[data-cell="7"]').text() === playerTurn
+        ) || ($('[data-cell="2"]').text() === playerTurn &&
+             $('[data-cell="5"]').text() === playerTurn &&
+             $('[data-cell="8"]').text() === playerTurn
+           ) || ($('[data-cell="0"]').text() === playerTurn &&
+                $('[data-cell="1"]').text() === playerTurn &&
+                $('[data-cell="2"]').text() === playerTurn
+              ) || ($('[data-cell="3"]').text() === playerTurn &&
+                   $('[data-cell="4"]').text() === playerTurn &&
+                   $('[data-cell="5"]').text() === playerTurn
+                 ) || ($('[data-cell="6"]').text() === playerTurn &&
+                      $('[data-cell="7"]').text() === playerTurn &&
+                      $('[data-cell="8"]').text() === playerTurn
+                    ) || ($('[data-cell="0"]').text() === playerTurn &&
+                         $('[data-cell="4"]').text() === playerTurn &&
+                         $('[data-cell="8"]').text() === playerTurn
+                       ) || ($('[data-cell="2"]').text() === playerTurn &&
+                            $('[data-cell="4"]').text() === playerTurn &&
+                            $('[data-cell="6"]').text() === playerTurn
+                          )
+      )
+        {
+        return true;
+       } else {
+         return false;
+       }
+  }
+})
