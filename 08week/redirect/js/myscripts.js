fc0f028826b92cabd9274bd017c2da5dd3c9484e
Pamela Bensin <Pamela@Pamelas-MacBook-Air.local>
2017-03-09 20:53:35 -0600

    redirect

---
+++ 08week/redirect/js/myscripts.js
@@ -0,0 +1,8 @@
+    function redirectYes() {
+            window.location = '../../07week/Checkpoint2/index.html'
+        }
+
+
+        function redirectNo() {
+                window.location = '../../08week/redirect/landing.html'
+            }
