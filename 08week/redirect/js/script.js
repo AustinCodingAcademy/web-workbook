bbaa7f0f7bacd5ac29a6e2f20daa32b1552a0ddd
demarcogleason <demarcogleason@gmail.com>
2017-03-10 00:37:21 -0600

    age-verify

---
+++ 08week/redirect/js/script.js
@@ -0,0 +1,20 @@
+function redirect(){
+if (document.getElementById("team").value==="red"){
+  window.location="./red.html";
+}
+else if
+  (document.getElementById("team").value==="blue"){
+    window.location="./blue.html";
+  }
+  else {
+    alert("Please choose red or blue or suffer the consequence!");
+  }
+}
+
+function redirect1(){
+    window.location="../../07week/Checkpoint2/index.html";
+}
+
+function redirect2(){
+    window.location="https://www.consumer.ftc.gov/features/feature-0028-we-dont-serve-teens";
+}
