63c7bceded22edd4058fb68a41ea2fa4d4a3ed70
Stu Kilgore <stuart.kilgore@gmail.com>
2017-03-12 17:46:49 -0500

    actually finished ageverify

--- 08week/ageverify/js/ageverify.js
+++ 08week/ageverify/js/ageverify.js
@@ -3,14 +3,15 @@ document.getElementById('submit-button').addEventListener("click", redirect);
 function redirect() {
   const ageNum = Number(document.getElementById('input-age').value);
   console.log(ageNum);
-  if (ageNum < 21) {
-    console.log("Too young");
+  if (ageNum < 0 || ageNum === NaN) {
+    console.log("Not a number");
+    document.getElementById('wrong-age').innerHTML = "Please enter valid age.";
   } else if (ageNum >= 21) {
     console.log("Just right");
-    // window.location.replace('../../../07week/Checkpoint2/index.html');
-  } else if (ageNum < 0 || ageNum === NaN) {
-    console.log("Not a number");
-    // document.getElementById('wrong-age').innerHTML = "Please enter valid age.";
-  }
+    window.location.href = '../../../07week/Checkpoint2/index.html';
+  } else if (ageNum < 21) {
+    window.location.href = './denied.html';
+    console.log("Too young");
+  } else {}
 
   }
