e68b97c5891642ea42993ed836380733fb694a40
Ethan Crowley <Ethan Crowley>
2017-03-14 13:10:48 -0500

    redirect page

--- 08week/redirect/js/file.js
+++ 08week/redirect/js/file.js
@@ -3,7 +3,7 @@ function redirect() {
   if(document.getElementById('team').value === 'yes'){
     window.location = '../../07week/Checkpoint2/index.html';
   } else if(document.getElementById('team').value === 'no') {
-      window.location = './blue.html';
+      window.location = 'https://www.niaaa.nih.gov/alcohol-health/special-populations-co-occurring-disorders/underage-drinking';
     } else {
     alert('Please say yes or no');
     }
