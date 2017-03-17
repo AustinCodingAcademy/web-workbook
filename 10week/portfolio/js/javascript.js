6c04e77619ad6744de1bad738e8c3d1b340eab0d
Keenan Goldis <keenangoldis@gmail.com>
2017-03-17 16:15:01 -0500

    delay

--- 10week/portfolio/js/javascript.js
+++ 10week/portfolio/js/javascript.js
@@ -1,3 +1,15 @@
+  //
+  // $('#projecta').hide(); // this or use css to hide the div
+  // $('#projecta').delay(8000).fadeIn('slow');
 
-  $('#projecta').hide(); // this or use css to hide the div
-  $('#projecta').delay(2000).fadeIn('slow');
+  <script>
+  $(document).ready(function(){
+    $("button").click(function(){
+      $("#div1").delay("slow").fadeIn();
+      $("#div2").delay("fast").fadeIn();
+      $("#div3").delay(800).fadeIn();
+      $("#div4").delay(2000).fadeIn();
+      $("#div5").delay(4000).fadeIn();
+    });
+  });
+  </script>
