ea84df150ed9938aa2c82f7ceaca6fa5c22b4808
stackbaxter <stackbaxter@gmail.com>
2017-03-20 16:58:48 -0500

    messing around with particles

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function(){
+
+  // $('#particles').particleground({
+  //          dotColor: '#8dd816',
+  //          lineColor: '#161665',
+  //          density: '10000',
+  //          parallax: 'false',
+  //          parallaxMultiplier: '8',
+  //          particleRadius: '1',
+  //          proximity: '100'
+  //
+  //  particlesJS.load('#particles-js', 'particlesJS.json', function() {
+  //            console.log('callback - particles.js config loaded');
+  //          });
+
+ })
