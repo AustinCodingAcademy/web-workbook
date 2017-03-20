cadbac9d9245f5415447d2a519dad91034d64643
RFloydUT3 <rileyfloyd3@yahoo.com>
2017-03-20 11:59:08 -0500

    Winery update

---
+++ 08week/redirect/js/redirect.js
@@ -0,0 +1,9 @@
+
+
+        function redirect1() {
+
+                window.location = '../../07week/Checkpoint2/index.html';
+            }
+        function redirect2(){
+          window.location = "./sorry.html"
+        }
