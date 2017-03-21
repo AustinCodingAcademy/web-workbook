296a128aebf02e676665b922916aeb0ec722a015
Stu Kilgore <stuart.kilgore@gmail.com>
2017-03-21 14:48:31 -0500

    hanoi fixes

--- 10week/hanoi/js/script.js
+++ 10week/hanoi/js/script.js
@@ -1,21 +1,33 @@
 $(document).ready(function() {
-
   // set initial global variables
   let $block = null;
   let $blockSize = null;
+  let $lastBlock = null;
   // START click functions
   $('[data-stack]').click(function() {
-    if (!$block) {
+    $lastBlock = $(this).children().last().data('block');
+    // nothing held, nothing in stack
+    if (!$block && !$lastBlock) {
+      $lastBlock = null;
+      console.log('Invalid move: nothing to pick up');
+    }
+    // nothing held >> pick up last block
+    else if (!$block) {
       $block = $(this).children().last().detach();
       $blockSize = $block.data('block');
       console.log(`${$blockSize} held`);
-    } else {
-      if ($blockSize < $(this).children().last().data('block') ||
-          $(this).children().length === 0) {
-            $(this).append($block);
-            $block = null;
-            console.log('Nothing held');
-      }
+    }
+    // check for last block size >> place block
+    else if (!$lastBlock || $blockSize < $lastBlock) {
+      $(this).append($block);
+      console.log(`${$blockSize} placed`);
+      $block = null;
+      $blockSize = null;
+      console.log('Nothing held');
+    }
+    // block held, bigger than last block
+    else {
+      console.log('Invalid move: block held larger than last block');
     }
   })
   // END click functions
