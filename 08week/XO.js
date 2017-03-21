a25af6a2b182be61ff288fba45684a2d20c8be6b
araixaruiz <arxrz08@hotmail.com>
2017-03-20 20:06:33 -0500

    xoxoxo

--- 08week/XO.js
+++ 08week/XO.js
@@ -1,26 +1,95 @@
-$(document).ready(function() {
-    let playerTurn = 'X';
-    $('[data-cell]').on('click', function() {
-      $(this).text('playerTurn');
-      if
-      $('[data-cell="0"]').text === playerTurn &&
-        $('[data-cell="3"]').text === playerTurn &&
-        $('[data-cell="6"]').text === playerTurn {
-          console.log('${playerTurn} Wins!');
+$(function() {
+  let player = 'X';
 
-          if (playerTurn === "X") {}
-          playerTurn = 'O';
-        } else {
-          playerTurn = 'X'
-        }
-    });
-
-    function checkForWin() {
-      if (
-
-      )
+  $('[data-cell]').click(function() {
+    $(this).text(player);
+    if(checkWin()) {
+      $('#announce-winner').text(`${player} wins!`)
+    } else {
+      if (player === 'X') {
+        player = 'O';
+      }else {
+        player = 'X';
+      }
     }
 
-  }
+  })
 
-}
+  function checkWin() {
+    if (
+      $('[data-cell="0"]').text() === 'X' &&
+      $('[data-cell="3"]').text() === 'X' &&
+      $('[data-cell="6"]').text() === 'X' ||
+      $('[data-cell="0"]').text() === 'O' &&
+      $('[data-cell="3"]').text() === 'O' &&
+      $('[data-cell="6"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="0"]').text() === 'X' &&
+      $('[data-cell="4"]').text() === 'X' &&
+      $('[data-cell="8"]').text() === 'X' ||
+      $('[data-cell="0"]').text() === 'O' &&
+      $('[data-cell="4"]').text() === 'O' &&
+      $('[data-cell="8"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="0"]').text() === 'X' &&
+      $('[data-cell="1"]').text() === 'X' &&
+      $('[data-cell="2"]').text() === 'X' ||
+      $('[data-cell="0"]').text() === 'O' &&
+      $('[data-cell="1"]').text() === 'O' &&
+      $('[data-cell="2"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="1"]').text() === 'X' &&
+      $('[data-cell="4"]').text() === 'X' &&
+      $('[data-cell="7"]').text() === 'X' ||
+      $('[data-cell="1"]').text() === 'O' &&
+      $('[data-cell="4"]').text() === 'O' &&
+      $('[data-cell="7"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="2"]').text() === 'X' &&
+      $('[data-cell="4"]').text() === 'X' &&
+      $('[data-cell="6"]').text() === 'X' ||
+      $('[data-cell="2"]').text() === 'O' &&
+      $('[data-cell="4"]').text() === 'O' &&
+      $('[data-cell="6"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="2"]').text() === 'X' &&
+      $('[data-cell="5"]').text() === 'X' &&
+      $('[data-cell="8"]').text() === 'X' ||
+      $('[data-cell="2"]').text() === 'O' &&
+      $('[data-cell="5"]').text() === 'O' &&
+      $('[data-cell="8"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="3"]').text() === 'X' &&
+      $('[data-cell="4"]').text() === 'X' &&
+      $('[data-cell="5"]').text() === 'X' ||
+      $('[data-cell="3"]').text() === 'O' &&
+      $('[data-cell="4"]').text() === 'O' &&
+      $('[data-cell="5"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="6"]').text() === 'X' &&
+      $('[data-cell="7"]').text() === 'X' &&
+      $('[data-cell="8"]').text() === 'X' ||
+      $('[data-cell="6"]').text() === 'O' &&
+      $('[data-cell="7"]').text() === 'O' &&
+      $('[data-cell="8"]').text() === 'O'
+    ) {
+      return true;
+    }else {
+      return false;
+    }
+  }
+})
