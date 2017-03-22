afa61d29337c626940fc456775e27982c3d79e07
meganroone <emailgena@gmail.com>
2017-03-22 14:44:01 -0500

    revisions to game

---
+++ 09week/tictactoe/script2.js
@@ -0,0 +1,92 @@
+$(
+  function() {
+
+  let $player = 'X';
+  $('#current-turn').text($player);
+
+  $('[data-cell]').click(function() {
+    $(this).text($player);
+    if ($player === 'X') {
+      checkWin();
+      $player = 'O';
+      $('#current-turn').text($player);
+    } else {
+      checkWin();
+      $player = 'X';
+      $('#current-turn').text($player);
+    }
+  })
+
+  $('#clear').click(function() {
+    $('[data-cell]').text(null);
+    $('#announce-winner').text(null);
+  })
+
+  function checkWin() {
+    if (
+      $('[data-cell="0"]').text() === $player &&
+      $('[data-cell="1"]').text() === $player &&
+      $('[data-cell="2"]').text() === $player
+    )
+      {
+        $('#announce-winner').text($player + ' wins!');
+      }
+    else if (
+      $('[data-cell="3"]').text() === $player &&
+      $('[data-cell="4"]').text() === $player &&
+      $('[data-cell="5"]').text() === $player
+    )
+      {
+        $('#announce-winner').text($player + ' wins!');
+      }
+    else if (
+      $('[data-cell="6"]').text() === $player &&
+      $('[data-cell="7"]').text() === $player &&
+      $('[data-cell="8"]').text() === $player
+    )
+      {
+        $('#announce-winner').text($player + ' wins!');
+      }
+    else if (
+      $('[data-cell="0"]').text() === $player &&
+      $('[data-cell="3"]').text() === $player &&
+      $('[data-cell="6"]').text() === $player
+    )
+      {
+        $('#announce-winner').text($player + ' wins!');
+      }
+    else if (
+      $('[data-cell="1"]').text() === $player &&
+      $('[data-cell="4"]').text() === $player &&
+      $('[data-cell="7"]').text() === $player
+    )
+      {
+        $('#announce-winner').text($player + ' wins!');
+      }
+    else if (
+      $('[data-cell="2"]').text() === $player &&
+      $('[data-cell="5"]').text() === $player &&
+      $('[data-cell="8"]').text() === $player
+    )
+      {
+        $('#announce-winner').text($player + ' wins!');
+      }
+    else if (
+      $('[data-cell="0"]').text() === $player &&
+      $('[data-cell="4"]').text() === $player &&
+      $('[data-cell="8"]').text() === $player
+    )
+      {
+        $('#announce-winner').text($player + ' wins!');
+      }
+    else if (
+      $('[data-cell="2"]').text() === $player &&
+      $('[data-cell="4"]').text() === $player &&
+      $('[data-cell="6"]').text() === $player
+    )
+      {
+        $('#announce-winner').text($player + ' wins!');
+      }
+    else {}
+    }
+})
---
+++ 09week/towers/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  // Put app logic here
+});
+//notes
+
+
+
+
+
+//
+//
+//
+// let $ block = null
+// stack focus on stack in order to take first child and move to another peg.
+//
+// $stack.click(function)(){}
+// if (!blank) {
+// $block=(this).children().last().detach ()
+//   }else}
+//   $(this).append($block)($click)
+//   $block=null
+// }
+// })
+// e
+// lse {
+//   if$block.data()<$(this)children(not)data)
+//   append
+//   =null
+//   let $block=null
+//   if $!block
+//   detach
+// }
+// else if .children.last > block || no children
+// append
+// reset
+//
+//
+// jquery
+// data block= "25"
+// .data("block") will return 25
