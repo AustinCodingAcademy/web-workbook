87499f249b106b06fbf341e238b93458e110d25d
Greg Swan <greg.c.swan@gmail.com>
2017-03-22 15:36:47 -0500

    tic tac toe

--- 09week/script.js
+++ 09week/script.js
@@ -1,27 +1,46 @@
 $(function() {
-  let color = 'rgb(0, 128, 0)';
+  let playerTurn = 'X';
   $('[data-cell]').click(function() {
-    $(this).css('background-color', color);
-    if (checkWin()) {
-      console.log(`${color} wins!`)
-    } else {
-      if (color === 'rgb(0, 128, 0)') {
-        color = 'rgb(0, 0, 255)';
-      } else {
-        color = 'rgb(0, 128, 0)'
-      }
-    }
-  })
+    $(this).text(playerTurn);
+    checkWin()
+    playerTurn = playerTurn === 'X' ? 'O' : 'X';
+  });
+  $('#clear').click(clearBoard)
+
 
   function checkWin() {
     if (
-      $('[data-cell="0"]').css('background-color') === color &&
-      $('[data-cell="3"]').css('background-color') === color &&
-      $('[data-cell="6"]').css('background-color') === color
-    ) {
-      return true;
-    } else {
+      $('[data-cell="0"]').text() === playerTurn &&
+      $('[data-cell="3"]').text() === playerTurn &&
+      $('[data-cell="6"]').text() === playerTurn ||
+      $('[data-cell="1"]').text() === playerTurn &&
+      $('[data-cell="4"]').text() === playerTurn &&
+      $('[data-cell="7"]').text() === playerTurn ||
+      $('[data-cell="2"]').text() === playerTurn &&
+      $('[data-cell="5"]').text() === playerTurn &&
+      $('[data-cell="8"]').text() === playerTurn ||
+      $('[data-cell="0"]').text() === playerTurn &&
+      $('[data-cell="1"]').text() === playerTurn &&
+      $('[data-cell="2"]').text() === playerTurn ||
+      $('[data-cell="3"]').text() === playerTurn &&
+      $('[data-cell="4"]').text() === playerTurn &&
+      $('[data-cell="5"]').text() === playerTurn ||
+      $('[data-cell="6"]').text() === playerTurn &&
+      $('[data-cell="7"]').text() === playerTurn &&
+      $('[data-cell="8"]').text() === playerTurn ||
+      $('[data-cell="0"]').text() === playerTurn &&
+      $('[data-cell="4"]').text() === playerTurn &&
+      $('[data-cell="8"]').text() === playerTurn ||
+      $('[data-cell="2"]').text() === playerTurn &&
+      $('[data-cell="4"]').text() === playerTurn &&
+      $('[data-cell="6"]').text() === playerTurn)
+      {
+      $('#announce-winner').text(`${playerTurn} Wins`)
+      } else {
       return false;
-    }
+      }
   }
 })
+ function clearBoard()  {
+   $('[data-cell]').empty();
+ }
--- 10week/Towers/script.js
+++ 10week/Towers/script.js
@@ -7,11 +7,20 @@ $(document).ready(function() {
       $(this).append($block);
       $block = null;
       checkForWin();
-    }
+        }
+      $('#reset').click(resetBoard)
   });
   function checkForWin() {
     if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4) {
-      console.log('YOU WIN!');
+      $('#announce-game-won').text(`You Win!`);
     }
+  }
+  function resetBoard() {
+    $block100 = $('[data-block="100"]').detach();
+    $block75 = $('[data-block="75"]').detach();
+    $block50 = $('[data-block="50"]').detach();
+    $block25 = $('[data-block="25"]').detach();
+    $('[data-stack="1"]').append($block100, $block75, $block50, $block25);
+
   }
 });
