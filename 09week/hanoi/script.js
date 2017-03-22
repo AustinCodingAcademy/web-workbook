39094fb16757cd7e70a47882a3098ee407b97c41
stackbaxter <stackbaxter@gmail.com>
2017-03-22 01:11:55 -0500

    functional, still needs win confirmation

--- 09week/hanoi/script.js
+++ 09week/hanoi/script.js
@@ -1,17 +1,33 @@
 $(function() {
   // Put app logic here
-  let $ringVal = $(this).children().last();
-  let $block = 'null';
+  // let $ringVal = $(this).children().last();
+  let $ring = 'null';
   let $length = $(this).length;
 
   $('[data-stack]').click(function(){
-      if(!$block){
-        $block = $(ringVal);
-        console.log($block);
-      }
 
-    else {
-      if($ringVal > $block || $length === 0){
-        $(this).append($block);
-      }}
-    }  )})
+      if($ring === 'null'){
+        $ring = $(this).children().last().detach();
+        console.log($ring);
+        console.log($(this).children().length);
+        }
+
+      else {
+
+        if($(this).children().length === 0 || ($ring.data('block') < $(this).children().last().data('block') )) {
+          $(this).append($ring);
+          $ring = 'null';
+        }
+        else {
+
+        }
+// || ($(this).children().last().data('block') > $ring.data('block') )
+
+      }
+      });
+
+    // else {
+    //   if($ringVal > $block || $length === 0){
+    //     $(this).append($block);
+    //   }}
+  })
