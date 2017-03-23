0bf870fa120f380a3df411cf1863cc76b0424dc2
Cam H <Cam H>
2017-03-22 20:40:31 -0500

    towers

--- 09week/TowerHanoi/towerhanoi.js
+++ 09week/TowerHanoi/towerhanoi.js
@@ -3,10 +3,26 @@ $(function() {
     $('[data-stack]').click(function() {
         if (!$block) {
             $block = $(this).children().last().detach();
-        } else {
-            if ($block.data("block") < $(this).children().last().data()) {
-                $(this).append();
+        } else if (isLegal(this) || isEmpty(this)) {
+            $(this).append($block);
             $block = null;
-        }}
+            if (checkForWin()){
+              $('#announce-game-won').text('You Win!');
+            }
+
+        }
     })
+
+    function isLegal(stack) {
+        return $block.data("block") < $(this).children().last().data("block")
+    }
+
+    function isEmpty(stack) {
+        return $(stack).children().length === 0;
+    }
+
+    function checkForWin() {
+        return $('[data-stack="2"]').children().length === 4 ||
+        $('[data-stack="3"]').children().length === 4;
+    }
 });
