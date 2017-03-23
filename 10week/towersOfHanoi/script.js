8940ac49e851b3a1db37844f2e7eaaf10fc07503
Alberto Soto-Martinez <albertosoto1221@gmail.com>
2017-03-22 20:51:43 -0500

    removed folder again

--- 10week/towersOfHanoi/script.js
+++ 10week/towersOfHanoi/script.js
@@ -1,39 +1,31 @@
 $(document).ready(function() {
 
-  let $block = null;
+    let $block = null;
 
-$('[data-stack]').click(function() {
+    $('[data-stack]').click(function() {
 
-  if (!$block) {
-    $block = $(this).children().last().detach();
-  } else {
-    if (
-      $(this).children().last() <
-        $(this).children().last().data('block') ||
-      $(this).children().length === 0
-  ) {
-      $(this).append($block);
-      $block = null;
+        if (!$block) {
+            $block = $(this).children().last().detach();
+        } else if (isLegal(this) || isEmpty(this)) {
+            $(this).append($block);
+            $block = null;
+            if (checkForWin()) {
+              $('#announce-game-won').text('YOU WIN!');
+            }
+        }
+    });
+
+
+    function isLegal(stack) {
+      return $block.data('block') < $(stack).children().last().data('block');
     }
-  }
 
+    function isEmpty(stack){
+      return $(stack).children().length === 0;
+    }
 
-
-
-
-
-
-
-
-})
-
-
-
-
-
-
-
-
-
-
+    function checkForWin () {
+      return $('[data-stack="2"]').children().length === 4 ||
+             $('[data-stack="3"]').children().length === 4;
+    }
 });
