fac967823ac921712f9b15cd948519983f2e97d7
Eric Garcia <egarcia1868@gmail.com>
2017-03-22 20:46:23 -0500

    I think ToH is complete.

--- 09week/ToH/toh.js
+++ 09week/ToH/toh.js
@@ -1,4 +1,28 @@
-```
 $(document).ready(function() {
-  // Put app logic here
+  let $block = null;
+
+
+  $('[data-stack]').click(function() {
+    if (!$block)  {
+    $block = $(this).children().last().detach();
+  } else if (isLegal(this) || isEmpty(this)) {
+
+    $(this).append($block);
+    $block = null;
+    if (checkForWin()){$('#announce-game-won').text("Congratulations!  You're not a complete idiot!");
+  }
+}
+  });
+  function isLegal(stack) {
+    return $block.data('block') < $(stack).children().last().data('block')
+    // ask if the return here basically means "check this process"
+  }
+  function isEmpty(stack) {
+    return $(stack).children().length === 0;
+  }
+
+  function checkForWin() {
+    return $('[data-stack="2"]').children().length === 4 ||
+    $('[data-stack="3"]').children().length === 4
+  }
 });
--- 09week/tictactoe/script.js
+++ 09week/tictactoe/script.js
@@ -1,59 +1,60 @@
-$(function() {
-  let playerUp = 'X';
-  $('[data-cell]').click(function() {
-    $(this).text(playerUp);
 
-    if (checkWin()) {
-      $('#announce-winner').text(`${playerUp} wins!`);
-    } else {
-      if (playerUp === 'X') {
-        playerUp = 'O';
+  $(function() {
+    let playerUp = 'X';
+    $('[data-cell]').click(function() {
+      $(this).text(playerUp);
+
+      if (checkWin()) {
+        $('#announce-winner').text(`${playerUp} wins!`);
       } else {
-        playerUp = 'X';
+        if (playerUp === 'X') {
+          playerUp = 'O';
+        } else {
+          playerUp = 'X';
+        }
+      }
+    })
+
+    function checkWin() {
+      if (
+  // Column wins
+        ($('[data-cell="0"]').text() === playerUp &&
+          $('[data-cell="3"]').text() === playerUp &&
+          $('[data-cell="6"]').text() === playerUp) ||
+
+        ($('[data-cell="1"]').text() === playerUp &&
+          $('[data-cell="4"]').text() === playerUp &&
+          $('[data-cell="7"]').text() === playerUp) ||
+
+        ($('[data-cell="2"]').text() === playerUp &&
+          $('[data-cell="5"]').text() === playerUp &&
+          $('[data-cell="8"]').text() === playerUp) ||
+  // Row wins
+        ($('[data-cell="0"]').text() === playerUp &&
+          $('[data-cell="1"]').text() === playerUp &&
+          $('[data-cell="2"]').text() === playerUp) ||
+
+        ($('[data-cell="3"]').text() === playerUp &&
+          $('[data-cell="4"]').text() === playerUp &&
+          $('[data-cell="5"]').text() === playerUp) ||
+
+        ($('[data-cell="6"]').text() === playerUp &&
+          $('[data-cell="7"]').text() === playerUp &&
+          $('[data-cell="8"]').text() === playerUp) ||
+  // Diagonal wins
+        ($('[data-cell="0"]').text() === playerUp &&
+          $('[data-cell="4"]').text() === playerUp &&
+          $('[data-cell="8"]').text() === playerUp) ||
+
+        ($('[data-cell="2"]').text() === playerUp &&
+          $('[data-cell="4"]').text() === playerUp &&
+          $('[data-cell="6"]').text() === playerUp)
+
+      ) {
+        return true;
+      } else {
+        return false;
       }
     }
-  })
-
-  function checkWin() {
-    if (
-// Column wins
-      ($('[data-cell="0"]').text() === playerUp &&
-        $('[data-cell="3"]').text() === playerUp &&
-        $('[data-cell="6"]').text() === playerUp) ||
-
-      ($('[data-cell="1"]').text() === playerUp &&
-        $('[data-cell="4"]').text() === playerUp &&
-        $('[data-cell="7"]').text() === playerUp) ||
-
-      ($('[data-cell="2"]').text() === playerUp &&
-        $('[data-cell="5"]').text() === playerUp &&
-        $('[data-cell="8"]').text() === playerUp) ||
-// Row wins
-      ($('[data-cell="0"]').text() === playerUp &&
-        $('[data-cell="1"]').text() === playerUp &&
-        $('[data-cell="2"]').text() === playerUp) ||
-
-      ($('[data-cell="3"]').text() === playerUp &&
-        $('[data-cell="4"]').text() === playerUp &&
-        $('[data-cell="5"]').text() === playerUp) ||
-
-      ($('[data-cell="6"]').text() === playerUp &&
-        $('[data-cell="7"]').text() === playerUp &&
-        $('[data-cell="8"]').text() === playerUp) ||
-// Diagonal wins
-      ($('[data-cell="0"]').text() === playerUp &&
-        $('[data-cell="4"]').text() === playerUp &&
-        $('[data-cell="8"]').text() === playerUp) ||
-
-      ($('[data-cell="2"]').text() === playerUp &&
-        $('[data-cell="4"]').text() === playerUp &&
-        $('[data-cell="6"]').text() === playerUp)
-
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 
 })
