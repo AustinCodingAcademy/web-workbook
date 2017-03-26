6c22506682336b8b06e698ad0484c98f5887ce67
Nancy Montiel <nancysarahi@Nancys-MacBook-Pro.local>
2017-03-25 23:53:29 -0500

    towers

---
+++ 10week/Towers/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  let $block = null;
+  $('[data-stack]').click(function() {
+    let stack = this;
+    if (!$block) {
+      $block = $(stack).children().last().detach();
+    } else if (isLegal(stack) || isEmpty(stack)) {
+      $(stack).append($block);
+      $block = null;
+      if (checkForWin()) {
+        $('#announce-game-won').text('You Win!');
+      }
+    }
+  });
+
+  function isLegal(stack) {
+    return $block.data('block') < $(stack).children().last().data('block')
+   }
+
+  function isEmpty(stack) {
+    return $(stack).children().length === 0;
+  }
+
+  function checkForWin() {
+    return $('[data-stack="2"]').children().length === 4 ||
+    $('[data-stack="3"]').children().length === 4
+  }
+
+});
