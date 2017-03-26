b44c459d1d9fc122c38a256310a94bed4b27cede
stackbaxter <stackbaxter@gmail.com>
2017-03-26 18:47:45 -0500

    progress on index
    flex animations and breathing button

--- mainscript.js
+++ mainscript.js
@@ -2,4 +2,14 @@ $(function(){
     $("#menu").menu();
     // $('#menu').click('expand');
 
-  } );
+  $('.flexmiddle').hover(function(){
+      $('.flexleft').animate({
+        width: '100%', color: 'blue'}, 'slow', 'linear');
+      $('.flexleft').animate({
+          height: '200px', }, 'slow', 'linear');
+      $('.flexmiddle').animate({
+        width: '600px',  }, 2000, 'linear');
+      $('.flexmiddle').animate({
+        height: '700px', }, 2000, 'linear');
+      })
+    })
