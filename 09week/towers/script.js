4d5471c9f66b71ed294cc06e46e4e10a00eda3bf
Maggie <mymonstermaggie@Maggies-MacBook-Pro.local>
2017-03-25 21:00:06 -0500

    towers

--- 09week/towers/script.js
+++ 09week/towers/script.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
 
-  $('[data-stack]').click(function() {
-  console.log("hello");
+  let $block = null;
 
+  $('[data-stack]').click(function() {
+    let stack = this;
+    if (!$block) {
+      $block = $(stack).children().last().detach();
+    } else if (isLegal(stack) || isEmpty(stack)){
+      $(stack).append($block);
+      $block = null;
+      if (checkForWin()){
+        $('#announce-game-won').text('You Win!')
+      };
+    }
   });
+
+  function isLegal(stack) {
+    return ($block.data('block') < $(stack).children().last().data('block'))
+  }
+
+  function isEmpty(stack) {
+    return $(stack).children().length === 0;
+  }
+
+  function checkForWin() {
+    return $('[data-stack="2"]').children().length === 4 ||
+      $('[data-stack="3"]').children().length === 4;
+  }
 });
