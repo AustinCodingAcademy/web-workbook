c61681dbd8278387f65866f2cdfd5ff3d95451f4
demarcogleason <demarcogleason@gmail.com>
2017-03-28 10:58:01 -0500

    tic-tac-toe

---
+++ 08week/redirect/js/typed.js
@@ -0,0 +1,11 @@
+<script>
+	document.addEventListener("DOMContentLoaded", function(){
+		Typed.new(".element", {
+			strings: ["First sentence.", "Second sentence."],
+			typeSpeed: 0
+		});
+	});
+</script>
+...
+
+<span class="element"></span>
