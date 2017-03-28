2629026516ca85e7a3449d025adb9e2fb0313a4a
RFloydUT3 <rileyfloyd3@yahoo.com>
2017-03-28 16:34:44 -0500

    updates2

--- 08week/redirect/js/redirect.js
+++ 08week/redirect/js/redirect.js
@@ -1,9 +1,8 @@
+function redirect1() {
 
+    window.location = '../../07week/Checkpoint2/index.html';
+}
 
-        function redirect1() {
-
-                window.location = '../../07week/Checkpoint2/index.html';
-            }
-        function redirect2(){
-          window.location = "./sorry.html"
-        }
+function redirect2() {
+    window.location = "./sorry.html"
+}
