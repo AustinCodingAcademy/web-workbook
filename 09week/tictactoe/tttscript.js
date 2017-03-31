2ebc7ae98a736f1342317dbca6334b429d764bc8
Greg Devany <greg.devany@yahoo.com>
2017-03-30 23:54:40 -0500

    Making click work on mobile devices

--- 09week/tictactoe/tttscript.js
+++ 09week/tictactoe/tttscript.js
@@ -1,6 +1,6 @@
 $(function() {
   let playerTurn = 'X';
-  $('[data-cell]').bind("vclick touchstart", function() {
+  $('[data-cell]').bind("click touchstart", function() {
     $(this).text(playerTurn);
 // Check to see if there's a winner
     if (checkWin()) {
@@ -8,7 +8,7 @@ $(function() {
       // $('#announce-winner').text(`Player ${playerTurn} Wins!`);
     }
 // Clears the board and fades a clean board back in
-    $("button").bind("vclick touchstart", function() {
+    $("button").bind("click touchstart", function() {
       $("div[data-cell]").fadeOut(1000,function(){
         $("div[data-cell]").empty();
         $('#announce-winner').empty();
