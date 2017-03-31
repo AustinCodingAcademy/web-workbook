fd6c62b9469cef49e09990452ba12e805eee26bf
Greg Devany <greg.devany@yahoo.com>
2017-03-31 10:43:30 -0500

    Making click work on mobile devices

--- 09week/tictactoe/tttscript.js
+++ 09week/tictactoe/tttscript.js
@@ -1,6 +1,6 @@
 $(function() {
   let playerTurn = 'X';
-  $('[data-cell]').on('click touchstart', function(event) {
+  $('[data-cell]').on('click', function(event) {
     event.preventDefault();
     alert("Was preventDefault() called: " + event.isDefaultPrevented());
     $(this).text(playerTurn);
@@ -8,30 +8,28 @@ $(function() {
 // Check to see if there's a winner
     if (checkWin()) {
       $('#announce-winner').text(playerTurn + " wins!");
-      // $('#announce-winner').text(`Player ${playerTurn} Wins!`);
+// $('#announce-winner').text(`Player ${playerTurn} Wins!`);
     }
-// Clears the board and fades a clean board back in
-    $("button").on('click touchstart', function(event) {
-      event.preventDefault;
-      $("div[data-cell]").fadeOut(1000,function(){
-        $("div[data-cell]").empty();
-        $('#announce-winner').empty();
-        $("div").fadeIn(1000);
-        playerTurn = 'X';
-      });
-      //$('#clear').click(function() {
-      //$('[data-cell], #announce-winner').text(null);
-      //playerTurn = 'X';
-
-    })
 // Switches from X to O and visa versa
-      if (playerTurn === 'X') {
-        playerTurn = 'O';
-      } else {
-        playerTurn = 'X'
-      }
-      // playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+    if (playerTurn === 'X') {
+      playerTurn = 'O';
+    } else {
+      playerTurn = 'X'
+    }
+// playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+  });
+
+// Clears the board and fades a clean board back in
+  $("button").on('click', function(event) {
+    event.preventDefault;
+    $("div[data-cell]").fadeOut(1000,function(){
+      $("div[data-cell]").empty();
+      $('#announce-winner').empty();
+      $("div").fadeIn(1000);
+      playerTurn = 'X';
+    })
   })
+  
 // Check to see if there's a winner function
   function checkWin() {
     if(($('[data-cell="0"]').text() === playerTurn &&
