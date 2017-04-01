c3d56070e6253ca6c0c1dd3c2809efcda86b96c9
Keenan Goldis <keenangoldis@gmail.com>
2017-04-01 11:55:16 -0500

    fix links

---
+++ 08week/bizzfuzz/js/javascript.js
@@ -0,0 +1,15 @@
+function fizzbuzz(i)
+for(var i = 0; i < 100; i++){
+  if(i % 15 === 0){
+    console.log('fizzbuzz')
+    }
+    else if(i % 5 ===0){
+    console.log('buzz');
+    }
+    else if(i % 3 ===0){
+    console.log('fizz');
+    }
+    else{
+    console.log(i);
+    }
+}
---
+++ Seatbelt2/js/javascript.js
@@ -0,0 +1,148 @@
+! 
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+
+
+            $(document).ready(function(){
+                $(document).ready(function(){
+                    $("#faqanswer1").hide();
+                });
+                $("#faqquestion1").hover(function(){
+                    $("#faqanswer1").show();
+                });
+              });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#faqanswer2").hide();
+              });
+              $("#faqquestion2").hover(function(){
+                  $("#faqanswer2").show();
+              });
+            });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#faqanswer3").hide();
+              });
+              $("#faqquestion3").hover(function(){
+                  $("#faqanswer3").show();
+              });
+            });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#faqanswer4").hide();
+              });
+              $("#faqquestion4").hover(function(){
+                  $("#faqanswer4").show();
+              });
+            });
+  //START OF BILLING SECTION
+            $(document).ready(function(){
+                $(document).ready(function(){
+                    $("#bilanswer1").hide();
+                });
+                $("#bilquestion1").hover(function(){
+                    $("#bilanswer1").show();
+                });
+              });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#bilanswer2").hide();
+              });
+              $("#bilquestion2").hover(function(){
+                  $("#bilanswer2").show();
+              });
+            });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#bilanswer3").hide();
+              });
+              $("#bilquestion3").hover(function(){
+                  $("#bilanswer3").show();
+              });
+            });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#bilanswer4").hide();
+              });
+              $("#bilquestion4").hover(function(){
+                  $("#bilanswer4").show();
+              });
+            });
+  //START OF PROMOTIONS SECTION
+            $(document).ready(function(){
+                $(document).ready(function(){
+                    $("#proanswer1").hide();
+                });
+                $("#proquestion1").hover(function(){
+                    $("#proanswer1").show();
+                });
+              });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#proanswer2").hide();
+              });
+              $("#proquestion2").hover(function(){
+                  $("#proanswer2").show();
+              });
+            });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#proanswer3").hide();
+              });
+              $("#proquestion3").hover(function(){
+                  $("#proanswer3").show();
+              });
+            });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#proanswer4").hide();
+              });
+              $("#proquestion4").hover(function(){
+                  $("#proanswer4").show();
+              });
+            });
+  //START OF NOTIFICATIONS SECTION
+            $(document).ready(function(){
+                $(document).ready(function(){
+                    $("#notanswer1").hide();
+                });
+                $("#notquestion1").hover(function(){
+                    $("#notanswer1").show();
+                });
+              });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#notanswer2").hide();
+              });
+              $("#notquestion2").hover(function(){
+                  $("#notanswer2").show();
+              });
+            });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#notanswer3").hide();
+              });
+              $("#notquestion3").hover(function(){
+                  $("#notanswer3").show();
+              });
+            });
+
+            $(document).ready(function(){
+              $(document).ready(function(){
+                  $("#notanswer4").hide();
+              });
+              $("#notquestion4").hover(function(){
+                  $("#notanswer4").show();
+              });
+            });
