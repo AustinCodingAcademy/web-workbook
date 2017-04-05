f9d44d7265b19e33ade7731f91ae94698dcb1a5a
Greg Devany <greg.devany@yahoo.com>
2017-04-05 08:51:01 -0500

    Click on mobile. Now working. Editing down.

--- 09week/tictactoe/tttscript.js
+++ 09week/tictactoe/tttscript.js
@@ -1,15 +1,13 @@
-alert('tttscript before')
 $(function() {
-alert('tttscript accessed');
   let playerTurn = 'X';
-  $('[data-cell]').on('tap', function() {
-    alert('into tap listener')
+  $('[data-cell]').on('click', function(event) {
+    event.preventDefault();
     $(this).text(playerTurn);
 
 // Check to see if there's a winner
     if (checkWin()) {
       $('#announce-winner').text(playerTurn + " wins!");
-// $('#announce-winner').text(`Player ${playerTurn} Wins!`);
+      // $('#announce-winner').text(`Player ${playerTurn} Wins!`);
     }
 // Switches from X to O and visa versa
     if (playerTurn === 'X') {
@@ -17,13 +15,12 @@ alert('tttscript accessed');
     } else {
       playerTurn = 'X'
     }
-// playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+    // playerTurn = (playerTurn === 'X') ? 'O' : 'X';
   });
 
 // Clears the board and fades a clean board back in
-  $("button").on('tap', function(event) {
-    // event.preventDefault();
-    alert("clear button pushed");
+  $("button").on('click', function(event) {
+    event.preventDefault();
     $("[data-cell]").fadeOut(1000,function(){
       $("[data-cell]").empty();
       $('#announce-winner').empty();
