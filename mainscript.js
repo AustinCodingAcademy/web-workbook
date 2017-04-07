133e0ba78e2ed9374353bce5010290838138e6fe
stackbaxter <stackbaxter@gmail.com>
2017-04-06 22:15:45 -0500

    spacing and breathing elements

--- mainscript.js
+++ mainscript.js
@@ -2,16 +2,16 @@ $(function(){
     $("#menu").menu();
     // $('#menu').click('expand');
 
-  $('.flexmiddle').hover(function(){
-      $('.flexleft').animate({
-        width: '100%', color: 'blue'}, 'slow', 'linear');
-      $('.flexleft').animate({
-          height: '200px', }, 'slow', 'linear');
-      $('.flexmiddle').animate({
-        width: '600px',  }, 2000, 'linear');
-      $('.flexmiddle').animate({
-        height: '700px', }, 2000, 'linear');
-      })
+  // $('.flexmiddle').hover(function(){
+  //     $('.flexleft').animate({
+  //       width: '100%', color: 'blue'}, 'slow', 'linear');
+  //     $('.flexleft').animate({
+  //         height: '200px', }, 'slow', 'linear');
+  //     $('.flexmiddle').animate({
+  //       width: '600px',  }, 2000, 'linear');
+  //     $('.flexmiddle').animate({
+  //       height: '700px', }, 2000, 'linear');
+  //     })
 
     $('#weeknum').change(function(){
       location.href = $(this).val();
