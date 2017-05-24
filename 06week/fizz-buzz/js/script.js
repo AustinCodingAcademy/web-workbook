657b5cdabf2e05c6368376fe134967ca7a74e7c6
situlillian <mslilliansitu@gmail.com>
2017-05-23 19:04:49 -0500

    Added comments to JS file

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -1,14 +1,22 @@
 'use strict';
 
 for (var i = 1; i <= 100; i++) {
+
+  // is this a factor of 3? subsequently, it is also a factor of 5?
   if (i % 3 === 0) {
     if (i % 5 === 0) {
       document.write('fizzbuzz');
+
+      // if not both, then just apply to factor of 3
     } else {
       document.write('fizz');
     }
+
+    // catch the remaining 5s
   } else if (i % 5 === 0) {
     document.write('buzz');
+
+    // every other number
   } else {
     document.write(i);
   }
