2cbc2d5862940ce99649136a0b17d749aa1fe6b7
Katie Littell <littell_k@yahoo.com>
2017-05-24 20:54:45 -0500

    Revised fizzbuzz

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -1,13 +1,13 @@
-var i = 0;
-var total = 100;
-for (var i = 0; i <= total; i++) {
-if (i / 3) {
-  console.log('Fizz');
-}
-if (i / 5) {
-  console.log('Buzz');
-}
-if (i / 3 && i / 5) {
+
+for (var i = 1; i <= 100; i++) {
+if (i % 3 === 0 && i % 5 === 0)
   console.log('FizzBuzz');
-}
-}
+
+else if (i % 3 == 0)
+  console.log('Fizz');
+
+else if (i % 5 == 0)
+  console.log('Buzz');
+
+else
+  console.log(i); }
