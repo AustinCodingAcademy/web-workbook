b056a9dd9f0fd2bd3e631257506fbe17dcba3677
James Walker <james.walker@contractor.net>
2017-05-25 14:46:26 -0500

    added document open/close, corrected even more indents

--- 06week/fizz-buzz/js/app.js
+++ 06week/fizz-buzz/js/app.js
@@ -1,21 +1,30 @@
 var first = 1, last = 100;
 
-var comparison = [
-  ["fizz", 3],
-  ["buzz", 5]
-];
+var fizz = {
+  name: 'Fizz',
+  value: 3
+};
+
+var buzz = {
+  name: 'Buzz',
+  value: 5
+};
+
+document.open();
 
 for (var i = first; i<last+1; i++) {
 
-  if (i % comparison[0][1] == 0) {
-    if (i % comparison[1][1]  == 0) {
-      document.write(comparison[0][0], comparison[1][0], "<br>");
+  if (i % fizz.value === 0) {
+    if (i % buzz.value  === 0) {
+      document.write(fizz.name, buzz.name, "<br>");
     } else {
-        document.write(comparison[0][0], "<br>");
-      }
-  } else if (i % comparison[1][1]  == 0) {
-    document.write(comparison[1][0], "<br>");
+      document.write(fizz.name, "<br>");
+    }
+  } else if (i % buzz.value  === 0) {
+    document.write(buzz.name, "<br>");
   } else {
     document.write(i, "<br>");
   }
 }
+
+document.close();
