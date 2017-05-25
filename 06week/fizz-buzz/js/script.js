2ad76e18d1d6c67a043fcfa7fa4e43c61a37f0f8
adamgrieder <adamgrieder92@gmail.com>
2017-05-24 18:07:00 -0700

    Fizz BuZZ

---
+++ 06week/fizz-buzz/js/script.js
@@ -0,0 +1,14 @@
+
+for (var i =0;  i < 100; i++) {
+  console.log(i);
+
+  if (i % 15 === 0){
+    console.log("FizzBuzz")
+  }
+
+else if (i %  3=== 0) {
+  console.log("Fizz");
+} else {
+  console.log(i);
+}
+};
