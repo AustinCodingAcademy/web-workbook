426753f1583a5874baf7250aba3cdd6625e5c28e
kyle rhodes <rhodestraveler@gmail.com>
2017-05-26 10:55:40 -0500

    FizzBuzz completed

---
+++ 06week/fizz-buzz/js/script.js
@@ -0,0 +1,14 @@
+for (i=0; i<=100; i++) {
+  if (i%15===0) {
+    console.log('FizzBuzz');
+  }
+  else if (i%5===0) {
+    console.log('Buzz');
+  }
+  else if (i%3===0) {
+    console.log('Fizz');
+  }
+  else {
+    console.log(i)
+  }
+}
