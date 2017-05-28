cfe8ee8f6fc704986c9199c048057cceff0d99d2
ReilyHalversen <ReilyHalversen@gmail.com>
2017-05-28 10:50:02 -0500

    Trying again...

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -1,13 +1,15 @@
-for (var i = 1; i <= 100; i++) {
+    for (var i = 1; i <= 100; i++) {
 
-if (i % 5 === 0 && i % 3 === 0)
-  console.log("Fizzbuzz");
+    if (i % 5 === 0 && i % 3 === 0)
+      console.log("Fizzbuzz");
 
-else if (i % 3 === 0)
-  console.log("Fizz");
+    else if (i % 3 === 0)
+      console.log("Fizz");
 
-else if (i % 5 === 0)
-  console.log("Buzz");
+    else if (i % 5 === 0)
+      console.log("Buzz");
 
-else
-  console.log(i); }
+    else
+      console.log(i); }
+
+  var Change = "Changemade to turn in homework"
