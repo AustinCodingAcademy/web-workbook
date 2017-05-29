752e9878cd492bb6870a675adee32864aa15c8d8
Jason Kelley <jasonkelley@Jasons-MacBook-Pro.local>
2017-05-29 13:19:06 -0500

    complete

---
+++ javascripting/if-statement.js
@@ -0,0 +1,8 @@
+var fruit = 'orange';
+var n = fruit.length;
+if (n > 5) {
+  console.log('The fruit name has more than five characters.');
+}
+else {
+  console.log('The fruit name has five characters or less.');
+}
