6cc38423e021146a9cf03c6c9e2265151fc4813a
Ke Tang <ket.tang@gmail.com>
2017-05-29 20:58:38 -0500

    week 06

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -1,15 +1,14 @@
-var print = ['Fizz', 'Buzz', 'FizzBuzz'];
+var printout = ['Fizz', 'Buzz', 'FizzBuzz'];
 
 for (i=0; i<101; i++) {
-  if ( i % 3 ===0 ) {
-    console.log(print[0]);
-    // return print[1];
-  }
-  if ( i % 5 ===0 ) {
-    console.log(print[1]);
-  }
-  if ( i % 15 ===0 ) {
-    console.log(print[2]);
-  }
+  if ( i % 3  ===0  && i % 5 ===0 ) {
+    console.log(printout[2]);
+    }
+  else if ( i % 3 ===0 ) {
+      console.log(printout[0]);
+      }
+  else if ( i % 5 ===0 ) {
+      console.log(printout[1]);
+      }
   else { console.log(i);}
 }
