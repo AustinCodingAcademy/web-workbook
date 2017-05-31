52c3be834af00c24ea336775ae8eb1db3e6173fc
rjones1984 <rjones1984@users.noreply.github.com>
2017-05-30 20:47:54 -0500

    finished DOM challenge

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -1,15 +1,32 @@
+// function fizzBuzz (){
+//   for (var i=1; i<=100; i++){
+//   if (i%3 === 0 && i %5 === 0) {
+//   document.write ('fizzBuzz');
+//   } else if (i%3 === 0) {
+//   document.write ('Fizz');
+//   } else if (i%5 === 0) {
+//   document.write('Buzz');
+//   } else {
+//   document.write(i);
+//     }
+//   document.write('<br>', '<br>');
+//   }
+// }
+//
+// fizzBuzz();
+
 function fizzBuzz (){
   for (var i=1; i<=100; i++){
   if (i%3 === 0 && i %5 === 0) {
-  document.write ('fizzBuzz');
+  console.log ('fizzBuzz');
   } else if (i%3 === 0) {
-  document.write ('Fizz');
+  console.log ('Fizz');
   } else if (i%5 === 0) {
-  document.write('Buzz');
+  console.log('Buzz');
   } else {
-  document.write(i);
+  console.log(i);
     }
-  document.write('<br>', '<br>');
+  console.log('<br>', '<br>');
   }
 }
 
