9cd19b5b182f79dbeafaf18fd4c808d756210def
situlillian <mslilliansitu@gmail.com>
2017-05-31 19:03:20 -0500

    cleaned up javascript functions

---
+++ 07week/dom-practice/js/script.js
@@ -0,0 +1,35 @@
+// alert the user to the number of list items
+window.onload = function () {
+  var listItems = document.getElementsByTagName('LI');
+  alert ('Number of list items: ' + listItems.length);
+}
+
+// remove the fourth paragraph
+function removeFourth () {
+  var list = document.getElementById('P4');
+  list.remove();
+}
+
+
+// add fifth item
+function addFifth () {
+  var newItem = document.createElement('LI');
+  var textnode = document.createTextNode('Fifth List Item');
+  newItem.appendChild(textnode);
+
+  var list = document.getElementById('myList');
+  list.appendChild(newItem);
+}
+
+
+function header() {
+
+  var heading = document.createElement('h1');
+  return ('Manipulating the DOM!')
+  // heading.appendChild(heading_text);
+  // document.body.appendChild(heading);
+}
+
+document.getElementById("h1").innerHTML = header();
+
+// innerHTML = header();
