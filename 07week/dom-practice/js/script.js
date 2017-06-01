564708c7fc2b0261801e28f16d5ad8477a119ec0
situlillian <mslilliansitu@gmail.com>
2017-05-31 19:20:52 -0500

    cleaned up javascript code

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,18 +1,17 @@
 // alert the user to the number of list items
-window.onload = function () {
+window.onload = function() {
   var listItems = document.getElementsByTagName('LI');
-  alert ('Number of list items: ' + listItems.length);
+  alert('Number of list items: ' + listItems.length);
 }
 
 // remove the fourth paragraph
-function removeFourth () {
+function removeFourth() {
   var list = document.getElementById('P4');
   list.remove();
 }
 
-
 // add fifth item
-function addFifth () {
+function addFifth() {
   var newItem = document.createElement('LI');
   var textnode = document.createTextNode('Fifth List Item');
   newItem.appendChild(textnode);
@@ -21,15 +20,9 @@ function addFifth () {
   list.appendChild(newItem);
 }
 
-
 function header() {
-
   var heading = document.createElement('h1');
   return ('Manipulating the DOM!')
-  // heading.appendChild(heading_text);
-  // document.body.appendChild(heading);
 }
 
 document.getElementById("h1").innerHTML = header();
-
-// innerHTML = header();
