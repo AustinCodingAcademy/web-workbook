a2c4b94abeb3829c93ea3ac054de01003d23a546
Mar10God1 <mar10_godwin@yahoo.com>
2017-06-04 18:48:49 -0500

    finished tic-tac-toe

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -3,26 +3,32 @@
 
 $(document).ready(function() {
   // Put app logic in here
-  $('body').css('background-color', 'gray');
+
 
   var turn = 1;
-
+  var square = ['div[data-cell="0"]', 'div[data-cell="1"]', 'div[data-cell="2"]', 'div[data-cell="3"]', 'div[data-cell="4"]', 'div[data-cell="5"]', 'div[data-cell="6"]', 'div[data-cell="7"]', 'div[data-cell="8"]'];
+//Click event
   $('div[data-cell="0"], div[data-cell="1"], div[data-cell="2"], div[data-cell="3"], div[data-cell="4"], div[data-cell="5"], div[data-cell="6"], div[data-cell="7"], div[data-cell="8"]').on( 'click', function(){
-    // Player select
+// Player select
      if (turn % 2 === 0) {
        var playerTurn = 'o';
      } else {
        var playerTurn = 'x';
      };
+//Take turn
     $(this).text(playerTurn);
-
-
+//Check for winner
+    if ((($(square[0]).text() === playerTurn) && ($(square[1]).text() === playerTurn) && ($(square[2]).text() === playerTurn)) || (($(square[3]).text() === playerTurn) && ($(square[4]).text() === playerTurn) && ($(square[5]).text() === playerTurn)) || (($(square[6]).text() === playerTurn) && ($(square[7]).text() === playerTurn) && ($(square[8]).text() === playerTurn)) || (($(square[6]).text() === playerTurn) && ($(square[4]).text() === playerTurn) && ($(square[2]).text() === playerTurn)) || (($(square[0]).text() === playerTurn) && ($(square[4]).text() === playerTurn) && ($(square[8]).text() === playerTurn))) {
+      alert(playerTurn + ' wins!');
+      $('#announce-winner').text(playerTurn + ' wins!');
+    };
+// Next turn
   turn++;
 });
 
-
 // Clear board
 $('#clear').click(function() {
-  $('div[data-cell="0"], div[data-cell="1"], div[data-cell="2"], div[data-cell="3"], div[data-cell="4"], div[data-cell="5"], div[data-cell="6"], div[data-cell="7"], div[data-cell="8"]').removeChild();
+  $('div[data-cell="0"], div[data-cell="1"], div[data-cell="2"], div[data-cell="3"], div[data-cell="4"], div[data-cell="5"], div[data-cell="6"], div[data-cell="7"], div[data-cell="8"]').text('');
+  $('#announce-winner').text('');
   });
 });
