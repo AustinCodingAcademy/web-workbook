434d0bd732737c694271aefa93f60b8a92c527b1
Ellen <lattelizard@gmail.com>
2017-06-08 09:00:08 -0500

    Add files via upload

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,51 @@
 'use strict';
 
-$(document).on('ready', function() {
+  $(document).ready(function() {
   // Put app logic in here
+
+  var play = true;
+  var toggle = function() {
+      if(play) {
+        $(this).text('x');
+
+      }
+      else {
+        $(this).text('o');
+      }
+
+      // switch to make play flip
+      play = !play;
+    }
+
+
+    $('[data-cell]').click(toggle);
+
+
+    $('#clear').click(function(){
+    $('[data-cell]').text('');
+  });
+
+
+
+
+    var winner = true;
+    var announce = (function() {
+      if('[data-cell]' == ["0"] && '[data-cell]') ||
+      ('[data-cell]' == ["1"] && '[data-cell] ')
+      {
+        $('[data-cell = "o"').text("#announce-winner");
+      }
+        else {
+        $('[data-cell = "x"]').text("#announce-winner")
+    }
+
+
+}
+/* $('div[attr1="value1"][attr2="value2"]')
+    });
+
+    if (('row' == '[data-cell]' && '[data-cell]' &&  '[data-cell]' == o) || ('row' == '[data-cell]' && '[data-cell]' &&  '[data-cell]' == o)) {
+            winner= document.getElementById('#announce-winner').text(' You WON!');
+*/
+
 });
