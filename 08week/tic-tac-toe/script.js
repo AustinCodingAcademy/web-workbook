0a83612394205d697741d3c659c8877f34d2c8f1
situlillian <mslilliansitu@gmail.com>
2017-06-07 23:00:52 -0500

    added clear board function

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,49 +1,50 @@
 'use strict';
 
-$(document).on('ready', function() {
-  // Put app logic in here
+$(document).ready(function() {
+  playerActions();
+  displayTurn();
+  clearBoard();
 });
 
-// add or remove css X:after
+// 2 players, X & Os, one player at a time
 
-
-// * 2 players, X & Os, one player at a time
-//
-// * Use variable to keep track of the player’s turn (1 = X or 2 = O)
+// Use variable to keep track of the player’s turn (1 = X or 2 = O)
 var playerTurn = 1;
-console.log('hi');
 
-// * When a player clicks on a box, if the box is a data cell, then assign the X or O depending on the turn
+function displayTurn() {
+  $('.currentTurn').html('Player ' + playerTurn + ' \'s turn');
+}
 
-$('div').click(function() {
-  var cell = $(this);
+function playerActions() {
 
-  // Use CSS class X:after or O:after to add respective to div
-  if (cell.attr('data-cell')) {
-    if (!cell.attr('class')) {
+  // * When a player clicks on a box, if the box is a data cell, then assign the X or O depending on the turn
+  $('div').click(function() {
+    var cell = $(this);
 
-      // TODO: put into function
-      var className = 'O';
-      if (playerTurn === 1) {
-        className = 'X';
+    // first check if the box already has a class, if it does not, use CSS class X or O to add respective to div
+    if (cell.attr('data-cell')) {
+      if (!cell.attr('class')) {
+
+        // TODO: put into function
+        var className = ((playerTurn === 1) ? 'X' : 'O');
+        cell.addClass(className);
+
+      } else {
+        alert('Silly goose! That\'s not how you play. Please choose another box!');
       }
-      cell.addClass(className);
-    } else {
-      alert('Please choose another box!');
-    }
       // check for a win function
-      
-      // switch the player turn
+
+      // switch the player turn and then display it
       if (playerTurn === 1) {
         playerTurn = 2;
       } else {
-        playerTurn = 1
+        playerTurn = 1;
       }
 
+      displayTurn();
     }
-});
-
-
+  });
+}
 
 // * Check for a win function
 //     * If win
@@ -59,10 +60,26 @@ $('div').click(function() {
 //         * Enable reset button
 //     * If draw, display game over message with no winner
 //         * Enable reset button
-//     * Else: switch the variable for the player’s turn
-// * Reset the game function
-//     * When a user clicks the reset game function
-//         * Remove all css classes from data-cell divs
-//         * Reset the player variable to 1
-//         * Set content of of display game over message to ‘ '
-//         * Disable the reset button
+
+// * Clear board
+function clearBoard() {
+
+  // When a user clicks the reset game function
+  $('#clear').click(function() {
+    // Remove all css classes from data-cell divs
+    $('.X, .O').removeClass();
+    // Reset the player variable to 1
+    playerTurn = 1;
+    // Set content of of display game over message to ‘ '
+    $('#displayMessage').html(' ');
+    // Disable the button
+    $(this).prop('disabled', true);
+  });
+}
+
+// work in progress
+var arr = $('.row>div').val();
+// $('.row').each(function(){
+//   arr.push($(this));
+// });
+console.log(arr[2]);
