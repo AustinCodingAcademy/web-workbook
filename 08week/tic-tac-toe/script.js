e27483be7a62b13dc264401a983b5ad5b50802ec
situlillian <mslilliansitu@gmail.com>
2017-06-09 12:56:29 -0500

    cleaned up code

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,17 +1,14 @@
 'use strict';
 
 $(document).ready(function() {
-  $('#clear').prop('disabled', true);
-  playerActions();
+  disableButton();
   displayTurn();
+  playerActions();
   clearBoard();
 });
 
-// 2 players, X & Os, one player at a time
-
 // Use variable to keep track of the player’s turn (1 = X or 2 = O)
 var playerTurn = 1;
-
 // other constants
 var clearButton = $('#clear');
 var displayMsg = $('#displayMessage');
@@ -20,39 +17,41 @@ function displayTurn() {
   $('.currentTurn').html('Player ' + playerTurn + ' \'s turn');
 }
 
+function disableButton () {
+  clearButton.prop('disabled', true);
+}
+
+function nextTurn () {
+  if (playerTurn===1) {
+    playerTurn = 2;
+  } else {
+    playerTurn = 1;
+  }
+}
+
+// * When a player clicks on a box, if the box is a data cell, then assign the X or O depending on the turn
 function playerActions() {
 
-  // * When a player clicks on a box, if the box is a data cell, then assign the X or O depending on the turn
   $('div').click(function() {
     var cell = $(this);
 
-    // first check if the box already has a class, if it does not, use CSS class X or O to add to respective to div
     if (cell.attr('data-cell')) {
-      // TODO: drill down into the class names
-      // if (!cell.attr('class'))
-      if (!$(this).is('.taken')) {
+
+      // first check if the box already has taken class, if it does not, use CSS class X or O to add to respective to div
+      if (!$(this).is('.taken')) /* if (!cell.attr('class')) */ {
         var className = ((playerTurn === 1) ? 'X taken' : 'O taken');
         cell.addClass(className);
       } else {
         alert('Silly goose! That\'s not how you play. Please choose another box!');
-        // allows the current player to choose another box without switching turns
-        if (playerTurn === 1) {
-          playerTurn = 2;
-        } else {
-          playerTurn = 1;
-        }
+        nextTurn(); // allows the current player to choose another box without switching turns
       }
-      // check for a win function
+
+      // check for a win function, then a draw, if neither, continue
       checkWin();
       checkDraw();
 
       // switch the player turn and then display it
-      if (playerTurn === 1) {
-        playerTurn = 2;
-      } else {
-        playerTurn = 1;
-      }
-
+      nextTurn();
       displayTurn();
     }
   });
@@ -63,51 +62,40 @@ function checkDraw() {
   if ($('.taken').length === 9) {
     displayMsg.html('Game Over! It\'s a draw!');
     clearButton.prop('disabled', false);
-    return false;
+    nextTurn ();
   }
 }
 
-// * Clear board
+// reset everything to original state
 function clearBoard() {
 
-  // When a user clicks clear board button
   $('#clear').click(function() {
-    // Remove all css classes from data-cell divs
     $('.X, .O').removeClass();
-    // Reset the player variable to 1
     playerTurn = 1;
-    // Set content of display game over message to ‘'
     displayMsg.html('');
-    // Disable the button
-    $(this).prop('disabled', true);
+    disableButton();
     displayTurn();
   });
 }
 
 
-// check for win
+// let's see if there's a win! all possible combos
 function checkWin() {
   var wins = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
   ];
 
   var playersMarker = playerTurn === 1 ? 'X' : 'O'; // dynamic based on user's turn
 
   var all = $('.board div.' + playersMarker);
 
-  var coords = []
-  all.each(function(i) {
+  var coords = [] // create array that keeps track of X and O coordinates
+  all.each (function (i) {
     coords.push(Number($(this).attr('data-cell')));
   });
   coords = coords.sort(); // not sure if needed
   coords = coords.join();
+  console.log(coords);
 
   var userWon = false;
 
@@ -132,6 +120,7 @@ function checkWin() {
   if (userWon) {
     displayMsg.html('Game Over! Player ' + playerTurn + ' has won!');
     clearButton.prop('disabled', false);
+    nextTurn ();
   }
 
 }
