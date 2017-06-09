45fbda4d3ce8c1bd1945fd19f1587e476b8fbdc2
RobNeu78 <neureither402@gmail.com>
2017-06-09 09:21:57 -0500

    Tic-tac-toe game

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,47 @@
 'use strict';
 
-$(document).on('ready', function() {
+$(document).ready(function() {
   // Put app logic in here
+  var start = true;
+  var toggle = function () {
+    if (!this.innerHTML) {
+      if(start){
+        $(this).text("X");
+      }else{
+        $(this).text("O");
+      }
+      start = !start;
+    }else {
+      alert("That space is already played!");
+    }
+    //winner();
+  };
+
+  var clear = function(){
+    $('[data-cell]').empty();
+  };
+
+
+
+  $("[data-cell]").click(toggle);
+
+  $('#clear').click(clear);
+
+
+
+
 });
+
+/*var winner = function(){
+  if (
+  $("#0")[0].innerHTML === $("#1")[0].innerHTML && $("#1")[0].innerHTML === $("#2")[0].innerHTML){
+    alert("Winner");
+  }
+  if (
+  $("#6")[0].innerHTML === $("#7")[0].innerHTML && $("#7")[0].innerHTML === $("#8")[0].innerHTML){
+    alert("Winner");
+
+
+
+
+};*/
