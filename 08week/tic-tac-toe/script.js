35189fe18d789164d9082f1fe63f97e99311ba92
Chase Turner <chasethemigthy1@gmail.com>
2017-06-12 18:05:11 -0500

    Tic-tac-toe Finished?

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,26 @@
 'use strict';
 
-$(document).on('ready', function() {
-  // Put app logic in here
-});
+$(document).ready(function (){
+  var happy=true;
+  var toggle=function(){
+    if (happy) {
+      $(this).text('x');
+    }
+    else {
+      $(this).text('o');
+    }
+    happy=!happy;
+  }
+  $('[data-cell]').click(toggle);
+  $('#clear').click(function(){
+    $('[data-cell]').empty();
+  var test=$('[data-cell="0"]')[0].innerHTML;
+  });
+//var win=function(){
+  //if ($('[data-cell="0"]')[0].innerHTML === $('[data-cell="1"]')[0].innerHTML){
+//$("#announce-winner").text('You win!');
+//}
+
+
+
+);
