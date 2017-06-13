6615f3f1fa9ed4e8291d502e939292165387c775
Casey O'Keefe <the0surfer@aol.com>
2017-06-12 20:31:55 -0500

    finishing towers

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
 $(document).ready(function() {
-
-
   var gameover = false; //boolean for ending the game
   var $stack = $('[data-stack]');
   var $block = $('[data-block]');
@@ -10,7 +8,6 @@ $(document).ready(function() {
 // lastBlock is a legalMove in the game.
   var lastBlock = $('[data-block]:last-child');
   lastBlock.addClass("legalMove");
-
 
 // code states that all move will be reverted unless they meet the conditions to make the revert false
   $block.draggable({revert: true});
@@ -35,10 +32,9 @@ $(document).ready(function() {
           checkForWin();
         }
       } else {
-        resetGame();
       }
     }
-  });
+  }); // droppable ending
 
 // this function declares if the block is smaller than the block it will be on
 // placed into an empty field
@@ -50,53 +46,24 @@ $(document).ready(function() {
   } else {
     return false;
   }
-  }
+} // legalDrop ending
   function checkForWin() {
     if ($('[data-stack="3"]').children().length === 5) {
       $('#announcement').html("You Won!");
       gameover = true;
 
     }
-  }
+  } // check for win ending
 
-  //reset needs original code to work with draggable after newly created element
+  //reset and moves all elements to original starting position
     function resetGame() {
-      $('[data-stack="1"]').html('<div data-block="125"></div><div data-block="100"></div><div data-block="75"></div><div data-block="50"></div><div data-block="25"></div>');
-      $('[data-stack="2"]').empty();
-      $('[data-stack="3"]').empty();
+      $('[data-block="125"]').appendTo($('[data-stack="1"]'));
+      $('[data-block="100"]').appendTo($('[data-stack="1"]'));
+      $('[data-block="75"]').appendTo($('[data-stack="1"]'));
+      $('[data-block="50"]').appendTo($('[data-stack="1"]'));
+      $('[data-block="25"]').appendTo($('[data-stack="1"]'));
       $('#announcement').empty();
       gameover = false;
-      var $stack = $('[data-stack]');
-      var $block = $('[data-block]');
-      var lastBlock = $('[data-block]:last-child');
-      lastBlock.addClass("legalMove");
-      $block.draggable({revert: true});
-      $stack.droppable({
-      accept: ".legalMove",
-      drop: function(event, ui) {
-        if (!gameover) {
-          if (legalDrop($(this), ui.draggable)) {
-            ui.draggable.draggable('option', 'revert', false);
-            $(this).append(ui.draggable.detach());
-            ui.draggable.css({
-              'top':0,
-              'left':0
-            });
-            lastBlock = $('[data-block]:last-child');
-            $('[data-block]').removeClass("legalMove");
-            lastBlock.addClass("legalMove");
-            $block.draggable({
-              revert: true
-            });
-            checkForWin();
-          }
-        } else {
-          resetGame();
-        }
-      }
-    });
-  }
+    }
 $('#clear').click(resetGame);
-
-
-});
+}); //document ready ending
