fd9e8d62551d9dfd1ec76427d11e1902fb9b7483
adamgrieder <adamgrieder92@gmail.com>
2017-06-14 15:42:28 -0500

    Towers

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -6,9 +6,16 @@ $(document).ready(function() {
   });
 $('[data-stack]').droppable({
   drop: function(event, ui) {
-    $(ui.draggable).appendTo(this).attr('style','position: "realative"');
+
+var drag = $(ui.draggable).attr('data-block');
+var lastChild = $(this).children().last()[0];
+var LastChildval = $(lastChild).attr('data-block');
+if (parseInt(drag) > parseInt(LastChildval)) {
+  $(ui.draggable).draggable('option','revert', true);
+} else {
+  $(ui.draggable).appendTo(this).attr('style','position: "realative"');
   }
-
-
+}
 });
+
 });
