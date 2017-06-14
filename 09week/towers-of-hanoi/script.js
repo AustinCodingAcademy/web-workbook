f40e47b639e0020ae328ae5bb63aa4a2ef3f2f93
Mar10God1 <mar10_godwin@yahoo.com>
2017-06-14 12:09:42 -0500

    Towers of Hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -3,5 +3,22 @@
 $(document).ready(function() {
   // Put app logic here
 
-  $( "#block25" ).draggable();
+  $('[data-block]').draggable({
+    revert: "invalid"});
+  $('[data-stack]').droppable({
+    drop: function(event, ui) {
+
+    var drag = $(ui.draggable).attr('data-block');
+    var lastChild = $(this).children().last()[0];
+    var lastChildval = $(lastChild).attr('data-block');
+    if (parseInt(drag) > parseInt(lastChildval)) {
+      $(ui.draggable).draggable('option','revert',true);
+    } else {
+    $(ui.draggable).appendTo(this).attr('style', 'position: relative');
+    }
+    }
+  });
+
+
+
 });
