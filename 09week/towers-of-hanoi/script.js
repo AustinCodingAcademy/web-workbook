3deafcd76cd80bc8dca6bc0e79ecc65ad8d6e43b
Chase Turner <chasethemigthy1@gmail.com>
2017-06-14 10:54:29 -0500

    Towers of Hanoi Fin

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,25 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  function dropIt(event, object, ui){
+  var currentvalue = ui.draggable[0].attributes[0].value;
+  var lastchild = this.lastChild[0].attributes[0].value;
+  if (parseInt(lastChild) >= parseInt(currentvalue) || lastChild==0){
+    $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+}
+  else {
+    $(ui.draggable).draggable('option','revert',true);
+  }
+}
+  $('[data-block]').draggable({
+    revert:'invalid'
+  });
+  $('[data-stack]').droppable({
+    drop: function(event , ui){
+    $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+    dropIt(event, this, ui);
+  }
+
+});
+
 });
