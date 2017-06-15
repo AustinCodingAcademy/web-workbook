6b41ce2639a7dd790f8f5068541769ac5890afa9
robert-sasaki <rsmp41@gmail.com>
2017-06-15 16:52:37 -0500

    Hanoi CSS update

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -50,10 +50,18 @@ $(document).ready(function () {
      }
   function checkForWin() {
     if ($('[data-tower="3"]').children().length === 4) {
-    $('#winner').html("You Won!");
+    $('.winner').html("You Won!");
     gameEnd = true;
 
-   }
-    }
+      }
+  }
+
+  function resetGame() {
+    $('[data-tower="1"]').html('<div data-block="4"></div><div data-block="3"></div><div data-block="2"></div><div data-block="1"></div>');
+    $('[data-tower="2"]').empty();
+    $('[data-tower="3"]').empty();
+    $(',winner').empty();
+    gameEnd = false;
+  }
 
 });
