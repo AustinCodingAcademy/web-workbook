22e38056499e14d5c672cca673ddf70047e5de92
Ellen Martinez <lattelizard@gmail.com>
2017-06-15 11:39:12 -0500

    forms

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,4 +2,25 @@
 
 $(document).ready(function() {
   // Put app logic here
+
+
+
+var lastChild = ('[data-block]')
+var lastchildValue =
+var currentValue =
+if (!lastChildValue || )
+
+
+  //drag & drop
+$('[data-block]').draggable({ revert: 'invalid' });
+$('[data-stack]').droppable({
+  drop:function(event, ui) {
+$(ui.draggable).appendTo(this).attr('style', 'postition: relative');
+  }
+});
+
+function checkWinner() {
+    if((dataCell0.text() != '') && (dataCell0.text () == dataCell1.text())
+     && (dataCell1.text() == dataCell2.text())){
+
 });
