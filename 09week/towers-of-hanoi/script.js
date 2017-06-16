d9ba476f5e837b9a9bcd9fdfd4149d9678994543
ryanr73 <ryanrroberts73@gmail.com>
2017-06-16 17:22:45 -0500

    survey

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,31 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+    var block;
+
+    function checkForWin() {
+        var gameWon = false;
+        $('[data-stack]').each(function () {
+            if ($(this).data('stack') !== 1 && $(this).children().length === 4) {
+                gameWon = true;
+            }
+        });
+        return gameWon;
+    }
+
+    $('[data-stack]').on('click', function() {
+        if (!block) {
+            block = $(this).children().last().detach();
+        } else {
+            if ( !$(this).children().last().data('block') || $(this).children().last().data('block') > $(block).data('block') ) {
+                $(this).append(block);
+                block = null;
+            }
+
+            if (checkForWin()) {
+                $('#announce-game-won').text('Winner!');
+            }
+        }
+    });
+    FastClick.attach(document.body);
 });
