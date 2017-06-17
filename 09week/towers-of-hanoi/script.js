5a21650b853a7a2d4731d381e84eefa285663a11
Ellen <lattelizard@gmail.com>
2017-06-17 15:43:12 -0700

    tower

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -9,11 +9,11 @@ $(document).ready(function() {
     var lastchild = this.lastChild[0].attributes[0].value;
     if (parseInt(lastChild) >= parseInt(currentvalue) || lastChild==0){
       $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
-  }
+    }
     else {
       $(ui.draggable).draggable('option','revert',true);
     }
-  }
+
 
   //drag & drop
     $('[data-block]').draggable({
@@ -25,6 +25,7 @@ $(document).ready(function() {
       dropIt(event, this, ui);
     }
   });
+    });
 
   function checkWinner() {
       if((dataCell0.text() != '') && (dataCell0.text () == dataCell1.text())
