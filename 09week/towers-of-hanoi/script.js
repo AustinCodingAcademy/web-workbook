bca2a3146177b3fa540b91376d49298e16079acc
Ellen <lattelizard@gmail.com>
2017-06-17 10:30:20 -0700

    towers

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -4,23 +4,35 @@ $(document).ready(function() {
   // Put app logic here
 
 
-
-var lastChild = ('[data-block]')
-var lastchildValue =
-var currentValue =
-if (!lastChildValue || )
-
+  (document).ready(function() {
+    function dropIt(event, object, ui){
+    var currentvalue = ui.draggable[0].attributes[0].value;
+    var lastchild = this.lastChild[0].attributes[0].value;
+    if (parseInt(lastChild) >= parseInt(currentvalue) || lastChild==0){
+      $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+  }
+    else {
+      $(ui.draggable).draggable('option','revert',true);
+    }
+  }
 
   //drag & drop
-$('[data-block]').draggable({ revert: 'invalid' });
-$('[data-stack]').droppable({
-  drop:function(event, ui) {
-$(ui.draggable).appendTo(this).attr('style', 'postition: relative');
-  }
-});
+    $('[data-block]').draggable({
+      revert:'invalid'
+    });
+    $('[data-stack]').droppable({
+      drop: function(event , ui){
+      $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+      dropIt(event, this, ui);
+    }
+  
+  });
 
-function checkWinner() {
-    if((dataCell0.text() != '') && (dataCell0.text () == dataCell1.text())
-     && (dataCell1.text() == dataCell2.text())){
+  function checkWinner() {
+      if((dataCell0.text() != '') && (dataCell0.text () == dataCell1.text())
+       && (dataCell1.text() == dataCell2.text())){
 
-});
+  });
+
+
+  });
