406e958fdb8e6e8b0169238e74a44135728a539a
adamgrieder <adamgrieder92@gmail.com>
2017-06-19 17:27:28 -0500

    TTT

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var turn = 1;
   var square = ['div[data-cell="0"]', 'div[data-cell="1"]', 'div[data-cell="2"]', 'div[data-cell="3"]', 'div[data-cell="4"]', 'div[data-cell="5"]', 'div[data-cell="6"]', 'div[data-cell="7"]', 'div[data-cell="8"]'];
-// jquery .click event
+/* jquery .click event */
   $('div[data-cell="0"], div[data-cell="1"], div[data-cell="2"], div[data-cell="3"], div[data-cell="4"], div[data-cell="5"], div[data-cell="6"], div[data-cell="7"], div[data-cell="8"]').on( 'click', function(){
 
      if (turn % 2 === 0) {
@@ -10,20 +10,20 @@ $(document).ready(function() {
        var playerTurn = 'X';
      };
 
-// If space is taken, will not override
+/* If space is taken, will not override */
      if (this.innerHTML) {
        return false;
      };
 
          $(this).text(playerTurn);
 
-     //If game ends, the board will not do anything
+     /* If game ends, the board will not do anything*/
      if ($('#announce-winner').html().length > 0) {
        return false;
      };
 
 
-//See if there is a winner/ alert page viewers of a win -->
+/*See if there is a winner/ alert page viewers of a win */
     if ((($(square[0]).text() === playerTurn) && ($(square[1]).text() === playerTurn) && ($(square[2]).text() === playerTurn)) || (($(square[3]).text() === playerTurn) && ($(square[4]).text() === playerTurn) && ($(square[5]).text() === playerTurn)) || (($(square[6]).text() === playerTurn) && ($(square[7]).text() === playerTurn) && ($(square[8]).text() === playerTurn)) || (($(square[6]).text() === playerTurn) && ($(square[4]).text() === playerTurn) && ($(square[2]).text() === playerTurn)) || (($(square[0]).text() === playerTurn) && ($(square[4]).text() === playerTurn) && ($(square[8]).text() === playerTurn)) || (($(square[0]).text() === playerTurn) && ($(square[3]).text() === playerTurn) && ($(square[6]).text() === playerTurn)) || (($(square[1]).text() === playerTurn) && ($(square[4]).text() === playerTurn) && ($(square[7]).text() === playerTurn)) || (($(square[2]).text() === playerTurn) && ($(square[5]).text() === playerTurn) && ($(square[8]).text() === playerTurn))) {
       $('#announce-winner').text(playerTurn + ' is the Winner, Great Job!');
       turn = 1;
@@ -36,7 +36,7 @@ $(document).ready(function() {
   turn++;
 });
 
-// click function to clear board
+/* click option to clear board */
 $('#clear').click(function() {
   $('div[data-cell="0"], div[data-cell="1"], div[data-cell="2"], div[data-cell="3"], div[data-cell="4"], div[data-cell="5"], div[data-cell="6"], div[data-cell="7"], div[data-cell="8"]').text('');
   $('#announce-winner').text('');
