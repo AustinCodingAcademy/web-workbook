4b0e623d151589fd2b0cb0d72c41cd2409907cdc
situlillian <mslilliansitu@gmail.com>
2017-06-19 19:58:05 -0500

    Added video, audio, and canvas

---
+++ 10week/bio-part-2/js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+$(document).ready(function() {
+  draw();
+});
+
+function draw() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+     var ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+    ctx.strokeStyle='white';
+    ctx.lineWidth = 12;
+    ctx.stroke();
+  }
+}
