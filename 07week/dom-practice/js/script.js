313dd2443214f74807f57c19154cd288baef3ee1
James Walker <james.walker@contractor.net>
2017-06-22 13:18:42 -0500

    dom-practice completed

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,11 +1,35 @@
-alert("There are " + document.getElementsByTagName("LI").length + " list items on the page");
+document.getElementById("start").addEventListener("click", jsDoms);
 
-var titleElement = document.createElement("title");
-var parent = document.getElementById("head");
-parent.appendChild(titleElement);
-titleElement.innerHTML = "Manipulating the DOM!";
+function jsDoms(){
 
-var liElement = document.createElement("li");
-var parent = document.getElementById("ul");
-parent.appendChild(liElement);
-titleElement.innerHTML = "Fifth List Item";
+  alert("There are " + document.getElementsByTagName("LI").length + " list items on the page");
+
+  var titleElement = document.createElement("title");
+  var parent = document.getElementById("head");
+  parent.appendChild(titleElement);
+  titleElement.innerHTML = "Manipulating the DOM!";
+
+  var listElement = document.createElement("li");
+  var parent = document.getElementById("ul");
+  parent.appendChild(listElement);
+  listElement.innerHTML = "Fifth List Item";
+
+
+  var oldButton = document.getElementById("start");
+  oldButton.parentNode.removeChild(oldButton);
+
+  var newButton = document.createElement("button");
+  newButton.innerHTML = "REMOVE 4th Paragraph";
+  newButton.setAttribute("id","remove")
+
+  var buttonParent = document.getElementById("button-parent");
+  buttonParent.appendChild(newButton);
+  document.getElementById("remove").addEventListener("click", jsRemove);
+
+  function jsRemove() {
+    var element = document.getElementById("p4");
+    element.parentNode.removeChild(element);
+    newButton.parentNode.removeChild(newButton);
+  }
+
+};
