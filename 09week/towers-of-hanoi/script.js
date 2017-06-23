ea2847bdfae15732f8b0e32029e568fa14f3f5a4
Ellen <lattelizard@gmail.com>
2017-06-23 00:02:42 -0500

    bio

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,21 +1,22 @@
 'use strict';
 //only drop smaller part
-  (document).ready(function() {
-    function dropIt(event, object, ui){
-    var currentvalue = ui.draggable[0].attributes[0].value;
-    var lastchild = this.lastChild[0].attributes[0].value;
-    if (parseInt(lastChild) >= parseInt(currentvalue) || lastChild==0){
-      $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+(document).ready(function() {
+
+  function drop(event, object, ui){
+  var currentvalue = ui.draggable[0].attributes[0].value;
+  var lastchild = this.lastChild[0].attributes[0].value;
+
+  if (parseInt(lastChild) >= parseInt(currentvalue) || lastChild==0){
+    $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+  }
+  else {
+    $(ui.draggable).draggable('option','revert',true);
+  }
     }
-    else {
-      $(ui.draggable).draggable('option','revert',true);
-    }
-    });
-      });
   //drag & drop
     $('[data-block]').draggable({
       revert:'invalid'
-    });
+});
     $('[data-stack]').droppable({
       drop: function(event , ui){
       $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
@@ -23,7 +24,4 @@
     }
     });
 
-  function checkWinner() {
-      if((dataCell0.text() != '') && (dataCell0.text () == dataCell1.text())
-       && (dataCell1.text() == dataCell2.text()))
-  });
+      });
