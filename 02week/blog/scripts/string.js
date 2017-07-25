1761f4d1c7c71ff2149435a218c22b48a62553db
aguevara1 <albguevara12@gmail.com>
2017-07-25 18:25:25 -0500

    blog

---
+++ 02week/blog/scripts/string.js
@@ -0,0 +1,5 @@
+ src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+
+  $(document).ready(function(){
+      $("section:contains('math')").css("background-color", "yellow");
+  }
