f8ab92d06d32158c65399ea47a28f9e2f50c25c9
Teko Smith <teko0010@gmail.com>
2017-07-26 20:59:43 -0500

    javascript

---
+++ 06week/fizz-buzz/js/script.js
@@ -0,0 +1,71 @@
+for(i = 1; i <= 100; i++){
+
+if(i%3 === 3 && i % 5 ===0){
+console.log("FizzBuzz")
+}
+ else if(i%3 === 0){
+ console.log("fizz");
+}
+else if (i % 5 === 0) {
+console.log("Buzz");
+}
+
+else {
+console.log(i)
+}
+}
+
+
+
+
+
+
+
+
+
+
+//Uncomment the console.log's to see your code in action!
+
+// Data Types
+// var booleanExample = true;
+//
+// var numberExample = 2;
+//
+// var stringExample = "a";
+//
+// var otherStringExample = "This is a string1";
+
+//To make these datatypes print to console, use console.log.
+
+// console.log(booleanExample);
+// console.log(numberExample);
+// console.log(stringExample);
+// console.log(otherStringExample);
+
+//To determine a string length, use the lenth method.
+
+// console.log(stringExample.length);
+// console.log(otherStringExample.length);
+
+//Conditionals
+
+// if (numberExample === 2){
+//  console.log("The number example is equal to 2.");
+// }
+// else {
+//  console.log("the number in the example is not greater than 2.")
+// }
+
+// for loop
+
+//This loop will console.log numbers starting at 0, less than 10, and increasing 1 at a time.
+
+// for(i = 0; i < 10; i++){
+//  console.log(i);
+// }
+
+// Arrays
+
+// var arrayExample = ["item1", "item2", "item3", "item4"];
+// for(var index = 0; index < arrayExample.length; index++){
+// console.log(arrayExample[index]);}
