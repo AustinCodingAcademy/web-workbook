6b2da2cc604d86032b5eca82b099f10463415e8b
Erin Finley <erinfinley@Erins-MBP.attlocal.net>
2017-08-09 17:14:51 -0500

    finished Week 8 jQuery homework

---
+++ 07week/dom-practice/js/script.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+ var myListItems = document.getElementsByTagName("li");
+ alert("<li> elements:" + myListItems.length);
+
+ createTitle();
+
+ createListItem ();
+}
+
+function createTitle() {
+  var newTitle = document.createElement("title"); //this creates <title></title>
+  var titleContent = document.createTextNode("Manipulating the DOM!"); //this creates Manipulating the DOM!
+    newTitle.appendChild(titleContent); //this creates <title>Manipulating the DOM!</title>
+  var head = document.getElementsByTagName("head"); //this targets <head></head> in html
+    head[0].appendChild(newTitle); //this puts everything into the <head></head>
+}
+
+function createListItem () {
+  var newListItem = document.createElement("li");
+  var listItemContent = document.createTextNode("Fifth List Item");
+    newListItem.appendChild(listItemContent);
+  var ul = document.getElementsByTagName("ul");
+    ul[0].appendChild(newListItem);
+}
+
+function removeParagraphFour () {
+  var paragraphFour = document.getElementById("#P4");
+    paragraphFour.remove
+}
