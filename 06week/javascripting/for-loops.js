d18f7e32379d56eeca1cd59511807e2c9837b04f
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-08-15 23:01:54 -0500

    Javascripting plus News site debug

---
+++ 06week/javascripting/for-loops.js
@@ -0,0 +1,4 @@
+var total = (0);
+var limit = (10);
+for (var i = total, i < limit, i ++);
+console.log(i);
---
+++ 06week/javascripting/if-statements.js
@@ -0,0 +1,6 @@
+var fruit = ('orange');
+if (5 < fruit.length) {
+  console.log("The fruit name has more than five characters.");
+} else {
+  console.log("The fruit has five characters or less");
+}
