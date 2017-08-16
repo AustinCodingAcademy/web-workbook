8b6cf0ec70579b5b88dcb5cd756100a6b2761db5
Teko Smith <teko0010@gmail.com>
2017-08-16 14:43:29 -0500

    Started projects/code to move blocks, code to drop block

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,52 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
-});
+
+  function dragBlock(e){  //ondragstart
+  var blockValue = $(e.target).data("block")
+  var stackValue = $(e.target).data("stack")
+
+
+  //data("block") data-block
+    e.dataTransfer.setData("block", blockValue);
+    e.dataTransfer.setData("stack", stackValue);
+
+    // console.log("I am being dragged");
+    console.log("block Value:" + blockValue);
+    console.log("stack Value: " + stackValue);
+  }
+
+
+function allowDrop(e){ //ondragover
+  e.preventDefault();
+  var last_block = stack.children().last();
+  console.log(last_block);
+}
+
+function dropBlock(e){//ondrop
+  e.preventDefault();
+
+var blockValue = e.dataTransfer.getData("block");
+var lastBlockValue = $(e.target).children().last();
+
+  //when the stack empty we append
+  if($(e.target).children().length === 0){
+  $(e.target).append($("[data-block=" + blockValue +"]"));
+}
+
+  if($(e.target).children().length !== 0){ //just to get it to move(take out)
+  $(e.target).append($("[data-block=" + blockValue +"]"));
+}
+
+
+
+// Example
+// function checkForWin() {
+//   if($('[data-stack="3"]').children().length===4) {
+//     $('#announce-game-won').html("You Won!");
+//     gameover=true;
+//
+//   }
+
+
+
+}
