66509ac22d8f0f8b7fe35b87e1762fcbf67bbb53
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-08-16 23:16:56 -0500

    Lint bug fixes first 5 weeks

--- 06week/javascripting/for-loops.js
+++ 06week/javascripting/for-loops.js
@@ -1,4 +1,5 @@
-var total = (0);
-var limit = (10);
-for (var i = total, i < limit, i ++);
-console.log(i);
+var total = (0)
+var limit = (10)
+for (var i = total; i < limit; i++ ) {
+  console.log(total += i)
+}
