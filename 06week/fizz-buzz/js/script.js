c48499c1c01967ca6e6fabe8aec08778026f222b
walzer85 <walzer85@gmail.com>
2017-08-17 19:30:13 -0500

    began fizzbuzz and updated index page

---
+++ 06week/fizz-buzz/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function fizzBuzz() {
+
+    for (var i = 0; i <= 100; i++)
+    if (i % 3 === 0 && i % 5 === 0)
+    if (i % 3 === 0) {
+      document.write('Fizz');
+    }
+    else (i % 5 === 0) {
+      document.write('Buzz');
+    }
+
+}
+
+fizzBuzz (i);
+
+$('body').append('Fizz');
