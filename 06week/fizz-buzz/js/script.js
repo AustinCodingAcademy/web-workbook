4ecc00146bf8441d53b548047e99365e656a0ec4
cfissell50 <cfissell@hotmail.com>
2017-08-17 20:10:59 -0500

    fizzlemynizzle

---
+++ 06week/fizz-buzz/js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function fizzBuzz() {
+
+  for(var i = 1; i <= 100; i++) {
+
+    if(i % 5 === 0 && i % 3 ===0) {
+      $("ul").append('<li>FizzBuzz</li>');
+    }
+
+    else if(i % 3 === 0) {
+      $("ul").append('<li>Fizz</li');
+    }
+    else if(i % 5 === 0) {
+      $("ul").append('<li>Buzz</li>');
+    }
+    else {
+      $("ul").append(`<li>${i}</li>`)
+    }
+    }
+
+}
+  fizzBuzz();
