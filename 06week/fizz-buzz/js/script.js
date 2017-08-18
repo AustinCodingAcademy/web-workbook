4fff22220e8cd954e057c0613e9a286cf02d360e
walzer85 <walzer85@gmail.com>
2017-08-17 20:11:02 -0500

    finished fizzbuzz

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -2,17 +2,17 @@
 
 function fizzBuzz() {
 
-    for (var i = 0; i <= 100; i++)
-    if (i % 3 === 0 && i % 5 === 0)
-    if (i % 3 === 0) {
-      document.write('Fizz');
+  for (var i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      $('ul').append('<li>FizzBuzz</li>');
+    } else if (i % 3 === 0) {
+      $('ul').append('<li>Fizz</li>');
+    } else if (i % 5 === 0) {
+      $('ul').append('<li>Buzz</li>');
+    } else {
+      $('ul').append(`<li>${i}</li>`);
     }
-    else (i % 5 === 0) {
-      document.write('Buzz');
-    }
-
+  }
 }
 
-fizzBuzz (i);
-
-$('body').append('Fizz');
+  fizzBuzz();
