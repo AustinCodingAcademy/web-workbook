6e6d0bf85edf6e43b0fb402d5113c163360432b8
Jon <contact@jongorman.top>
2017-08-18 21:48:35 -0500

    88

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -1,14 +1,1 @@
-'use strict';
 
-function fizzBuzz() {
-  for (var i =1; i <= 100; i++){
-    if (i % 3 === 0 ){
-      console.log("Fizz");
-    }
-    else (i % 5 === 0){
-      console.log("Buzz");
-
-    }
-  }
-  fizzBuzz();
-}
