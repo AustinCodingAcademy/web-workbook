ebcd64d7a845c185e7fd865321f697de74e48a20
cfissell50 <cfissell@hotmail.com>
2017-08-19 11:22:05 -0500

    fizzbuzz
    do it

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -2,24 +2,21 @@
 
 function fizzBuzz() {
 
-  for(var i = 1; i <= 100; i++) {
+  for (var i = 1; i <= 100; i++) {
 
-    if(i % 5 === 0 && i % 3 ===0) {
+    if (i % 5 === 0 && i % 3 === 0) {
       $("ul").append('<li>FizzBuzz</li>');
-    }
-    else if(i % 3 === 0) {
+    } else if (i % 3 === 0) {
       $("ul").append('<li>Fizz</li');
-    }
-    else if(i % 5 === 0) {
+    } else if (i % 5 === 0) {
       $("ul").append('<li>Buzz</li>');
-    }
-    else {
+    } else {
       $("ul").append(`<li>${i}</li>`)
     }
-    }
+  }
 
 }
-  fizzBuzz();
+fizzBuzz();
 
-  // var x == (i % 3 == 0), y = (i % 5 == 0);
-  //   document.write(x ? y ? "FizzBuzz" : "Fizz" : y ? "Buzz" : i)
+// var x == (i % 3 == 0), y = (i % 5 == 0);
+//   document.write(x ? y ? "FizzBuzz" : "Fizz" : y ? "Buzz" : i)
