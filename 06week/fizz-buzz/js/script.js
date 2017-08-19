1b87db46f4d2d0c4595ce129b79ab663f57b05ed
aguevara1 <albguevara12@gmail.com>
2017-08-19 15:51:17 -0500

    fizzbuzzUpdate

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -1,1 +1,17 @@
 
+function fizzbuzz(){
+  for(i=1;i<=100;i++){
+       if (i % 3== 0 && i % 5== 0){
+        document.writeln("<center>FizzBuzz</center>");
+        }
+        else if (i % 5=== 0){
+         document.writeln("<center>Buzz</center>");
+         }
+         else if (i % 3=== 0 ){
+         document.writeln("<center>Fizz</center>");
+         }
+         else{
+         document.writeln("<center>"+i +"</center>");
+       }
+   }
+}
