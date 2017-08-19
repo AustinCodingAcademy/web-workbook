a30d3f850947f77a52af7120ae36e4f1e0a44d40
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-08-19 17:29:58 -0500

    Checkpoint portfolio retree and rough draft outline

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -1,19 +1,20 @@
 'use strict';
-/*
 $(document).ready(function(){
-*/
+
 function fizzBuzz(){
 	for(var i=1;i<=100;i++){
 		if(i % 5 === 0 && i % 3 === 0){
-			console.log('FizzBuzz');
+		  document.write("FizzBuzz ");
+      $('body').append('FizzBuzz');
 		} else if(i % 3 === 0){
-			console.log('Fizz');
+			document.write("Fizz ");
+      $('body').append('Fizz');
 		} else if(i % 5 === 0){
-			console.log();('Buzz');
+			document.write("Buzz ");
+      $('body').append('Buzz');
 		} else {
-			console.log(i);
+			document.write(i);
 		}
 	}
 }
-/*
 })
