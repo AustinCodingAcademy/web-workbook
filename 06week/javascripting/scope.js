622d3e6575cf8b3a0741fac1997a14bc660909e3
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-08-21 00:37:31 -0500

    NPM lint error fixes today

--- 06week/javascripting/scope.js
+++ 06week/javascripting/scope.js
@@ -6,14 +6,14 @@ var a = 1, b = 8, c = 6;
   (function secondFunction(){
     var b = 8;
 
-      (function thirdFunction(){
-        var a = 7, c = 9;
+    (function thirdFunction(){
+      var a = 7, c = 9;
 
-        (function fourthFunction(){
-          var a = 1, c = 8;
-          
-            })();
-        })();
+      (function fourthFunction(){
+        var a = 1, c = 8;
+
+      })();
     })();
+  })();
 })()
 console.log("a: "+a+", b: "+b+", c: "+c);
