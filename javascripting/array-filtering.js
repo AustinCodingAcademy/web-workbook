419bd94b33c79662c8ad1b37a3b1cb52443ac6fc
Troy <troyvcardona@gmail.com>
2017-08-21 21:01:18 -0500

    javascripting

---
+++ javascripting/array-filtering.js
@@ -0,0 +1,8 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function evenNumbers (x) {
+       return x % 2 === 0;
+     }
+     var filtered = numbers.filter(evenNumbers);
+
+       console.log(filtered);
