2e624138a0e6de4457ac89c99398a2023ca0e0f7
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-08-22 17:49:23 -0500

    Fizzbuzz fix

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -4,17 +4,17 @@ $(document).ready(function(){
   function fizzBuzz(){
     for(var i=1;i<=100;i++){
 		  if(i % 5 === 0 && i % 3 === 0){
-		    document.write("FizzBuzz ");
-      	$('body').append('FizzBuzz');
-	 } else if(i % 3 === 0){
-   document.write("Fizz ");
-      	$('body').append('Fizz');
-	 } else if(i % 5 === 0){
-   document.write("Buzz ");
-      	$('body').append('Buzz');
-	 } else {
-   document.write(i);
-		 }
-	 }
+		  document.write("FizzBuzz ");
+	    } else if(i % 3 === 0){
+      document.write("Fizz ");
+	    } else if(i % 5 === 0){
+      document.write("Buzz ");
+	    } else {
+      document.write(i);
+		  }
+      document.write('<br>');
+	  }
   }
+
+  fizzBuzz();
 })
