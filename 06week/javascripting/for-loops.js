ff9b88d934575a1be83d9c247afbd8e663b5f87b
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-08-22 20:54:41 -0500

    Javascripting arrays update

--- 06week/javascripting/for-loops.js
+++ 06week/javascripting/for-loops.js
@@ -1,5 +1,6 @@
-var total = (0)
-var limit = (10)
-for (var i = total; i < limit; i++ ) {
-  console.log(total += i)
+var total = 0
+var limit = 10
+for (var i = total; i < limit; i++) {
+  (total+=i);
 }
+console.log(total);
--- 06week/javascripting/looping-through-arrays.js
+++ 06week/javascripting/looping-through-arrays.js
@@ -1,1 +1,3 @@
 var pets = ['cat', 'dog', 'rat'];
+for (var i = [pets]; [pets]+ 's';);
+console.log(pets);
