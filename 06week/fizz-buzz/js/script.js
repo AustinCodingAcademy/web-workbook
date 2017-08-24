306cfde910d0ac5899c58ec92a4f61a790c2973d
worthingtonml <worthington.ml@gmail.com>
2017-08-24 00:24:14 -0500

    ughh

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -1,12 +1,20 @@
-'use strict';
-for (var i = 0; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0){
-    console.log('FizzBuzz')
-  } else if (i % 5 === 0){
-    console.log('Fizz')
-  } else if (i % 3 === 0){
-    console.log('Buzz')
-  } else {
-    console.log(i);
+'use strict'
+
+$(document).ready(function() {
+   function fizzBuzz() {
+  for (var i = 0; i <=100; i++) {
+       if(i % 5 === 0 && i % 3 === 0){
+         $('main').append("FizzBuzz");
+       } else if (i % 5 === 0){
+         $('main').append("Fizz");
+       } else if (i % 3 === 0){
+         $('main').append("Buzz")
+       } else {
+         $('main').append(i);
+       }
+      $('main').append('<br><br>');
+    }
   }
-}
+// // $('body').append("hello")
+  fizzBuzz(i);
+});
