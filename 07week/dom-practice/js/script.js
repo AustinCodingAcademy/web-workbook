5379f0bf75f57ab5cf5b19db9c4a45f1755f53bd
walzer85 <walzer85@gmail.com>
2017-08-23 19:57:09 -0500

    First commit of DOM practice, updated index page

---
+++ 07week/dom-practice/js/script.js
@@ -0,0 +1,9 @@
+alert('There are four list items.');
+
+title.insertAdjacentHTML(afterbegin, 'Manipulating the DOM!');
+
+li.insertAdjacentHTML(afterend, 'Fifth List Item');
+
+var parent = document.getElementById("div");
+var child = document.getElementById("p4");
+parent.removeChild(child);
