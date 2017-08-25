05ea508edc442a82cd64c5aab680e86fe62189ae
aguevara1 <albguevara12@gmail.com>
2017-08-24 23:01:09 -0500

    lastest updates

--- 02week/blog/scripts/string.js
+++
@@ -1,5 +0,0 @@
- src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"
-
-  $(document).ready(function(){
-      $("section:contains('math')").css("background-color", "yellow");
-  }
---
+++ 07week/dom-practice/js/script.js
@@ -0,0 +1,6 @@
+'use strict';
+document.addEventListener('DOMContentLoaded',function(){
+  // Your code here
+  var paragraphs = document.getElementsByClassName("subPara");
+  console.log("<p> elements with class subPara: " + paragraphs.length);
+});
