c44fe2372910b0ac3fb6603f87dc82a6db38ec8b
Jon <contact@jongorman.top>
2017-08-25 12:47:52 -0500

    95

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -1,1 +1,30 @@
 
+'use strict';
+$(function(){
+
+function fizzBuzz() {
+
+  for (var i =1; i <= 100; i++) {
+
+    if (i%5===0 && i%3===0) {
+      $("main").append("Fizz-Buzz")
+      console.log("Fizz-Buzz");
+
+    } else if (i % 3 === 0 ) {
+      $("main").append("Fizz")
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      $("main").append("Buzz")
+      console.log("Buzz");
+    } else {
+      $("main").append(i);
+      // document.write(i);
+
+    }
+  }
+}
+
+
+fizzBuzz();
+
+})
