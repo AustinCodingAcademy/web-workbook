317b6e5434cdd61396c9743aa2055564937f6408
edvidaurri <edward@vidaurrimail.com>
2017-08-24 22:34:24 -0500

    DOM rev

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,38 +1,35 @@
 'use strict';
 // document.addEventListener('DOMcontentloaded', function() {
 
+window.onload = function () {
 
+  // Title changed.
+  console.log(document.title = 'Manipulating the DOM!');
 
-// window.onload = function () {
-//     var list = document.getElementsByTagName('ol');
-//     var message = 'this page has '
-//     + list[0].child.length + 'list items';
-//     // alert(message);}
-// }
+  // h3 tag added.
+  document.querySelector('body').insertAdjacentHTML ('beforebegin', '<h3>Title has been changed!</h3>');
 
-document.title = 'Manipulating the DOM!';
-document.querySelector('body').insertAdjacentHTML ('beforebegin', '<h3>Title has been changed!</h3>');
-
-// Alert the user to how many list items there are on the page.
-console.log(document.querySelectorAll ('li').length);
+  // Alert the user in JavaScript Console how many list items there are on the page.
+  console.log(document.querySelectorAll ('li').length);
 
 
 
-//Remove 'Paragraph 4'.
-document.querySelector ('#P4').remove();
-document.querySelector ('#div').insertAdjacentHTML ('beforeend', '<h4>Paragraph 4 has been removed!</h4>');
+  //Remove 'Paragraph 4'.
+  document.querySelector ('#P4').remove();
+  document.querySelector ('#div').insertAdjacentHTML ('beforeend', '<h4>Paragraph 4 has been removed!</h4>');
 
-//Add 'Fifth List Item' to the list.
-document.querySelector ('ul').insertAdjacentHTML ('beforeend', '<li>Fifth List Item</li>');
+  //Add 'Fifth List Item' to the list.
+  document.querySelector ('ul').insertAdjacentHTML ('beforeend', '<li>Fifth List Item</li>');
 
 
-// Trying differernt queries.
-const items = ['Sixth Element', 'Seventh List Item', 'Eight List Item', 'Nineth List      Item'];
-for (let i = 0; i < items.length; i++) {
-  document.querySelector ('ul').insertAdjacentHTML ('beforeend', `<li>${items[i]}</li>`);
+  // Trying differernt queries. Adding multiple li's.
+  const items = ['Sixth Element', 'Seventh List Item', 'Eight List Item', 'Nineth List      Item'];
+  for (let i = 0; i < items.length; i++) {
+    document.querySelector ('ul').insertAdjacentHTML ('beforeend', `<li>${items[i]}</li>`);
+  }
+
+
+  document.querySelector ('#P1').insertAdjacentHTML ('beforeend', '<ol><li>List Item A</li></ol>');
+  //
+  // })
 }
-
-
-document.querySelector ('#P1').insertAdjacentHTML ('beforeend', '<ol><li>List Item A</li></ol>');
-//
-// })
