80ca0447f387ca8b3becb9497bcbbbb459985c4f
Jon <contact@jongorman.top>
2017-08-25 19:25:44 -0500

    :octocat: 100

---
+++ 07week/dom-practice/js/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+window.onload = function(){
+
+console.log(document.title = "I love OctoCat");
+
+console.log(document.querySelectorAll ('li').length);
+
+document.querySelector ('ul').insertAdjacentHTML ('beforeend', '<li>Fifth list item</li');
+
+document.querySelector ('#P4').remove();
+
+alert("There is five list items on this page!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+// Create a JS file and write code the will do the following:
+// Alert the user to how many list items there are on the page.
+// Add a title to the page that says 'Manipulating the DOM!'. Do not use html to do this. Use JS.
+// Add 'Fifth List Item' to the list.
+// Remove 'Paragraph 4'.
