119ac75970a684b12bb41992162d73555411a461
aguevara1 <albguevara12@gmail.com>
2017-08-26 18:55:09 -0500

    update checkpoint2

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,5 @@
 'use strict';
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function() {
   // Your code here
   var paragraphs = document.getElementsByClassName("subPara");
   console.log("<p> elements with class subPara: " + paragraphs.length);
