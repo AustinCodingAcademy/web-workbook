a21b7cf15fe1d1e02df05160e6bdfcf670651572
deytonk <deyton.koch@gmail.com>
2017-08-27 15:47:22 -0500

    finish class js project

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -5,6 +5,7 @@
 // $(document).ready(function){}
 
 // document.addEventListener("DOMcontentloaded", function(){ })
+// (does't work ^)
 
 window.onload = function() {
 
@@ -13,25 +14,31 @@ window.onload = function() {
 
   // Alert the user to how many list items there are on the page.
 
+  alert("There was originally 4 list items on this page.");
+
   console.log(document.querySelectorAll ('li').length);
 
   // Add 'Fifth List Item' to the list.
 
   document.querySelector ('ul').insertAdjacentHTML ('beforeend', '<li>Fifth List Item</li>');
+  document.querySelector ('ul').insertAdjacentHTML ('beforebegin', '<h1>I added this Title in Javascript! Whoo!</h1>');
+  document.querySelector ('p').insertAdjacentHTML ('beforebegin', '<img src="http://welovekitties.com/wp-content/uploads/2015/12/cuteadorablekitty-145124828048cpl.jpg" alt="cute kitty"/>');
 
   // Remove 'Paragraph 4'.
 
   document.querySelector ('#P4').remove();
 
-  // var list = document.getElementByTagName('ol');
-  // var message = 'this page has ' + list[0], child, length + 'list items';
-  //
-  // var newelement = document.createElement('h1');
-  //   newelement.innerHTML = 'JavaScript';
-  //   document.body.appendChild(newelement);
-  //
-  //   var $p = document.getElementById('P4');
-  //
-  //     $p.remove();
-
 }
+
+// class notes:
+
+// var list = document.getElementByTagName('ol');
+// var message = 'this page has ' + list[0], child, length + 'list items';
+//
+// var newelement = document.createElement('h1');
+//   newelement.innerHTML = 'JavaScript';
+//   document.body.appendChild(newelement);
+//
+//   var $p = document.getElementById('P4');
+//
+//     $p.remove();
