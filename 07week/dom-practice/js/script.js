ee873031d96505c0de51ea64360415be58f72043
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-08-28 01:00:52 -0500

    DOM Practice finalization

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -2,27 +2,18 @@
 
 $(document).ready(function(){
 
-  var newelement = document.createTextNode('Manipulating the DOM!');
-  document.body.appendChild(newelement);
+  var listItems = document.querySelectorAll('li').length;
+  alert(listItems);
+
+  document.querySelector('body').insertAdjacentHTML('beforebegin', '<h1>Manipulating the DOM!</h1>');
+
+  document.querySelector('ul').insertAdjacentHTML('beforeend', '<li>Fifth List Item</li>');
+
+  document.querySelector('#p4').remove();
+
 })
 /*
 
-   var list = document.getElementsByTagName('ol');
-   var message = 'this page has' + list[0].children.length + 'list items';
-
-
-});
-
-
-
-   var newelement = document.createElement('h1');
-    newelement.innerHTML = 'Manipulating the DOM!';
-    document.body.appendChild(newelement);
-
-})
-
-
-/*
     -Alert the user to how many list items there are on the page.
 
     -Add a title to the page that says 'Manipulating the DOM!'. Do not use html to do this. Use JS.
