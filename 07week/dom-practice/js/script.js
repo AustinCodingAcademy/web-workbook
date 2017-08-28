0afe8fb2aab2b78557bec9cd6161c2d1ac2abe47
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-08-27 21:20:53 -0500

    DOM practice updates and error fixes

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -2,24 +2,33 @@
 
 $(document).ready(function(){
 
+  var newelement = document.createTextNode('Manipulating the DOM!');
+  document.body.appendChild(newelement);
+})
+/*
+
    var list = document.getElementsByTagName('ol');
    var message = 'this page has' + list[0].children.length + 'list items';
 
- })
 
-/*
+});
+
+
+
    var newelement = document.createElement('h1');
-    newelement.innerHTML = 'Javascript';
+    newelement.innerHTML = 'Manipulating the DOM!';
     document.body.appendChild(newelement);
 
+})
+
+
+/*
     -Alert the user to how many list items there are on the page.
-    
+
     -Add a title to the page that says 'Manipulating the DOM!'. Do not use html to do this. Use JS.
 
     -Add 'Fifth List Item' to the list.
 
     -Remove 'Paragraph 4'.
-
-
 
 */
