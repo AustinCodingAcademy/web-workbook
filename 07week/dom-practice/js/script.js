7d3976f5582e15d2afd21457ba6f62fd774b7654
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-08-29 13:41:30 -0500

    completed dom-practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,17 +1,34 @@
-window.onload = function (){
-  var list = document.getElementsByTagName('ol');
-  var message = 'this page has' + list[0].child.length + 'list items';
+'use strict';
 
-  var newelement = document.createElement('h1');
-  newElement.innerHTML = 'Javascript';
-  document.body.appendChild(newElement);
+window.onload = function() {
 
 
+
+
+  // Setting Title
+  var title = document.createElement('H1');
+  title.textContent = 'Manipulating the Dom';
+  document.body.appendChild(title);
+  document.body.insertBefore(title, document.body.firstChild);
+
+
+  // Adding Fifth List Item
+  var par = document.createElement("LI");
+  var node = document.createTextNode("Fifth list item");
+  par.appendChild(node);
+
+  // Placing newly created list item
+  var element1 = document.getElementById("ul");
+  element1.appendChild(par);
+
+
+  //Remove Paragraph 4
+  var parent = document.getElementById("div");
+  var child = document.getElementById("p4");
+
+  parent.removeChild(child);
+
+  var total = document.getElementsByTagName('LI').length;
+  console.log(total);
+  alert(total);
 }
-
-var $p = document.getElementById('P3');
-$p.remove();
-
-var $p = document.getElementById('P4');
-
-  $p.remove();
