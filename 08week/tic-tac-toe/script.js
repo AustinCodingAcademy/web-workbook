d21a2730c30d2589f37438a357f58ce8bc176d14
edvidaurri <edward@vidaurrimail.com>
2017-08-31 23:22:45 -0500

    ttt/play butts

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,17 @@
 'use strict';
 
-$(document).on('ready', function() {
+$(document).ready(function() {
   // Put app logic in here
+
+
+  let playerTurn = 0;
+  $('[data-cell]').click(function() {
+    if (playerTurn %2 === 0){
+      $(this).text('X');
+    } else {
+      $(this).text('O');
+    }
+    playerTurn++;
+  })
+
 });
