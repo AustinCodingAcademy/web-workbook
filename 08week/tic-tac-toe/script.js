cdd8b6ec78a81bea5bbaf276a2a52dfa493a35ca
deytonk <deyton.koch@gmail.com>
2017-08-31 20:48:08 -0500

    class progress for tic tac toe :middlefinger:

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,31 @@
 'use strict';
 
-$(document).on('ready', function() {
+$(document).ready(function() {
   // Put app logic in here
+  let turn = 0;
+  $('[data-cell]').click(function() {
+    if (turn % 2 === 0) {
+      $(this).text('X');
+      // checkVictory('X');
+    } else {
+      //player 2's turn (O)
+      $(this).text('O');
+      // checkVictory('O');
+    }
+  turn++;
+  })
+  
 });
+
+  // let playerTurn = 'playerOne';
+  //   $('.box').click(function() {
+  //     $(this).text();
+  //     if (playerTurn === 'playerTwo') {
+  //       playerTurn = 'playerOne';
+  //     } else {
+  //       playerTurn = 'playerTwo';
+  //     }
+  //   });
+
+  // if($('[data-cell="0"]').text() === "X" && $('[data-cell="1"]').text() === "X" && $('[data-cell="2"]').text() === "X") ||
+  //   ($('[data-cell="0"]').text() === "X" && $('[data-cell="1"]').text() === "X" && $('[data-cell="2"]').text() === "X")
