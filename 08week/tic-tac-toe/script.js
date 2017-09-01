098417d5679dd6b40d61a8a875fe8ecfd5da5c04
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-08-31 20:51:32 -0500

    Tic Tac Toe rough draft begin

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,46 @@
 'use strict';
 
-$(document).on('ready', function() {
+$(document).ready(function() {
   // Put app logic in here
+var playerTurn = 0;
+
+$('[data-cell]').on('click', function(){
+            if (playerTurn % 2 === 0){
+              $(this).text('X');
+              checkVictory('X');
+            } else {
+           //player 2's turn (O)
+              $(this).text('O');
+              checkVictory('O');
+            }
+          playerTurn++;
+
+ })
+
+function checkVictory(){
+
+    if ($('[data-cell="0"]').text() === playerTurn && $('[data-cell="1"]').text() === playerTurn && $('[data-cell="2"]').text() === playerTurn)
+    alert("you win");
+}
+/*
+if ($('[data-cell="3"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn)
+
+if ($('[data-cell="6"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)
+
+if ($('[data-cell="0"]').text() === playerTurn && $('[data-cell="3"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn)
+
+if ($('[data-cell="1"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn)
+
+if ($('[data-cell="2"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)
+
+if ($('[data-cell="0"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)
+
+if ($('[data-cell="2"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn)
+
+
+
+*/
+
+
+
 });
