70df071ba71933369f0beb05c0c8303fa5b34032
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-09-03 14:40:03 -0500

    updated tic-tac-toe

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -5,148 +5,91 @@ $(document).ready(function() {
   var turnCount = 0;
   var xwin = "Player 1 Wins";
   var owin = "Player 2 Wins";
- var checkVictory = function(){
-  //  top row win
-   if($('#0').text() === 'X'){
-     if($('#1').text() === 'X'){
-       if($('#2').text() === 'X'){
-         alert(xwin);
-       }
-     }
+  var checkVictory = function(player) {
+    //  top row win
+    if ($('#0').text() === player) {
+      if ($('#1').text() === player) {
+        if ($('#2').text() === player) {
+          alert(player + " is the winner");
+        }
+      }
 
- }
- if($('#0').text() === 'O'){
-   if($('#1').text() === 'O'){
-     if($('#2').text() === 'O'){
-       alert(owin);
-     }
-   }
+    }
 
-}
- //mid row win
- if($('#3').text() === 'X'){
-   if($('#4').text() === 'X'){
-     if($('#5').text() === 'X'){
-       alert(xwin);
-     }
-   }
-
- }
- if($('#3').text() === 'O'){
-   if($('#4').text() === 'O'){
-     if($('#5').text() === 'O'){
-       alert(owin);
-     }
-   }
-
- }
- //bottom row win
- if($('#6').text() === 'X'){
-   if($('#7').text() === 'X'){
-     if($('#8').text() === 'X'){
-       alert(xwin);
-     }
-   }
- }
-   if($('#6').text() === 'O'){
-     if($('#7').text() === 'O'){
-       if($('#8').text() === 'O'){
-         alert(owin);
-       }
-     }
-   }
-     //first column win
-     if($('#0').text() === 'X'){
-       if($('#3').text() === 'X'){
-         if($('#6').text() === 'X'){
-           alert(xwin);
-         }
-       }
-     }
-     if($('#0').text() === 'O'){
-       if($('#3').text() === 'O'){
-         if($('#6').text() === 'O'){
-           alert(owin);
-         }
-       }
-     }
-    //  2nd column win
-    if($('#1').text() === 'X'){
-      if($('#4').text() === 'X'){
-        if($('#7').text() === 'X'){
-          alert(xwin);
+    //mid row win
+    if ($('#3').text() === player) {
+      if ($('#4').text() === player) {
+        if ($('#5').text() === player) {
+          alert(player + " is the winner");
         }
       }
     }
-    if($('#1').text() === 'O'){
-      if($('#4').text() === 'O'){
-        if($('#7').text() === 'O'){
-          alert(owin);
+    //bottom row win
+    if ($('#6').text() === player) {
+      if ($('#7').text() === player) {
+        if ($('#8').text() === player) {
+          alert(player + " is the winner");
+        }
+      }
+    }
+    //first column win
+    if ($('#0').text() === player) {
+      if ($('#3').text() === player) {
+        if ($('#6').text() === player) {
+          alert(player + " is the winner!");
+        }
+      }
+    }
+    //  2nd column win
+    if ($('#1').text() === player) {
+      if ($('#4').text() === player) {
+        if ($('#7').text() === player) {
+          alert(player + " is the winner!");
         }
       }
     }
     // 3rd column win
-    if($('#2').text() === 'X'){
-      if($('#5').text() === 'X'){
-        if($('#8').text() === 'X'){
-          alert(xwin);
-        }
-      }
-    }
-    if($('#2').text() === 'O'){
-      if($('#5').text() === 'O'){
-        if($('#8').text() === 'O'){
-          alert(owin);
+    if ($('#2').text() === player) {
+      if ($('#5').text() === player) {
+        if ($('#8').text() === player) {
+          alert(player + "is the winner!");
         }
       }
     }
     // diagonal win
-    if($('#0').text() === 'X'){
-      if($('#4').text() === 'X'){
-        if($('#8').text() === 'X'){
-          alert(xwin);
+    if ($('#0').text() === player) {
+      if ($('#4').text() === player) {
+        if ($('#8').text() === player) {
+          alert(player + " is the winner!");
         }
       }
     }
-    if($('#0').text() === 'O'){
-      if($('#4').text() === 'O'){
-        if($('#8').text() === 'O'){
-          alert(owin);
+    if ($('#2').text() === player) {
+      if ($('#4').text() === player) {
+        if ($('#6').text() === player) {
+          alert(player + " is the winner!");
         }
       }
     }
-    if($('#2').text() === 'X'){
-      if($('#4').text() === 'X'){
-        if($('#6').text() === 'X'){
-          alert(xwin);
-        }
-      }
-    }
-    if($('#2').text() === 'O'){
-      if($('#4').text() === 'O'){
-        if($('#6').text() === 'O'){
-          alert(owin);
-        }
-      }
-    }
- };
+  };
 
-  $('.row').find('div').on('click',function(){
-    if($(this).text() === ""){
-    if(turnCount % 2 === 0){
-      $(this).text('X');
-      checkVictory('X');
-    } else{
-      $(this).text('O');
-      checkVictory('O');
+  $('.row').find('div').on('click', function() {
+    if ($(this).text() === "") {
+      if (turnCount % 2 === 0) {
+        $(this).text('X');
+        checkVictory('X');
+      } else {
+        $(this).text('O');
+        checkVictory('O');
+      }
+      turnCount++;
+      console.log(turnCount);
     }
-    turnCount ++;
-    console.log(turnCount);
-  }
-});
+  });
 
-  $('body').find('#clear').on('click',function(){
-    $('.row').find('div').text("");
+  $('body').find('#clear').on('click', function() {
+    $('[data-cell]').text("");
+    turnCount = 0;
   });
 
 });
