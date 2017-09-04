6eab222703e99bb0cab2da0f2850deaa65ee8285
Jon <contact@jongorman.top>
2017-09-04 09:24:01 -0500

    :smile: 113

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -6,37 +6,64 @@ $(document).ready(function() {
   // var turn2 = 2;
 
   $('[data-cell]').click(function() {
-    if (turn % 2 === 0) {
-      $(this).text(playerTwo);
-    } else {
+    if (turn % 2 === 1) {
       $(this).text(playerOne);
+      checkPlayerWin(playerOne);
+    } else {
+      $(this).text(playerTwo);
+      checkPlayerWin(playerTwo);
+
     }
     turn++;
+  });
+  //check winner
+  // if (checkPlayerWin()) {
+  //
+  //   $('#announce-winner').text('playerOne' + "Wins");
+  //   alert('this is a start!');
+  // }
 
-//check winner
-if (checkPlayerWin()) {
-  $('#announce-winner').text(playerOne + "Wins")
-}
+  function checkPlayerWin(player) {
+    if (($('[data-cell="0"]').text() === playerOne) && ($('[data-cell="1"]').text() === playerOne) && ($('[data-cell="2"]').text() === playerOne) ||
+      ($('[data-cell="3"]').text() === playerOne) && ($('[data-cell="4"]').text() === playerOne) && ($('[data-cell="5"]').text() === playerOne) ||
+      ($('[data-cell="6"]').text() === playerOne) && ($('[data-cell="7"]').text() === playerOne) && ($('[data-cell="8"]').text() === playerOne) ||
+      ($('[data-cell="6"]').text() === playerOne) && ($('[data-cell="4"]').text() === playerOne) && ($('[data-cell="2"]').text() === playerOne) ||
+      ($('[data-cell="8"]').text() === playerOne) && ($('[data-cell="4"]').text() === playerOne) && ($('[data-cell="0"]').text() === playerOne) ||
+      ($('[data-cell="0"]').text() === playerOne) && ($('[data-cell="3"]').text() === playerOne) && ($('[data-cell="6"]').text() === playerOne) ||
+      ($('[data-cell="1"]').text() === playerOne) && ($('[data-cell="4"]').text() === playerOne) && ($('[data-cell="7"]').text() === playerOne) ||
+      ($('[data-cell="2"]').text() === playerOne) && ($('[data-cell="5"]').text() === playerOne) && ($('[data-cell="8"]').text() === playerOne)) {
 
-   function checkPlayerWin() {
-     if (($('[data-cell="0"]').text)==="playerOne") {
-
-
-    // if (($('[data-cell="0"]').text() === "playerOne" && $('[data-cell="1"]').text() === "playerOne" && $('[data-cell="2"]').text() === "playerOne")){
-
-    // (($('[data-cell="0"]').text() === "playerTwo" && $('[data-cell="1"]').text() === "playerTwo" && $('[data-cell="2"]').text() === "playerTwo") {
-    //    console.log('playerTwo');
-console.log('WHAT');
-return true;
+      alert('Player #1 has just won the game!!!!!');
+      console.log(player + "won the game!!!!");
 
 
-    } else {
+      // (($('[data-cell="0"]').text() === "playerTwo" && $('[data-cell="1"]').text() === "playerTwo" && $('[data-cell="2"]').text() === "playerTwo") {
+      //    console.log('playerTwo');
+
+      // if (($('[data-cell="0"]').text()) === player) {
+      // return true;
+    } else if (($('[data-cell="0"]').text() === playerTwo) && ($('[data-cell="1"]').text() === playerTwo) && ($('[data-cell="2"]').text() === playerTwo) ||
+      ($('[data-cell="3"]').text() === playerTwo) && ($('[data-cell="4"]').text() === playerTwo) && ($('[data-cell="5"]').text() === playerTwo) ||
+      ($('[data-cell="6"]').text() === playerTwo) && ($('[data-cell="7"]').text() === playerTwo) && ($('[data-cell="8"]').text() === playerTwo) ||
+      ($('[data-cell="6"]').text() === playerTwo) && ($('[data-cell="4"]').text() === playerTwo) && ($('[data-cell="2"]').text() === playerTwo) ||
+      ($('[data-cell="8"]').text() === playerTwo) && ($('[data-cell="4"]').text() === playerTwo) && ($('[data-cell="0"]').text() === playerTwo) ||
+      ($('[data-cell="0"]').text() === playerTwo) && ($('[data-cell="3"]').text() === playerTwo) && ($('[data-cell="6"]').text() === playerTwo) ||
+      ($('[data-cell="1"]').text() === playerTwo) && ($('[data-cell="4"]').text() === playerTwo) && ($('[data-cell="7"]').text() === playerTwo) ||
+      ($('[data-cell="2"]').text() === playerTwo) && ($('[data-cell="5"]').text() === playerTwo) && ($('[data-cell="8"]').text() === playerTwo)) {
+
+
+
+      alert('Player #2 has just won the game!!!!!');
+      console.log(playerTwo + "has won the game!!!!");
+
       // console.log('playerTwo wins');
-      return false;
 
     }
 
-}
+
+  }
+  // return false;
+
 
 });
-});
+//investigate (is)
