063a23c4e117c6f47980912fed757f0655be8d32
walzer85 <walzer85@gmail.com>
2017-09-04 13:09:05 -0500

    Adding win conditions to tic tac toe

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,131 +1,39 @@
 'use strict';
 $(document).ready(function() {
-  // alert('Let\'s Play Tic-Tac-Toe!')
-var playerTurn = 0;
-var xWin = 'Player One Wins!';
-var yWin = 'Player Two Wins!';
+      // alert('Let\'s Play Tic-Tac-Toe!')
+      var playerTurn = 0;
+      var xWin = 'Player One Wins!';
+      var yWin = 'Player Two Wins!';
 
-var winCondition = function(player) {
-  console.log(player);
-  if ($('#0').text() === player) {
-    if ($('#1').text() === player) {
-      if ($('#2').text() === player) {
-        alert('win');
-      }
-    }
-  }
-};
+      var winCondition = function(player) {
+        if (($('#0') === player && $('#1') === player && $('#2') === player) ||
+          ($('#3') === player && $('#4') === player && $('#5') === player) ||
+          ($('#6') === player && $('#7') === player && $('#8') === player) ||
+          ($('#0') === player && $('#3') === player && $('#6') === player) ||
+          ($('#1') === player && $('#4') === player && $('#7') === player) ||
+          ($('#2') === player && $('#5') === player && $('#8') === player) ||
+          ($('#0') === player && $('#4') === player && $('#8') === player) ||
+          ($('#2') === player && $('#4') === player && $('#6') === player));
+        alert('Winner, Winner, Chicken Dinner!');
+        else {
+          alert('Draw.  Everyone\'s a winner and/or loser!');
+        }
 
-var winCondition = function(player) {
-  console.log(player);
-  if ($('#3').text() === player) {
-    if ($('#4').text() === player) {
-      if ($('#5').text() === player) {
-        alert('win');
-      }
-    }
-  }
-};
+        $('[id]').on('click', function() {
+          if ($(this).text() === '') {
+            if (playerTurn % 2 === 0) {
+              $(this).text('X');
+              winCondition('X');
+            } else {
+              $(this).text('O');
+              winCondition('O');
+            }
+            playerTurn++;
 
-var winCondition = function(player) {
-  console.log(player);
-  if ($('#6').text() === player) {
-    if ($('#7').text() === player) {
-      if ($('#8').text() === player) {
-        alert('win');
-      }
-    }
-  }
-};
-
-var winCondition = function(player) {
-  console.log(player);
-  if ($('#0').text() === player) {
-    if ($('#3').text() === player) {
-      if ($('#6').text() === player) {
-        alert('win');
-      }
-    }
-  }
-};
-
-var winCondition = function(player) {
-  console.log(player);
-  if ($('#1').text() === player) {
-    if ($('#4').text() === player) {
-      if ($('#7').text() === player) {
-        alert('win');
-      }
-    }
-  }
-};
-
-var winCondition = function(player) {
-  console.log(player);
-  if ($('#0').text() === player) {
-    if ($('#1').text() === player) {
-      if ($('#2').text() === player) {
-        alert('win');
-      }
-    }
-  }
-};
-
-var winCondition = function(player) {
-  console.log(player);
-  if ($('#0').text() === player) {
-    if ($('#1').text() === player) {
-      if ($('#2').text() === player) {
-        alert('win');
-      }
-    }
-  }
-};
-
-var winCondition = function(player) {
-  console.log(player);
-  if ($('#0').text() === player) {
-    if ($('#1').text() === player) {
-      if ($('#2').text() === player) {
-        alert('win');
-      }
-    }
-  }
-};
-
-var winCondition = function(player) {
-  console.log(player);
-  if ($('#0').text() === player) {
-    if ($('#1').text() === player) {
-      if ($('#2').text() === player) {
-        alert('win');
-      }
-    }
-  }
-};
-
-var winCondition = function(player) {
-  console.log(player);
-  if ($('#0').text() === player) {
-    if ($('#1').text() === player) {
-      if ($('#2').text() === player) {
-        alert('win');
-      }
-    }
-  }
-};
-
-$('[id]').on('click', function() {
-  if ($(this).text() === '') {
-    if (playerTurn % 2 === 0) {
-      $(this).text('X');
-      winCondition('X');
-      } else {
-      $(this).text('O');
-      winCondition('O');
-    }
-    playerTurn++;
-
-  }
-});
-});
+          }
+        });
+        $(winCondition() {
+          location.reload();
+        });
+      };
+      });
