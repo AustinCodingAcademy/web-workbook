a26bcaa118f4938cb54bde98eb3494384ee9370b
greggandrews <gandrewstx@gmail.com>
2017-09-05 13:39:51 -0500

    tic-tac-toe

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,52 +1,52 @@
 'use strict';
 
-$(document).ready (function() {
+$(document).ready(function() {
 
-let playerTurn = 'X';
-$('[data-cell]').on('click', function() {
+  let playerTurn = 'X';
+  $('[data-cell]').click(function() {
     $(this).text(playerTurn);
     checkForWinner();
     if (playerTurn === 'X') {
       playerTurn = 'O';
-  } else {
+    } else {
       playerTurn = 'X';
-  }
-});
+    }
+  });
 
-function checkForWinner() {
-      if(
-        ($('[data-cell="0"]').text() === playerTurn &&
+  function checkForWinner() {
+    if (
+      ($('[data-cell="0"]').text() === playerTurn &&
         $('[data-cell="3"]').text() === playerTurn &&
         $('[data-cell="6"]').text() === playerTurn) ||
-        ($('[data-cell="1"]').text() === playerTurn &&
+      ($('[data-cell="1"]').text() === playerTurn &&
         $('[data-cell="4"]').text() === playerTurn &&
         $('[data-cell="7"]').text() === playerTurn) ||
-        ($('[data-cell="2"]').text() === playerTurn &&
+      ($('[data-cell="2"]').text() === playerTurn &&
         $('[data-cell="5"]').text() === playerTurn &&
         $('[data-cell="8"]').text() === playerTurn) ||
-        ($('[data-cell="0"]').text() === playerTurn &&
+      ($('[data-cell="0"]').text() === playerTurn &&
         $('[data-cell="1"]').text() === playerTurn &&
         $('[data-cell="2"]').text() === playerTurn) ||
-        ($('[data-cell="3"]').text() === playerTurn &&
+      ($('[data-cell="3"]').text() === playerTurn &&
         $('[data-cell="4"]').text() === playerTurn &&
         $('[data-cell="5"]').text() === playerTurn) ||
-        ($('[data-cell="6"]').text() === playerTurn &&
+      ($('[data-cell="6"]').text() === playerTurn &&
         $('[data-cell="7"]').text() === playerTurn &&
         $('[data-cell="8"]').text() === playerTurn) ||
-        ($('[data-cell="0"]').text() === playerTurn &&
+      ($('[data-cell="0"]').text() === playerTurn &&
         $('[data-cell="4"]').text() === playerTurn &&
         $('[data-cell="8"]').text() === playerTurn) ||
-        ($('[data-cell="2"]').text() === playerTurn &&
+      ($('[data-cell="2"]').text() === playerTurn &&
         $('[data-cell="4"]').text() === playerTurn &&
         $('[data-cell="6"]').text() === playerTurn)
-      ) {
-    $('#announce-winner').text(`Player ${playerTurn} Wins`)
-      }
+    ) {
+      $('#announce-winner').text(`Player ${playerTurn} Wins`)
     }
+  }
 
-    $('#clear').click(function() {
-      $('[data-cell], #announce-winner').text('');
-      playerTurn = 'X';
-    });
-
+  $('#clear').click(function() {
+    $('[data-cell], #announce-winner').text('');
+    playerTurn = 'X';
   });
+
+});
