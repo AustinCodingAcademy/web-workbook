31913d562df8441f97d351d5e41e4249c407b72e
cfissell50 <cfissell@hotmail.com>
2017-09-04 22:01:45 -0500

    tictac

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
-$(document).on('ready', function() {
+$(document).ready (function() {
   // Put app logic in here
+
+  let playerTurn = 0;
+  $('[data-cell]').on('click', function() {
+    if (playerTurn % 2 === 0) {
+      $(this).text('X');
+      // checkVictory('X');
+    } else {
+      $(this).text('O');
+      // checkVictory('O');
+    }
+    playerTurn++;
+  })
+
+  function checkVictory() {
+    if (
+      $('[data-cell="0"]').text() ==='X' && $('[data-cell="1"]').text() ==='X' && $('[data-cell="2"]').text() ==='X' )
+    }
+
 });
