cbababd758215ada740481bc7b2f5702d7e5d07f
cfissell50 <cfissell@hotmail.com>
2017-09-05 14:35:08 -0500

    ticytacytoey
    yooo

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,23 +1,49 @@
 'use strict';
+$(document).ready(function() {
+  var playerOne = 'X';
+  var playerTwo = 'O';
+  var turn = 1;
 
-$(document).ready (function() {
-  // Put app logic in here
+  $('[data-cell]').click(function() {
+    // sound.play();
+    // played = true;
+    // console.log("Playing");
+  });
+  $('[data-cell]').click(function() {
 
-  let playerTurn = 0;
-  $('[data-cell]').on('click', function() {
-    if (playerTurn % 2 === 0) {
-      $(this).text('X');
-      // checkVictory('X');
+    if (turn % 2 === 1) {
+      $(this).text(playerOne);
+      checkPlayerWin(playerOne);
     } else {
-      $(this).text('O');
-      // checkVictory('O');
+      $(this).text(playerTwo);
+      checkPlayerWin(playerTwo);
     }
-    playerTurn++;
-  })
+    turn++;
+  });
+  function checkPlayerWin(player) {
+    if (($('[data-cell="0"]').text() === playerOne) && ($('[data-cell="1"]').text() === playerOne) && ($('[data-cell="2"]').text() === playerOne) ||
+      ($('[data-cell="3"]').text() === playerOne) && ($('[data-cell="4"]').text() === playerOne) && ($('[data-cell="5"]').text() === playerOne) ||
+      ($('[data-cell="6"]').text() === playerOne) && ($('[data-cell="7"]').text() === playerOne) && ($('[data-cell="8"]').text() === playerOne) ||
+      ($('[data-cell="6"]').text() === playerOne) && ($('[data-cell="4"]').text() === playerOne) && ($('[data-cell="2"]').text() === playerOne) ||
+      ($('[data-cell="8"]').text() === playerOne) && ($('[data-cell="4"]').text() === playerOne) && ($('[data-cell="0"]').text() === playerOne) ||
+      ($('[data-cell="0"]').text() === playerOne) && ($('[data-cell="3"]').text() === playerOne) && ($('[data-cell="6"]').text() === playerOne) ||
+      ($('[data-cell="1"]').text() === playerOne) && ($('[data-cell="4"]').text() === playerOne) && ($('[data-cell="7"]').text() === playerOne) ||
+      ($('[data-cell="2"]').text() === playerOne) && ($('[data-cell="5"]').text() === playerOne) && ($('[data-cell="8"]').text() === playerOne)) {
 
-  function checkVictory() {
-    if (
-      $('[data-cell="0"]').text() ==='X' && $('[data-cell="1"]').text() ==='X' && $('[data-cell="2"]').text() ==='X' )
+      // alert("Player \ " + player + "\ Wins!");
+      console.log("Player \ " + player + "\ Wins!");
+      $('#announce-winner').text("Player \ " + player + "\ Wins!");
+    } else if (($('[data-cell="0"]').text() === playerTwo) && ($('[data-cell="1"]').text() === playerTwo) && ($('[data-cell="2"]').text() === playerTwo) ||
+      ($('[data-cell="3"]').text() === playerTwo) && ($('[data-cell="4"]').text() === playerTwo) && ($('[data-cell="5"]').text() === playerTwo) ||
+      ($('[data-cell="6"]').text() === playerTwo) && ($('[data-cell="7"]').text() === playerTwo) && ($('[data-cell="8"]').text() === playerTwo) ||
+      ($('[data-cell="6"]').text() === playerTwo) && ($('[data-cell="4"]').text() === playerTwo) && ($('[data-cell="2"]').text() === playerTwo) ||
+      ($('[data-cell="8"]').text() === playerTwo) && ($('[data-cell="4"]').text() === playerTwo) && ($('[data-cell="0"]').text() === playerTwo) ||
+      ($('[data-cell="0"]').text() === playerTwo) && ($('[data-cell="3"]').text() === playerTwo) && ($('[data-cell="6"]').text() === playerTwo) ||
+      ($('[data-cell="1"]').text() === playerTwo) && ($('[data-cell="4"]').text() === playerTwo) && ($('[data-cell="7"]').text() === playerTwo) ||
+      ($('[data-cell="2"]').text() === playerTwo) && ($('[data-cell="5"]').text() === playerTwo) && ($('[data-cell="8"]').text() === playerTwo)) {
+      // alert("Player \ " + player + "\ Wins!");
+      console.log("Player \ " + player + "\ Wins!");
+      $('#announce-winner').text("Player \ " + player + "\ Wins!");
     }
-
+  }
 });
