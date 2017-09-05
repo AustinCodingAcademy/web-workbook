bd1635642769387249542ed26f151dd9386c85a5
walzer85 <walzer85@gmail.com>
2017-09-05 07:07:10 -0500

    Still nee to figure out winCondition and clear button

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,37 +1,39 @@
 'use strict';
 $(document).ready(function() {
-      // alert('Let\'s Play Tic-Tac-Toe!')
-      var playerTurn = 0;
-      var xWin = 'Player One Wins!';
-      var yWin = 'Player Two Wins!';
+  // alert('Let\'s Play Tic-Tac-Toe!')
+  var playerTurn = 0;
+  var xWin = 'Player One Wins!';
+  var yWin = 'Player Two Wins!';
 
-      var winCondition = function(player) {
-        if (($('#0') === player && $('#1') === player && $('#2') === player) ||
-          ($('#3') === player && $('#4') === player && $('#5') === player) ||
-          ($('#6') === player && $('#7') === player && $('#8') === player) ||
-          ($('#0') === player && $('#3') === player && $('#6') === player) ||
-          ($('#1') === player && $('#4') === player && $('#7') === player) ||
-          ($('#2') === player && $('#5') === player && $('#8') === player) ||
-          ($('#0') === player && $('#4') === player && $('#8') === player) ||
-          ($('#2') === player && $('#4') === player && $('#6') === player)) {
-        alert('Winner, Winner, Chicken Dinner!')
-      } else if ('[id]' === 'X' || 'O'){
-          alert('Draw.  Everyone\'s a winner and/or loser!');
-        }
-      };
-        $('[id]').on('click', function() {
-          if ($(this).text() === '') {
-            if (playerTurn % 2 === 0) {
-              $(this).text('X');
-              winCondition('X');
-            } else {
-              $(this).text('O');
-              winCondition('O');
-            }
-            playerTurn++;
-          }
-        });
-        $('#clear').onclick( function() {
-          $(['id']).reset();
-        });
-      });
+  var winCondition = function(player) {
+    if (($('#0') === player && $('#1') === player && $('#2') === player) ||
+      ($('#3') === player && $('#4') === player && $('#5') === player) ||
+      ($('#6') === player && $('#7') === player && $('#8') === player) ||
+      ($('#0') === player && $('#3') === player && $('#6') === player) ||
+      ($('#1') === player && $('#4') === player && $('#7') === player) ||
+      ($('#2') === player && $('#5') === player && $('#8') === player) ||
+      ($('#0') === player && $('#4') === player && $('#8') === player) ||
+      ($('#2') === player && $('#4') === player && $('#6') === player)) {
+      alert('Winner, Winner, Chicken Dinner!');
+    } else if ($('[id]').checkEmpty()) {
+      alert('Next Turn');
+    } else {
+      alert('Draw!  Both of you are winners AND losers.');
+    }
+  };
+  $('[id]').on('click', function() {
+    if ($(this).text() === '') {
+      if (playerTurn % 2 === 0) {
+        $(this).text('X');
+        winCondition('X');
+      } else {
+        $(this).text('O');
+        winCondition('O');
+      }
+      playerTurn++;
+    }
+  });
+  $('#clear').onclick(function() {
+    $(['id']).reset();
+  });
+});
