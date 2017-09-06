2476179123f6dc058a9d89d3a2c4809df3653e3e
cfissell50 <cfissell@hotmail.com>
2017-09-06 13:16:55 -0500

    towersofannoy

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,4 +2,25 @@
 
 $(document).ready(function() {
   // Put app logic here
+
+     $( "[data-block]" ).draggable({
+       revert: "invalid"
+     });
+
+     $( "[data-stack]" ).droppable({
+       drop: function(event, ui){
+
+         var last = $(this).children().last() [0].attr('data-block');
+         var drag = $(ui.draggable).attr(['data-block']
+
+        if(parseInt(drag) > parseInt(last) ) {
+          $(ui.dragable).draggable('option', 'revert', true);
+        }else { $(ui.draggable).appendTo(this).attr('style', 'position: relative');
+        }
+       }
+     });
 });
+
+
+// var lastChild = $(this).children().last()[0];
+// parseInt
