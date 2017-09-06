453505db9f3088b8a17e5375f09e790fdf2b3b5a
walzer85 <walzer85@gmail.com>
2017-09-05 20:28:15 -0500

    Correct insertion to data stacks, but not correct child order yet

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
   $('[data-block]').draggable({
     revert: 'invalid'
   })
+  $('[data-stack]').droppable({
+    drop:function(event, ui) {
+      var last = $(this).children().last()[0];
+      $(ui.draggable).appendTo(this).attr('style', 'position: relative')
+    }
+  })
 });
