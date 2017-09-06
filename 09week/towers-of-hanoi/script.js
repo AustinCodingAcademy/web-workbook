2804b771749e687ab4ba5955d5b34f645aaa6fe7
Jon <contact@jongorman.top>
2017-09-06 09:39:48 -0500

    :phone: 118

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,33 @@
 'use strict';
-
+f
 $(document).ready(function() {
   // Put app logic here
-});
+
+
+  $('.draggable').draggable({
+    revert: "invalid"
+  });
+
+  $('[data-stack]').droppable( {
+    drop: function(event, ui){
+      var drag = $(ui.draggable).attr('data-block');
+      var last = $(this).children().last()[0]).attr('data-block');
+      if(parseInt(drag) > parseInt(last)){
+        $(ui.draggable).appendTo(this).attr('style', 'position:relative');
+      }else {
+        $(ui.draggable).appendTo(this).attr('style', 'position: relative');
+
+
+
+      // $(ui.draggable).appendTo(this).attr('style', 'position:relative');
+      // if($parseInt($(ui.draggable).attr('[data-block]')>parseInt(last) )){
+      // console.log(p)arseInt($(ui.draggable).attr('[data-block]'))
+      // $(ui.draggable).draggable('option', 'revert', true);
+
+    }
+    }
+
+  });
+
+  });
+// });
