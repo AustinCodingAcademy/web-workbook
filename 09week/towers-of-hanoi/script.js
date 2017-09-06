334e1bebcacc5cb51544416c627eb8a9b6a27c85
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-09-05 21:03:01 -0500

    worked on towers

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,4 +2,25 @@
 
 $(document).ready(function() {
   // Put app logic here
+  $( function() {
+
+   $("[data-block]").draggable({
+     revert: "invalid"
+   });
+
+   $('[data-stack]').droppable({
+     drop: function(event, ui){
+       var last = $(this).children().last([0]);
+      $(ui.draggable).appendTo(this).attr('style','position: relative');
+      if($(ui,draggable).attr('[data-block]')) > parseInt(last){
+          $(ui.draggable).draggable()
+      }
+     }
+   });
+
+
+
+
+} );
+
 });
