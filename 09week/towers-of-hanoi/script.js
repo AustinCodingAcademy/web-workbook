7f51f02110aaee0166b0fa3a45ba778a36c0dd86
greggandrews <gandrewstx@gmail.com>
2017-09-05 21:00:43 -0500

    towers-of-hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,4 +1,42 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+      // Put app logic here
+      // var stacks = $('[data-stack]');
+      // var blocks = $('[data-block]');
+
+      $('[data-block]').draggable({
+        revert: 'invalid'
+      });
+
+      $('[data-stack]').droppable({
+        drop: function(event, ui){
+          var drag = $(ui.draggable).attr('data-block');
+          var last = $($(this).children().last()).attr('data-block');
+          if(parseLint(drag) > parseInt(last) ){
+            $(ui.draggable).draggable('option', 'revert', true);
+          // if(parseInt($(ui.draggable).attr(['data-block']) > parseInt(last) )){
+          } else {
+              $(ui.draggable).appendTo(this).attr('style', 'position: relative');
+            }
+        }
+      });
+
+      });
+
+
+
+
+
+
+      //   $('.data-block').draggable({
+      //       revert: 'invalid'
+      //     });
+      //
+      // }
+      //
+      // $('#dropArea').droppable( {
+      // drop:function(event, ui)  {
+      //     $(ui.draggable).appendTo9(this).attr('style', 'position:relative');
+      //   }
+      // });
