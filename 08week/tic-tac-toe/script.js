f9f00910fa0c09258174c151a19678430b948bc5
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-09-05 20:06:23 -0500

    Towers of Hanoi start

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -2,45 +2,61 @@
 
 $(document).ready(function() {
   // Put app logic in here
+
   var playerTurn = ``;
 
   $('[data-cell]').on('click', function(){
     if (playerTurn % 2 === 0){
       $(this).text('X');
-      checkVictory('X');
+      //checkVictory('X');
     } else {
     //player 2's turn (O)
       $(this).text('O');
-      checkVictory('O');
+      //checkVictory('O');
     }
     playerTurn++;
 
   })
-
+/*
   function checkVictory(){
 
-    if ($('[data-cell="0"]').text() === 'X' && $('[data-cell="1"]').text() === 'X' &&     $('[data-cell="2"]').text() === 'X');
-//  alert("you win");
+   if ($('[data-cell="0"]').text() === 'X' && $('[data-cell="1"]').text() === 'X' &&     $('[data-cell="2"]').text() === 'X');
+
+    else if ($('[data-cell="0"]').text() === 'O' && $('[data-cell="1"]').text() === 'O' &&     $('[data-cell="2"]').text() === 'O');
+
+    if ($('[data-cell="3"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="5"]').text() === 'X');
+
+    else if ($('[data-cell="3"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="5"]').text() === 'O');
+
+    if ($('[data-cell="6"]').text() === 'X' && $('[data-cell="7"]').text() === 'X' && $('[data-cell="8"]').text() === 'X');
+
+    else if ($('[data-cell="6"]').text() === 'O' && $('[data-cell="7"]').text() === 'O' && $('[data-cell="8"]').text() === 'O');
+
+    if ($('[data-cell="0"]').text() === 'X' && $('[data-cell="3"]').text() === 'X' && $('[data-cell="6"]').text() === 'X');
+
+    else if ($('[data-cell="0"]').text() === 'O' && $('[data-cell="3"]').text() === 'O' && $('[data-cell="6"]').text() === 'X');
+
+    if ($('[data-cell="1"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="7"]').text() === 'X');
+
+    else if ($('[data-cell="1"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="7"]').text() === 'X');
+
+    if ($('[data-cell="2"]').text() === 'X' && $('[data-cell="5"]').text() === 'X' && $('[data-cell="8"]').text() === 'X');
+
+    else if ($('[data-cell="2"]').text() === 'O' && $('[data-cell="5"]').text() === 'O' && $('[data-cell="8"]').text() === 'O');
+
+    if ($('[data-cell="0"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="8"]').text() === 'X');
+
+    else if ($('[data-cell="0"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="8"]').text() === 'O');
+
+    if ($('[data-cell="2"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="6"]').text() === 'X');
+
+    else if ($('[data-cell="2"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="6"]').text() === 'O');
+
+
+
+
+    console.log(checkVictory);
   }
-/*
-if ($('[data-cell="3"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn)
-
-if ($('[data-cell="6"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)
-
-if ($('[data-cell="0"]').text() === playerTurn && $('[data-cell="3"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn)
-
-if ($('[data-cell="1"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn)
-
-if ($('[data-cell="2"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)
-
-if ($('[data-cell="0"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)
-
-if ($('[data-cell="2"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn)
-
-
-
 */
-
-
 
 });
--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,4 +2,22 @@
 
 $(document).ready(function() {
   // Put app logic here
+  $('[data-block]').draggable({
+    revert: "invalid"
 });
+
+  $('[data-stack]').droppable({
+    drop:function(event, ui){
+      $(ui.draggable).appendTo(this).attr('style', 'position: relative');
+
+    }
+  });
+
+});
+
+  //make blocks draggable/droppable
+  //rules: smaller on top(children/lastchild)
+  //winstate
+  //can't drop outside of the blocks
+  //(if event is larger that lastchild)
+  // reset lastchild => var lastchild=$9this.children().last()[0];
