e6c9e2e558fb1bbb45588e75c6008399306b75aa
walzer85 <walzer85@gmail.com>
2017-09-06 22:20:53 -0500

    defined which block can be dropped where.  only need to finish the winState function

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,42 +1,58 @@
 'use strict';
 
 $(document).ready(function() {
-  //Main variables used for the game. Starts the game. Defines the Stacks, Blocks, and which Block you can move.//
-  var gameOver = false;
-  var $stacks = $('[data-stack]');
-  var $blocks = $('[data-block]');
-  var $movableBlocks = $('[data-block]:last-child');
-  $movableBlocks.addClass('movable');
+      //Main variables used for the game. Starts the game. Defines the Stacks, Blocks, and which Block you can move.//
+      var gameOver = false;
+      var $stacks = $('[data-stack]');
+      var $blocks = $('[data-block]');
+      var $movableBlocks = $('[data-block]:last-child');
+      $movableBlocks.addClass('movable');
 
-//$blocks are draggable.  $stacks will only accept the last child a .//
-  $blocks.draggable({
-    revert: true
-  });
-  $stacks.droppable({
-    accept: '.movable',
-    drop : function(event, ui) {
-      if (!gameover) {
-        if (goodToDrop($(this), ui.draggable)) {
-          ui.draggable.draggable('option', 'revert', false);
-          $(this).append(ui.draggable.detach());
-          ui.draggable.css({
-            'top': 0,
-            'left': 0
-          });
-          //This resets the $blocks onto their initial $stack//
-          // $movableBlocks = $('[data-block]:last-child');
-          $blocks.removeClass('movable');
-          $movableBlocks.addClass('movable');
-          $blocks.draggable({
-            revert: true
-          });
-
+      //$blocks are draggable.  $stacks will only accept the last child a .//
+      $blocks.draggable({
+        revert: true
+      });
+      $stacks.droppable({
+        accept: '.movable',
+        drop: function(event, ui) {
+          if (!gameOver) {
+            if (dropReady($(this), ui.draggable)) {
+              ui.draggable.draggable('option', 'revert', false);
+              $(this).append(ui.draggable.detach());
+              ui.draggable.css({
+                'top': 0,
+                'left': 0
+              });
+              //This resets the $blocks onto their initial $stack//
+              // $movableBlocks = $('[data-block]:last-child');
+              $blocks.removeClass('movable');
+              $movableBlocks.addClass('movable');
+              $blocks.draggable({
+                revert: true
+              });
+              //Winner!!//
+              winState();
+            } else {
+              newGame();
+            }
+          }
         }
-      }
-    }
-  });
+      });
 
-
-
-
-  });
+      //Variable that triggers a new game//
+      function newGame() {
+        $('[data-stack="1"]').html('<div data-stack="1"><div data-block="100"></div><div data-block="75"></div><div data-block="50"></div><div data-block="25"></div></div>');
+        $('[data-stack="2"]').empty();
+        $('[data-stack="3"}').empty();
+        gameOver = false;
+        }
+      //Defines which blocks can be dropped into a stack.//
+      function dropReady($stacks, $blocks) {
+        var $last_block = $stacks.children().last();
+        if (paresInt($blocks) < paresInt($last-block) || $stacks.children().length === 0) {
+          return true;
+        }  else {
+          return false}
+        }
+      })
+      });
