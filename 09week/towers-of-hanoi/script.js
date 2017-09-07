8e72ab7270d8e04fac26112608a05d94e79509f0
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-09-07 17:25:31 -0500

    updated hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -9,17 +9,17 @@
 //
 //   //  $('[data-stack]').droppable({
 //   //    drop: function(event, ui){
-//   //      var last = $(this).children().last([0]);
+//   //
 //   //     $(ui.draggable).appendTo(this).attr('style','position: relative');
 
 $(document).ready(function() {
   $(function() {
     var x = 0; //dragged data-block reference
-    var lastChild = null; //last data-block in stack
+    var last = 0; //last data-block in stack
 
-    $('[data-stack]').click( function(){
-      lastChild = $(this).children().last().attr('data-block')
-    });
+    $('[data-stack]').click(function(){
+    }
+    );
 
     $('[data-block]').draggable({
       appendTo: "body",
@@ -40,8 +40,9 @@ $(document).ready(function() {
       hoverClass: "ui-state-hover",
       drop: function(event, ui) {
         $(ui.draggable).appendTo(this).attr('style', 'position: relative');
-        // var lastChild = $(this).children().last([0]).attr('[data-block]');
-        // alert(lastChild);
+
+        var last = $(this).children().attr('data-block');
+        if(last > x){alert('ok')}
         console.log("moved block " + x + " to data stack " + ($(this).attr('data-stack')));
         }
       // if($(ui, draggable).attr('[data-block]')) > parseInt(last){
