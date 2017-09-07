f6e4b5d3bf6d950cef6959e7d3082debe5300187
walzer85 <walzer85@gmail.com>
2017-09-06 21:21:48 -0500

    restarted towers of hanoi. defined more variables. gaining an understanding of structure and when to define a function.

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,13 +1,42 @@
 'use strict';
 
 $(document).ready(function() {
-  $('[data-block]').draggable({
-    revert: 'invalid'
-  })
-  $('[data-stack]').droppable({
-    drop:function(event, ui) {
-      var last = $(this).children().last()[0];
-      $(ui.draggable).appendTo(this).attr('style', 'position: relative')
+  //Main variables used for the game. Starts the game. Defines the Stacks, Blocks, and which Block you can move.//
+  var gameOver = false;
+  var $stacks = $('[data-stack]');
+  var $blocks = $('[data-block]');
+  var $movableBlocks = $('[data-block]:last-child');
+  $movableBlocks.addClass('movable');
+
+//$blocks are draggable.  $stacks will only accept the last child a .//
+  $blocks.draggable({
+    revert: true
+  });
+  $stacks.droppable({
+    accept: '.movable',
+    drop : function(event, ui) {
+      if (!gameover) {
+        if (goodToDrop($(this), ui.draggable)) {
+          ui.draggable.draggable('option', 'revert', false);
+          $(this).append(ui.draggable.detach());
+          ui.draggable.css({
+            'top': 0,
+            'left': 0
+          });
+          //This resets the $blocks onto their initial $stack//
+          // $movableBlocks = $('[data-block]:last-child');
+          $blocks.removeClass('movable');
+          $movableBlocks.addClass('movable');
+          $blocks.draggable({
+            revert: true
+          });
+
+        }
+      }
     }
-  })
-});
+  });
+
+
+
+
+  });
