4815577aef34846bf3302a9b125d3f614421e0bb
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-09-06 23:59:03 -0500

    worked on towers

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,26 +1,82 @@
 'use strict';
+//
+// $(document).ready(function() {
+//   // Put app logic here
+//
+//    $( function() {
+//     $( "[data-block]" ).draggable();
+//   } );
+//
+//   //  $('[data-stack]').droppable({
+//   //    drop: function(event, ui){
+//   //      var last = $(this).children().last([0]);
+//   //     $(ui.draggable).appendTo(this).attr('style','position: relative');
 
-$(document).ready(function() {
-  // Put app logic here
-  $( function() {
+//   //    }
+//   //  });
+//
+//
+//
+//
+// };
+//
+// });
 
-   $("[data-block]").draggable({
-     revert: "invalid"
-   });
 
-   $('[data-stack]').droppable({
-     drop: function(event, ui){
-       var last = $(this).children().last([0]);
+$(document).ready(function(){
+  var lastChild = null;
+
+  console.log('ready');
+  $(function() {
+    console.log("function");
+
+    $('[data-block]').draggable({
+      appendTo:"body",
+      cursor:"move",
+      revert:"invalid"
+    });
+
+    $('#ds1').droppable({
+
+      tolerance:"intersect",
+      accept:'[data-block]',
+      activeClass:"ui-state-default",
+      hoverClass:"ui-state-hover",
+      drop:function(event, ui){
+        var last = $(this).children().last([0]);
+        console.log(this.attr('[data-stack]'));
       $(ui.draggable).appendTo(this).attr('style','position: relative');
-      if($(ui,draggable).attr('[data-block]')) > parseInt(last){
-          $(ui.draggable).draggable()
-      }
-     }
-   });
+    }
+    // if($(ui, draggable).attr('[data-block]')) > parseInt(last){
+    //     $(ui.draggable).draggable()
+    // }
+    });
+
+    $('#ds2').droppable({
+      tolerance:"intersect",
+      accept:'[data-block]',
+      activeClass:"ui-state-default",
+      hoverClass:"ui-state-hover",
+      drop:function(event, ui){
+      $(ui.draggable).appendTo(this).attr('style','position: relative');
+    }
+  });
+
+    $('#ds3').droppable({
+      tolerance:"intersect",
+      accept:'[data-block]',
+      activeClass:"ui-state-default",
+      hoverClass:"ui-state-hover",
+      drop:function(event, ui){
+      $(ui.draggable).appendTo(this).attr('style','position: relative');
+    }
+    });
 
 
 
 
-} );
 
+
+
+  });
 });
