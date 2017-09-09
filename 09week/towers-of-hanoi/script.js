9909e8831f485d247c4ba142528d8a40983312e7
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-09-09 16:31:51 -0500

    towers ok

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -17,37 +17,85 @@ $(document).ready(function() {
     var x = 0; //dragged data-block reference
     var last = 0; //last data-block in stack
 
-    $('[data-stack]').click(function(){
-    }
-    );
+    // $('[data-stack]').click(function(){
+    //
+    // var last = $(this).children().last().attr('data-block');
+    //   console.log(last);
+    // }
+    // );
 
     $('[data-block]').draggable({
       appendTo: "body",
       cursor: "move",
       revert: "invalid",
-      drag:function(event, ui){
+      drag: function(event, ui) {
         x = $(this).attr('data-block');
+        var last = 0;
 
       }
 
     });
 
-    $('[data-stack]').droppable({
+    $('[data-stack="2"]').droppable({
 
       tolerance: "intersect",
       accept: '[data-block]',
       activeClass: "ui-state-default",
       hoverClass: "ui-state-hover",
       drop: function(event, ui) {
+
+        if ($(this).children().length < 1) {
+          last = 105;
+        } else {
+          last = $(this).children().last().attr('data-block');
+        }
+
         $(ui.draggable).appendTo(this).attr('style', 'position: relative');
 
-        var last = $(this).children().attr('data-block');
-        if(last > x){alert('ok')}
+        if (last > x) {
+          alert("ok");
+        };
+        console.log(last + x);
         console.log("moved block " + x + " to data stack " + ($(this).attr('data-stack')));
+
+        last = 0;
+
+
+
+        // if($(ui, draggable).attr('[data-block]')) > parseInt(last){
+        //     $(ui.draggable).draggable()
+        // }
+      }
+    });
+
+    $('[data-stack="3"]').droppable({
+
+      tolerance: "intersect",
+      accept: '[data-block]',
+      activeClass: "ui-state-default",
+      hoverClass: "ui-state-hover",
+      drop: function(event, ui) {
+        if ($(this).children().length < 1) {
+          last = 105;
+        } else {
+          last = $(this).children().last().attr('data-block');
         }
-      // if($(ui, draggable).attr('[data-block]')) > parseInt(last){
-      //     $(ui.draggable).draggable()
-      // }
+
+        $(ui.draggable).appendTo(this).attr('style', 'position: relative');
+
+
+        if (last > x) {
+          alert("ok");
+        };
+        console.log(last + x)
+        console.log("moved block " + x + " to data stack " + ($(this).attr('data-stack')));
+        last = 0;
+
+
+        // if($(ui, draggable).attr('[data-block]')) > parseInt(last){
+        //     $(ui.draggable).draggable()
+        // }
+      }
     });
   });
 });
