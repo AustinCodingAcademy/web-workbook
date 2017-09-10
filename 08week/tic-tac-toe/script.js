0fb01e9f5d01e5e780c6b9db91246148f1717c8f
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-09-10 13:04:44 -0500

    Towers of Hanoi update plus customer satisfaction rough draft

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -3,8 +3,6 @@
 $(document).ready(function() {
   // Put app logic in here
 
-  var playerTurn = ``;
-
   $('[data-cell]').on('click', function(){
     if (playerTurn % 2 === 0){
       $(this).text('X');
@@ -15,48 +13,5 @@ $(document).ready(function() {
       //checkVictory('O');
     }
     playerTurn++;
-
-  })
-/*
-  function checkVictory(){
-
-   if ($('[data-cell="0"]').text() === 'X' && $('[data-cell="1"]').text() === 'X' &&     $('[data-cell="2"]').text() === 'X');
-
-    else if ($('[data-cell="0"]').text() === 'O' && $('[data-cell="1"]').text() === 'O' &&     $('[data-cell="2"]').text() === 'O');
-
-    if ($('[data-cell="3"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="5"]').text() === 'X');
-
-    else if ($('[data-cell="3"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="5"]').text() === 'O');
-
-    if ($('[data-cell="6"]').text() === 'X' && $('[data-cell="7"]').text() === 'X' && $('[data-cell="8"]').text() === 'X');
-
-    else if ($('[data-cell="6"]').text() === 'O' && $('[data-cell="7"]').text() === 'O' && $('[data-cell="8"]').text() === 'O');
-
-    if ($('[data-cell="0"]').text() === 'X' && $('[data-cell="3"]').text() === 'X' && $('[data-cell="6"]').text() === 'X');
-
-    else if ($('[data-cell="0"]').text() === 'O' && $('[data-cell="3"]').text() === 'O' && $('[data-cell="6"]').text() === 'X');
-
-    if ($('[data-cell="1"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="7"]').text() === 'X');
-
-    else if ($('[data-cell="1"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="7"]').text() === 'X');
-
-    if ($('[data-cell="2"]').text() === 'X' && $('[data-cell="5"]').text() === 'X' && $('[data-cell="8"]').text() === 'X');
-
-    else if ($('[data-cell="2"]').text() === 'O' && $('[data-cell="5"]').text() === 'O' && $('[data-cell="8"]').text() === 'O');
-
-    if ($('[data-cell="0"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="8"]').text() === 'X');
-
-    else if ($('[data-cell="0"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="8"]').text() === 'O');
-
-    if ($('[data-cell="2"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="6"]').text() === 'X');
-
-    else if ($('[data-cell="2"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="6"]').text() === 'O');
-
-
-
-
-    console.log(checkVictory);
-  }
-*/
-
+  });
 });
--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,20 +2,80 @@
 
 $(document).ready(function() {
   // Put app logic here
-  $('[data-block]').draggable({
-    revert: "invalid"
-  });
+  //$('[data-block]').draggable({
+  //  revert: "invalid"
+  //});
 
-  $('[data-stack]').droppable({
-    drop: function(event, ui){
+//  $('[data-stack]').droppable({
+//    drop: function(event, ui){
   //    var drag = $(ui.draggable).attr('data-block');
   //      var last = $($(this).children().last()[0]);
   //      if (parseInt($(drag)) > parseInt($(last))); {
   //        $(ui.draggable).draggable('option', 'revert', true);
-      $(ui.draggable).appendTo(this).attr('style', 'position: relative');
+//      $(ui.draggable).appendTo(this).attr('style', 'position: relative');
   //      }
+//    }
+//  });
+
+var endGame = false;
+  var $stacks = $('[data-stack]');
+  var $blocks = $('[data-block]');
+  var $movableBlocks = $('[data-block]:last-child');
+  $movableBlocks.addClass("movable");
+  $blocks.draggable({
+    revert: true
+  });
+
+  $stacks.droppable({
+    accept: ".movable",
+    drop: function(event, ui) {
+      if (!endGame) {
+        if (goodToDrop($(this), ui.draggable)) {
+          ui.draggable.draggable('option', 'revert', false);
+          $(this).append(ui.draggable.detach());
+          ui.draggable.css({
+            'top': 0,
+            'left': 0
+          });
+          //reset
+          $movableBlocks = $('[data-block]:last-child');
+          $('[data-block]').removeClass("movable");
+          $movableBlocks.addClass("movable");
+          $blocks.draggable({
+            revert: true
+          });
+
+          winState();
+        }
+      } else {
+        reset();
+      }
     }
   });
+
+  function goodToDrop($stack, $block) {
+    var $lastBlock = $stack.children().last();
+    if (parseInt($block.attr("data-block")) < parseInt($lastBlock.attr("data-block")) || $stack.children().length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function winState() {
+    if ($('[data-stack="3"]').children().length === 4) {
+      $('#announce-game-won').html("YOU'RE A WINNER!");
+      endGame = true;
+    }
+  }
+
+  function reset() {
+    $('[data-stack="1"]').html('<div data-block="100"></div><div data-block="75"></div><div data-block="50"></div><div data-block="25"></div>');
+    $('[data-stack="2"]').empty();
+    $('[data-stack="3"]').empty();
+    $('#announce-game-won').empty();
+    endGame = false;
+  }
 
 
 });
