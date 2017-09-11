9cc7cca1b5d1177fc3841443f7cfe1b80093822a
Patrick Creamer <patrickseancreamer@gmail.com>
2017-09-11 15:39:20 -0500

    Towers of Hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,27 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
-});
+  var $block = null;
+  var largeblock
+  $('[data-stack]').click(function() {
+    if (!$block) {
+      $block = $(this).children().last().detach();
+    } else {
+      // console.log($block.attr("data-block"),$(this).children().last().attr("data-block"))
+      if(Number(($block.attr("data-block")))>Number($(this).children().last().attr("data-block"))) {
+        alert("BAD MOVE");
+      } else {
+      $(this).append($block);
+      $block = null;
+      // console.log($("#winner").children().length);
+    }
+    }
+    endgame();
+  })
+  //win condition
+  function endgame() {
+    if ($("#winner").children().length==4) {
+    setTimeout(function(){ alert("You win!"); location.reload(); }, 250);
+    }
+  }
+})
