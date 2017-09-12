a20ca8283445f3752fecd493801c488b7973f503
Cory Hayes <Home@Corys-MacBook-Air.local>
2017-09-12 18:23:43 -0500

    uploading all worked on projects in week 9 and checkpoint 3

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,14 +1,47 @@
+
 'use strict';
 
 $(document).ready(function() {
 
-  let $rings = $('[data-stack]').children().last();
+  let currentStack;
+  let playerMove = null;
+  let discNumber = 4;
 
-  console.log($rings);
+  $('[data-stack]').click(function(){
+    if(playerMove === null && $(this).children().length > 0){
+      playerMove = $(this).children().last().detach();
+      currentStack = $(this).attr('data-stack');
+    } else if($(this).children().length > 0){
+      if(parseInt($(this).children().last().attr('data-block')) < parseInt(playerMove.attr('data-block'))){
+        playerMove.appendTo($('[data-stack]').eq(currentStack - 1));
+        playerMove = null;
+      } else if(parseInt($(this).children().last().attr('data-block')) > parseInt(playerMove.attr('data-block'))){
+        $(this).append(playerMove);
+        playerMove = null;
+      }
+    } else if($(this).children().length === 0){
+      playerMove.appendTo($(this));
+      playerMove = null;
+    }
+
+    if($('[data-stack]').eq(2).children().length === discNumber){
+      $('#announce-game-won').append('<h1>YOU WIN!</h1>');
+    }
+
+  });
+
+/*
+  console.log(parseInt($(this).attr('[data-block][0]')));
+
+  // if(parseInt($rings) < 100){
+  //   $('[data-block]').draggable({
+  //       revert: 'invalid'
+  //   });
+  // }
 
   $('[data-block]').draggable({
-      revert: 'invalid'
-  });
+    revert: 'invalid'
+  })
 
   $('[data-stack]').each(function(){
     $(this).droppable({
@@ -16,6 +49,8 @@ $(document).ready(function() {
 
         let drag = $(ui.draggable).attr('data-block');
         let last = $($(this).children().last()).attr('data-block');
+
+        console.log(drag + " " + last);
 
         if(parseInt(drag) > parseInt(last)){
           $(ui.draggable).draggable('option', 'revert', true);
@@ -25,4 +60,5 @@ $(document).ready(function() {
       }
     });
   });
+  */
 });
