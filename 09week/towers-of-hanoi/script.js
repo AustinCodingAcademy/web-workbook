e6d798af12b4fff2dd4119af15c8de1cde2bdc43
maxfielde gaj <maxfieldegaj@gmail.com>
2017-09-11 20:48:19 -0500

    week 9

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,33 @@
 'use strict';
+    $(document).ready(function() {
+      var $block = null;
+// On Click!!
+      $("[data-stack]").click(function(){
+        var $children = $(this).children();
+        var $lastChild = $children.last();
 
-$(document).ready(function() {
-  // Put app logic here
+        if ($block == null) {
+          $block = $lastChild.detach();
+        } else {
+          if ( $lastChild.data("block") < $block.data("block")) {
+// Invalid Move!!
+            alert("Invalid Move");
+          } else {
+            $(this).append($block);
+            $block = null;
+          }
+    }
+    // Chicken Dinner!!
+    if ( $("[data-stack='3']").children().length >= 4 )
+    {
+      alert("Winner, Winner, Chicken Dinner!!");
+    }
+  });
 });
+
+//
+// if ($block) {
+//   $(this).append($block) {
+//     if ('data-stack').children().last().height(<$this)
+//   }
+// }
