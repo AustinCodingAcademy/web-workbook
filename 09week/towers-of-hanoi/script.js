d0c21fafb0d8c291499fd9895ad7a054e1e95b79
Jon <contact@jongorman.top>
2017-09-12 14:35:25 -0500

    :bunny: 119

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,37 +1,39 @@
 'use strict';
 
 $(document).ready(function() {
-//   var sound = document.createElement('audio');
-//   sound.setAttribute('src', 'moon.mp3');
-//   $.get();
-//   // Put app logic here
-//   $('[data-stack]').click(function() {
-//     sound.play();
-//     // played = true;
-//     // console.log("Playing");
-// });
+  //   var sound = document.createElement('audio');
+  //   sound.setAttribute('src', 'moon.mp3');
+  //   $.get();
+  //   // Put app logic here
+  //   $('[data-stack]').click(function() {
+  //     sound.play();
+  //     // played = true;
+  //     // console.log("Playing");
+  // });
   // $('#song').get(0).play();
   $('.draggable').draggable({
     revert: "invalid"
   });
-  $('[data-stack]').droppable( {
-    drop: function(event, ui){
+  $('[data-stack]').droppable({
+    drop: function(event, ui) {
       var drag = $(ui.draggable).attr('data-block');
       var last = $(this).children().last().attr('data-block');
-      if(parseInt(drag) > parseInt(last)){
-        $(ui.draggable).draggable({ revert: true });
+      if (parseInt(drag) > parseInt(last)) {
+        $(ui.draggable).draggable({
+          revert: true
+        });
         checkWin()
 
-      }else {
+      } else {
         $(ui.draggable).appendTo(this).attr('style', 'position: relative');
         checkWin();
 
-    }
+      }
     }
 
-});
+  });
 
-  function checkWin(){
+  function checkWin() {
     let $total1 = $('[data-stack="1"]').children().length;
     let $total2 = $('[data-stack="2"]').children().length;
     let $total3 = $('[data-stack="3"]').children().length;
