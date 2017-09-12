713c586a6671b0450c9ff5a8c3dd62bfae56935f
russell-cole <rjcole85@gmail.com>
2017-09-11 20:46:24 -0500

    commit

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  var pegClicked = ""
+  $("[data-peg]").click(function() {
+    console.log("clicked")
+  if (pegClicked == "") {
+    pegClicked = $(this).data( "peg" );
+    console.log( "firstClick " + pegClicked );
+    console.log($( this ).first().css( "width" ));
+  } else if ( $( this ).children().length = 0 ) {
+    console.log("secondClick");
+  }
+  /*  $( "peg1" ).data( "peg" );*/
+  })
+
 });
+
+
+// $( "[data-peg='" + pegClicked + "']" ).first().css("width") > $( this ).first().css( "width" ) |
--- cp3/script.js
+++ cp3/script.js
@@ -1,8 +1,27 @@
 'use strict';
-// This probably won't work but let's give it a fuckin try
+/*Ready?*/
 $(document).ready(function() {
-  function click() {
-    var $confusing =
-    $('.xSmall').removeClass('.xSmall').replaceWith("");
+  // Put app logic here
+var $block = null;
+/*Target the last child of a stack*/
+  $("[data-stack]").click(function(){
+    var $children = $(this).children();
+    var $lastChild = $children.last();
+/*Detach the last child and append it to an empty stack or one with a bigger block than the one you've detached*/
+    if ($block == null) {
+      $block = $lastChild.detach();
+    } else {
+      if ( $lastChild.data("block") < $block.data("block")) {
+        alert("Invalid Move");
+      } else {
+        $(this).append($block);
+        $block = null;
+      }
+    }
+    /*You win if either of the last 2 stacks have all four blocks*/
+    if ( $("[data-stack='2']").children().length >= 4 || $("[data-stack='3']").children().length >= 4 )
+  {
+    alert("You Win!!");
   }
-})
+  });
+});
