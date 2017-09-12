366e3526c56ba30c27c52e66bccdbffbb7b1cf7d
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-09-11 22:02:04 -0500

    Indexing updates

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -3,15 +3,18 @@
 $(document).ready(function() {
   // Put app logic in here
 
-  $('[data-cell]').on('click', function(){
-    if (playerTurn % 2 === 0){
-      $(this).text('X');
-      //checkVictory('X');
-    } else {
-    //player 2's turn (O)
-      $(this).text('O');
-      //checkVictory('O');
-    }
-    playerTurn++;
-  });
+  var turnCount = 0;
+
+        $('[data-cell]').on('click', function(){
+              if (turnCount % 2 === 0){
+                $(this).text('X');
+                //checkVictory('X');
+              } else {
+             //player 2's turn (O)
+                $(this).text('O');
+                //checkVictory('O');
+              }
+            turnCount++;
+
+        });
 });
