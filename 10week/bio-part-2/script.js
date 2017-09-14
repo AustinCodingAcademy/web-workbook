be4be9aed042d99b86ed19a618834528dfd3b3f2
edvidaurri <edward@vidaurrimail.com>
2017-09-14 12:54:56 -0500

    lint update

--- 10week/bio-part-2/script.js
+++ 10week/bio-part-2/script.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
   var c = document.getElementById("myCanvas");
   var ctx = c.getContext("2d");
   ctx.beginPath();
-  ctx.arc(95,50,40,0,2*Math.PI);
+  ctx.arc(95, 50, 40, 0, 2 * Math.PI);
   ctx.stroke();
-  var grd = ctx.createLinearGradient(0,0,200,0);
-grd.addColorStop(0,"red");
+  var grd = ctx.createLinearGradient(0, 0, 200, 0);
+  grd.addColorStop(0, "red");
 
-// Fill with gradient
-ctx.fillStyle = grd;
-ctx.fillRect(20,10,150,80);
+  // Fill with gradient
+  ctx.fillStyle = grd;
+  ctx.fillRect(20, 10, 150, 80);
 
 });
