1456301585ab61dca8639fdde6df94540c7ba89c
edvidaurri <edward@vidaurrimail.com>
2017-09-14 16:39:10 -0500

    ChkPtup

--- 10week/bio-part-2/script.js
+++ 10week/bio-part-2/script.js
@@ -1,17 +1,43 @@
 'use strict';
 
 $(document).ready(function() {
+var c = document.getElementById('canv');
+var $ = c.getContext('2d');
 
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  ctx.beginPath();
-  ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-  ctx.stroke();
-  var grd = ctx.createLinearGradient(0, 0, 200, 0);
-  grd.addColorStop(0, "red");
 
-  // Fill with gradient
-  ctx.fillStyle = grd;
-  ctx.fillRect(20, 10, 150, 80);
+var col = function(x, y, r, g, b) {
+  $.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+  $.fillRect(x, y, 1,1);
+}
+var R =
+ function(x, y, t) {
+  return( Math.floor(192 + 64*Math.cos( (x*x-y*y)/300 + t )) );
+}
 
+var G = function(x, y, t) {
+  return( Math.floor(192 + 64*Math.sin( (x*x*Math.cos(t/4)+y*y*Math.sin(t/3))/300 ) ) );
+}
+
+var B = function(x, y, t) {
+  return( Math.floor(192 + 64*Math.sin( 5*Math.sin(t/9) + ((x-100)*(x-100)+(y-100)*(y-100))/1100) ));
+}
+
+var t = 0;
+
+var run = function() {
+  for(x=0;x<=35;x++) {
+    for(y=0;y<=35;y++) {
+      col(x, y, R(x,y,t), G(x,y,t), B(x,y,t));
+    }
+  }
+  t = t + 0.120;
+  window.requestAnimationFrame(run);
+}
+
+run();
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 100, 100);
 });
