d0ad3b23d5f639cda58b6c1b4c41d823365cdde9
Kenneth Vandivort <kennethvandivort@Kenneths-MacBook-Pro.local>
2017-09-14 00:03:52 -0500

    Checkpoint 3 Towers of Hanoi rewrite

--- Checkpoint/Checkpoint-3/script.js
+++ Checkpoint/Checkpoint-3/script.js
@@ -1,89 +1,107 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
-  //$('[data-block]').draggable({
-  //  revert: "invalid"
-  //});
 
-//  $('[data-stack]').droppable({
-//    drop: function(event, ui){
-  //    var drag = $(ui.draggable).attr('data-block');
-  //      var last = $($(this).children().last()[0]);
-  //      if (parseInt($(drag)) > parseInt($(last))); {
-  //        $(ui.draggable).draggable('option', 'revert', true);
-//      $(ui.draggable).appendTo(this).attr('style', 'position: relative');
-  //      }
-//    }
-//  });
+  main();
 
-var endGame = false;
-  var $stacks = $('[data-stack]');
-  var $blocks = $('[data-block]');
-  var $movableBlocks = $('[data-block]:last-child');
-  $movableBlocks.addClass("movable");
-  $blocks.draggable({
-    revert: true
-  });
+  function getLastElement($el) {
+    var $dataBlock = $el.find('div[data-block]');
+    if ($dataBlock.length > 0) {
+      return $dataBlock.last();
+    }
+    return undefined;
+  }
 
-  $stacks.droppable({
-    accept: ".movable",
-    drop: function(event, ui) {
-      if (!endGame) {
-        if (goodToDrop($(this), ui.draggable)) {
-          ui.draggable.draggable('option', 'revert', false);
-          $(this).append(ui.draggable.detach());
-          ui.draggable.css({
-            'top': 0,
-            'left': 0
-          });
-          //reset
-          $movableBlocks = $('[data-block]:last-child');
-          $('[data-block]').removeClass("movable");
-          $movableBlocks.addClass("movable");
-          $blocks.draggable({
-            revert: true
-          });
+  function getElementSize($el) {
+    var dataBlockValue = parseInt($el.attr('data-block'));
+    if (isNaN(dataBlockValue)) {
+      throw "Invalid number";
+    }
+    return dataBlockValue;
+  }
 
-          winState();
+  function isDataStackSizeEmpty(i) {
+    var $dataBlock = $("div[data-stack='" + i + "']").find('div[data-block]');
+    return $dataBlock.length === 0;
+  }
+
+  function checkIfWon() {
+    //if datastack1 and datastack2 are empty, then it's a winstate
+    if (isDataStackSizeEmpty(1) && isDataStackSizeEmpty(2)) {
+      $('#announce-game-won').text("You're a Winner!!!");
+    }
+  }
+
+  function isMoveAllowed($stack, $block) {
+    var $dataBlock = getLastElement($stack);
+    if ($dataBlock) {
+      var dataBlockValue = getElementSize($dataBlock);
+      var blockElementValue = getElementSize($block);
+      return blockElementValue < dataBlockValue;
+    }
+    return true;
+  }
+
+  function push($stack, $block) {
+    $block.draggable('option', 'revert', false);
+    //resetting position when dropping blocks
+    $block.css({
+      'top': 0,
+      'left': 0
+    });
+
+    $stack.append($block.detach());
+    // reset moveable blocks
+    $('div[data-block]').removeClass('allowMove');
+    $('div[data-block]:last-child').addClass('allowMove');
+    //make block draggable again
+    $block.draggable({
+      revert: true
+    });
+
+    checkIfWon();
+  }
+
+  function clickClear() {
+    //find all datastacks and empty them
+    $('div[data-stack]').empty();
+    var stack1 = $('div[data-stack="1"]');
+
+    for (var i = 1; i <= 4; i++) {
+      stack1.prepend('<div data-block="' + i + '"></div>');
+    }
+    //make blocks draggable again
+    setBlockDraggable();
+    $('#announce-game-won').text('');
+  }
+
+  function setStackDroppable() {
+    // find all the stacks
+    $('div[data-stack]').droppable({
+      accept: '.allowMove',
+      drop: function(event, ui) {
+        var $stack = $(this);
+        var $block = ui.draggable;
+        if (isMoveAllowed($stack, $block)) {
+          push($stack, ui.draggable);
         }
-      } else {
-        reset();
       }
-    }
-  });
-
-  function goodToDrop($stack, $block) {
-    var $lastBlock = $stack.children().last();
-    if (parseInt($block.attr("data-block")) < parseInt($lastBlock.attr("data-block")) || $stack.children().length === 0) {
-      return true;
-    } else {
-      return false;
-    }
+    });
   }
 
-  function winState() {
-    if ($('[data-stack="3"]').children().length === 4) {
-      $('#announce-game-won').html("YOU'RE A WINNER!");
-      endGame = true;
-    }
+  function setBlockDraggable() {
+    //find last-child in datablock and allow move
+    $('div[data-block]:last-child').addClass("allowMove");
+    $('div[data-block]').draggable({
+      revert: true
+    });
   }
 
-  function reset() {
-    $('[data-stack="1"]').html('<div data-block="100"></div><div data-block="75"></div><div data-block="50"></div><div data-block="25"></div>');
-    $('[data-stack="2"]').empty();
-    $('[data-stack="3"]').empty();
-    $('#announce-game-won').empty();
-    endGame = false;
-  }
+  function main() {
+    setStackDroppable();
+    setBlockDraggable();
 
+    $('#clear').click(clickClear);
+  }
 
 });
-
-  //make blocks draggable/droppable
-  //rules: smaller on top(children/lastchild)
-  //winstate
-  //can't drop outside of the blocks
-  //(if event is larger that lastchild)
-  // reset lastchild => var lastchild=$(this.children().last()[0];
-  //convert data-block to value parseInt()
