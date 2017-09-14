8a56d122ec7d378944c1daa4fefb551efd03f66d
edvidaurri <edward@vidaurrimail.com>
2017-09-14 16:56:27 -0500

    lint run

--- 10week/bio-part-2/script.js
+++ 10week/bio-part-2/script.js
@@ -1,43 +1,43 @@
 'use strict';
 
 $(document).ready(function() {
-var c = document.getElementById('canv');
-var $ = c.getContext('2d');
+  var c = document.getElementById('canv');
+  var $ = c.getContext('2d');
 
 
-var col = function(x, y, r, g, b) {
-  $.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-  $.fillRect(x, y, 1,1);
-}
-var R =
- function(x, y, t) {
-  return( Math.floor(192 + 64*Math.cos( (x*x-y*y)/300 + t )) );
-}
-
-var G = function(x, y, t) {
-  return( Math.floor(192 + 64*Math.sin( (x*x*Math.cos(t/4)+y*y*Math.sin(t/3))/300 ) ) );
-}
-
-var B = function(x, y, t) {
-  return( Math.floor(192 + 64*Math.sin( 5*Math.sin(t/9) + ((x-100)*(x-100)+(y-100)*(y-100))/1100) ));
-}
-
-var t = 0;
-
-var run = function() {
-  for(x=0;x<=35;x++) {
-    for(y=0;y<=35;y++) {
-      col(x, y, R(x,y,t), G(x,y,t), B(x,y,t));
-    }
+  var col = function(x, y, r, g, b) {
+    $.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+    $.fillRect(x, y, 1, 1);
   }
-  t = t + 0.120;
-  window.requestAnimationFrame(run);
-}
+  var R =
+    function(x, y, t) {
+      return (Math.floor(192 + 64 * Math.cos((x * x - y * y) / 300 + t)));
+    }
 
-run();
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+  var G = function(x, y, t) {
+    return (Math.floor(192 + 64 * Math.sin((x * x * Math.cos(t / 4) + y * y * Math.sin(t / 3)) / 300)));
+  }
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 100, 100);
+  var B = function(x, y, t) {
+    return (Math.floor(192 + 64 * Math.sin(5 * Math.sin(t / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100)));
+  }
+
+  var t = 0;
+
+  var run = function() {
+    for (x = 0; x <= 35; x++) {
+      for (y = 0; y <= 35; y++) {
+        col(x, y, R(x, y, t), G(x, y, t), B(x, y, t));
+      }
+    }
+    t = t + 0.120;
+    window.requestAnimationFrame(run);
+  }
+
+  run();
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = 'green';
+  ctx.fillRect(10, 10, 100, 100);
 });
