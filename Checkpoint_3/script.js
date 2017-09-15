6a424629ed511eafdaf3f0ffb55b6c6279fef497
worthingtonml <worthington.ml@gmail.com>
2017-09-14 19:51:08 -0500

    wtf

--- Checkpoint_3/script.js
+++ Checkpoint_3/script.js
@@ -2,34 +2,69 @@
 
 $(document).ready(function() {
   var $block = null;
+  var gameover = false;
+  var $stacks = $('[data-stack]');
+  var $blocks = $('[data-block]');
 
-  $('[data-block]').draggable({
-      revert:"invalid"
-    });
-      $('[data-stack]').droppable({
-        drop: function(event,ui) {
-          var drag = $(ui.draggable).attr('data-block');
-          accept: 'data-block'
-          var last = $(this).children().last().attr('data-block');
+      //since only last child of every stack is allowed to make the move, give them class movable
 
-          console.log(parseInt(last,10));
-          console.log(parseInt(drag,10));
+      var $movableBlocks = $('[data-block]:last-child');
+      $movableBlocks.addClass("movable");
 
-          if(parseInt(drag)>parseInt(last)){
-          $(ui.draggable).draggable('option','revert',true);
-        }else{
-          $(ui.draggable).detach().css({top: 0, left: 0}).appendTo(this);
-     }
-   }
-  });
+      $blocks.draggable({
+        revert: true
+      });
 
-        //   var colors = ['#ff0000', '#00ff00', '#0000ff'];
-        // var random_color = colors[Math.floor(Math.random() * colors.length)];
-        // document.getElementById('title').style.color = random_color;
-        // $('#title').css('color', random_color);
+      //All blocks can move, but only the last child can be dropped.
 
-      var stackThree = $('[data-stack="3"]').children().length;
-      if(stackThree === 4) {
-        alert("You Win");
-    };
+      $stacks.droppable({
+      accept: ".movable",
+      drop: function(event, ui) {
+        if(!gameover){
+          if(goodToDrop($(this), ui.draggable)) {
+            ui.draggable.draggable('option', 'revert', false);
+            $(this).append(ui.draggable.detach());
+            ui.draggable.css({'top': 0, 'left': 0});
+            //reset things
+            $movableBlocks = $('[data-block]:last-child');
+            $('[data-block]').removeClass("movable");
+            $movableBlocks.addClass("movable");
+            $blocks.draggable({
+              revert: true
+            });
+            //checkForWin
+            checkForWin();
+          }
+        }
+        else {
+          resetGame();
+    }
+  }
+});
+
+function goodToDrop($stack, $block) {
+  var $last_block = $stack.children().last();
+  if( parseInt($block.attr("data-block"))<parseInt($last_block.attr("data-block")) || $stack.children().length===0) {
+     return true;
+  }
+  else {
+     return false;
+  }
+}
+
+  function checkForWin() {
+  if($('[data-stack="3"]').children().length===4) {
+    $('#announce-game-won').html("Nice Job!");
+    gameover=true;
+
+  }
+}
+      function resetGame() {
+        $('[data-stack="1"]').html('<div data-block="100"></div><div data-block="75"></div><div data-block="50"></div><div data-block="25"></div>');
+        $('[data-stack="2"]').empty();
+        $('[data-stack="3"]').empty();
+        $('#announce-game-won').empty();
+        gameover = false;
+      }
+
   });
