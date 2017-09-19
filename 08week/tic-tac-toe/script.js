e826a792f53f9fe0fa278dbfac9a49c69bef81f7
Andrew Thomas <athomas3@trinity.edu>
2017-09-18 19:47:07 -0500

    .

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
 $(document).on('ready', function() {
+
+  $('.0').click(function(event){
+    alert('add symbol here')
+  });
   // Put app logic in here
 });
---
+++ 10week/bio-part-2/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+$(document).ready(function() {
+//var c = $('#myCanvas');
+//var ctx = c.getContext("2d");
+//ctx.beginPath();
+// ctx.arc(95,50,40,0,2*Math.PI);
+// ctx.stroke();
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95,50,40,0,2*Math.PI);
+ctx.stroke();
+
+
+
+
+
+});
