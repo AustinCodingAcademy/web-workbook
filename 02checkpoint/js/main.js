e9998218831814c739cf81767773ed8709217250
Hector Garcia <hektor11@utexas.edu>
2017-10-24 16:48:26 -0500

    Adding conditional to check if on mobile

--- 02checkpoint/js/main.js
+++ 02checkpoint/js/main.js
@@ -1,11 +1,13 @@
 
 $(document).ready(function()
 {
-
-  $('#fullpage').fullpage({
-    verticalCentered: true,
-  	anchors: ['Home','About','Products'],
-  	menu: '#myMenu'
-  });
-
+  //Check if mobile to not trigger fullpage.js
+  var isMobileDevice = (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+  if(!isMobileDevice){
+    $('#fullpage').fullpage({
+      verticalCentered: true,
+      anchors: ['Home','About','Products'],
+      menu: '#myMenu'
+    });
+  }
 });
