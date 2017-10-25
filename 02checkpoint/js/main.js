c293e9d5ba8cc4bfd9358e64c5caed70c5835c72
Hector Garcia <hektor11@utexas.edu>
2017-10-24 23:35:34 -0500

    fixed indentation and updated jquery script

--- 02checkpoint/js/main.js
+++ 02checkpoint/js/main.js
@@ -4,11 +4,11 @@ $(document).ready(function()
   //Check if mobile to not trigger fullpage.js
   //var isMobileDevice = (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
   //if(!isMobileDevice){
-    $('#fullpage').fullpage({
-      verticalCentered: true,
-      anchors: ['Home','About','Products'],
-      menu: '#myMenu',
-      responsiveWidth: 1100
-    });
+  $('#fullpage').fullpage({
+    verticalCentered: true,
+    anchors: ['Home','About','Products'],
+    menu: '#myMenu',
+    responsiveWidth: 1100
+  });
   //}
 });
