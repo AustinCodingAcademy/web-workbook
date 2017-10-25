b91559ce5e276f6b1482e21e7ea2cbdb0f894926
Hector Garcia <hektor11@utexas.edu>
2017-10-24 23:28:08 -0500

    removed isMobile conditional

--- 02checkpoint/js/main.js
+++ 02checkpoint/js/main.js
@@ -2,13 +2,13 @@
 $(document).ready(function()
 {
   //Check if mobile to not trigger fullpage.js
-  var isMobileDevice = (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-  if(!isMobileDevice){
+  //var isMobileDevice = (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+  //if(!isMobileDevice){
     $('#fullpage').fullpage({
       verticalCentered: true,
       anchors: ['Home','About','Products'],
       menu: '#myMenu',
       responsiveWidth: 1100
     });
-  }
+  //}
 });
