10b1fceb849ce2daaf5958c436b0fa88a993dcb6
Julie Adams <lareinajul@htmail>
2017-11-07 18:29:59 -0600

    changes

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -7,10 +7,12 @@ var title = document.getElementsByTagName('h1');
 var subtitle = '<h2>You have '+ listOfItems.length + 'items in your shopping cart</h2>';
 var d1 = document.getElementByID('title');
 d1.insertAdjacentHTML ('afterend', subtitle);
+
 });
-var list = document.getElementById("myList");
-list.removeChild(list.childNodes[0]);
-var node = document.createElement("LI");
-var textnode = document.createTextNode("Water");
-node.appendChild(textnode);
-document.getElementById("myList").appendChild(node);
+function addItem() {
+var newitem=prompt ('Enter new item');
+//format my newitem as a list item element
+newitem='<li>' + newitem + '</li>';
+var list=document.getElementByID('list');
+list.insertAdjacentHTML('afterend', newitem);
+}
