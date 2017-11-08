eac695a1bbd3daccece5ae54523d56d72a3cb81c
Julie Adams <lareinajul@htmail>
2017-11-07 19:13:20 -0600

    dompracticechanges

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -4,15 +4,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
 var listOfItems = document.getElementsByTagName('li');
 alert('Number of Items: ' + listOfItems.length );
 var title = document.getElementsByTagName('h1');
-var subtitle = '<h2>You have '+ listOfItems.length + 'items in your shopping cart</h2>';
+var subtitle = '<h2 id="subtitle">You have '+ listOfItems.length + 'items in your shopping cart</h2>';
 var d1 = document.getElementByID('title');
+var subtitlePresent = document.getElementsByTagName('title');
+if (subtitlePresent=="")
+else {
+  subtitlePresent.innerHTML= subtitleText;
+}
 d1.insertAdjacentHTML ('afterend', subtitle);
-
 });
 function addItem() {
 var newitem=prompt ('Enter new item');
 //format my newitem as a list item element
 newitem='<li>' + newitem + '</li>';
 var list=document.getElementByID('list');
-list.insertAdjacentHTML('afterend', newitem);
+var listItem= list.lastElementChild;
+listItem.insertAdjacentHTML('afterend', newitem);
 }
+function
