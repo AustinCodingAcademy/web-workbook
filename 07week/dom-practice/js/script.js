7682faf77a59e84ec6da124ad7098a2824697e05
scottgray1 <smg0144@hotmail.com>
2017-11-07 18:53:28 -0600

    updating js code

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,39 +1,30 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-
-    var listOfItems = document.getElementsByTagName('li');
-    alert('Number of items ' + listOfItems.length);
+document.addEventListener('DOMContentLoaded', function(event) {
 })
 
-  function addItem(){
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
-    var li = document.createElement("li");
-    li.setAttribute('id',candidate.value);
-    li.appendChild(document.createTextNode(candidate.value));
-    ul.appendChild(li);
+function addItem(){
+  var ul = document.getElementById('dynamic-list');
+  var item = document.getElementById('item');
+  var li = document.createElement('li');
+  li.setAttribute('id',item.value);
+  li.appendChild(document.createTextNode(item.value));
+  ul.appendChild(li);
 
-    // alert new items in cart
-    alert("Number of Items " + document.getElementsByTagName("li").length);
-    var SubTitle = document.getElementById('UpdatedItems');
-    SubTitle.innerHTML = document.getElementsByTagName("li").length;
+  // alert new items in cart
+  alert('Number of Items ' + document.getElementsByTagName('li').length);
+  var SubTitle = document.getElementById('UpdatedItems');
+  SubTitle.innerHTML = document.getElementsByTagName('li').length;
 }
 
 function removeItem(){
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
-    var item = document.getElementById(candidate.value);
-    ul.removeChild(item);
+  var ul = document.getElementById('dynamic-list');
+  var item = document.getElementById('item');
+  var item = document.getElementById(item.value);
+  ul.removeChild(item);
 
-    // alert new items in cart
-    alert("Number of Items " + document.getElementsByTagName("li").length);
-    var SubTitle = document.getElementById('UpdatedItems');
-    SubTitle.innerHTML = document.getElementsByTagName("li").length;
-
-
-}
-function addThis() {
-  console.log('hello');
-
+  // alert new items in cart
+  alert('Number of Items ' + document.getElementsByTagName('li').length);
+  var SubTitle = document.getElementById('UpdatedItems');
+  SubTitle.innerHTML = document.getElementsByTagName('li').length;
 }
