dc91773994d3be309c8dff479bdd3efcebedb1e6
Ty Sweet <tysweet77@gmail.com>
2017-11-11 10:05:33 -0600

    fixed js file

--- 06week/dice/js/script.js
+++ 06week/dice/js/script.js
@@ -20,6 +20,12 @@ function report() {
   let diceHtml = document.getElementsByClassName('die'); // this puts all the individual die HTML elements in diceHtml
   var pair = 0;
   var three = 0;
+  var one = 0;
+  var two = 0;
+  var three = 0;
+  var four = 0;
+  var five = 0;
+  var six = 0;
   for (var dieVal = 1; dieVal < 7; dieVal++) {  // we set up a nested loop, first we check for all the 1s, then 2s, etc
     let howManyDice = 0; // we zero out our counter
     for (var die = 0; die < 5; die++) {  // we create the inner loop that cycles through the rolled dice
@@ -27,19 +33,40 @@ function report() {
         howManyDice++; // if so, we add one to the count
       }
     }
+    //
+    if (howManyDice===1) {
+      if(dieVal===1) {
+        one=true;
+      }
+      else if(dieVal===2) {
+        two=true;
+      }
+      else if(dieVal===3) {
+        three=true;
+      }
+      else if(dieVal===4) {
+        four=true;
+      }
+      else if(dieVal===5) {
+        five=true;
+      }
+      else if(dieVal===6) {
+        six=true;
+      }
+    }
+    //
     if (howManyDice===2) { // we check after the counting is done if it is equal to '2'
       pair++;
       document.getElementById("report").innerHTML += "There are a pair of "+dieVal+"s<br>";  // if yes, we report a pair
     }
 // two of dice have the same points, like 3 6 5 6 1 - called pair: example solved for you. Complete the other cases below:
 //
-     if (howManyDice===3) {
+    if (howManyDice===3) {
        three++;
        document.getElementById('report').innerHTML += 'There are three '+dieVal+'s<br>';
      }
 // three of dice have the same points, like 2 4 5 4 4 - called three;
-//
-    if (howManyDice===4) {
+       if (howManyDice===4) {
        document.getElementById('report').innerHTML += 'There are four '+dieVal+'s<br>';
      }
 // four of dice have the same points, like 1 4 1 1 1 - called four;
@@ -47,18 +74,24 @@ function report() {
     if (howManyDice===5) {
       document.getElementById('report').innerHTML += 'YACHT!!';
     }
+  }
 // all five dice have the same points, like 2 2 2 2 2 - called yacht;
 //
     if (pair===2) {
-      document.getElementById('report').innerHTML += 'There are two pairs <br>';
+      document.getElementById('report').innerHTML += 'There are two pairs<br>';
     }
 // two pairs at once, like 3 6 5 3 5 - called two-pairs;
     if (pair===1 && three===1) {
-      document.getElementById('report').innerHTML += 'FULL HOUSE';
+      document.getElementById('report').innerHTML += 'FULL HOUSE<br>';
     }
-  }
 // pair and three at once, like 1 6 6 1 6 - called full-house;
+    if(one===true && two===true && three===true && four===true && five===true) {
+      document.getElementById('report').innerHTML += 'Small Straight<br>';
+    }
 // sequence from 1 to 5, like 2 4 3 5 1 - called small-straight;
+    if(two===true && three===true && four===true && five===true && six===true) {
+      document.getElementById('report').innerHTML += 'Big Straight<br>';
+    }
 // sequence from 2 to 6, like 6 3 4 2 5 - called big-straight.
 }
 
