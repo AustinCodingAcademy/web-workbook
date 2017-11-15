13d0086d7b6f1aa92c8e973a28608ac19326b791
Ty Sweet <tysweet77@gmail.com>
2017-11-14 19:33:45 -0600

    fixed js file

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,41 +1,37 @@
-'use strict';
+function addNumber(num) {
+  document.getElementById("results").value += num;
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  function addNumber(num) {
-    document.getElementById("results").value += num;
-  }
+function clearResults() {
+  document.getElementById("results").value = "";
+}
 
-  function clearResults() {
-    document.getElementById("results").value = "";
-  }
+function addition() {
+  document.getElementById("results").value += "+";
+}
 
-  function addition() {
-    document.getElementById("results").value += "+";
-  }
+function subtraction() {
+  document.getElementById("results").value += "-";
+}
 
-  function subtraction() {
-    document.getElementById("results").value += "-";
-  }
+function multiplication() {
+  document.getElementById("results").value += "*";
+}
 
-  function multiplication() {
-    document.getElementById("results").value += "*";
-  }
+function division() {
+  document.getElementById("results").value += "/";
+}
 
-  function division() {
-    document.getElementById("results").value += "/";
-  }
+function plusMinus() {
+  document.getElementById("results").value += "*-1";
+  equals();
+}
 
-  function plusMinus() {
-    document.getElementById("results").value += "*-1";
-    equals();
-  }
+function equals() {
+  document.getElementById("results").value = eval(document.getElementById("results").value);
+}
 
-  function equals() {
-    document.getElementById("results").value = eval(document.getElementById("results").value);
-  }
-
-  function deleteLast() {
-    var current=document.getElementById("results").value;
-    document.getElementById("results").value = current.substring(0, current.length-1);
-  }
-});
+function deleteLast() {
+  var current=document.getElementById("results").value;
+  document.getElementById("results").value = current.substring(0, current.length-1);
+}
