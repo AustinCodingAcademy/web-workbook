4b4b2ef3129ac9961471c2faae484539c146bf2b
Julie Adams <lareinajul@htmail>
2017-11-14 19:36:32 -0600

    tictactoechanges

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -11,37 +11,43 @@ if (isBoxFilled){
 alert ("Box is filled! Choose Another One!!");
 }else{
 var pattern = determinePattern();
-placeTurn(box,pattern);
+box.text(pattern); //set board
 if (checkForWinner(pattern)){
 alert("Player" + player + "won!");
 }
 changePlayer();
   }
+});
+function checkBoxForPrevTurn(box){
+if (box.innerHTML === 'x' || box.innerHTML === "0")
+return true;
+else
+return false;
 }
-})
-function checkForWinner(pattern){
-  // insert code here to check the current determinePattern
-  // against the board
-  // check first horizontal row
-  if (board [0].innerHTML === pattern && board [1].innerHTML === pattern && board)
-  return true;
-  // 2nd row
+// insert code here to check the current determinePattern
+// against the board
+// check first horizontal row
+if (board [0].innerHTML === pattern && board [1].innerHTML === pattern && board)
+return true;
+// 2nd row
 { else if (board [3].innerHTML === pattern && board [4].innerHTML === pattern)}
-  // 3rd row
-  // diagonals
-  // columns
-  // nothing matched
-  return false;
+// 3rd row
+// diagonals
+// columns
+// nothing matched
+return false;
+function placeTurn (box, pattern){
+box.text (pattern);
 }
 function reset (){
-  // change the player to 1
-  player =1;
-  //clear the innerHTML for all nodes on the Board
-  for (let i = 0; i<9; i++){
-    board [i].innerHTML='';
+// change the player to 1
+player =1;
+//clear the innerHTML for all nodes on the Board
+for (let i = 0; i<9; i++){
+board [i].innerHTML='';
   }
 }
 $('#clear').click(function(){
-  reset();
+reset();
 })
 });
