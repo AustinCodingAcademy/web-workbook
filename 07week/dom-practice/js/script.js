5772ef14b2947e722be34fccf1d64b96e04d54ed
hampsonn <hampsonn@sualumni.net>
2017-11-14 20:53:21 -0600

    um

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,26 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+function primeCheck() {
+  var input = document.getElementById("primeField").value;
+  let prime = true;
+  for (let i = 2; i <= Math.sqrt(input); i++) {
+    if (input % i == 0) {
+      prime = false;
+      break;
+    }
+  }
+  alert(prime);
+}
+
+function palindromeCheck() {
+  var pCheck = document.getElementById("primeField").value;
+  // var pLength = pCheck.length;
+  var pArray = pCheck.split();
+  var pReverse = pArray.reverse();
+
+  if (pArray === pReverse) {
+    alert("Yes!")
+  } else {
+    alert("No.")
+  }
+}
