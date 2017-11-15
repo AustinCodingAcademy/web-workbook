35e279f27e62d2437ec576a842bb7e472782a2a7
hampsonn <hampsonn@sualumni.net>
2017-11-14 19:15:14 -0600

    calculator

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,36 @@
-'use strict';
+function addNumber(num) {
+  document.getElementById("results").value += num;
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+function clearResults() {
+  document.getElementById("results").value = "";
+}
+
+function addition() {
+  document.getElementById("results").value += "+";
+}
+
+function subtraction() {
+  document.getElementById("results").value += "-";
+}
+
+function multiplication() {
+  document.getElementById("results").value += "*";
+}
+
+function inverse() {
+  document.getElementById("results").value = results.value*-1;
+}
+
+function division() {
+  document.getElementById("results").value += "/";
+}
+
+function equals() {
+  document.getElementById("results").value = eval(document.getElementById("results").value);
+}
+
+function deleteLast() {
+  var current=document.getElementById("results").value;
+  document.getElementById("results").value = current.substring(0, current.length-1);
+}
