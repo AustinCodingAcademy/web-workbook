a30e77ab840e47d078a663a8217694c7fcf5fd62
James <jamesaeberwine@gmail.com>
2017-11-14 20:14:59 -0600

    dice and calc

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,41 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
+  function addNumber(num) {
+  document.getElementById("results").value += num;
+}
+
+function clearResults() {
+  document.getElementById("results").value = "";
+}
+
+function addition() {
+  document.getElementById("results").value += "+";
+}
+
+function subtraction() {
+  document.getElementById("results").value += "-";
+}
+
+function multiplication() {
+  document.getElementById("results").value += "*";
+}
+
+function division() {
+  document.getElementById("results").value += "/";
+}
+
+function plusminus() {
+  document.getElementById("results").value += "*-1";
+  equals();
+}
+
+function equals() {
+  document.getElementById("results").value = eval(document.getElementById("results").value);
+}
+
+function deleteLast() {
+  var current=document.getElementById("results").value;
+  document.getElementById("results").value = current.substring(0, current.length-1);
+}
 });
