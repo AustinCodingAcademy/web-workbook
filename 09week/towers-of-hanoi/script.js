09cce3e85f9485b103f3f9df1c170416fa8e1351
jamiefox5 <32071306+jamiefox5@users.noreply.github.com>
2017-11-15 08:21:57 -0600

    tic tac toe

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,24 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  var $block = null; // detach block and set it//
+
+    $('[data-stack]').click(function() {
+      if (!$block) {
+        $block = $(this).children().last().detach();
+      } else {
+        //size of last one//
+      if ($(this).children().length === 0 ||
+        parseInt($block.attr('datablock')) <
+        parseInt($(this).children().last().attr('data-block'))) {
+        $(this).append($block);
+        $block = null;// reset ready to try again since you set it//
+      } else {
+        alert("You can't place that block there!");
+      }
+    }
+    if ($(this).attr('data-stack')==='3' && $(this).children().length === 4){
+      $('#announce-game-won').innerHTML = "You won!";
+    }
+})
 });
