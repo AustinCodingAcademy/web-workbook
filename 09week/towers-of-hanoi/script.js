439de71dbca75dcd7ec710d5fc987aad2f1390f3
nickalantitus <nickalantitus@gmail.com>
2017-11-16 13:42:56 -0600

    towers

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,23 +1,70 @@
 'use strict';
 
-  $(document).ready(function() {
-  var $block = null;
+$(function() {
 
-  $('[data-stack]').click(function() {
-    if (!$block) {
-      $block = $(this).children().last().detach();
-    } else {
-    if  ($(this).children().length === 0 ||
-    parseInt($block.attr('data-block')) <
-    parseInt($(this).children().last().attr('data-block')));
-      $(this).append($block);
-      $block = null;
-    } else {
-      alert("You can't palce that block there!");
-    }
+  var gameover = false;
+  var $stacks = $('[data-stack]');
+  var $blocks = $('[data-block]');
+  //since only last child of every stack is allowed to make the move, give them class movable
+  var $movableBlocks = $('[data-block]:last-child');
+  $movableBlocks.addClass("movable");
 
-    this.attr('data-stack') ==='3' && (this).children().length === 4){
-      $( '#announce-game-won').text = "You won!";
+  //any block is draggable, but they rever to original position unless condition is met
+  $blocks.draggable({
+    revert: true
+  });
+
+  $stacks.droppable({
+    accept: ".movable",
+    drop: function(event, ui) {
+      if (!gameover) {
+        if (goodToDrop($(this), ui.draggable)) {
+          ui.draggable.draggable('option', 'revert', false);
+          $(this).append(ui.draggable.detach());
+          ui.draggable.css({
+            'top': 0, 
+            'left': 0
+          });
+          //reset things
+          $movableBlocks = $('[data-block]:last-child');
+          $('[data-block]').removeClass("movable");
+          $movableBlocks.addClass("movable");
+          $blocks.draggable({
+            revert: true
+          });
+          //checkForWin
+          checkForWin();
+        }
+      } else {
+        resetGame();
+      }
     }
-  })
+  });
+
+  function goodToDrop($stack, $block) {
+    var $last_block = $stack.children().last();
+    if (parseInt($block.attr("data-block")) < parseInt($last_block.attr("data-block")) ||
+    $stack.children().length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function checkForWin() {
+    if ($('[data-stack="3"]').children().length === 4) {
+      $('#announce-game-won').html("You Won!");
+      gameover = true;
+
+    }
+  }
+
+  function resetGame() {
+    $('[data-stack="1"]').html('<div data-block="100"></div><div data-block="75"></div><div data-block="50"></div><div data-block="25"></div>');
+    $('[data-stack="2"]').empty();
+    $('[data-stack="3"]').empty();
+    $('#announce-game-won').empty();
+    gameover = false;
+  }
+
 });
