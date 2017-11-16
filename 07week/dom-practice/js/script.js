c7beffef1d1b0e0103393cd9f528de619e6cdbbc
Agata Drozdowska <Agata@Agatas-MacBook-Pro.local>
2017-11-15 21:02:34 -0600

    MOD project

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,18 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+document.addEventListener("DOMContentLoaded", myAlert);
+
+function myAlert() {
+
+  var item = document.getElementsByTagName("LI");
+  alert("There are " + item.length + " list items on the page");
+}
+
+
+window.onload = function() {
+
+var heading = document.createElement("h2");
+var headingText = document.createTextNode("You have _ items in your shopping cart");
+heading.appendChild(headingText);
+document.getElementById("heading").appendChild(heading);
+}
