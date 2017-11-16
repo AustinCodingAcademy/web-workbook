50ce6a0e4ab874f99d21493f265801c0efb5e27c
Eric Summers <tilcboy44@gmail.com>
2017-11-16 03:56:43 -0600

    dom-practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+document.addEventListener("DOMContentLoaded", alertFunction);
+document.addEventListener("DOMContentLoaded", addH2);
+
+function alertFunction() {
+  var x = document.getElementsByTagName('LI');
+  //alert (x.length);
+}
+
+function addH2() {
+  var x = document.getElementsByTagName('LI').length;
+  var para = document.createElement("H2");
+  var node = document.createTextNode('You have _ items in your shopping cart');
+  para.appendChild(node);
+
+  var element = document.getElementById("header1");
+  element.appendChild(para);
+}
