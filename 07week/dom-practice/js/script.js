b60fdc48405d1ff8cdf3f1c55afc98cf90f18408
Nathan Simmons <nathanhbsimmons@gmail.com>
2017-11-16 20:48:32 -0600

    DOM practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,36 @@
-'use strict';
+// 'use strict';
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//
+window.onload = function() {
+  var list = document.getElementsByTagName("ul");
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+  var count = "The cart has " + list[0].children.length + " items in it";
+
+  // alert(count);
+
+  // var newListItem = document.createElement('li');
+  // newListItem.innerHTML='Coleman campin stove';
+
+  var newElement = document.createElement('h2');
+  newElement.innerHTML = 'You have ' + list[0].children.length + ' in your shopping cart';
+
+  document.body.appendChild(newElement);
+
+};
+
+var listItem = document.getElementById('items');
+
+function changeText2() {
+
+  var addItem = document.getElementById('addItem').value;
+  var entry = document.createElement('li');
+  entry.appendChild(document.createTextNode(addItem));
+  listItem.appendChild(entry);
+
+}
+//
+//
+//
+//
+// });
