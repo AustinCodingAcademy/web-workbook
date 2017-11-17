6cf885d3821202b3f535e90de8f6bb2cf9addf10
Ty Sweet <tysweet77@gmail.com>
2017-11-16 21:13:22 -0600

    DOM

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -4,25 +4,28 @@
 
 window.onload = function() {
   var list = document.getElementById('list');
-  // list.insertAdjacentHTML('beforeend', '<li>List Item</li>');
-
   var count = 'You have ' +list.children.length+ ' items in your shopping cart';
   window.alert(count);
-
-
+  itemsInCart();
 };
 
 function addItem() {
+  var remove = document.getElementsByTagName('h2')[0];
+  document.body.removeChild(remove)
   var listItem = document.createElement('li');
   var input = document.getElementById('add');
   listItem.innerHTML = input.value;
   input.value = '';
   document.getElementById('list').appendChild(listItem);
+  itemsInCart();
+};
+
+function itemsInCart() {
+  var list = document.getElementById('list');
   var newElement = document.createElement('h2');
   newElement.innerHTML = 'You have ' +list.children.length+ ' items in your shopping cart';
   document.getElementsByTagName('h1')[0].appendChild(newElement);
-};
-
+}
 
 
 // });
