1dcf7736a50a3388d9408596d64e1d8ce9f1954e
unknown <hunter-conley@sbcglobal.net>
2017-11-22 17:52:38 -0600

    back up

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,20 @@
 'use strict';
+window.onload = function() {
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+  var items = document.getElementsByTagName('ul');
+  var count = 'You have '+items[0].children.length+' items in your cart.';
+  // alert(count);
+
+  var newElement = document.createElement('h2');
+  newElement.innerHTML = count;
+  document.body.appendChild(newElement);
+
+}
+
+function addItems() {
+
+}
+
+function removeItems() {
+
+}
