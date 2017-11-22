5643d130d79d5733aaf8b235009baf01883e2bcd
Hector Garcia <hektor11@utexas.edu>
2017-11-22 14:02:22 -0600

    refactored jquery code

--- 03checkpoint/js/main.js
+++ 03checkpoint/js/main.js
@@ -1,6 +1,14 @@
 var score = 0;
 var multiplier = 0;
 var rate = 1000;
+//var buttonIds = [['#babyLuigi','#babyLuigiSound', "babyLuiguis"], ['#yoshi','#yoshiSound', 'yoshis'],
+//  ['#babyBowser','#babyBowserSound', 'babyBowsers']];
+var upgradeDictionary = {
+  babyMario: ['#coin', 'babyMarios'],
+  babyLuigi: ['#babyLuigiSound', '#babyLuigis'],
+  yoshi: ['#yoshiSound', '#yoshis'],
+  babyBowser: ['#babyBowserSound', '#babyBowser']
+};
 
 $(document).ready(function() {
   //increment score variable when baby mario image is clicked
@@ -10,46 +18,57 @@ $(document).ready(function() {
     scoreUpdater();
   });
 
-  $('#babyLuigi').click(function(){
-    if(score >= 10){
-      score = score - 10;
-      multiplier++;
-      $('#babyLuigiSound').trigger('play');
-      $('#babyLuigis').append('<img style="width:30px;" src="./assets/img/babyLuigi.png"/>');
-    }
-    else{
-      playDenySound();
-    }
-  });
-
-  $('#yoshi').click(function(){
-    if(score >= 100){
-      score = score - 100;
-      multiplier = multiplier + 10;
-      $('#yoshiSound').trigger('play');
-      $('#yoshis').append('<img style="width:30px;" src="./assets/img/yoshi.png"/>');
-    }
-    else{
-      playDenySound();
-    }
-  });
-
-  $('#babyBowser').click(function(){
-    if(score >= 1000){
-      score = score - 1000;
-      multiplier = multiplier + 20;
-      $('#babyBowserSound').trigger('play');
-      $('#babyBowsers').append('<img style="width:30px;" src="./assets/img/babyBowser.png"/>');
-    }
-    else{
-      playDenySound();
-    }
+  $('.upgrade-buttons').click(function(){
+    buttonClicked(this.id);
   });
 });
 
 function scoreUpdater(){
   $('#score').text(score);
   $('#coinsPerSecond').text(multiplier);
+}
+
+function buttonClicked(id){
+  var buttonId = id;
+
+  switch(buttonId){
+    case 'babyLuigi':
+      if(score >= 10){
+        score = score - 10;
+        multiplier++;
+        playSounds(buttonId);
+        addImage(buttonId);
+      }
+      else{
+        playDenySound();
+      }
+      break;
+    case 'yoshi':
+      if(score >= 100){
+        score = score - 100;
+        multiplier = multiplier + 10;
+        playSounds(buttonId);
+        addImage(buttonId);
+      }
+      else{
+        playDenySound();
+      }
+      break;
+    case 'babyBowser':
+      if(score >= 1000){
+        score = score - 1000;
+        multiplier = multiplier + 20;
+        playSounds(buttonId);
+        addImage(buttonId);
+      }
+      else{
+        playDenySound();
+      }
+      break;
+    default:
+      alert('Error');
+      break;
+  }
 }
 
 window.setInterval(superCharge, rate);
@@ -62,6 +81,14 @@ function superCharge(){
 
 function playSound(){
   $('#coin').trigger('play');
+}
+
+function playSounds(id){
+  $(upgradeDictionary[id][0]).trigger('play');
+}
+
+function addImage(id){
+  $(upgradeDictionary[id][1]).append('<img style="width:30px;" src="./assets/img/'+ id +'.png"/>');
 }
 
 function playDenySound(){
