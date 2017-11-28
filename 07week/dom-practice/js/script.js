8c6896357cd2ebd5a1fd5f0f7f2f986ebe7d0e63
Amanda Silva <amandasilva@Amandas-MacBook-Pro.local>
2017-11-28 05:11:02 -0600

    updated tictactoe

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,48 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
+
+    var listOfItems = document.getElementsByTagName('li');
+
+    alert('Number of items ' + listOfItems.length);
+
+    var subtitle = '<h2>You have ' + listOfItems.length + ' items in your shopping cart</h2>';
+
+    var d1 = document.getElementById('title');
+    d1.insertAdjacentHTML('afterend', subtitle);
+
+
 });
+
+
+
+function addItem() {
+  var newitem = prompt ('Enter new item');
+  //format my newitem as a list item element
+  newitem = '<li>' +newitem + '</li>';
+  var list = document.getElementById('list');
+  var listItem = list.lastElementChild;
+  listItem.insertAdjacentHTML('afterend', newitem);
+}
+
+//recalculate the items in the list
+funchtion subtitle() {
+  var listOfItems = document.getElementsByTagName('li');
+  alert('Number of items ' + listOfItems.length);
+  var subtitleText = '<h2 id="subtitle">You have ' + listOfItems.length + ' items in your shopping cart</h2>';
+
+  var d1 = document.getElementById('title');
+  var subtitlePresent = document.getElementById('subtitle');
+  if (subtitlePresent == ""){
+    d1.insertAdjacentHTML('afterend', subtitleText);
+  } else {
+    subtitlePresent.innerHTML = subtitleText;
+  }
+
+
+}
+
+
+//    var title = document.getElementsByTagName('h1');
+
+//    title += '<h2>You have ' + listOfItems.length + ' items in your shopping cart</h2>';
