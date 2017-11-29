2c9ad87ee9a395b8f49ce2ae6d9c30755ee2ad1a
Wade Cosby (CE CEN) <1303596@wholefoods.com>
2017-11-28 18:24:20 -0600

    added the ability to update the counter

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -8,15 +8,15 @@ window.onload = function() {
   var textMsg = "You have " + count + " items in your shopping cart."
   alert(count);
 
-  // Add a <h2> to the page under the title that says 'You have _ items in your shopping cart'. Do not use html to do this. Use JavaScript.
+// Add a <h2> to the page under the title that says 'You have _ items in your shopping cart'. Do not use html to do this. Use JavaScript.
   var whereIsH = document.body.childNodes[0];
   var addMsg = document.createElement('h2');
   addMsg.innerHTML = textMsg;
   whereIsH.appendChild(addMsg);
   addListButtons();
 
-  // Create the ability to add more items to the list using JavaScript.
-  // Create the ability to remove an item from the shopping cart using javascript.
+// Create the ability to add more items to the list using JavaScript.
+// Create the ability to remove an item from the shopping cart using javascript.
   function addListButtons() {
     var theList = document.body.children[1];
     var listEnd = theList.children.length - 1;
@@ -24,9 +24,7 @@ window.onload = function() {
     addButton.innerHTML = "Add to Your Cart";
     addButton.onclick = addStuff;
 
-
     document.body.children[0].append(addButton);
-    // console.log(listCollection);
     var listCollection = Array.from(document.getElementsByTagName('li'));
     for (var i = 0; i < listCollection.length; i++) {
       var removeButton = document.createElement('button');
@@ -35,12 +33,13 @@ window.onload = function() {
       removeButton.onclick = removeStuff;
       listCollection[i].append(removeButton);
     }
-    //This function will remove the line of which the x button is positioned
+//This function will remove the line of which the x button is positioned
     function removeStuff() {
       var parentLi = this.parentNode
       parentLi.remove();
+      clearMsg();
     }
-
+//This function adds a new item
     function addStuff() {
       var response = prompt("What do you want to add");
       var newItem = document.createElement('li');
@@ -48,8 +47,19 @@ window.onload = function() {
       newItem.innerHTML = response;
       theList.append(newItem);
       newItemRemove();
+      clearMsg();
     }
-
+//Updates the list count message
+      function clearMsg() {
+        var topMsg = document.getElementsByTagName('h2');
+        var list = document.getElementsByTagName('ul');
+        var count = list[0].children.length
+        var textMsg = "You have " + count + " items in your shopping cart."
+        topMsg[0].innerHTML = "";
+        topMsg[0].innerHTML = textMsg;
+        alert(count);
+      }
+//adds the remove button to new items
     function newItemRemove() {
       var listCollection = Array.from(document.getElementsByTagName('li'));
       var x = listCollection.length-1;
