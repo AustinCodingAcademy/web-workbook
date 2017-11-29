eda361a0b58d40a7c9dfd2c63e12bff120ce2240
Ty Sweet <tysweet77@gmail.com>
2017-11-28 18:15:34 -0600

    fixed cart issue

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -4,14 +4,15 @@
 
 window.onload = function() {
   var list = document.getElementById('list');
+  var newElement = document.createElement('h2');
+  var header = document.getElementsByTagName('h2');
+  newElement.innerHTML = 'You have ' +list.children.length+ ' items in your shopping cart';
+  document.getElementsByTagName('h1')[0].appendChild(newElement);
   var count = 'You have ' +list.children.length+ ' items in your shopping cart';
   window.alert(count);
-  itemsInCart();
 };
 
 function addItem() {
-  // var remove = document.getElementsByTagName('h2')[0];
-  // document.body.removeChild(remove)
   var listItem = document.createElement('li');
   var input = document.getElementById('add');
   listItem.innerHTML = input.value;
@@ -24,13 +25,14 @@ function deleteItem() {
   var listItem = document.getElementById('add');
   document.getElementById('list').removeChild(list.lastChild);
   itemsInCart();
+
 };
 
 function itemsInCart() {
   var list = document.getElementById('list');
-  var newElement = document.createElement('h2');
-  newElement.innerHTML = 'You have ' +list.children.length+ ' items in your shopping cart';
-  document.getElementsByTagName('h1')[0].appendChild(newElement);
+  var header = document.getElementsByTagName('h2');
+  header[0].innerHTML = "";
+  header[0].innerHTML = 'You have ' +list.children.length+ ' items in your shopping cart';
 };
 
 // });
