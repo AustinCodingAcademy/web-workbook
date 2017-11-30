946862aefcb543ef7ab4d0706ef21443d5e141c3
jamiefox5 <32071306+jamiefox5@users.noreply.github.com>
2017-11-30 15:44:20 -0600

    bio try 2

--- 10week/bio-part-2/js/script.js
+++ 10week/bio-part-2/js/script.js
@@ -6,8 +6,10 @@ var canvas = document.getElementById("myCanvas");
      ctx.shadowOffsetY = 10
      ctx.shadowBlur = 10;
      ctx.font = "50px Acme";
+
      var gradient = ctx.createLinearGradient(0, 0, 150, 100);
      gradient.addColorStop(0, "rgb(255, 0, 128)");
      gradient.addColorStop(1, "rgb(255, 153, 51)");
      ctx.fillStyle = gradient;
      ctx.fillText("Jamie Fox", 30, 60);
+     
