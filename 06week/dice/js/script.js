0eba3ede8eaec7926bcf405aceac815b65699d39
ChrisL20 <ctopher.lu44@gmail.com>
2017-11-30 17:05:41 -0600

    dice

--- 06week/dice/js/script.js
+++ 06week/dice/js/script.js
@@ -18,6 +18,14 @@ roll();
 function report() {
   document.getElementById("report").innerHTML = ""; // clear out the report box
   let diceHtml = document.getElementsByClassName('die'); // this puts all the individual die HTML elements in diceHtml
+  let twopair = 0;
+  let threepair = 0;
+  var one = 0;
+  var two = 0;
+  var three = 0;
+  var four = 0;
+  var five = 0;
+  var six = 0;
   for (var dieVal = 1; dieVal < 7; dieVal++) {  // we set up a nested loop, first we check for all the 1s, then 2s, etc
     let howManyDice = 0; // we zero out our counter
     for (var die = 0; die < 5; die++) {  // we create the inner loop that cycles through the rolled dice
@@ -25,10 +33,60 @@ function report() {
         howManyDice++; // if so, we add one to the count
       }
     }
-    if (howManyDice===2) { // we check after the counting is done if it is equal to '2'
-      document.getElementById("report").innerHTML += "There are a pair of "+dieVal+"s<br>";  // if yes, we report a pair
+
+    if (howManyDice===1){
+    if (dieVal===1) {
+      one++;
     }
+    else if (dieVal===2) {
+      two++;
+    }
+    else if (dieVal===3) {
+      three++;
+    }
+    else if (dieVal===4) {
+      four++;
+    }
+    else if (dieVal===5) {
+      five++;
+    }
+    else if (dieVal===6) {
+      six++;
+    }
+    }
+
+
+
+
+    if (howManyDice===2) {
+       document.getElementById("report").innerHTML += "There is a pair of "+dieVal+"s<br>";
+      twopair++;
+    }
+
+      if (howManyDice===3) {
+       document.getElementById("report").innerHTML += "There is a three of "+dieVal+"s<br>";
+        threepair++;
+    }
+     if (howManyDice===4) {
+       document.getElementById("report").innerHTML += "There is a four of "+dieVal+"s<br>";
+    }
+      if (howManyDice===5) {
+       document.getElementById("report").innerHTML += "There is a yacht of "+dieVal+"s<br>";
+    }
+
   }
+    if (twopair===2){
+      document.getElementById("report").innerHTML += "There is a two pair<br>"
+    }
+     if (twopair===1 && threepair===1){
+        document.getElementById("report").innerHTML += "There is a full house<br>"
+      }
+    if (one && two && three && four && five){
+      document.getElementById("report").innerHTML += "There is a small straight<br>"
+    }
+  if (two && three && four && five && six){
+      document.getElementById("report").innerHTML += "There is a big straight<br>"
+    }
 
 // two of dice have the same points, like 3 6 5 6 1 - called pair: example solved for you. Complete the other cases below:
 
--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,36 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+
+window.onload = function() {
+  var list = document.getElementById('list');
+  var count = 'You have ' +list.children.length+ ' items in your shopping cart';
+  window.alert(count);
+  itemsInCart();
+};
+
+function addItem() {
+  // var remove = document.getElementsByTagName('h2')[0];
+  // document.body.removeChild(remove)
+  var listItem = document.createElement('li');
+  var input = document.getElementById('add');
+  listItem.innerHTML = input.value;
+  input.value = '';
+  document.getElementById('list').appendChild(listItem);
+  itemsInCart();
+};
+
+function deleteItem() {
+  var listItem = document.getElementById('add');
+  document.getElementById('list').removeChild(list.lastChild);
+  itemsInCart();
+};
+
+function itemsInCart() {
+  var list = document.getElementById('list');
+  var newElement = document.createElement('h2');
+  newElement.innerHTML = 'You have ' +list.children.length+ ' items in your shopping cart';
+  document.getElementsByTagName('h1')[0].appendChild(newElement);
+};
+
+// });
