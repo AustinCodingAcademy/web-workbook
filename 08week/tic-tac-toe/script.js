0ea30c8d998a99600b99d668b33ba6ca6124c663
Nathan Simmons <nathanhbsimmons@gmail.com>
2017-11-30 20:33:13 -0600

    tictactoe complete

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,20 +1,60 @@
 'use strict';
 
 $(document).ready(function() {
-  let tic = 'x';
+
+  let turn = 'x';
   $('[data-cell]').click(function() {
-    $(this).text(tic);
-    if (tic === 'o') {
-      tic = 'x';
-    } else {
-      tic = 'o';
+    if ($(this).text() === '') {
+      $(this).text(turn);
+      checkWin();
+      checkTie();
+      if (turn === 'x') {
+        turn = 'o';
+      } else {
+        turn = 'x';
+      }
+
+
     }
   });
 
-  $('#clear').click(function() {
+  function clear() {
     $('[data-cell]').empty()
-  })
+    $('#announce-winner').empty()
+    turn = 'x'
+  }
 
+  $('#clear').click(clear);
+
+
+
+  function checkWin() {
+    if (($('[data-cell=1]').text() === turn && $('[data-cell=4]').text() === turn && $('[data-cell=7]').text() === turn)
+    || ($('[data-cell=2]').text() === turn && $('[data-cell=5]').text() === turn && $('[data-cell=8]').text() === turn)
+    || ($('[data-cell=0]').text() === turn && $('[data-cell=4]').text() === turn && $('[data-cell=8]').text() === turn)
+    || ($('[data-cell=6]').text() === turn && $('[data-cell=4]').text() === turn && $('[data-cell=2]').text() === turn)
+    || ($('[data-cell=0]').text() === turn && $('[data-cell=1]').text() === turn && $('[data-cell=2]').text() === turn)
+    || ($('[data-cell=3]').text() === turn && $('[data-cell=4]').text() === turn && $('[data-cell=5]').text() === turn)
+    || ($('[data-cell=6]').text() === turn && $('[data-cell=7]').text() === turn && $('[data-cell=8]').text() === turn)
+    || ($('[data-cell=0]').text() === turn && $('[data-cell=3]').text() === turn && $('[data-cell=6]').text() === turn)) {
+    }
+
+    $('#announce-winner').text(`player ${turn} wins`)
+
+  }
+
+  function checkTie() {
+    let tie = true
+    $('[data-cell]').each(function(){
+      if ($(this).text() == '') {
+        tie = false
+      }
+    })
+
+    if (tie) {
+      $('#announce-winner').text(`tie`)
+    }
+  }
 
 
 
