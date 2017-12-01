27734479a10971c0ceb383c9dddffe2616e831e7
Nathan Simmons <nathanhbsimmons@gmail.com>
2017-11-30 18:54:17 -0600

    tictactoe game

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+  let tic = 'x';
+  $('[data-cell]').click(function() {
+    $(this).text(tic);
+    if (tic === 'o') {
+      tic = 'x';
+    } else {
+      tic = 'o';
+    }
+  });
+
+  $('#clear').click(function() {
+    $('[data-cell]').empty()
+  })
+
+
+
+
+
+
 });
