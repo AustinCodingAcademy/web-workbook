e877f2568bbdbdd56aed2489a399317753a0e874
Garret Toombs <garrettoombs@Garrets-MacBook-Pro.local>
2017-11-30 19:16:14 -0600

    tictac

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,27 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+  let playerTurn = 'X';
+  $('.column').click(function() {
+    $(this).text('playerTurn');
+    if (playerTurn === 'O') {
+      playerTurn = 'X';
+    } else {
+      playerTurn = 'O';
+    }
+  });
 });
+
+
+
+
+
+
+
+
+// put 'x' or 'o' on board
+// alternate players
+// check for win
+// 8 ways to win
+// declare winner/tie
+// clear board
