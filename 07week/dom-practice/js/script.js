f6f4795fc59025046b892d829790405c40b2e89d
Shelby Inloes <shelbyinloes@Shelbys-MacBook-Air.local>
2017-12-01 14:33:01 -0600

    tictactoe

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,67 +1,100 @@
 'use strict';
 
 
-window.onload = function() {
-  var list = document.getElementsByTagName('ul');
-  var count = 'This page has ' + list[0].children.length + ' list items';
-  alert(count);
+// window.onload = function() {
+//   var list = document.getElementsByTagName('ul');
+//   var count = 'This page has ' + list[0].children.length + ' list items';
+//   alert(count);
+//
+//   var newElement = document.createElement('h2');
+//   newElement.innerHTML = "You have " + list[0].children.length + " items in your shopping cart.";
+//   document.getElementsByTagName('h1')[0].appendChild(newElement);
+// }
 
-  var newElement = document.createElement('h2');
-  newElement.innerHTML = "You have " + list[0].children.length + " items in your shopping cart.";
-  document.getElementsByTagName('h1')[0].appendChild(newElement);
+
+window.onload = function() {
+  //TASK 1:  Alert the user to how many list items there are on the page.
+  let listCountMessage = setListMessage();
+  alert(listCountMessage);
+
+  //TASK 2:  Add a <h2> to the page under the title that says 'You have _ items in your shopping cart'. Do not use html to do this. Use JavaScript.
+  createListCountHeaderElement();
+  updateListCountHeaderMessage();
+
+  //Part of Task 4
+  addRemoveButtonsToExistingItems();
+
+  //Used to implement task 3
+  createNewItemTextInput();
+
 }
 
+//Used for task 1 & 2
+function setListMessage() {
+  let list = document.getElementById("shopping-list");
+  return "This page has " + list.children.length + " items in the shopping cart.";
+}
 
+//Used for task 2
+function createListCountHeaderElement(message) {
+  let listHeading = document.createElement("h2");
+  document.getElementsByTagName("h1")[0].appendChild(listHeading);
+  listHeading.setAttribute("id", "list-heading");
+}
 
-//THIS CODE WORKS - BUT I DONT UNDERSTAND IT
-// function get_todos() {
-//     var todos = new Array;
-//     var todos_str = localStorage.getItem('todo');
-//     if (todos_str !== null) {
-//         todos = JSON.parse(todos_str);
-//     }
-//     return todos;
-// }
-//
-// function add() {
-//     var task = document.getElementById('task').value;
-//
-//     var todos = get_todos();
-//     todos.push(task);
-//     localStorage.setItem('todo', JSON.stringify(todos));
-//
-//     show();
-//
-//     return false;
-// }
-//
-// function remove() {
-//     var id = this.getAttribute('id');
-//     var todos = get_todos();
-//     todos.splice(id, 1);
-//     localStorage.setItem('todo', JSON.stringify(todos));
-//
-//     show();
-//
-//     return false;
-// }
-//
-// function show() {
-//     var todos = get_todos();
-//
-//     var html = '<ul>';
-//     for(var i=0; i<todos.length; i++) {
-//         html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
-//     };
-//     html += '</ul>';
-//
-//     document.getElementById('todos').innerHTML = html;
-//
-//     var buttons = document.getElementsByClassName('remove');
-//     for (var i=0; i < buttons.length; i++) {
-//         buttons[i].addEventListener('click', remove);
-//     };
-// }
-//
-// document.getElementById('add').addEventListener('click', add);
-// show();
+//Used for task 2, 3, & 4
+function updateListCountHeaderMessage() {
+  let listAlert = setListMessage();
+  let listHeading = document.getElementById("list-heading");
+  listHeading.innerHTML = listAlert;
+}
+
+//Used for task 4
+function addRemoveButtonsToExistingItems() {
+  let list = document.getElementById("shopping-list");
+  for (let i = 0; i < list.children.length; i++) {
+    addRemoveButton(list.children[i]);
+  }
+}
+
+//Used for task 3 & 4 ()
+function addRemoveButton(appendElement) {
+  let removeButton = document.createElement("button");
+  removeButton.innerHTML = "Remove";
+  removeButton.onclick = deleteItem;
+  appendElement.appendChild(removeButton);
+  updateListCountHeaderMessage();
+}
+
+//Used for task 3
+function createNewItemTextInput() {
+  let textField = document.getElementById("item-text");
+  textField.setAttribute("placeholder", "type item to add");
+
+  let addButton = document.getElementById("add-button");
+  addButton.innerHTML = "Add Item";
+  addButton.onclick = addItem;
+
+  document.getElementById("add-item").appendChild(textField);
+  document.getElementById("add-item").appendChild(addButton);
+}
+
+//TASK 3:  Create the ability to add more items to the list using JavaScript.
+function addItem(event) {
+  event.preventDefault();
+
+  let newItemText = document.getElementById("item-text").value;
+  let newItem = document.createElement("li");
+  newItem.innerHTML = newItemText;
+  document.getElementById("shopping-list").appendChild(newItem);
+  updateListCountHeaderMessage();
+
+  addRemoveButton(newItem);
+  document.getElementById('add-item').reset();
+}
+
+//TASK 4: Create the ability to remove an item from the shopping cart using javascript.
+function deleteItem() {
+  this.parentNode.remove(this);
+  updateListCountHeaderMessage();
+}
--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,51 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
-});
+    var player = 1;
+    $('.square').on('click', function(event) {
+        var squareSelected = $(this);
+        if(squareSelected.hasClass('ex') || squareSelected.hasClass('oh')) {
+          alert('Oops! Select a blank square.');
+        } else {
+          if(player === 1) {
+            squareSelected.addClass('ex');
+            if(announceWinner('ex')) {
+              alert('Congrats! Player ' + player + ' has won!');
+            } else {
+            player = 2;
+          }
+        } else {
+          squareSelected.addClass('oh');
+          if(announceWinner('oh')) {
+              alert('Congrats! Player ' + player + ' has won!');
+            } else {
+          player = 1;
+        }
+      }
+    }
+  });
+
+
+
+function announceWinner(symbol) {
+      if($('.zero').hasClass(symbol) && $('.one').hasClass(symbol) && $('.two').hasClass(symbol)) {
+        return true;
+    } else if($('.three').hasClass(symbol) && $('.four').hasClass(symbol) && $('.five').hasClass(symbol)) {
+      return true;
+    }else if($('.six').hasClass(symbol) && $('.seven').hasClass(symbol) && $('.eight').hasClass(symbol)) {
+      return true;
+    }else if($('.zero').hasClass(symbol) && $('.four').hasClass(symbol) && $('.six').hasClass(symbol)) {
+      return true;
+    }else if($('.one').hasClass(symbol) && $('.four').hasClass(symbol) && $('.seven').hasClass(symbol)) {
+      return true;
+    }else if($('.three').hasClass(symbol) && $('.five').hasClass(symbol) && $('.eight').hasClass(symbol)) {
+      return true;
+    }else if($('.zero').hasClass(symbol) && $('.four').hasClass(symbol) && $('.eight').hasClass(symbol)) {
+      return true;
+    }else if($('.two').hasClass(symbol) && $('.four').hasClass(symbol) && $('.six').hasClass(symbol)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+})
