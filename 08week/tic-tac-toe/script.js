ee95c374be9864358cfd240de4f0678f794f8146
Wade Cosby (CE CEN) <1303596@wholefoods.com>
2017-11-30 20:57:37 -0600

    initial push

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,86 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+  var turn = 'X';
+  $('[data-cell]').on('click', function(){
+    if($(this).text()===''){
+      $(this).text(turn);
+        wins();
+        if(turn === 'O'){
+          turn = 'X';
+        }else {
+          turn = 'O';
+    }
+  }
+  tie();
+  })
+
+  function wins (){
+    if($('[data-cell = "0"]').text() === turn &&
+      $('[data-cell = "1"]').text() === turn &&
+      $('[data-cell = "2"]').text() === turn )
+      $('#announce-winner').html(`Player ${turn} wins!`);
+
+      if($('[data-cell = "3"]').text() === turn &&
+        $('[data-cell = "4"]').text() === turn &&
+        $('[data-cell = "5"]').text() === turn )
+        $('#announce-winner').html(`Player ${turn} wins!`);
+
+        if($('[data-cell = "6"]').text() === turn &&
+          $('[data-cell = "7"]').text() === turn &&
+          $('[data-cell = "8"]').text() === turn )
+          $('#announce-winner').html(`Player ${turn} wins!`);
+
+          if($('[data-cell = "0"]').text() === turn &&
+            $('[data-cell = "3"]').text() === turn &&
+            $('[data-cell = "6"]').text() === turn )
+            $('#announce-winner').html(`Player ${turn} wins!`);
+
+            if($('[data-cell = "1"]').text() === turn &&
+              $('[data-cell = "4"]').text() === turn &&
+              $('[data-cell = "7"]').text() === turn )
+              $('#announce-winner').html(`Player ${turn} wins!`);
+
+              if($('[data-cell = "2"]').text() === turn &&
+                $('[data-cell = "5"]').text() === turn &&
+                $('[data-cell = "8"]').text() === turn )
+                $('#announce-winner').html(`Player ${turn} wins!`);
+
+                if($('[data-cell = "0"]').text() === turn &&
+                  $('[data-cell = "4"]').text() === turn &&
+                  $('[data-cell = "8"]').text() === turn )
+                  $('#announce-winner').html(`Player ${turn} wins!`);
+
+                  if($('[data-cell = "2"]').text() === turn &&
+                    $('[data-cell = "4"]').text() === turn &&
+                    $('[data-cell = "6"]').text() === turn )
+                    $('#announce-winner').html(`Player ${turn} wins!`);
+  }
+
+function tie() {
+  var i = 0;
+  
+}
+
+
+  $("#clear").click(clear);
+
+
+    function clear(){
+    $('[data-cell]').empty();
+    $('#announce-winner').empty();
+    // turn = 'X';
+  }
+
+  // function alternate(){
+  //   var counter = 0;
+  //   $('[data-cell]').each(function(){
+  //     counter += Number($(this).text());
+  //   })
+  //   if(counter === 25){
+  //     $('#announce-winner').text("You Hit 25!!");
+  //   }
+  // }
+  //
+
 });
