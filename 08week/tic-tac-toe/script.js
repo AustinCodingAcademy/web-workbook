83b09c0daf19c7f37bf88385af999652f2eab130
TODD FREDERKING <toddfrederking@gmail.com>
2017-12-02 10:54:10 -0600

    can i have a tic tac

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -2,4 +2,73 @@
 
 $(document).ready(function() {
   // Put app logic in here
+  var turns = 0;
+  var win = false;
+  var playerTurn = 'X';
+  $('[data-cell]').click(function() {
+    if ($(this).text() === '') {
+      $(this).text(playerTurn);
+      checkIfPlayerWon();
+      checkIfTie();
+      if (playerTurn === 'X') {
+        playerTurn = 'O';
+      } else {
+        playerTurn = 'X';
+      }
+    }
+
+  });
+
+  function checkIfPlayerWon() {
+    if (($('[data-cell = "0"]').text() === playerTurn &&
+        $('[data-cell = "1"]').text() === playerTurn &&
+        $('[data-cell = "2"]').text() === playerTurn) ||
+
+      ($('[data-cell = "3"]').text() === playerTurn &&
+        $('[data-cell = "4"]').text() === playerTurn &&
+        $('[data-cell = "5"]').text() === playerTurn) ||
+
+      ($('[data-cell = "6"]').text() === playerTurn &&
+        $('[data-cell = "7"]').text() === playerTurn &&
+        $('[data-cell = "8"]').text() === playerTurn) ||
+
+      ($('[data-cell = "0"]').text() === playerTurn &&
+        $('[data-cell = "4"]').text() === playerTurn &&
+        $('[data-cell = "8"]').text() === playerTurn) ||
+
+      ($('[data-cell = "2"]').text() === playerTurn &&
+        $('[data-cell = "4"]').text() === playerTurn &&
+        $('[data-cell = "6"]').text() === playerTurn) ||
+
+      ($('[data-cell = "0"]').text() === playerTurn &&
+        $('[data-cell = "3"]').text() === playerTurn &&
+        $('[data-cell = "6"]').text() === playerTurn) ||
+
+      ($('[data-cell = "1"]').text() === playerTurn &&
+        $('[data-cell = "4"]').text() === playerTurn &&
+        $('[data-cell = "7"]').text() === playerTurn) ||
+
+      ($('[data-cell = "2"]').text() === playerTurn &&
+        $('[data-cell = "5"]').text() === playerTurn &&
+        $('[data-cell = "8"]').text() === playerTurn)) {
+      win = true;
+      $('#announce-winner').text(`Player ${playerTurn} Wins!`);
+    }
+  }
+
+  $('#clear').click(function() {
+    $(this).click(function() {
+      $('[data-cell]').empty();
+      turns = 0;
+      win = false;
+      $('#announce-winner').empty();
+    });
+  });
+
+  function checkIfTie() {
+    turns++;
+    if (turns === 9 && !win) {
+      $('#announce-winner').text('Tie');
+    }
+  }
 });
