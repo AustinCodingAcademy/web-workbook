cc84ee633f2c62695fe811c0c0cbb065044f4a79
Cory <cbosselmann1029@gmail.com>
2017-12-04 23:25:50 -0600

    tic-tac-toe, not quite finished

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -6,82 +6,71 @@ $(document).ready(function() {
   var numOfClicks = 0;
   var win = false;
   $('[data-cell]').click(function() {
-    if ($(this).text() === '' && playerTurn === 'X') {
-      $(this).text('X');
-      playerTurn = 'O';
-
-    
-    }
-    if ($(this).text() === '' && playerTurn === 'O') {
-      $(this).text('O');
-      playerTurn = 'X';
-
-
+    if ($(this).text() === '') {
+      $(this).text(playerTurn);
     }
     checkForWins();
     numOfClicks++;
     checkForTie();
+    if (playerTurn==='X') {
+      playerTurn = 'O';
+    } else {
+      playerTurn = 'X';
+    }
+    // if ($(this).text() === '' && playerTurn === 'X') {
+    //   $(this).text('X');
+    //   playerTurn = 'O';
+    //
+    //
+    // }
+    // if ($(this).text() === '' && playerTurn === 'O') {
+    //   $(this).text('O');
+    //   playerTurn = 'X';
+    //
+    // }
+
     // checkForWins();
     // checkForTie();
-console.log(win)
+    console.log(win)
+    console.log(playerTurn);
   })
 
   function checkForWins() {
     //Checking top row for wins;
     if ($('[data-cell ="0"]').text() === playerTurn &&
       $('[data-cell ="1"]').text() === playerTurn &&
-      $('[data-cell ="2"]').text() === playerTurn) {
-      $('#announce-winner').text(`Player ${playerTurn} Wins!`);
-      win = true;
-    }
-    //Checking middle row for wins;
-    if ($('[data-cell ="3"]').text() === playerTurn &&
-      $('[data-cell ="4"]').text() === playerTurn &&
-      $('[data-cell ="5"]').text() === playerTurn) {
-      $('#announce-winner').text(`Player ${playerTurn} Wins!`);
-        win = true;
-    }
-    //Checking bottom row for wins;
-    if ($('[data-cell ="6"]').text() === playerTurn &&
-      $('[data-cell ="7"]').text() === playerTurn &&
-      $('[data-cell ="8"]').text() === playerTurn) {
-      $('#announce-winner').text(`Player ${playerTurn} Wins!`);
-        win = true;
-    }
-    //Checking left column for wins;
-    if ($('[data-cell ="0"]').text() === playerTurn &&
+      $('[data-cell ="2"]').text() === playerTurn ||
+      //Checking middle row for wins;
       $('[data-cell ="3"]').text() === playerTurn &&
-      $('[data-cell ="6"]').text() === playerTurn) {
-      $('#announce-winner').text(`Player ${playerTurn} Wins!`);
-        win = true;
-    }
-    //Checking middle column for wins;
-    if ($('[data-cell ="1"]').text() === playerTurn &&
       $('[data-cell ="4"]').text() === playerTurn &&
-      $('[data-cell ="7"]').text() === playerTurn) {
-      $('#announce-winner').text(`Player ${playerTurn} Wins!`);
-        win = true;
-    }
-    //Checking right column for wins;
-    if ($('[data-cell ="2"]').text() === playerTurn &&
+      $('[data-cell ="5"]').text() === playerTurn ||
+      //Checking bottom row for wins;
+      $('[data-cell ="6"]').text() === playerTurn &&
+      $('[data-cell ="7"]').text() === playerTurn &&
+      $('[data-cell ="8"]').text() === playerTurn ||
+      //Checking left column for wins;
+      $('[data-cell ="0"]').text() === playerTurn &&
+      $('[data-cell ="3"]').text() === playerTurn &&
+      $('[data-cell ="6"]').text() === playerTurn ||
+      //Checking middle column for wins;
+      $('[data-cell ="1"]').text() === playerTurn &&
+      $('[data-cell ="4"]').text() === playerTurn &&
+      $('[data-cell ="7"]').text() === playerTurn ||
+      //Checking right column for wins;
+      $('[data-cell ="2"]').text() === playerTurn &&
       $('[data-cell ="5"]').text() === playerTurn &&
-      $('[data-cell ="8"]').text() === playerTurn) {
-      $('#announce-winner').text(`Player ${playerTurn} Wins!`);
-        win = true;
-    }
-    //Checking top right to bottom left for wins;
-    if ($('[data-cell ="2"]').text() === playerTurn &&
+      $('[data-cell ="8"]').text() === playerTurn ||
+      //Checking top right to bottom left for wins;
+      $('[data-cell ="2"]').text() === playerTurn &&
       $('[data-cell ="4"]').text() === playerTurn &&
-      $('[data-cell ="6"]').text() === playerTurn) {
-      $('#announce-winner').text(`Player ${playerTurn} Wins!`);
-        win = true;
-    }
-    //Checking top left to bottom right for wins;
-    if ($('[data-cell ="0"]').text() === playerTurn &&
+      $('[data-cell ="6"]').text() === playerTurn ||
+
+      //Checking top left to bottom right for wins;
+      $('[data-cell ="0"]').text() === playerTurn &&
       $('[data-cell ="4"]').text() === playerTurn &&
       $('[data-cell ="8"]').text() === playerTurn) {
+      win = true;
       $('#announce-winner').text(`Player ${playerTurn} Wins!`);
-        win = true;
     }
 
   }
