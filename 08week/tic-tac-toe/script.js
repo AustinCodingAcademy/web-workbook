3c78319d1203f5bb0ec8f0bb21427bc9e120dd5c
Ty Sweet <tysweet77@gmail.com>
2017-12-05 18:31:02 -0600

    fixed win and tie

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -3,74 +3,66 @@
 $(document).ready(function() {
   let playerTurn = 'X';
   var counter = 0;
+  var win = false;
   $('[data-cell]').click(function() {
     if($(this).text() === ''){
     $(this).text(playerTurn);
+    checkForWins();
+
     if (playerTurn === 'X') {
       playerTurn = 'O';
     } else {
       playerTurn = 'X';
     }
-    checkForWins();
+
     counter++;
+    console.log(counter);
+    console.log(win);
     checkTie();
   }
 
   });
 
   function checkForWins(){
-    if($('[data-cell = "0"]').text() === playerTurn &&
+    if(($('[data-cell = "0"]').text() === playerTurn &&
       $('[data-cell = "1"]').text() === playerTurn &&
-      $('[data-cell = "2"]').text() === playerTurn)
-      {
-          alert(`Player ${playerTurn} Wins!`);
-        }
-      else if($('[data-cell = "3"]').text() === playerTurn &&
+      $('[data-cell = "2"]').text() === playerTurn) ||
+
+      ($('[data-cell = "3"]').text() === playerTurn &&
       $('[data-cell = "4"]').text() === playerTurn &&
-      $('[data-cell = "5"]').text() === playerTurn)
-      {
-          alert(`Player ${playerTurn} Wins!`);
-        }
-      else if($('[data-cell = "6"]').text() === playerTurn &&
+      $('[data-cell = "5"]').text() === playerTurn) ||
+
+      ($('[data-cell = "6"]').text() === playerTurn &&
       $('[data-cell = "7"]').text() === playerTurn &&
-      $('[data-cell = "8"]').text() === playerTurn)
-      {
-          alert(`Player ${playerTurn} Wins!`);
-        }
-      else if($('[data-cell = "0"]').text() === playerTurn &&
+      $('[data-cell = "8"]').text() === playerTurn) ||
+
+      ($('[data-cell = "0"]').text() === playerTurn &&
       $('[data-cell = "4"]').text() === playerTurn &&
-      $('[data-cell = "8"]').text() === playerTurn)
-      {
-          alert(`Player ${playerTurn} Wins!`);
-        }
-      else if($('[data-cell = "0"]').text() === playerTurn &&
+      $('[data-cell = "8"]').text() === playerTurn) ||
+
+      ($('[data-cell = "0"]').text() === playerTurn &&
       $('[data-cell = "3"]').text() === playerTurn &&
-      $('[data-cell = "6"]').text() === playerTurn)
-      {
-          alert(`Player ${playerTurn} Wins!`);
-        }
-      else if($('[data-cell = "1"]').text() === playerTurn &&
+      $('[data-cell = "6"]').text() === playerTurn) ||
+
+      ($('[data-cell = "1"]').text() === playerTurn &&
       $('[data-cell = "4"]').text() === playerTurn &&
-      $('[data-cell = "7"]').text() === playerTurn)
-      {
-          alert(`Player ${playerTurn} Wins!`);
-        }
-      else if($('[data-cell = "2"]').text() === playerTurn &&
+      $('[data-cell = "7"]').text() === playerTurn) ||
+
+      ($('[data-cell = "2"]').text() === playerTurn &&
       $('[data-cell = "5"]').text() === playerTurn &&
-      $('[data-cell = "8"]').text() === playerTurn)
-      {
-          alert(`Player ${playerTurn} Wins!`);
-        }
-      else if($('[data-cell = "6"]').text() === playerTurn &&
+      $('[data-cell = "8"]').text() === playerTurn) ||
+
+      ($('[data-cell = "6"]').text() === playerTurn &&
       $('[data-cell = "4"]').text() === playerTurn &&
-      $('[data-cell = "2"]').text() === playerTurn)
+      $('[data-cell = "2"]').text() === playerTurn))
      {
-        alert(`Player ${playerTurn} Wins!`);
+        $('#announce-winner').text(`Player ${playerTurn} Wins!`);
+        win = true;
       }
   };
 
   function checkTie (){
-    if (counter === 9 && !checkForWins())
+    if (counter === 9 && !win)
     {
       alert('Players Tie!');
     }
@@ -78,6 +70,7 @@ $(document).ready(function() {
 
   $('#clear').click(function(){
     $('[data-cell]').empty();
+    $('#announce-winner').empty();
   });
 
 });
