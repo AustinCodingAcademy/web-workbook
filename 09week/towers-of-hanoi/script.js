861c9ae347a8e6b8ae66f3b3e711faef235975f7
Wade Cosby (CE CEN) <1303596@wholefoods.com>
2017-12-06 14:21:02 -0600

    Finished with logic of game.  Next steps: add announce and counter

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -7,20 +7,35 @@ $(document).ready(function() {
   const $midBottom = $('[data-block=75]');
   const $bottom = $('[data-block=100]');
 
-
-  console.log($top, $midTop, $midBottom, $bottom);
-
-  $('[data-stack]').click(function() {
   let $playerHand = null;
   let $currentStack;
-
+//Click event added
+  $('[data-stack]').click(function() {
+//Test1: No block in hand and target has more than 0 blocks
     if ($playerHand === null && $(this).children().length > 0) {
-      $currentStack = $(this).attr('data-stack')
-      $playerHand = $(this).children().last().detach()
-      
+//When true set variables
+      $playerHand = $(this).children().last()
+      $currentStack = $(this)
+      $playerHand.detach()  //Detach the block held under variable $playerHand
+    } else{ //Time to find out how to handle the drop behavior
+//Test a: Test if var $playerHand is not null and the new clicked stack's last block's value is greater than the
+  //the picked up block
+        if ($playerHand != null && $(this).children().last().data('block') > $playerHand.data('block')) {
+//When True:
+          $(this).append($playerHand); //add the block to the top of the stack
+          $playerHand = null; //reset the $playerHand
+//When Test a is False:
+//Test b: Test if var $playerHand is not null and the new clicked stack's last block's value is less than the
+  //the picked up block
+        }else if ($playerHand != null && $(this).children().last().data('block') < $playerHand.data('block')){
+          //When True:
+          $currentStack.append($playerHand); //Send the block back to the original clicked stack
+          $playerHand = null; //reset the $playerHand
+//When Test b is False:
+        }else {
+          $(this).append($playerHand); //add the block to the new stack
+          $playerHand = null; //reset $playerHand
+      }
     }
-
   })
-
-
 });
