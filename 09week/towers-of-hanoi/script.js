161229b8835c1cc8ab89a8d171dc889c7fde3458
Wade Cosby (CE CEN) <1303596@wholefoods.com>
2017-12-05 21:20:07 -0600

    initial push for towers game

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,4 +2,25 @@
 
 $(document).ready(function() {
   // Put app logic here
+  const $top = $('[data-block=25]');
+  const $midTop = $('[data-block=50]');
+  const $midBottom = $('[data-block=75]');
+  const $bottom = $('[data-block=100]');
+
+
+  console.log($top, $midTop, $midBottom, $bottom);
+
+  $('[data-stack]').click(function() {
+  let $playerHand = null;
+  let $currentStack;
+
+    if ($playerHand === null && $(this).children().length > 0) {
+      $currentStack = $(this).attr('data-stack')
+      $playerHand = $(this).children().last().detach()
+      
+    }
+
+  })
+
+
 });
