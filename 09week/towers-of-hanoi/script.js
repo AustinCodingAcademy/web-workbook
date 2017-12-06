0c91567d2ca6e0f26d3f17c0f865e56278b5b27a
Shelby Inloes <shelbyinloes@Shelbys-MacBook-Air.local>
2017-12-06 10:57:02 -0600

    drag drop

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -3,15 +3,31 @@
 $(document).ready(function() {
   //making element draggable
   $('[data-block]').draggable({
-    // helper: 'clone'
+    revert: 'invalid'
   });
 
-//making element drop into another data-stack
-  $('[data-block]').droppable({
-    drop: function(event,ui) {
-      $(ui.draggable).detach([data-stack]).css({float: left}).appendTo([data-stack]);
-    }
+  //  //making element drop only into big div
+  // $('[data-stack]').droppable({
+  //   drop: function(event, ui) {
+  //     $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+  //   }
+  // });
+
+//checking for what the div is equal to
+  let $current = null;
+  $('[data-stack]').click(function(){
+    $current = $(this).children().last().detach();
+    console.log($current);
+      if($(this).children().length === 0) {
+        $(this).appendTo('[data-stack]')
+      }  
   })
 
 
+
+//announce winner
+  var checkWinner = $('[data-stack="3"]').children().length
+  if(checkWinner === 4) {
+    alert('You win!');
+  }
 });
