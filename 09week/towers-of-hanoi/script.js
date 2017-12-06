92eb7a07405846c2efbf79ac5d3b813f08cc67cd
Nathan Simmons <nathanhbsimmons@gmail.com>
2017-12-06 10:20:57 -0600

    towers game

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,37 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+
+
+  function checkWin() {
+
+    if ($('[data-stack=3]').children().length == 4) {
+
+      $('#announce-game-won').text(`Winner winner winner!`)
+    }
+  }
+
+  $('.draggable').draggable({
+    revert: "invalid"
+  });
+
+  $('#dropArea, #dropArea2, #dropArea3' ).droppable({
+
+    drop: function(event, ui){
+      let drag = $(ui.draggable).data('block');
+      let last = ($(this).children().last()).data('block');
+
+
+      if((drag) > (last)) {
+        $(ui.draggable).draggable('option', 'revert', true);
+      } else {
+        $(ui.draggable).appendTo($(this)).attr('style', 'position: relative');
+      }
+
+      checkWin();
+
+    }
+  });
+
+
 });
