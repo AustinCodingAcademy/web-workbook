a23dc1cb579b1ec896c96640d142ebadb8fab5a5
TODD FREDERKING <toddfrederking@gmail.com>
2017-12-06 19:12:10 -0600

    hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,4 +2,55 @@
 
 $(document).ready(function() {
   // Put app logic here
+
+  // $('[data-block]').draggable({
+  //   revert: "invalid"
+  // });
+  // $('[data-stack]').droppable({
+  //   drop:function(event,ui){
+  //     $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+  //   }
+  // });
+
+  let $current = null;
+  $('[data-stack]').click(function() {
+
+
+
+    if ($current === null && $(this).children().length > 0) {
+      $current = $(this).children().last().detach();
+      let $currentStack=$(this).data('stack');
+    }else if($(this).children().length > 0){
+      if($(this).children().last().data('block') < ($current.data('block'))){
+        $current = null;
+        // checkIfPlayerWon();
+      }
+
+      else if($(this).children().last().data('block') > ($current.data('block'))){
+        $(this).append($current);
+        $current = null;
+        // checkIfPlayerWon();
+      }
+    }
+
+    else if ($(this).children().length === 0) {
+      $current.appendTo($(this));
+      $current = null;
+      // checkIfPlayerWon();
+    }
+    checkIfPlayerWon();
+
+
+  })
+  function checkIfPlayerWon(){
+    if ($('[data-stack="3"]').children().length === 4){
+      alert('Winner Winner Chicken Dinner');
+    }
+  };
+
+
+
+
+
+
 });
