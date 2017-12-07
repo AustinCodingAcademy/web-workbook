0e334c476b3c07d1ec02eb7b2d84cee9b51740b0
Cory <cbosselmann1029@gmail.com>
2017-12-07 16:29:57 -0600

    tower of hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,4 +2,39 @@
 
 $(document).ready(function() {
   // Put app logic here
+
+  // var $block = null;
+  // $('[data-stack]').click(function(){
+  // if (!$block) {
+  //   $block = $(this).children().last().detach();
+  // } else {
+  //   $(this).append($block);
+  //   $block = null;
+  // }
+  // });
+  let $playerMove = null;
+  let currentStack;
+let moveCounter = 0;
+  $('[data-stack]').click(function(){
+    if ($playerMove === null && $(this).children().length > 0) {
+      $playerMove = $(this).children().last().detach();
+    } else if (($(this).children().length >0)){
+      if (($(this).children().last().data('block'))<($playerMove.data('block'))){
+        $playerMove.appendTo($('[data-stack]').eq(currentStack - 1));
+      } else if (($(this).children().last().data('block')) > ($playerMove.data('block'))){
+        $(this).append($playerMove);
+        $playerMove = null;
+
+      }
+    } else if ($(this).children().length === 0) {
+      $playerMove.appendTo($(this));
+      $playerMove = null;
+    }
+    checkForWin();
+});
+function checkForWin() {
+  if ($('[data-stack="3"]').children().length === 4) {
+    $('#announce-game-won').text('You Won!');
+  }
+}
 });
