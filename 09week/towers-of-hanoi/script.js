47a526b3b25a328c5814f65337549e75d8b5faf2
Shelby Inloes <shelbyinloes@Shelbys-MacBook-Air.local>
2017-12-07 13:21:49 -0600

    fixed detach/append and added check winner alert

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,33 +1,34 @@
 'use strict';
 
 $(document).ready(function() {
-  //making element draggable
-  $('[data-block]').draggable({
-    revert: 'invalid'
-  });
 
-  //  //making element drop only into big div
-  // $('[data-stack]').droppable({
-  //   drop: function(event, ui) {
-  //     $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
-  //   }
-  // });
 
-//checking for what the div is equal to
-  let $current = null;
-  $('[data-stack]').click(function(){
+let $current = null;
+$('[data-stack]').click(function() {
+  if ($current === null && $(this).children().length > 0) {
     $current = $(this).children().last().detach();
-    console.log($current);
-      if($(this).children().length === 0) {
-        $(this).appendTo('[data-stack]')
-      }  
-  })
+    let $currentStack=$(this).data('stack');
+  }else if($(this).children().length > 0) {
+    if($(this).children().last().data('block') < ($current.data('block'))) {
+      $current = null;
+    }
+    else if($(this).children().last().data('block') > ($current.data('block'))) {
+      $(this).append($current);
+      $current = null;
+    }
+  }
+  else if ($(this).children().length === 0) {
+    $current.appendTo($(this));
+    $current = null;
+  }
 
+  var $checkWinner = $('[data-stack="3"]').children().length;
+  if ($checkWinner === 4) {
+    alert('You win!');
+  };
+})
 
 
 //announce winner
-  var checkWinner = $('[data-stack="3"]').children().length
-  if(checkWinner === 4) {
-    alert('You win!');
-  }
+
 });
