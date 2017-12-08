35d42d6e011a6ab6c15b28ff798a9522309e78da
unknown <hunter-conley@sbcglobal.net>
2017-12-07 19:03:50 -0600

    week 9 updates

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,35 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+
+  // Declare Variables
+  var $userMove = null;
+  var currentStack;
+
+  // Function to click and store the stack element to an empty block
+  $('[data-stack]').click(function() {
+    if($userMove === null && $(this).children().length > 0){
+      $userMove = $(this).children().last().detach();
+      currentStack = $(this).attr('data-stack');
+    } else if (($(this).children().length > 0)) {
+      if(($(this).children().last().data('block')) < ($userMove.data('block'))){
+        $userMove.appendTo($('[data-stack]').eq(currentStack - 1));
+        $userMove = null;
+          } else if (($(this).children().last().data('block')) > ($userMove.data('block'))) {
+            $(this).append($userMove);
+            $userMove = null;
+          } else if ($(this).children().length === 0) {
+            $userMove.appendTo($(this));
+            $userMove = null;
+          }
+    }
+    //checkWins();
+  })
+
+  // function checkWins() {
+  //   if($('[data-stack = "3"]').children().length === 4) {
+  //     $('#announce-game-won').text("You Won!");
+  //   }
+  // }
+
 });
