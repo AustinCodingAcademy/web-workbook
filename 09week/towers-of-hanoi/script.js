1f7d78f1e5fd63330fe849a2353f52e7dd44dc8e
Nathan Simmons <nathanhbsimmons@gmail.com>
2017-12-09 10:25:21 -0600

    beautify

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,29 +2,25 @@
 
 $(document).ready(function() {
 
-
   function checkWin() {
     if ($('[data-stack=3]').children().length == 4) {
       $('#announce-game-won').text(`Winner winner winner!`)
     }
   }
 
-  var count=0;
+  var count = 0;
 
-  $('.draggable').draggable({
-    revert: "invalid",
-  });
+  $('.draggable').draggable({revert: "invalid"});
 
-  $('#dropArea, #dropArea2, #dropArea3' ).droppable({
+  $('#dropArea, #dropArea2, #dropArea3').droppable({
 
-    drop: function(event, ui){
+    drop: function(event, ui) {
       count++;
       $('#count').text(`clicks = ${count}`);
       let drag = $(ui.draggable).data('block');
       let last = ($(this).children().last()).data('block');
 
-
-      if((drag) > (last)) {
+      if ((drag) > (last)) {
         $(ui.draggable).draggable('option', 'revert', true);
       } else {
         $(ui.draggable).appendTo($(this)).attr('style', 'position: relative');
@@ -35,22 +31,11 @@ $(document).ready(function() {
     }
   });
 
-  // function iterate() {
-  //
-  //   $( '.draggable').click(function(){
-  //     count++;
-  //     $('#count').text(`clicks = ${count}`);
-  //   });
-  //
-  // }
-
   $('#clear').click(function() {
     $('#count').empty();
     $('#announce-game-won').empty()
     count = 0;
-
+    location.reload();
   });
-
-
 
 });
