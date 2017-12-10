5766a845cf979bc60b09ce724c4752834e925010
Andrew <32229265+aselee@users.noreply.github.com>
2017-12-10 15:51:28 -0600

    Tower of Hanoi game JS

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,4 +2,51 @@
 
 $(document).ready(function() {
   // Put app logic here
+  var $blocks = $('[data-block]');
+  var $stacks = $('[data-stack]');
+  var $moveBlocks = $('[data-block]:last-child');
+  $moveBlocks.addClass("moveable");
+  var gameover = false;
+
+  $blocks.draggable({
+    revert: true
+  });
+
+  $stacks.droppable({
+    accept: ".moveable",
+    drop: function(event, ui) {
+      if(!gameover) {
+        if(dropBlock($(this), ui.draggable)) {
+          ui.draggable.draggable('option', 'revert', false);
+          $(this).append(ui.draggable.detach());
+          ui.draggable.css({
+            'top': 0,
+            'left': 0
+          });
+          $moveBlocks = $('[data-block]:last-child');
+          $('[data-block]').removeClass("moveable");
+          $moveBlocks.addClass("moveable");
+          $blocks.draggable({
+            revert: true
+          });
+          checkForWin();
+        }
+      }
+    }
+  });
+  function dropBlock($stack, $block) {
+    var $last_block = $stack.children().last();
+    if (parseInt($block.attr("data-block")) < parseInt($last_block.attr("data-block")) ||
+  $stack.children().length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+  function checkForWin() {
+    if($('[data-stack="3"]').children().length === 4) {
+      $('#announce-game-won').html("Nice job, you win!");
+      gameover = true;
+    }
+  }
 });
