3ebad25ac88bdfd7c8f10fbb1f2ec80be1754d3b
Nathan Simmons <nathanhbsimmons@gmail.com>
2017-12-11 10:56:06 -0600

    forms

---
+++ 09week/customer-satisfaction/js/script.js
@@ -0,0 +1,6 @@
+// function check(form) {
+//   if(form.email.value == "myemail" && form.pswrd.value == "mypassword") {
+//     window.open('index.html')
+//   } else {
+//     alert("Error Password or Username")
+//   }}
