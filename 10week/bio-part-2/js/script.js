7ae03e4cc8e641207bf0142548fa9612fdac6409
James <jamesaeberwine@gmail.com>
2017-12-19 22:53:47 -0600

    cp3

--- 10week/bio-part-2/js/script.js
+++ 10week/bio-part-2/js/script.js
@@ -1,11 +1,53 @@
-'use strict';
-
-$(document).ready(function() {
+// reference https://github.com/kevincolten/asteroids/blob/master/asteroids.js
 
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 
+// ctx.clearRect(0, 0, 400, 400);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+var i = 0;
+
+function drawCircle(x, y, radius, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(
+    (i / 10 + x) % 400, // x
+    (i / 10 + y) % 400, // y
+    radius, // radius
+    0, // start angle
+    2 * Math.PI, // end angle
+    false
+  );
+  ctx.fill();
+  i++;
+}
 
 
 
-})
+const positions = [];
+
+for (let i = 0; i < 20; i++) {
+  positions.push([getRandomInt(0, 400), getRandomInt(0, 400), getRandomInt(0, 200), getRandomColor()])
+}
+
+setInterval(() => {
+  ctx.clearRect(0, 0, 400, 400);
+  for(let i = 0; i < 20; i++) {
+    drawCircle(positions[i][0], positions[i][1], positions[i][2], positions[i][3])
+  }
+}, 10)
--- 10week/checkpoint3/script.js
+++ 10week/checkpoint3/script.js
@@ -18,7 +18,6 @@ function updateReport() {
   $("#rps").text((data.totalRPS/70.4).toFixed(3));
 }
 
-
 $("#wheel").click(function (){
   data.totalRevs ++;
   data.totalCurrent ++;
@@ -34,4 +33,19 @@ $(".button").click(function (){
     $( this ).data( "cost", parseInt($(this).data( "cost" ) * 1.15) );
   }
   updateReport();
+  var $meter = $('#rps').text();
+  console.log($("#rps").text())
+  if ($("#rps").text() >= .001 && $("#rps").text() < .01) {
+     $('img').attr('src', 'img/2.jpg');
+  } else if ($("#rps").text() >= .01 && $("#rps").text() < .1) {
+     $('img').attr('src', 'img/3.png');
+   } else if ($("#rps").text() >= .1 && $("#rps").text() < 1) {
+      $('img').attr('src', 'img/4.jpg');
+    } else if ($("#rps").text() >= 1 && $("#rps").text() < 10) {
+       $('img').attr('src', 'img/5.jpg');
+     } else if ($("#rps").text() >= 10 && $("#rps").text() < 100) {
+        $('img').attr('src', 'img/6.png');
+      }  else if ($("#rps").text() >= 100) {
+         $('img').attr('src', 'img/last.jpg');
+       }
 })
