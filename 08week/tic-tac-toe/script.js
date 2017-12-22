7cc9bc6b4f1dd6da481b556ee7c5e9ec2fc8008f
ChrisL20 <ctopher.lu44@gmail.com>
2017-12-21 21:47:47 -0600

    apparently i didnt push this yet

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -2,4 +2,85 @@
 
 $(document).ready(function() {
   // Put app logic in here
+    var playerTurn = 'X';
+  $('[data-cell]').click(function(){
+    if ($(this).text() === '') {
+        $(this).text(playerTurn);
+        checkForWins();
+    if(playerTurn === 'X'){
+      playerTurn = "O";
+    } else {
+      playerTurn = 'X';
+    }
+
+  }
+})
+
+
+
+function checkForWins(){
+  if ($('[data-cell = "0"]').text() === playerTurn &&
+      $('[data-cell = "1"]').text() === playerTurn &&
+      $('[data-cell = "2"]').text() === playerTurn) {
+        $("#announce-winner").text(`Player ${playerTurn} Wins!`);
+      }
+
+if ($('[data-cell = "3"]').text() === playerTurn &&
+    $('[data-cell = "4"]').text() === playerTurn &&
+    $('[data-cell = "5"]').text() === playerTurn) {
+      $("#announce-winner").text(`Player ${playerTurn} Wins!`);
+    }
+
+if ($('[data-cell = "6"]').text() === playerTurn &&
+    $('[data-cell = "7"]').text() === playerTurn &&
+    $('[data-cell = "8"]').text() === playerTurn) {
+      $("#announce-winner").text(`Player ${playerTurn} Wins!`);
+    }
+
+if ($('[data-cell = "0"]').text() === playerTurn &&
+    $('[data-cell = "3"]').text() === playerTurn &&
+    $('[data-cell = "6"]').text() === playerTurn) {
+      $("#announce-winner").text(`Player ${playerTurn} Wins!`);
+    }
+if ($('[data-cell = "1"]').text() === playerTurn &&
+        $('[data-cell = "4"]').text() === playerTurn &&
+        $('[data-cell = "7"]').text() === playerTurn) {
+          $("#announce-winner").text(`Player ${playerTurn} Wins!`);
+        }
+if ($('[data-cell = "2"]').text() === playerTurn &&
+    $('[data-cell = "5"]').text() === playerTurn &&
+    $('[data-cell = "8"]').text() === playerTurn) {
+      $("#announce-winner").text(`Player ${playerTurn} Wins!`);
+            }
+if ($('[data-cell = "0"]').text() === playerTurn &&
+$('[data-cell = "4"]').text() === playerTurn &&
+$('[data-cell = "8"]').text() === playerTurn) {
+$("#announce-winner").text(`Player ${playerTurn} Wins!`);
+}
+                        if ($('[data-cell = "3"]').text() === playerTurn &&
+                            $('[data-cell = "5"]').text() === playerTurn &&
+                            $('[data-cell = "7"]').text() === playerTurn) {
+                              $("#announce-winner").text(`Player ${playerTurn} Wins!`);
+}
+                                    if ($('[data-cell = "0"]').text() === playerTurn &&
+                                        $('[data-cell = "5"]').text() === playerTurn &&
+                                        $('[data-cell = "6"]').text() === playerTurn) {
+                                          $("#announce-winner").text(`No possible winner!`);
+}
+                                                if ($('[data-cell = "0"]').text() === playerTurn &&
+                                                    $('[data-cell = "7"]').text() === playerTurn &&
+                                                    $('[data-cell = "2"]').text() === playerTurn) {
+                                                      $("#announce-winner").text(`No winner possible!`);
+}
+if ($('[data-cell = "2"]').text() === playerTurn &&
+    $('[data-cell = "3"]').text() === playerTurn &&
+    $('[data-cell = "8"]').text() === playerTurn) {
+      $("#announce-winner").text(`Tie!`);
+}
+if ($('[data-cell = "1"]').text() === playerTurn &&
+    $('[data-cell = "6"]').text() === playerTurn &&
+    $('[data-cell = "8"]').text() === playerTurn) {
+      $("#announce-winner").text(`No winner possible!`);
+}
+    }
 });
--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,12 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
-});
+$(document).ready(function(){
+    let $playerMove = null;
+  $('[data-block]').click(function(){
+    if ($playerMove === null && $(this).children(). length > 0){
+      $playerMove = $(this).children().last().detach();
+    } else if (($(this).children().length > 0)){
+      if (($(this).children().last().data('block')).eq (currentstack - 1));
+      $playerMove = null;
+    } else if (($(this).))
+  })
