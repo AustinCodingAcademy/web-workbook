bb0e98fd64f2f4a7323e7dd88361851e128e27fc
Melissa Hale <melissa.hale@q2ebanking.com>
2018-02-21 17:53:37 -0600

    fix footer

--- 05week/news-website/scripts/like-counter.js
+++ 05week/news-website/scripts/like-counter.js
@@ -1,9 +1,6 @@
-var clicks = 1;
+var clicks = 0;
 
-document.getElementById("clicks").innerHTML = clicks;
-
-$('.like-counter').click(function() {
-  clicks += 1;
-  document.getElementById("clicks").innerHTML = clicks;
-  $('.like-counter').addClass("liked");
-});
+function onClick() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+      };
