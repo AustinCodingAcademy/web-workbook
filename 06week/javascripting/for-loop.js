db2194f42e89b3da817abe4fe5204cc8d33679d0
Rbrey <35275423+Rbrey@users.noreply.github.com>
2018-02-22 14:53:57 -0600

    javascripting

---
+++ 06week/javascripting/for-loop.js
@@ -0,0 +1,6 @@
+var total = 0;
+var limit = 10;
+for (var i = 0; i < 10; i++) {
+total += i;
+}
+console.log(total);
---
+++ 06week/javascripting/function-arguments.js
@@ -0,0 +1,4 @@
+function math (a1, a2, a3) {
+return (a2*a3+a1)
+}
+console.log(math(53,61,67));
