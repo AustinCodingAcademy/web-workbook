1c4075b30d3240bd8d021a2029902c5f88dfab92
David Gonzales <dgonzales033@satx.rr.com>
2018-02-21 20:30:42 -0600

    fizzbuzz challenge

---
+++ 06week/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,15 @@
+for (let i=0; i<=1000; i++){
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('fizzbuzz')
+  } else if (i % 3 === 0){
+    console.log('buzz')
+  } else if (i % 5 === 0){
+    console.log('fizz')
+  }
+
+  else {
+    console.log (i)
+  }
+
+
+} 
