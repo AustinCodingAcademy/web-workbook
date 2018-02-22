f56cd8b0ffd3aaf35a3381d3952b8ac0b41c2524
Briexmac <briexmac@gmail.com>
2018-02-21 20:29:44 -0600

    fix fizzbuzz

--- 06week/FizzBuzz/fizzBuzz.js
+++ 06week/FizzBuzz/fizzBuzz.js
@@ -1,7 +1,7 @@
 let num = 0;
 for (let i = 0; i < 100; i++ ) {
 	num += 1;
-  if ( num % 3 === 0 && num & 5 === 0) {
+  if (( num % 3) === 0 && (num & 5) === 0) {
   	console.log ("FizzBuzz");
   }
   else if ( num % 3 === 0) {
