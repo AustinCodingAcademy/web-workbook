9b341bb0bb156ba6f9c40e43a3650a149796ecea
woongsaeOLD <woongsaeOLD@woongsaes-MacBook-Pro.local>
2018-02-21 19:40:42 -0600

    troubleshooting circle ci for scope js

--- javascripting/scope.js
+++ javascripting/scope.js
@@ -3,19 +3,21 @@ var b = 2;
 var c = 3;
 
 (function firstFunction(){
-    var b = 5;
-    var c = 6;
+  var b = 5;
+  var c = 6;
 
-    (function secondFunction(){
-        var b = 8;
+  (function secondFunction(){
+    var b = 8;
+
 console.log("a: "+a+", b: "+b+", c: "+c);
-        (function thirdFunction(){
-            var a = 7;
-            var c = 9;
 
-            (function fourthFunction(){
-                var a = 1;
-                var c = 8;
+      (function thirdFunction(){
+          var a = 7;
+          var c = 9;
+
+        (function fourthFunction(){
+            var a = 1;
+            var c = 8;
 
             })();
         })();
