95101fb3dd8d5655862cc53749ab839af364644d
Briexmac <briexmac@gmail.com>
2018-02-21 20:24:53 -0600

    fizz buzz

---
+++ 06week/FizzBuzz/fizzBuzz.js
@@ -0,0 +1,16 @@
+let num = 0;
+for (let i = 0; i < 100; i++ ) {
+	num += 1;
+  if ( num % 3 === 0 && num & 5 === 0) {
+  	console.log ("FizzBuzz");
+  }
+  else if ( num % 3 === 0) {
+  	console.log ("Fizz");
+  }
+  else if ( num % 5 === 0) {
+  	console.log ("Buzz");
+  }
+  else {
+  	console.log (num);
+  }
+}
