4290d26993eba24ad54b39fc66d74d77bea3f974
Neafie <rjneafie@usgs.gov>
2018-02-23 16:15:07 -0600

    getting started on checkpoint

--- 06week/fizzbuzz/fizzbuzz.js
+++ 06week/fizzbuzz/fizzbuzz.js
@@ -17,6 +17,8 @@ for (var i = 1; i <= 1000; i++) {
 // And it works.
 // for(let i=0;i<1000;){
 //   console.log((++i %3?"":"fizz")+(i%5?"":"buzz")||i);
+// or
+//  console.log(!(++i%15)?’Fizzbuzz’:!(i%3)?’Fizz’:!(i%5)?’Buzz’:i));
 // }
 // Thoughts:
 // --Can increment looping variable within the loop. This seems like setting myself up for an infinite loop.  Definitely not best practice.
@@ -24,3 +26,4 @@ for (var i = 1; i <= 1000; i++) {
 // --Plus to handle the 3 or 5 cases
 // --Or to handle the final default
 // --Love that ternary operator  (test ? yes : no)
+// --Can reverse the ternary yes/no cases by using !(not) and then stack conditionals within the false case ... question is 'is there no remainder when dividing by 15?   How about by 3?  by 5?'
