88a41e3c707bc7c8807f7881cc24a4e4325c6b69
Alex Ponce <Hailthesun24@gmail.com>
2018-02-26 14:47:59 -0600

    Checkpoint, and Javawava files

---
+++ if-statement.js
@@ -0,0 +1,6 @@
+var fruit = 6;
+if (fruit > 5) {
+console.log("The fruit name has more than five characters.");
+} else {
+  console.log("The fruit name has five characters or less");
+}
---
+++ looping-through-arrays.js
@@ -0,0 +1,3 @@
+var pets = ['cat', 'dog', 'rat'];
+for (var i = 0; i <)
+console.log(pets);
