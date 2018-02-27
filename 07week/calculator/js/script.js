40825489aa84e1eca38e30ed14527aa6b98e1659
xianaustin <xianacevedo2019@gmail.com>
2018-02-27 11:45:40 -0600

    cp2

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,45 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiplication() {
+  document.querySelector("#results").value += "*";
+}
+
+function division() {
+  document.querySelector("#results").value += "/";
+}
+
+function plusMinus() {
+  let input = document.querySelector("#results").value;
+  console.log("input");
+
+    let newInput = -1*input;
+    document.querySelector("#results").value = newInput;
+  }
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
