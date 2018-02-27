32a14c369a55516099059fc712d14331a6e6d7a7
Neafie <rjneafie@usgs.gov>
2018-02-26 20:41:24 -0600

    calculator

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,50 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+// document.addEventListener("DOMContentLoaded",function(event){
+
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiply() {
+  document.querySelector("#results").value += "*";
+}
+
+function divide() {
+  document.querySelector("#results").value += "/";
+}
+
+function plusmin() {
+  let number = document.querySelector("#results").value;
+  let sign = number.substring(0, 1);
+  let notsign = number.substring(1);
+  if (sign == "+") {
+    document.querySelector("#results").value = "-" + notsign;
+  } else if (sign == "-") {
+    document.querySelector("#results").value = "+" + notsign;
+  } else {
+    document.querySelector("#results").value = "-" + number;
+  }
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
+// });
