729d94d54464e9991d2621c9e52522fd807c81bf
Melissa Hale <melissakayh@gmail.com>
2018-02-26 20:48:09 -0600

    commit calculator

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,41 @@
-'use strict';
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function subtract () {
+  document.querySelector("#results").value += "-";
+}
+
+function multiply() {
+  document.querySelector("#results").value += "*";
+}
+
+function divide() {
+  document.querySelector("#results").value += "/";
+}
+
+function switchNegPos() {
+  let current = document.querySelector("#results").value;
+  if (current.substring(0, 1) == '-') {
+    document.querySelector("#results").value = current.substring(1, current.length);
+  } else {
+    document.querySelector("#results").value = '-' + current;
+  }
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
