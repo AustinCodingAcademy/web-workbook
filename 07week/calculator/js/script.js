614818b092958683a1deed91808593666f192f48
Ukiah Richardson <urichardson74@gmail.com>
2018-02-27 01:28:24 -0600

    calc

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,21 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+function multiplication() {
+  document.querySelector("#results").value += "*";
+}
+function division() {
+  document.querySelector("#results").value += "/";
+}
+
+function negative() {
+  document.querySelector("#results").value += "-";
+}
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
