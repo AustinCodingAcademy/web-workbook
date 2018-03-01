60ae2f6a48b4f7c6e6b465002924777182c8de73
xianaustin <xianacevedo2019@gmail.com>
2018-03-01 16:55:34 -0600

    stuff

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,8 @@
 'use strict';
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+var shoppingList = document.querySelectorAll("li");
+function checkList() {
+window.alert('You have '+shoppingList.length +' items in your cart');
+}
+var h2Content = 'You have '+ shoppingList.length + ' items in your shopping cart';
+var header1 = document.querySelector('h1');
+header1.insertAdjacentHTML('afterend',`<h2>${h2Content}</h2>`);
