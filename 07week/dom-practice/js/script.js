68fb4ab91487732f9cb698fc5fce19d627d07d6f
Vanessa <tuohyvanessa@yahoo.com>
2018-02-28 21:11:52 -0600

    pushing CP2

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,18 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
+  // You code here
 });
+//Alert("It's linked");this is just a latest
+var node = document.createElement("li");
+var node = document.createTextNode("items");
+node.appendChild(textnode);
+document.getElementsbyID("mylist").appendchild(node);
+id = "myList"
+var table = document.getElementById("forecast-table");
+var cells = table.getElementsByTagName("li");
+for (var i = 0; i < cells.length; i++) {
+  var status = cells[i].getAttribute("data-status");
+  if (status == "open") {
+    // grab the data
+  }
