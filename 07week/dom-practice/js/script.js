54ababaf03fe0c0f39cea619a70190ea252e4449
Eric Voss <eric.w.voss@icloud.com>
2018-03-01 00:43:16 -0600

    checkpoint2

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+let items = document.querySelectorAll('li')
+
+alert(items.length)
+
+document.querySelector('h1').insertAdjacentHTML('afterend',)
+  <h2>You have ${items.length} items in your shopping cart<h2>
+
+function addItem() {
+  let itemName = document.querySelector('input').value
+  let itemDescription = document.querySelector('textarea').value;
+  document.querySelector('ul').insertAdjacentHTML('beforeend',)
+  <li><li>
+}
