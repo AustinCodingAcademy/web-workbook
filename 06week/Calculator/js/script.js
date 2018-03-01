f6c93981126323ed0edb7fa6401343781fc59914
Eric Voss <eric.w.voss@icloud.com>
2018-02-28 18:53:58 -0600

    Calculator

---
+++ 06week/Calculator/js/script.js
@@ -0,0 +1,39 @@
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiplication() {
+  document.querySelector("#results").value += "*";
+}
+
+function division() {
+  document.querySelector("#results").value += "/";
+}
+
+function negative1() {
+  var current=document.querySelector("#results").value;
+  document.querySelector("#results").value = current *-1;
+}
+
+
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  var current=document.querySelector("#results").value;
+  document.querySelector("#results").value = current.substring(0, current.length - 1);
+}
