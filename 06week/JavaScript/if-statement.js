600dbf10612ff75104c4a9fef7bf84e810deddaf
MichaelSam <michaelsam08@gmail.com>
2018-03-01 10:00:13 -0600

    updated CP2

---
+++ 06week/JavaScript/if-statement.js
@@ -0,0 +1,9 @@
+var fruit = 'orange';
+if (fruit > 1) {
+  console.log('The fruit name has more than five characters');
+} else {
+  console.log('The fruit name has five characters or less');
+}
+}
+
+console.log(orange);
