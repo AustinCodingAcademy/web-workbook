3667852109074c1931bc30aeca3c6bad2bef8472
Chris DeSilva <chris.a.desilva@gmail.com>
2018-02-28 20:58:25 -0600

    Worked on JS in DOM

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
+  // You code here
+  let listLength = document.getElementsByTagName('li').length;
+  // alert("There are " + listLength + " items in your cart.");
+  var h = document.createElement("H2");
+  var t = document.createTextNode("You have " + listLength + " items in your shopping cart.");
+  h.appendChild(t);
+  var element = document.getElementById("h1");
+  element.appendChild(h);
+
+  // add new items
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode("Something new");
+  node.appendChild(textnode);
+  document.getElementById("myList").appendChild(node);
+
+  //remove items
+  var list = document.getElementById("myList");
+  list.removeChild(list.childNodes[1]);
+
 });
