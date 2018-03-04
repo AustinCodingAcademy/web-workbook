5a2fc4a4a93a5f62c602fd7f4d60842800e78379
Alex Ponce <Hailthesun24@gmail.com>
2018-03-04 16:27:55 -0600

    checkpoint beautify, and tweaks.

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,16 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+let list = document.querySelectorAll('li');
+
+// window.alert(list.length);
+
+let ul = document.querySelector('ul');
+
+var itemCount = 0
+function addItem() {
+    itemCount++
+  document.querySelector('#item-counter').innerHTML = `
+   <h2>You Have ${itemCount} items in your shopping cart</h2>
+  `;
+  console.log(itemCount);
+}
