fee0f4903234ad0ae87fcb9e6d5df8e93c4bc728
Nicholas Smith <smith.komurov@gmail.com>
2018-03-05 14:41:00 -0600

    dom practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -2,4 +2,12 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
     // You code here
+    alert("There are "+document.getElementsByTagName("li").length+" items in your cart.");
+    var cartItems=document.getElementsByTagName("li").length;
+    var node=document.createElement("h2");
+    var textnode=document.createTextNode("You have "+cartItems+" items in your cart.")
+    node.appendChild(textnode);
+    document.getElementById('cartTitle').appendChild(node);
+
+    
 });
