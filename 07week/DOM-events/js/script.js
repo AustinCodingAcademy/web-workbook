857dc61b6a7220a18bc678e93d7708765b523f55
Peter Slavin <pbslavin@gmail.com>
2018-03-07 16:31:51 -0800

    added DOM-events

---
+++ 07week/DOM-events/js/script.js
@@ -0,0 +1,38 @@
+// when the document is done loading, run the script inside
+$(document).ready(function() {
+  alert($('#alert').text())
+  var num1 = 0;
+  var num10 = 0;
+  var num100 = 0;
+  $(function() {
+    $('.box').draggable();
+  });
+  $('#counter-1').click(function() {
+    num1++;
+    $(this).text(num1);
+  });
+  $('#counter-10').click(function() {
+    num10 += 10;
+    $(this).text(num10);
+  });
+  $('#counter-100').click(function() {
+    num100 += 100;
+    $(this).text(num100);
+  });
+  $('#all').click(function() {
+    num1 ++;
+    num10 += 10;
+    num100 += 100;
+    $('#counter-1').text(num1);
+    $('#counter-10').text(num10);
+    $('#counter-100').text(num100);
+  });
+  $('#reset').click(function() {
+    num1 = 1;
+    num10 = 10;
+    num100 = 100;
+    $('#counter-1').text(num1);
+    $('#counter-10').text(num10);
+    $('#counter-100').text(num100);
+  });
+});
--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
+
+    const liList = document.querySelectorAll("li");
+    alert("There are " + liList.length + " list items on this page.");
+
+    const itemsMessage = document.querySelector("#items-message");
+    const items = liList.length;
+    itemsMessage.innerHTML = `<h2>You have ${items} items in your shopping cart.</h2>`;
+
+    // function addItem() {
+      const itemNum = document.querySelector();
+      const item = document.querySelector('#' + itemNum);
+      const itemName = item.innerHTML.split(':')[0];
+      const listItem = document.createElement('li');
+      listItem.onclick = function(e) { this.parentNode.removeChild(this) }
+      listItem.innerHTML = itemName;
+      const unorderedList = document.querySelector('ul');
+      unorderedList.appendChild(listItem);
+
+    // }
 });
