c0e55bae46a0fb7770e3b89635bb1ad6627d87c2
Neafie <rjneafie@usgs.gov>
2018-03-08 23:35:04 -0600

    starting towers.  not reading attributes.

---
+++ 09week/towers-of-hanoi/js/script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+$(document).ready(function () {
+  var $block = null;
+  let $blocksize = 0;
+
+  $('[data-stack]').click(function () {
+    if (!$block) {
+      $blocksize = $(this).children().last().prop('[data-block]');
+      console.log(`${$blocksize}`);
+      $block = $(this).children().last().detach();
+    } else {
+      $(this).append($block);
+      $block = null;
+    }
+  })
+
+  function rainbow(numOfSteps, step) {
+    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+    // Adam Cole, 2011-Sept-14
+    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+    var r, g, b;
+    var h = step / numOfSteps;
+    var i = ~~(h * 6);
+    var f = h * 6 - i;
+    var q = 1 - f;
+    switch (i % 6) {
+    case 0:
+      r = 1;
+      g = f;
+      b = 0;
+      break;
+    case 1:
+      r = q;
+      g = 1;
+      b = 0;
+      break;
+    case 2:
+      r = 0;
+      g = 1;
+      b = f;
+      break;
+    case 3:
+      r = 0;
+      g = q;
+      b = 1;
+      break;
+    case 4:
+      r = f;
+      g = 0;
+      b = 1;
+      break;
+    case 5:
+      r = 1;
+      g = 0;
+      b = q;
+      break;
+    }
+    var c = "#" + ("00" + (~~(r * 255)).toString(16)).slice(-2) + ("00" + (~~(g * 255)).toString(16)).slice(-2) + ("00" + (~~(b * 255)).toString(16)).slice(-2);
+    return (c);
+  }
+  for (let i = 0; i < 5; i++) {
+    let color = rainbow(5, i);
+    console.log(color);
+  }
+});
