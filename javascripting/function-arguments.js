90e93c9adce0ef3f5fffaffce52494f7107a9812
Alex Ponce <Hailthesun24@gmail.com>
2018-03-12 16:07:44 -0500

    javascripting, and checkpoint edits.

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,4 @@
+function math(coheed,and,cambria) {
+  return and * cambria + coheed;
+}
+ console.log(math(53,61,67));
--- javascripting/looping-through-arrays.js
+++ javascripting/looping-through-arrays.js
@@ -1,3 +1,5 @@
 var pets = ['cat', 'dog', 'rat'];
-for (var i = 0; i < pets[i] = pets[i] + 's'; i++)
+for (var i = 0; i < pets.length; i++) {
+  pets[i] = pets[i] + 's';
+}
 console.log(pets);
