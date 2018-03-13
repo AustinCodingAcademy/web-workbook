e34217c18b3c411df484d6bc8e953ccd14c82bea
Alex Ponce <Hailthesun24@gmail.com>
2018-03-13 15:43:38 -0500

    Towers of Hanoi jquery

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,16 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  let $block = $('div');
+  if  ($block) {
+      $(this).append($blue)
+      $block = null;
+  }
+  else {
+    $block.click(function() {
+        $(this).children().last().detach();
+      })
+    }
+
+
 });
