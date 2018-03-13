1a97c68fefa3675854d6f55c5fd7c05fe2f43d8c
David Gonzales <dgonzales033@satx.rr.com>
2018-03-12 21:17:12 -0500

    towers of hanoi game

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,4 +2,23 @@
 
 $(document).ready(function() {
   // Put app logic here
+  var $block = null;
+
+  $('[data-stack]').click(function() {
+    if ($block) {
+      $(this).append($block);
+      $block = null;
+    } else {
+      $block =
+  $(this).children().last().detach();
+    }
+  })
+
+  // function gameRule() {
+  //   if $('[data-block="25"]') < $('[data-block="50"]') <  $('[data-block="75"]') < $('[data-block="100"]')
+  //     return false;
+  //   }
+  // }
+
+
 });
