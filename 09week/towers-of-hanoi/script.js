f803d87cf80b4bff840b9b36fe6b6676058d8f61
Peter Slavin <pbslavin@gmail.com>
2018-03-14 18:30:24 -0500

    added tic-tac-toe and towers-of-hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
+$(function() {
+  let $block = null;
+
+  $('[data-stack]').click(function() {
+    if ($block) {
+      if (!($(this).html().trim()) || $(this).children().last().attr('data-block') > $block.attr('data-block')) {
+        $(this).append($block);
+        $block = null;
+      }
+    } else {
+      $block = $(this).children().last().detach();
+    }
+    if ($(this).children().length === 4 && $(this).attr('data-stack') !== '1') {
+      $('#announce-game-won').html("<h1>You win!</h1>");
+    }
+  });
 });
