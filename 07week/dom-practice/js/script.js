111cfabe1cea64db7c1e6f69b5a5eb94bad2cf76
Briexmac <briexmac@gmail.com>
2018-03-14 15:53:02 -0500

    tic toe & towers of hanoi

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,21 @@
 'use strict';
+/*Create a JS file and write code the will do the following:
+Alert the user to how many list items there are on the page.
+Add a <h2> to the page under the title that says 'You have _ items in your shopping cart'. Do not use html to do this. Use JavaScript.
+Create the ability to add more items to the list using JavaScript.
+Create the ability to remove an item from the shopping cart using javascript.
+Extension Challenge: Create the ability to display a picture of the product when the mouse hovers over the name of the project.*/
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+window.alert( "Size: " + $( "li" ).length );
+
+var t = document.createTextNode("You have" + $( "li" ).length "in your cart");
+   para.appendChild(t);
+   document.getElementById("myDIV").appendChild(para);
+
+function adder(obj){
+    var item = $(obj).prev().andSelf().add();
+}
+
+function remover(obj){
+    var item = $(obj).prev().andSelf().remove();
+}
--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -2,4 +2,72 @@
 
 $(document).ready(function() {
   // Put app logic in here
+  var playerTurn = 'O';
+
+  $('[data-cell]').on('click', function() {
+    $(this).text(playerTurn);
+
+    //Check for win
+    verticalWin();
+    horizontalWin();
+    diagonalWin();
+
+
+    if (playerTurn === 'X'){
+      playerTurn = 'O';
+    } else {
+      playerTurn = 'X';
+    }
+
+  });
+
+
+  function verticalWin() {
+    if ($('[data-cell="2"]').text() === 'X' && $('[data-cell="5"]').text() === 'X' && $('[data-cell="8"]').text() === 'X'){
+      alert('X wins!');
+    } else if ($('[data-cell="1"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="7"]').text() === 'X'){
+      alert('X wins');
+    } else if ($('[data-cell="0"]').text() === 'X' && $('[data-cell="3"]').text() === 'X' && $('[data-cell="6"]').text() === 'X'){
+      alert('X wins');
+    } else if ($('[data-cell="2"]').text() === 'O' && $('[data-cell="5"]').text() === 'O' && $('[data-cell="8"]').text() === 'O'){
+      alert('O wins');
+    } else if ($('[data-cell="1"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="7"]').text() === 'O'){
+      alert('O wins');
+    } else if ($('[data-cell="0"]').text() === 'O' && $('[data-cell="3"]').text() === 'O' && $('[data-cell="6"]').text() === 'O'){
+      alert('0 wins');
+    }
+  }
+
+  function horizontalWin() {
+    if ($('[data-cell="0"]').text() === 'X' && $('[data-cell="1"]').text() === 'X' && $('[data-cell="2"]').text() === 'X'){
+      alert('X wins!');
+    } else if ($('[data-cell="3"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="5"]').text() === 'X'){
+      alert('X wins!');
+    } else if ($('[data-cell="6"]').text() === 'X' && $('[data-cell="7"]').text() === 'X' && $('[data-cell="8"]').text() === 'X'){
+      alert('X wins!');
+    } else if ($('[data-cell="0"]').text() === 'O' && $('[data-cell="1"]').text() === 'O' && $('[data-cell="2"]').text() === 'O'){
+      alert('O wins!');
+    } else if ($('[data-cell="3"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="5"]').text() === 'O'){
+      alert('O wins!');
+    } else if ($('[data-cell="6"]').text() === 'O' && $('[data-cell="7"]').text() === 'O' && $('[data-cell="8"]').text() === 'O'){
+      alert('0 wins!');
+    }
+  }
+
+  function diagonalWin() {
+    if ($('[data-cell="2"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="6"]').text() === 'X'){
+      alert('X wins!');
+    } else if ($('[data-cell="0"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="8"]').text() === 'X'){
+      alert('X wins!');
+    } else if ($('[data-cell="2"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="6"]').text() === 'O'){
+      alert('O wins!');
+    } else if ($('[data-cell="0"]').text() === 'O' && $('[data-cell="4"]').text() === 'O' && $('[data-cell="8"]').text() === 'O'){
+      alert('O wins!');
+    }
+  }
+
+  $('#clear').on('click', function() {
+    location.reload();
+  });
+
 });
