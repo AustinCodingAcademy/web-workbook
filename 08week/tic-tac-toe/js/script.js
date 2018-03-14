cf9a942b618742b410b4dab136efc96647bf2eaf
Melissa Hale <melissakayh@gmail.com>
2018-03-13 19:13:45 -0500

    push finished ttt

--- 08week/tic-tac-toe/js/script.js
+++ 08week/tic-tac-toe/js/script.js
@@ -1,61 +1,42 @@
 'use strict';
 
 $(document).ready(function() {
-  'use strict';
-
-$(document).ready(function() {
   let gamePiece = 'X';
+  let winningCombs = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
   $('div[data-cell]').click(function() {
     $(this).text(`${gamePiece}`);
+    checkForWin();
     if (gamePiece === 'O') {
       gamePiece = 'X';
     } else {
       gamePiece = 'O';
     };
-    checkForX();
   });
 
-// Winning positions //
-var horizontalTop = "0,1,2";
-var horizontalMid = "3,4,5";
-var horizontalBottom = "6,7,8";
-var diagonalTopLeft = "0,4,8";
-var diagonalTopRight = "2,4,6";
-var verticalLeft = "0,3,6";
-var verticalMid = "1,4,7";
-var verticalRight = "2,5,8";
+  $('#clear').click(function () {
+    location.reload();
+  });
 
-function checkForX () {
-  var allElements = document.querySelectorAll("div[data-cell]");
-  var xElementsArray = [];
-  var workingArray = [];
-  for (var i = 0; i < allElements.length; i++) {
-    if (allElements[i].textContent == 'X') {
-      xElementsArray.push(allElements[i].dataset.cell);
+  function checkForWin() {
+  for (let i = 0; i < winningCombs.length; i++) {
+    let won = true;
+    for (let j = 0; j < winningCombs[i].length; j++) {
+      won = ( ($(`[data-cell="${winningCombs[i][j]}"]`).text() === gamePiece) && won);
     }
-  }
-  console.log(xElementsArray);
-  // if (xElementsString.includes("0","1","2") == true) {
-  //   // document.querySelector('#clear').insertAdjacentHTML('afterend','<h5 class="red-text">X wins!</h5>');
-  //   console.log('X wins');
-  for (var i = 0; i < xElementsArray.length; i++) {
-    if (horizontalTop.indexOf(xElementsArray[i]) !== -1) {
-      workingArray.push(xElementsArray[i]);
+    if (won) {
+      document.querySelector('#clear').insertAdjacentHTML('afterend',`<h3 class="red-text blinking">${gamePiece} wins!</h3>`);
+      return true;
     }
-    console.log("this" + workingArray);
   }
 };
-
-
-
-});
-
-
-//
-// function verticalWin() {
-// if ($('[data-cell="2"]').text() === 'X' && $('[data-cell="5"]').text() === 'X' && $('[data-cell="8"]').text() === 'X'){
-//  console.log('X wins!');
-// } else if ($('[data-cell="1"]').text() === 'X' && $('[data-cell="4"]').text() === 'X' && $('[data-cell="7"]').text() === 'X'){
-//  console.log('X wins!');
-// } else if
 });
