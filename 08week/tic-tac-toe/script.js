4a1f3d7781f9abb49c26783497ad4e46965d23c7
Eric Voss <eric.w.voss@icloud.com>
2018-03-15 17:10:28 -0500

    Changes

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -2,4 +2,75 @@
 
 $(document).ready(function() {
   // Put app logic in here
+  var playerTurn = 'X';
+
+  $('[data-cell]').on('click', function() {
+    $(this).text(playerTurn);
+
+    //Check for win
+    verticalWin();
+    horizontalWin();
+    diagonalWin();
+
+
+    // if playerTurn is X, then make
+    //playerTurn 0. If playerTurn is
+    //not X, make it X.
+
+    if (playerTurn === 'X') {
+      playerTurn = 'O';
+    } else {
+      playerTurn = 'X';
+    }
+
+  });
+
+
+  function verticalWin() {
+    if ($('[data-cell="2"]').text() === $('[data-cell="5"]').text() && $('[data-cell="5"]').text() === $('[data-cell="8"]').text() && $('[data-cell="8"]').text() != '') {
+      console.log(playerTurn + " wins!");
+      document.getElementById("p-ann").innerHTML = (playerTurn + " wins!");
+    } else if ($('[data-cell="1"]').text() === $('[data-cell="4"]').text() && $('[data-cell="4"]').text() === $('[data-cell="7"]').text() && $('[data-cell="7"]').text() != '') {
+      console.log(playerTurn + " wins!");
+      document.getElementById("p-ann").innerHTML = (playerTurn + " wins!");
+    } else if ($('[data-cell="0"]').text() === $('[data-cell="3"]').text() && $('[data-cell="3"]').text() === $('[data-cell="6"]').text() && $('[data-cell="6"]').text() != '') {
+      console.log(playerTurn + " wins!");
+      document.getElementById("p-ann").innerHTML = (playerTurn + " wins!");
+    }
+
+  }
+
+  function horizontalWin() {
+    if ($('[data-cell="6"]').text() === $('[data-cell="7"]').text() && $('[data-cell="7"]').text() === $('[data-cell="8"]').text() && $('[data-cell="8"]').text() != '') {
+      console.log(playerTurn + " wins!");
+      document.getElementById("p-ann").innerHTML = (playerTurn + " wins!");
+    } else if ($('[data-cell="3"]').text() === $('[data-cell="4"]').text() && $('[data-cell="4"]').text() === $('[data-cell="5"]').text() && $('[data-cell="5"]').text() != '') {
+      console.log(playerTurn + " wins!");
+      document.getElementById("p-ann").innerHTML = (playerTurn + " wins!");
+    } else if ($('[data-cell="0"]').text() === $('[data-cell="1"]').text() && $('[data-cell="1"]').text() === $('[data-cell="2"]').text() && $('[data-cell="2"]').text() != '') {
+      console.log(playerTurn + " wins!");
+      document.getElementById("p-ann").innerHTML = (playerTurn + " wins!");
+    }
+  }
+
+
+  function diagonalWin() {
+    if ($('[data-cell="0"]').text() === $('[data-cell="4"]').text() && $('[data-cell="4"]').text() === $('[data-cell="8"]').text() && $('[data-cell="8"]').text() != '') {
+      console.log(playerTurn + " wins!");
+      document.getElementById("p-ann").innerHTML = (playerTurn + " wins!");
+    } else if ($('[data-cell="2"]').text() === $('[data-cell="4"]').text() && $('[data-cell="4"]').text() === $('[data-cell="6"]').text() && $('[data-cell="6"]').text() != '') {
+      console.log(playerTurn + " wins!");
+      document.getElementById("p-ann").innerHTML = (playerTurn + " wins!");
+    }
+  }
+
+$("button").click(function(){
+        $(".dc").empty();
+        $("#p-ann").empty();
+        playerTurn = 'X';
+    });
+
+
+
+
 });
--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,38 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
-});
+  let $block = null;
+
+  // $('[data-stack]').click(function() {
+  //   if ($block) {
+  //     $(this).append($block);
+  //     $block = null;
+  //   } else {
+  //     $block = $(this).children().last().detach();
+  //   }
+
+  $('[data-stack]').click(function() {
+
+    if ($block) {
+      let $lastBlock = $(this).children().last();
+      if (!$lastBlock.length|| $lastBlock.data('block') > 
+
+      $(this).append($block);
+      $block = null;
+    } else {
+      $block = $(this).children().last().detach();
+    }
+
+
+    function checkForWin() {
+      if ($)
+       $('announce-game-won').text("You won!")
+
+    }
+
+
+    $( "[data0stack]").sortable(}
+      connectWith
+
+  })
+})
