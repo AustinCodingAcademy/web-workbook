a696cebda9b65a9728494f7dd952ad4de558a747
Melissa Hale <melissa.hale@q2ebanking.com>
2018-03-15 17:38:01 -0500

    commit towers

--- 09week/towers-of-hanoi/js/script.js
+++ 09week/towers-of-hanoi/js/script.js
@@ -3,23 +3,26 @@
 $(document).ready(function() {
   // Put app logic here
 
-let $gamePiece = null;
+  let $gamePiece = null;
 
-$("[data-stack]").click(function() {
-  if ($gamePiece) {
-    let $lastBlock = $(this).children().last();
-    if (!$lastBlock.length || $lastBlock.data('block') > $gamePiece)
-    // console.log($gamePiece.data);  <-------
-    $(this).append($gamePiece);
-    $gamePiece = null;
-  } else {
-    $gamePiece = $(this).children().last().detach();
+  $("[data-stack]").click(function() {
+    if ($gamePiece) {
+      let $lastBlock = $(this).children().last();
+      if (!$lastBlock.length || $lastBlock.data('block') > $gamePiece.data('block')) {
+        $(this).append($gamePiece);
+        $gamePiece = null;
+        checkForWin();
+      }
+    } else {
+      $gamePiece = $(this).children().last().detach();
+    }
+  });
+
+  function checkForWin() {
+    if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4) {
+      $('#announce-game-won').text('You Won');
+    }
   }
-});
 
-function checkForWin () {
-  if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 3);
-  $('#acnnounce').text('You Won');  //<---------correct this
-}
 
 });
