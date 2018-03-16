b697be90dc1bc3b7bd20b412d5eec07bd9160fd5
xianaustin <xianacevedo2019@gmail.com>
2018-03-15 23:01:19 -0500

    install eslint

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,80 @@
 'use strict';
 
+// $(document).ready(function() {
+//   var $sign = 'X'
+//   $('[data-cell]').click(function() {
+//     $(this).text($sign);
+//     if ($sign === 'X') {
+//       $sign = 'O'
+//     } else {
+//       $sign = 'X';
+//     }
+//   })
+// });
+
+
+// 0 1 2
+// 3 4 5
+// 6 7 8 grid set up
+
+// winning combinations are :
+// 0 1 2
+// 3 4 5
+// 6 7 8
+// 0 3 6
+// 1 4 7
+// 2 5 8
+// 0 4 8
+// 6 4 2
+
+
 $(document).ready(function() {
-  // Put app logic in here
+  var $sign = 'X'
+  $('[data-cell]').on('click',function() {
+    $(this).text($sign);
+      checkForWin()
+    if ($sign === 'O') {
+      $sign = 'X'
+    } else {
+      $sign = 'O';
+    }
+
+  })
+  $('#clear').on('click', function () {
+    location.reload();
+  })
+
+  var winningArray = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [6,4,2]
+  ];
+
+
+  function checkForWin() {
+    for (let i=0;i<winningArray.length;i++) {
+      if ($(`[data-cell="${winningArray[i][2]}"]`).text() !== '') {
+        if ($(`[data-cell="${winningArray[i][0]}"]`).text() === $(`[data-cell="${winningArray[i][1]}"]`).text() &&
+          $(`[data-cell="${winningArray[i][1]}"]`).text() === $(`[data-cell="${winningArray[i][2]}"]`).text() ) {
+          $('#announce-winner').text(`${$sign} has won!`);
+        }}
+    }
+  }
+
+    // function checkForWin() {
+    //     for (let i=0;i<winningArray.length;i++) {
+    //         for (let j=0; j<winningArray[i].length;j++) {
+    //             if ($(`[data-cell="${winningArray[i][j]}"]`).text() === $sign && $(`[data-cell="${winningArray[i][j]}"]`).text() !== ''  ) {
+    //                 $('#announce-winner').text(`${$sign} has won!`);
+    //             }
+    //         }
+    //     }
+    // }
+
+
 });
--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  var $block = null;
+    $('[data-stack]').click(function() {
+        if ($block !== null && Number($block.attr('data-block')) < Number($(this).children().last().attr('data-block') )) {
+            console.log($block.attr('data-block'));
+
+            $(this).append($block);
+            $block = null;
+        }
+        else if ()
+
+        else {
+            $block = $(this).children().last().detach();
+        }
+
+    })
 });
