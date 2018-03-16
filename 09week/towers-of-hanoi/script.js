59b70dee45fc631cece42bb1c461186ebf76416c
Dylan Zocchi <dxocchi@gmail.com>
2018-03-15 23:34:01 -0500

    09week

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,26 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  let $block = null
+
+  $('[data-stack]').click(function() {
+    if ($block) {
+      let $lastBlock = $(this).children().last();
+      if (!$lastBlock.length || $lastBlock.data('block') > $block.data('block')) {
+      $(this).append($block);
+      $block = null;
+      checkForWin();
+      }
+    } else {
+      $block = $(this).children().last().detach()
+    }
+
+
+
+    function checkForWin() {
+      if ($('[data-stack="2"]').children().length === 4 ||  $('[data-stack="3"]').children().length === 4) {
+        $('#announce-game-won').text('WINNER');
+      }
+    }
+  })
 });
