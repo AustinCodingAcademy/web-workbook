a77aa70f472866867dbc152f3d885a2482d9ce4c
Eric Voss <eric.w.voss@icloud.com>
2018-03-16 17:17:34 -0500

    Towers of Hanoi changes

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,38 +1,28 @@
 'use strict';
 
 $(document).ready(function() {
-  let $block = null;
 
-  // $('[data-stack]').click(function() {
-  //   if ($block) {
-  //     $(this).append($block);
-  //     $block = null;
-  //   } else {
-  //     $block = $(this).children().last().detach();
-  //   }
+  let $block = null
 
   $('[data-stack]').click(function() {
 
     if ($block) {
       let $lastBlock = $(this).children().last();
-      if (!$lastBlock.length|| $lastBlock.data('block') > 
+      if (!$lastBlock.length || $lastBlock.data('block') > $block.data('block')){
 
       $(this).append($block);
       $block = null;
+      checkForWin();
+      }
     } else {
-      $block = $(this).children().last().detach();
+      $block = $(this).children().last().detach()
     }
-
+  })
 
     function checkForWin() {
-      if ($)
-       $('announce-game-won').text("You won!")
-
-    }
-
-
-    $( "[data0stack]").sortable(}
-      connectWith
-
-  })
-})
+      if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4)
+      {
+       $('#announce-game-won').text("You won!");
+      }
+}
+});
