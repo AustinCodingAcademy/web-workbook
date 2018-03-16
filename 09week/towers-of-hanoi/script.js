a0ebd21065fde36a1703e48dcb5e4ae2a6c6d021
Ukiah Richardson <urichardson74@gmail.com>
2018-03-16 05:08:54 -0500

    updates

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,27 @@
 'use strict';
-
+/*Ready?*/
 $(document).ready(function() {
   // Put app logic here
+var $block = null;
+/*Target the last child of a stack*/
+  $("[data-stack]").click(function(){
+    var $thisBlock = $(this).children();
+    var $lastBlock = $thisBlock.last();
+/*Detach the last child and append it to an empty stack or one with a bigger block than the one you've detached*/
+    if ($block == null) {
+      $block = $lastBlock.detach();
+    } else {
+      if ($lastBlock.data("block") < $block.data("block")) {
+        alert("Invalid Move");
+      } else {
+        $(this).append($block);
+        $block = null;
+      }
+    }
+    /*You win if either of the last 2 stacks have all four blocks*/
+    if ( $("[data-stack='2']").children().length >= 4 || $("[data-stack='3']").children().length >= 4 )
+  {
+    alert("You Win!!");
+  }
+  });
 });
