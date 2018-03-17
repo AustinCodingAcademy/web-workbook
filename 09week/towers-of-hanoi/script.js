e60992002c717bbf4e699618a021d1ca76b1cf48
David Gonzales <dgonzales033@satx.rr.com>
2018-03-17 16:31:04 -0500

    rev to form, add checkpoint3

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -4,20 +4,53 @@ $(document).ready(function() {
   // Put app logic here
   var $block = null;
 
-  $('[data-stack]').click(function() {
-    if ($block) {
+  $("[data-stack]").click(function(){
+  var $thisBlock = $(this).children();
+  var $lastBlock = $thisBlock.last();
+/*Detach the last child and append it to an empty stack or one with a bigger block than the one you've detached*/
+  if ($block == null) {
+    $block = $lastBlock.detach();
+  } else {
+    if ($lastBlock.data("block") < $block.data("block")) {
+      alert("Invalid Move");
+    } else {
       $(this).append($block);
       $block = null;
-    } else {
-      $block =
-  $(this).children().last().detach();
     }
-  })
+  }
+  /*You win if either of the last 2 stacks have all four blocks*/
+  if ($("[data-stack='3']").children().length >= 4)
+{
+  alert("You Win!!");
+}
+});
+
+
+
+  // $('[data-stack]').click(function() {
+  //     if ($block) {
+  //       $(this).append($block);
+  //       $block = null;
+  //     } else {
+  //       $block = $(this).children().last().detach();
+  //     }
+  //   })
+
+  // $('[data-stack]').on('click', function() {
+  //     if ($block) {
+  //       $block = $(this).children().last().detach();
+  //     } else {
+  //       $(this).append($block);
+  //       block = null;
+  //     }
+  //   })
 
   // function gameRule() {
-  //   if $('[data-block="25"]') < $('[data-block="50"]') <  $('[data-block="75"]') < $('[data-block="100"]')
-  //     return false;
-  //   }
+  //
+  //   if $('$block').children().last < $(event.currentTarget).children().last().detach();
+  // } else {
+  //   block = null
+  // }
   // }
 
 
