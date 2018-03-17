40a87534696b8ad212e8ce73a5b5dba26b51da28
Alex Ponce <Hailthesun24@gmail.com>
2018-03-17 12:58:26 -0500

    towers of hanoi, and customer satisfaction

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,16 +1,22 @@
 'use strict';
 
 $(document).ready(function() {
-  let $block = $('div');
-  if  ($block) {
-      $(this).append($blue)
-      $block = null;
-  }
-  else {
-    $block.click(function() {
-        $(this).children().last().detach();
-      })
-    }
+   var $block = null;
+   $('[data-stack]').click(function() {
+     if ($block) {
+      let $lastBlock = $(this).children().last();
+       if  (!$lastblock.length  || $(block.data('block') < $lastBlock.data)
+       $(this).append($block);
+       $block = null;
+       checkWin();
+     } else {
+       $block = $(this).children().last().detach();
+     }
+   });
 
+   function checkWin() {
+     if ($('[data-stack="2"]').children().length() === 4 || ($'[data-stack="3"]'))
+     $('announce-game-won').text('You Won!');
+   }
 
 });
