b646a8582b8cbfc3b3f35f242262a93e33509f7b
Hoegenauer <jhoegenauer@usgs.gov>
2018-03-18 10:40:52 -0500

    finished logic issues for Towers of Hanoi game, would like to make columns vertical

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,36 @@
 'use strict';
-
+/*start after page load */
 $(document).ready(function() {
-  // Put app logic here
-});
+  let $block = null;
+
+  $('[data-stack]').click(function() {
+
+    if ($block) {
+      let $lastBlock = $(this).children().last();
+      if (!$lastBlock.length || $lastBlock.data('block') > $block.data('block')){
+         /*this tells the game to let your block stack if it is smaller than the piece below*/
+        $(this).append($block);
+        $block = null;
+        checkForWin();
+      }
+
+    } else {
+      /*if your piece is bigger than the one below, you can't stack it*/
+      $block = $(this).children().last().detach();
+    }
+  })
+
+  function checkForWin() {
+    /*checks to see if stack has been rebuilt on different column*/
+    if ($('[data-stack="3"]').children().length === 4) {
+      alert('You Won!');
+    }
+    else if ($('[data-stack="2"]').children().length === 4) {
+      alert('You Won!');
+    }
+  }
+
+  $('#clear').on('click', function() {
+    location.reload();
+  });
+})
