f1e0d90934ca2205169196ae00785e34b1c6caf7
xianaustin <xianacevedo2019@gmail.com>
2018-03-17 19:58:00 -0500

    towers of hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,19 +1,22 @@
 'use strict';
 
 $(document).ready(function() {
-  var $block = null;
-    $('[data-stack]').click(function() {
-        if ($block !== null && Number($block.attr('data-block')) < Number($(this).children().last().attr('data-block') )) {
-            console.log($block.attr('data-block'));
-
-            $(this).append($block);
-            $block = null;
-        }
-        else if ()
-
-        else {
-            $block = $(this).children().last().detach();
-        }
-
-    })
+  let $block = null;
+  $('[data-stack]').click(function() {
+    if($block){
+      let $lastBlock = $(this).children().last();
+      if(!$lastBlock.length || $lastBlock.data('block') > $block.data('block')){
+        $(this).append($block);
+        $block = null;
+      }
+    }else {
+      $block = $(this).children().last().detach()
+    }
+    checkForWin();
+  });
+  function checkForWin() {
+    if ($('[data-stack="2"]').children().length === 5 ||$('[data-stack="3"]').children().length === 5) {
+      $('#announce-game-won').text('You won!');
+    }
+  }
 });
