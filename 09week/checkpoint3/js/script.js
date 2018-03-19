3eb252914ee27171cdb477f8efd4509316e34849
Dylan Zocchi <dxocchi@gmail.com>
2018-03-18 21:16:53 -0500

    ckpt3

--- 09week/checkpoint3/js/script.js
+++ 09week/checkpoint3/js/script.js
@@ -2,55 +2,57 @@
 
 $(document).ready(function() {
 
-var data = {
-  totalRevs:360,
-  totalCurrent:0,
-  totalRPS: 0
-};
+  var data = {
+    totalRevs: 360,
+    totalCurrent: 0,
+    totalRPS: 0
+  };
 
-setInterval(goGo,1000);
+  setInterval(goGo, 1000);
 
-function goGo() {
-  // var audio2 = $("#mysoundclip2")[0];
-  data.totalRevs += data.totalRPS;
-  data.totalCurrent += data.totalRPS;
-  $('#kick').css({ 'transform': 'rotate(' + data.totalRevs + 'deg)'});
-  updateReport();
-  // audio2.play();
+  function goGo() {
+    // var audio2 = $("#mysoundclip2")[0];
+    data.totalRevs += data.totalRPS;
+    data.totalCurrent += data.totalRPS;
+    $('#kick').css({
+      'transform': 'rotate(' + data.totalRevs + 'deg)'
+    });
+    updateReport();
+    // audio2.play();
 
-}
-
-
-function updateReport() {
-  var audio2 = $("#mysoundclip2")[0];
-  $("#currenttotal").text(Math.floor(data.totalCurrent));
-  $("#rps").text((data.totalRPS/70.4).toFixed(6));
-  audio2.play();
-}
-
-
-$("#kick").click(function (){
-  var audio = $("#mysoundclip")[0];
-  var audio2 = $("#mysoundclip2")[0];
-  data.totalRevs ++;
-  data.totalCurrent ++;
-  updateReport();
-  audio.play();
-  pulse();
-
-})
-
-
-$(".button").click(function (){
-  var addVal = $(this).data( "cost" );
-  if ($(this).data( "cost" ) < data.totalCurrent ) {
-    data.totalCurrent -=  parseFloat($(this).data( "cost" ).toPrecision(2));
-    data.totalRPS += parseFloat($(this).data( "val" ));
-    $( this ).children("span").html( parseInt($( this ).children("span").html()*1.15));
-    $( this ).data( "cost", parseInt($(this).data( "cost" ) * 1.15) );
   }
-  updateReport();
-})
+
+
+  function updateReport() {
+    var audio2 = $("#mysoundclip2")[0];
+    $("#currenttotal").text(Math.floor(data.totalCurrent));
+    $("#rps").text((data.totalRPS / 70.4).toFixed(6));
+    audio2.play();
+  }
+
+
+  $("#kick").click(function() {
+    var audio = $("#mysoundclip")[0];
+    var audio2 = $("#mysoundclip2")[0];
+    data.totalRevs++;
+    data.totalCurrent++;
+    updateReport();
+    audio.play();
+    pulse();
+
+  })
+
+
+  $(".button").click(function() {
+    var addVal = $(this).data("cost");
+    if ($(this).data("cost") < data.totalCurrent) {
+      data.totalCurrent -= parseFloat($(this).data("cost").toPrecision(2));
+      data.totalRPS += parseFloat($(this).data("val"));
+      $(this).children("span").html(parseInt($(this).children("span").html() * 1.15));
+      $(this).data("cost", parseInt($(this).data("cost") * 1.15));
+    }
+    updateReport();
+  })
 
 
 })
