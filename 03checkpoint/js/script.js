8919dde8875547754d880e05cf298117244b450c
Melissa Hale <melissa.hale@q2ebanking.com>
2018-03-19 17:47:48 -0500

    push checkpoint 3

--- 03checkpoint/js/script.js
+++ 03checkpoint/js/script.js
@@ -2,29 +2,45 @@
 
 $(document).ready(function() {
 
-  var timer;
+let totalBoxes = 0;
 
-  $('.box').one('click', function() {
+  $('#start').click(function() {
     console.log("start"); //can remove//
-    if (!timer) {
-      let timer = 10;
-      setInterval(function() {
-        timer--;
-        if (timer >= 0) {
-          let currTime = document.querySelector("#seconds");
-          console.log(currTime);
-          currTime.innerHTML = timer;
-        }
-        if (timer === 0) {
-          console.log('out of time');
-          clearInterval(timer);
-        }
-      }, 1000);
-    };
+    let timer = 3;
+
+    setInterval(function() {
+      timer--;
+      if (timer >= 0) {
+        let currTime = document.querySelector("#seconds");
+        console.log(currTime);
+        currTime.innerHTML = timer;
+      }
+      if (timer === 0) {
+        console.log('out of time');
+        clearInterval(timer);
+        totalBoxes = $('.hideBox').length;
+        console.log(totalBoxes);
+        reportWin (totalBoxes);
+      }
+    }, 1000);
   });
 
+
   $('.box').click(function() {
-    $(this).detach();
-  })
+    $(this).addClass("hideBox")
+  });
+
+  $('#playAgainButton').click(function() {
+    location.reload();
+  });
+
+function reportWin (totalBoxes) {
+  let reportWin = $("#reportWinBox");
+  reportWin = $("#reportWinBox").css("visibility", "visible");
+  console.log(reportWin);
+  if (totalBoxes > 5) {
+    reportWin.html(`You clicked ${totalBoxes} boxes! <br><button type="button" name="playAgain" id="playAgainButton">Play Again</button>`);
+  }
+};
 
 });
