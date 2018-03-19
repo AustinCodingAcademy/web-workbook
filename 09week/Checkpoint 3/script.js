bbe1810ee6a41cb770f443eb282d96a2291b2fed
Eric Voss <eric.w.voss@icloud.com>
2018-03-18 22:46:34 -0500

    Checkpoint 3

---
+++ 09week/Checkpoint 3/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+  $(document).ready(function() {
+    var count = 0;
+
+    function updateCounter( ) {
+        count++;
+        document.getElementById("counted").innerHTML = "Coins = " + count + "";
+    }
+
+
+  });
