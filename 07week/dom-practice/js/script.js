d6adac517e6f160b40cb7c8cc4ffc3786d74f8fb
Eric Voss <eric.w.voss@icloud.com>
2018-03-20 18:02:44 -0500

    misc changes

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,15 +1,15 @@
-'use strict';
-
-let items = document.querySelectorAll('li')
-
-alert(items.length)
-
-// document.querySelector('h1').insertAdjacentHTML('afterend',)
-  <h2>You have ${items.length} items in your shopping cart<h2>
-
-function addItem() {
-  let itemName = document.querySelector('input').value
-  let itemDescription = document.querySelector('textarea').value;
-  document.querySelector('ul').insertAdjacentHTML('beforeend',)
-  <li><li>
-}
+// 'use strict';
+//
+// let items = document.querySelectorAll('li')
+//
+// alert(items.length)
+//
+// // document.querySelector('h1').insertAdjacentHTML('afterend',)
+//   <h2>You have ${items.length} items in your shopping cart<h2>
+//
+// function addItem() {
+//   let itemName = document.querySelector('input').value
+//   let itemDescription = document.querySelector('textarea').value;
+//   document.querySelector('ul').insertAdjacentHTML('beforeend',)
+//   <li><li>
+// }
