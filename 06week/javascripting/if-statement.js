7777c960e377c5540e680611a685a5aafc89354b
Eric Voss <eric.w.voss@icloud.com>
2018-03-20 17:39:19 -0500

    misc. changes

--- 06week/javascripting/if-statement.js
+++ 06week/javascripting/if-statement.js
@@ -8,7 +8,7 @@ orange.length
 
 
 if (orange.length > 5) {
-       console.log('The fruit name has more than five characters.');
-     } else {
-       console.log('The fruit name has five characters or less.');
-     }
+  console.log('The fruit name has more than five characters.');
+} else {
+  console.log('The fruit name has five characters or less.');
+}
