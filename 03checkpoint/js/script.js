d4194f285eed730a7da6403589661a86dce1e400
Melissa Hale <melissakayh@gmail.com>
2018-03-19 21:00:48 -0500

    commit checkpoint 3

--- 03checkpoint/js/script.js
+++ 03checkpoint/js/script.js
@@ -2,45 +2,49 @@
 
 $(document).ready(function() {
 
-let totalBoxes = 0;
+  let totalBoxes = 0;
+  let started = false;
 
-  $('#start').click(function() {
-    console.log("start"); //can remove//
-    let timer = 3;
-
+  function start() {
+    hideStartBox ();
+    started = true;
+    let timer = 10;
     setInterval(function() {
       timer--;
       if (timer >= 0) {
         let currTime = document.querySelector("#seconds");
-        console.log(currTime);
         currTime.innerHTML = timer;
       }
       if (timer === 0) {
-        console.log('out of time');
         clearInterval(timer);
-        totalBoxes = $('.hideBox').length;
-        console.log(totalBoxes);
-        reportWin (totalBoxes);
+        totalBoxes = $('.hideGameBox').length;
+        reportWin(totalBoxes);
       }
     }, 1000);
-  });
+  };
 
+  function hideStartBox () {
+    $("#startBox").addClass("hideBox")
+  };
+
+  function reportWin(totalBoxes) {
+    let reportWin = $("#reportWinBox");
+    reportWin = $("#reportWinBox").css("visibility", "visible");
+    $("#reportWinBox").prepend(`<p>You cleared ${totalBoxes} boxes!</p>`);
+    // if (totalBoxes < 5) {
+    //   $("#reportWinBox").prepend(`<p>you can do better</p>`);
+    // }
+  };
 
   $('.box').click(function() {
-    $(this).addClass("hideBox")
+    if (!started) {
+      start()
+    };
+    $(this).addClass("hideGameBox")
   });
 
-  $('#playAgainButton').click(function() {
+  $("#reportWinBox").click(function() {
     location.reload();
   });
-
-function reportWin (totalBoxes) {
-  let reportWin = $("#reportWinBox");
-  reportWin = $("#reportWinBox").css("visibility", "visible");
-  console.log(reportWin);
-  if (totalBoxes > 5) {
-    reportWin.html(`You clicked ${totalBoxes} boxes! <br><button type="button" name="playAgain" id="playAgainButton">Play Again</button>`);
-  }
-};
 
 });
