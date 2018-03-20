79e4fdc5dae4b5b57af3f3364f2a4c26934103a2
Eric Voss <eric.w.voss@icloud.com>
2018-03-20 13:23:55 -0500

    checkpoint3

--- 09week/CoinDiverGame/script.js
+++ 09week/CoinDiverGame/script.js
@@ -29,20 +29,32 @@ function buyDiver() {
     document.getElementById('divers').innerHTML = divers;
     //updates the number of coins for the user
     document.getElementById('coins').innerHTML = coins;
+    addDiverImg ();
   };
   //works out the cost of the next diver
   var nextCost = Math.floor(10 * Math.pow(1.1, divers));
   //updates the diver cost for the user
   document.getElementById('diverCost').innerHTML = nextCost;
+
 };
 
-function resetButton (){
-            coins = 0;
-            divers = 0;
-            document.getElementById("coins").innerHTML = coins;
-            document.getElementById("divers").innerHTML = divers;
-            document.getElementById("button4").innerHTML = ("New Game");
-        };
+function addDiverImg() {
+  var img=document.createElement("img");
+      img.src="diver.png"
+      img.id="picture"
+      var foo = document.getElementById("scubaImage");
+      foo.appendChild(img);
+};
+
+function resetButton() {
+  $("#scubaImage").empty();
+  coins = 0;
+  divers = 0;
+  document.getElementById("coins").innerHTML = ("0");
+  document.getElementById("divers").innerHTML = ("0");
+  document.getElementById("diverCost").innerHTML = ("10");
+  document.getElementById("button4").innerHTML = ("New Game");
+};
 
 window.setInterval(function() {
   coinClick(divers);
