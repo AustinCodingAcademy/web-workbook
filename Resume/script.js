7607472b0fc7a0657a41cd32ac35f58ec283b4ce
Alex Ponce <Hailthesun24@gmail.com>
2018-03-19 22:44:03 -0500

    auto click attempt

--- Resume/script.js
+++ Resume/script.js
@@ -2,9 +2,24 @@
 
 $(document).ready(function() {
 var points = 0;
+var clicker1 = 100;
   $('#meatwad').click(function() {
     points += 1;
     $('#total').text("Total Clacks: " + points)
   });
+  $('.clicker-100').click(function () {
+    var f = $('#total') - 100;
+    $('#total').text("Total Clacks: " + f);
+    auto();
+  }); if {
+    $('#total') < 100;
+    $('.clicker1') = null;
+  }
+  function auto() {
+  let i = $(this).click($('.clicker1'));
+  for (var i = 0; i < $(this).click($('.clicker1').length; i++) {
+    $(this).click($('.clicker1')[i]
+  }
+  }
 
 });
