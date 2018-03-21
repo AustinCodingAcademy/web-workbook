ccb4ed412c2a09d11b578af622f60d8156f3eaa6
Eric Voss <eric.w.voss@icloud.com>
2018-03-21 13:38:58 -0500

    bio 2 changes

--- 10week/bio-part-2/js/script.js
+++ 10week/bio-part-2/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 });
 
 
-var c = document.getElementById("myCanvas1");
+var c = document.getElementById("mycanvas1");
 var ctx = c.getContext("2d");
 var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
 grd.addColorStop(0, "red");
@@ -15,13 +15,13 @@ grd.addColorStop(1, "white");
 ctx.fillStyle = grd;
 ctx.fillRect(0, 10, 750, 100);
 
-var canvas = document.getElementById("myCanvas1");
+var canvas = document.getElementById("mycanvas1");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#6495ed";
 ctx.fillRect(180, 20, 550, 70);
 
 
-var c = document.getElementById("myCanvas1");
+var c = document.getElementById("mycanvas1");
 var ctx = c.getContext("2d");
 ctx.font = "30px Arial";
 ctx.strokeText("Eric Voss Bio 2", 200, 65);
