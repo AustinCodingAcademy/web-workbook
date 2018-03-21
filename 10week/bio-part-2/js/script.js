b7103ec92cb8221296ec1c2af466d7821d90b274
Rbrey <35275423+Rbrey@users.noreply.github.com>
2018-03-21 15:02:46 -0500

    bio project

---
+++ 10week/bio-part-2/js/script.js
@@ -0,0 +1,27 @@
+'use strict'
+// ======================================
+// Variables
+// ======================================
+let c = document.getElementById("myCanvas"),
+    ctx = c.getContext("2d"),
+    grd=ctx.createRadialGradient(350,50,20,300,60,200);
+// ======================================
+// Radial Gradient
+// ======================================
+grd.addColorStop(0,"#0745c1");
+grd.addColorStop(1,"white");
+ctx.fillStyle=grd;
+ctx.fillRect(0,0,600,400);
+// ======================================
+// Name Text
+// ======================================
+ctx.font = "20px Georgia";
+ctx.strokeText("Robert W Brey", 10, 20);
+// ======================================
+// Favorite Color Filling Shape
+// ======================================
+ctx.beginPath();
+ctx.arc(100,75,50,0,2*Math.PI);
+ctx.stroke();
+ctx.fillStyle = "#153472";
+ctx.fill()
