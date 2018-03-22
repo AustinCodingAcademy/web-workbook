7a96b45cebe7c8143f668340189e92b3143f859e
Rbrey <35275423+Rbrey@users.noreply.github.com>
2018-03-21 22:05:03 -0500

    error fixing

--- 06week/javascripting/scope.js
+++ 06week/javascripting/scope.js
@@ -7,7 +7,7 @@ var a = 1, b = 2, c = 3;
     (function thirdFunction(){
       var a = 7, c = 9;
       (function fourthFunction(){
-          var a = 1, c = 8;
+        var a = 1, c = 8;
         })();
     })();
   })();
--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
   function win() {
     if ($('#tower2').children().length === 4 || $('#tower3').children().length === 4)
       $('#announce-game-won').text('Winner');
-    }
+  }
 });
 
 // Board*
