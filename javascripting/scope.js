d54c9b707afbe0e98fabab5a5d0064dba54d82f4
Ukiah Richardson <urichardson74@gmail.com>
2018-03-22 00:48:09 -0500

    last file?

--- javascripting/scope.js
+++ javascripting/scope.js
@@ -3,19 +3,19 @@ var b = 2;
 var c = 3;
 
 (function firstFunction(){
-    var b = 5;
-    var c = 6;
+  var b = 5;
+  var c = 6;
 
-    (function secondFunction(){
-        var b = 8;
-console.log("a: "+a+", b: "+b+", c: "+c);
-        (function thirdFunction(){
-            var a = 7;
-            var c = 9;
+  (function secondFunction(){
+      var b = 8;
+      console.log("a: "+a+", b: "+b+", c: "+c);
+          (function thirdFunction(){
+          var a = 7;
+          var c = 9;
 
-            (function fourthFunction(){
-                var a = 1;
-                var c = 8;
+          (function fourthFunction(){
+              var a = 1;
+              var c = 8;
 
             })();
         })();
