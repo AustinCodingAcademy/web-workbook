c72d5ea0cc98a4ed11aa4dff395491dea6832d1d
Ukiah Richardson <urichardson74@gmail.com>
2018-03-22 00:42:20 -0500

    green?

--- javascripting/scope.js
+++ javascripting/scope.js
@@ -1,19 +1,23 @@
-var a = 1, b = 2, c = 3;
+var a = 1;
+var b = 2;
+var c = 3;
 
 (function firstFunction(){
-  var b = 5, c = 6;
+    var b = 5;
+    var c = 6;
 
-  (function secondFunction(){
-    var b = 8;
-    console.log("a: "+a+", b: "+b+", c: "+c);
+    (function secondFunction(){
+        var b = 8;
+console.log("a: "+a+", b: "+b+", c: "+c);
+        (function thirdFunction(){
+            var a = 7;
+            var c = 9;
 
-    (function thirdFunction(){
-        var a = 7, c = 9;
+            (function fourthFunction(){
+                var a = 1;
+                var c = 8;
 
-          function fourthFunction(){
-            var a = 1, c = 8;
-
-          })();
+            })();
         })();
-  })();
+    })();
 })();
