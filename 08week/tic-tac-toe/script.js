a6eaad4181f65d36dc6fc2bde4ec0a1c9510cf80
jenny riess <jennyriess@gmail.com>
2018-03-22 15:23:15 -0500

    changes

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,29 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+
+var playerTurn = 'X';
+
+$('[data-cell]').on('click', function(){
+  $(this).text(playerTurn);
+  
+
+if (playerTurn === 'X'){
+  playerTurn = '0';
+} else {
+  playerTurn = 'X';
+}
 });
+
+function verticalWin() {
+  if ($('[data-cell="2"]').text() === 'X' && $...
+
+}
+
+horizontalWin()
+
+diagonalWin()
+
+checkForWin() {
+
+}
--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,21 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
-});
+  let $block = null;
+  $('[data-stack]').click(function(){
+    if ($block) {
+      let $$lastBlock = $(this).children().last();
+      if (!$lastBlock.length ll $block.data('block') < $lastBlock.data('block'))
+        $(this).append
+    }
+  })
+})
+
+
+
+
+
+
+function checkForWin() {
+  if ($)
+}
