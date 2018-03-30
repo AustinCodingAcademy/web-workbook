ed4ffdc2dc327d572232e8780a510daa4b0f180d
suneetha.kori <suneetha.kori@gmail.com>
2018-03-29 22:31:33 -0500

    Bio2 Audio

--- 10week/bio-part-2/js/script.js
+++ 10week/bio-part-2/js/script.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
 
-  let cnv = document.querySelector("#myCanvas");
+  let cnv = document.querySelector("#mycanvas");
   let ctx = cnv.getContext("2d");
 
   var grd = ctx.createLinearGradient(0, 0, 170, 0);
@@ -13,7 +13,6 @@ $(document).ready(function() {
   ctx.fillStyle = grd; //"#80ffff";
   ctx.fillRect(20, 20, 150, 100);
 
-
   ctx.beginPath();
 
   ctx.strokeStyle = grd;
@@ -23,46 +22,17 @@ $(document).ready(function() {
 
   ctx.font = "30px Verdana";
 
+  let cnv2 = document.querySelector("#my2canvas");
+  let ctx2 = cnv2.getContext("2d");
+
   // Create gradient
-  var gradient = ctx.createLinearGradient(0, 0, cnv.width, 0);
+  var gradient = ctx2.createLinearGradient(0, 0, cnv2.width, 0);
   gradient.addColorStop("0", "magenta");
   gradient.addColorStop("0.5", "blue");
   gradient.addColorStop("1.0", "red");
-  ctx.lineWidth = 2;
+  ctx2.lineWidth = 2;
 
   // Fill with gradient
-  ctx.strokeStyle = gradient;
-  ctx.strokeText("Suneetha Kori", 180, 200);
-
-
-  // var x = 250,
-  //     y = 250,
-  //     radius = 50,
-  //     // 0deg   - 1.5 * Pi,
-  //     // 90deg  - 0   * Pi,
-  //     // 180deg - 0.5 * Pi,
-  //     // 270deg - 1   * Pi
-  //     angleStart = 1.5 * Math.PI,
-  //     angleEnd = 0.7 * Math.PI;
-  //
-  // //Create gradient
-  // var gradient = ctx.createLinearGradient(0,500,0, 0);
-  // gradient.addColorStop(0, '#c0e674');
-  // gradient.addColorStop(1, '#40d6a5');
-  //
-  //
-  // //Draw circle
-  // ctx.beginPath();
-  // ctx.arc(x, x, radius, 0, 2*Math.PI, false);
-  // ctx.lineWidth = 30;
-  // ctx.strokeStyle = 'rgba(255,255,255, 0.2)'
-  // ctx.stroke();
-  //
-  // //Draw arc
-  // ctx.beginPath();
-  // ctx.arc(x, y, radius, angleStart, angleEnd);
-  // ctx.strokeStyle = gradient;
-  // ctx.lineWidth = 30;
-  // ctx.lineCap = 'round';
-  // ctx.stroke();
+  ctx2.strokeStyle = gradient;
+  ctx2.strokeText("Suneetha Kori", 180, 200);
 });
