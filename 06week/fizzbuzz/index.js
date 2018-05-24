b5b5d8c2b562e86df0a86ce7f71448ac3336d3f1
Sean Philippi <sean.philippi@gmail.com>
2018-05-24 14:21:13 -0500

    javascripting hw and fizzbuzz

---
+++ 06week/fizzbuzz/index.js
@@ -0,0 +1,20 @@
+//Fizzbuzz
+// Using the coding area below, looping through all numbers 1 through 1000,
+// I want you to log:
+//
+// 'Fizz' if the number is divisible by only three
+// 'Buzz' if the number is divisible by only five
+// 'FizzBuzz' if the number is divisible by three and five
+// the number itself if it is none of these
+
+
+for (x = 0; x < 1001; x++) {
+  if (x % 3 === 0)
+  console.log('Fizz');
+  if else (x % 5 === 0)
+  console.log('Buzz');
+  else ((x % 3 === 0) && (x % 3 === 0))
+  console.log('FizzBuzz');
+  else ()
+  console.log(x);
+}
