8334425785011ae3a9474e904361e9e52f952eab
miriam007 <miriamu88@gmail.com>
2018-05-28 01:24:11 -0500

    javascripting project not totally completed

---
+++ 06week/javascripting/function-arguments.js
@@ -0,0 +1,4 @@
+function math(firstArg, secondArg, thirdArg) {
+  return firstArg + (secondArg * thirdArg);
+}
+console.log(math(53, 61, 67));
---
+++ 06week/javascripting/looping-through-arrays.js
@@ -0,0 +1,5 @@
+var pets = ['cat', 'dog', 'rat'];
+for (var i=0; pets[i] < pets.length; i++) {
+return pets += pets[i];
+}
+console.log(pets + 's');
