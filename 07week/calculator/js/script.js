2e5d6a797ecfd00352e1b406c31feb4f3ab92840
sarahcheatham <sarah.cheatham14@gmail.com>
2018-05-29 20:22:08 -0500

    scheatham052918

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,36 @@
-'use strict';
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function integer() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value * -1);
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiply() {
+  document.querySelector("#results").value += "*";
+}
+
+function division() {
+  document.querySelector("#results").value += "/";
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
