e8b0e550c6a6fb27fb3ac4eef1d0b72882a2d0e7
Matt Lukso <mk.lukso@gmail.com>
2018-05-29 19:48:57 -0500

    Calculator

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,37 @@
-'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+  function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function subtract() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiply() {
+  document.querySelector("#results").value += "*";
+}
+
+function divide() {
+  document.querySelector("#results").value += "/";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function plusMinus() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value * -1);
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
