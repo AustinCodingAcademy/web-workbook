3b478316d106e5ea0c1020de5247f2933883da8e
Thobias Akwenye <thobias4@gmail.com>
2018-06-03 10:36:41 -0500

    dom practice Shopping Cart

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,6 +1,47 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+
+  var listItems = document.querySelectorAll('li');
+
+    console.log(listItems.length);
+
+
+
+        var h = document.createElement("H2");
+        var t = document.createTextNode("You have _ items in your shopping cart");
+        h.appendChild(t);
+        document.body.appendChild(h);
+
+function addItem() {
+  var ul = document.targetElementById("dynamic-list");
+  var candidate = document.targetElementById("candidate");
+  var li = document.createElement("li");
+  li.setAttribute('id',candidate.value);
+  li.appendChild(document.createTextNode(candidate.value));
+  ul.appendChild(li);
+}
+
+function removeItem(){
+	var ul = document.getElementById("dynamic-list");
+  var candidate = document.getElementById("candidate");
+  var item = document.getElementById(candidate.value);
+  ul.removeChild(item);
+}
+
+window.onload = function() {
+   init();
+  
+}
+
+function init() {
+   document.getElementById("P1").onmouseover = function() {
+      this.className = "paragraph";
+   }
+   document.getElementById("P1").onmouseout = function() {
+      this.className = "";
+   }
+}
+
+
 });
-document.querySelector('img')
