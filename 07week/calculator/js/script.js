d17630c6094358458c48cac3c49e359c428df380
Lisa Umhey <lisa.umhey@gmail.com>
2018-06-04 10:01:00 -0500

    calculator assignment ready for grading

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -3,3 +3,52 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   // You code here
 });
+
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function subtractNumber(num) {
+  document.querySelector("#results").value -= num;
+}
+
+function multiplyNumber(num) {
+  document.querySelector("#results").value *= num;
+}
+
+function divideNumber(num) {
+  document.querySelector("#results").value /= num;
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiplication() {
+  document.querySelector("#results").value += "*";
+}
+
+function division() {
+  document.querySelector("#results").value += "/";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function plusminus() {
+  document.querySelector("#results").value = document.querySelector("#results").value * -1;
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
