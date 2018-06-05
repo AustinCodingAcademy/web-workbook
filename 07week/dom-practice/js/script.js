d35d9e370546bb2cb479f17a32f3a1f57ebc9947
maryhepp <heppenstall.mary@gmail.com>
2018-06-05 15:18:44 -0500

    DOM

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,10 @@
 'use strict';
-
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+  let list = document.getElementsByTagName("li");
+  let title = document.querySelector("h1");
+  let newlist = document.querySelector('#lastitem');
+  title.insertAdjacentHTML('afterend', '<h2>You have ' + list.length + ' items in your shopping cart</h2>');
+  newlist.insertAdjacentHTML('afterend', '<button>Add Items HERE</button>');
 });
+$(document).ready(function(){
+    $("li").remove();});
