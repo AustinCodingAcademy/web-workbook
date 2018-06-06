8bcc4b5b3df3a8466ab15e0c350eabf6c8600231
Quincy Hammon <qlhammon@gmail.com>
2018-06-05 21:42:55 -0500

    ./script.js

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,12 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+  let ul = document.querySelectorAll("li").length;
+
+  let h1 = document.insertAdjacentHTML(afterend, <h2 id="headertwo">'You have '+ ul +' items in your shopping cart'</h2>);
+  console.log('You have _ items in your shopping cart');
+
+  let ul = document.appendChild(listitem)(afterend, 'li');
+
+  let ul = document.removeChild(listitem)
 });
