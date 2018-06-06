95660235362483cd4d5b83898b88946010890660
Christelle <hopeandash@gmail.com>
2018-06-06 18:51:44 -0500

    catchingup

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+console.log("The page has been loaded");
 });
+
+function sayHI (){
+  console.log("SayingHi");
+}
+
+function additem(){
+  console.log("I am in add item")
+  var newitem = document.createElement("Li");
+  var text= document.createTextNode("water");
+  newitem.appendChild(text);
+
+
+var thelist= document.getElementById("mylist");
+thelist.appendChild(newitem);
+console.log("end of addLi")
+
+}
