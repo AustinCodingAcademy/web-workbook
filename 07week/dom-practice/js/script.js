094c81ec1633b0f0dfdcebed85b96901951ed873
Lisa Umhey <lisa.umhey@gmail.com>
2018-06-06 20:56:30 -0500

    updated shopping cart as of end of class

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,107 +1,48 @@
 'use strict';
 
 var cart = [];
-var items = [];
 
 function countItems(){
   console.log("start countItems function");
-  items = document.querySelectorAll(".items");
-  return items.length;
+  var items = document.querySelectorAll(".items");
+  displayCount(items.length);
   console.log("end countItems function");
 }
 
-function addDiv1(){
+countItems();
+
+function displayCount(items){
+  console.log("start displayCount")
+  var node = document.getElementById("itemCounter");
+  console.log("the node I just fetched is", node);
+  var i = parseInt(node.innerHTML);
+  console.log("the old value is", i);
+  var n = i + items;
+  console.log("the n value is", n);
+  node.innerHTML = n;
+}
+
+
+function addDiv(itemNum){
   console.log("start addDiv1 function");
+  //need to create a div that will wrap around the div below and the button and give it a unique id
   var newDiv = document.createElement("DIV");
-  var itemName = document.getElementById("item-one").innerHTML;
+  var itemName = document.getElementById(itemNum).innerHTML;
   var text = document.createTextNode(itemName);
   newDiv.appendChild(text);
-
+  var id = 3 // going to figure out how to make this unique for each one
   cart = document.getElementById("cart");
   cart.appendChild(newDiv);
+
+  var newButton = document.createElement("BUTTON");
+  var value = document.createTextNode("remove from cart");
+  newButton.setAttribute("onclick", "removeItem("+id+")")
+  newButton.appendChild(value);
+  cart.appendChild(newButton);
+
   console.log("end addDiv1 function");
 }
 
-function addDiv2(){
-  console.log("start addDiv2 function");
-  var newDiv = document.createElement("DIV");
-  var itemName = document.getElementById("item-two").innerHTML;
-  var text = document.createTextNode(itemName);
-  newDiv.appendChild(text);
-
-  cart = document.getElementById("cart");
-  cart.appendChild(newDiv);
-  console.log("end addDiv2 function");
-}
-
-function addDiv3(){
-  console.log("start addDiv3 function");
-  var newDiv = document.createElement("DIV");
-  var itemName = document.getElementById("item-three").innerHTML;
-  var text = document.createTextNode(itemName);
-  newDiv.appendChild(text);
-
-  cart = document.getElementById("cart");
-  cart.appendChild(newDiv);
-  console.log("end addDiv3 function");
-}
-
-function addDiv4(){
-  console.log("start addDiv4 function");
-  var newDiv = document.createElement("DIV");
-  var itemName = document.getElementById("item-four").innerHTML;
-  var text = document.createTextNode(itemName);
-  newDiv.appendChild(text);
-
-  cart = document.getElementById("cart");
-  cart.appendChild(newDiv);
-  console.log("end addDiv4 function");
-}
-
-function addDiv5(){
-  console.log("start addDiv5 function");
-  var newDiv = document.createElement("DIV");
-  var itemName = document.getElementById("item-five").innerHTML;
-  var text = document.createTextNode(itemName);
-  newDiv.appendChild(text);
-
-  cart = document.getElementById("cart");
-  cart.appendChild(newDiv);
-  console.log("end addDiv5 function");
-}
-
-function addDiv6(){
-  console.log("start addDiv6 function");
-  var newDiv = document.createElement("DIV");
-  var itemName = document.getElementById("item-six").innerHTML;
-  var text = document.createTextNode(itemName);
-  newDiv.appendChild(text);
-
-  cart = document.getElementById("cart");
-  cart.appendChild(newDiv);
-  console.log("end addDiv6 function");
-}
-
-function addDiv7(){
-  console.log("start addDiv7 function");
-  var newDiv = document.createElement("DIV");
-  var itemName = document.getElementById("item-seven").innerHTML;
-  var text = document.createTextNode(itemName);
-  newDiv.appendChild(text);
-
-  cart = document.getElementById("cart");
-  cart.appendChild(newDiv);
-  console.log("end addDiv7 function");
-}
-
-function addDiv8(){
-  console.log("start addDiv8 function");
-  var newDiv = document.createElement("DIV");
-  var itemName = document.getElementById("item-eight").innerHTML;
-  var text = document.createTextNode(itemName);
-  newDiv.appendChild(text);
-
-  cart = document.getElementById("cart");
-  cart.appendChild(newDiv);
-  console.log("end addDiv8 function");
+function removeItem(id){
+  
 }
