a6daaf5c10343ac94f55ea20b94a66027fe168bc
Lisa Umhey <lisa.umhey@gmail.com>
2018-06-08 14:23:49 -0500

    shopping cart ready for grading

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -22,27 +22,37 @@ function displayCount(items){
   node.innerHTML = n;
 }
 
-
 function addDiv(itemNum){
-  console.log("start addDiv1 function");
-  //need to create a div that will wrap around the div below and the button and give it a unique id
+  console.log("start addDiv function");
+  var parentDiv = document.createElement("DIV");
+  var id = addID();
+  console.log("the id I just added is", id);
+  parentDiv.setAttribute("id", id);
   var newDiv = document.createElement("DIV");
   var itemName = document.getElementById(itemNum).innerHTML;
   var text = document.createTextNode(itemName);
   newDiv.appendChild(text);
-  var id = 3 // going to figure out how to make this unique for each one
   cart = document.getElementById("cart");
-  cart.appendChild(newDiv);
+  parentDiv.appendChild(newDiv);
 
   var newButton = document.createElement("BUTTON");
   var value = document.createTextNode("remove from cart");
   newButton.setAttribute("onclick", "removeItem("+id+")")
   newButton.appendChild(value);
-  cart.appendChild(newButton);
+  parentDiv.appendChild(newButton);
+  cart.appendChild(parentDiv);
 
-  console.log("end addDiv1 function");
+  console.log("end addDiv function");
+}
+
+function addID(){
+  var node = document.getElementById("cart");
+  console.log("I just fetched this node:", node);
+  return node.childNodes.length;
 }
 
 function removeItem(id){
-  
+  var item = document.getElementById(id);
+  var parent = item.parentNode;
+  parent.removeChild(item);
 }
