59d90fcb5d7bb49e4db19e10d2ac68bd46ab0384
James.Woodard <james@jungledisk.com>
2018-06-10 11:14:05 -0500

    finished project

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,41 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+
+//makes the divs with class of "draggable" draggable with the "draggable" function with revert set to invalid
+$('.draggable').draggable({
+  revert: 'invalid'
+})
+
+//makes the drop areas droppable boiii
+$('#dropArea3, #dropArea1, #dropArea2').droppable({
+  drop: function(event, ui){
+    let $dragging = $(ui.draggable).data('block');
+    console.log($dragging);
+    let $last = ($(this).children().last()).data('block');
+    console.log($last);
+
+    if($dragging > $last){
+      $(ui.draggable).draggable('option', 'revert', true)
+    }
+    else{
+      $(ui.draggable).appendTo($(this)).attr('style', 'position: relative');
+    }
+     checkWin();
+
+  }
+});
+
+  function checkWin() {
+    if($('[data-stack=3]').children().length === 4){
+      $('#announce-game-won').text('Winner!');
+    }
+  }
+
+  $('#clear').click(function(){
+    $('#announce-game-won').empty();
+    location.reload();
+
+  })
+
 });
