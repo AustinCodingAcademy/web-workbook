da9ceb1c4aadecaa491f8a64e4ae788c3be38d2c
miriam007 <miriamu88@gmail.com>
2018-06-11 17:08:00 -0500

    fizzbuzz

---
+++ 06week/fizzbuzz/script.js
@@ -0,0 +1,9 @@
+for  (var i=1; i < 1000; i++) {
+  if (i % 15 == 0)
+  console.log('fizzbuzz');
+  else if (i % 3 == 0)
+  console.log('fizz');
+  else if (i % 5 == 0)
+  console.log('buzz');
+  else console.log(i);
+}
