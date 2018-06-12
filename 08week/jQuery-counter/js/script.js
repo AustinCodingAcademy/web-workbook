1b246440961aeafe491f80b7776012b58c2b3a9d
Jason Connolly <jasonconnolly@Jasons-MacBook-Pro.local>
2018-06-11 20:43:54 -0500

    jQuery

---
+++ 08week/jQuery-counter/js/script.js
@@ -0,0 +1,13 @@
+'use script'
+
+
+$('.add').click(function(){
+  var plus = this.id;
+  var plusAsNumber = parseInt(plus);
+  var current = $('#counter').text();
+  console.log("current counter is " +current);
+  var currentAsNumber = parseInt(current);
+  var newCounter = currentAsNumber + plusAsNumber;
+  //**line 10 is reading the number in HTML "id"**//
+  $('#counter').text(newCounter);
+})
