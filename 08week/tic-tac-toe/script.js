2209d13220c5f2a6d2debeebbbb5fe1659e3d6c1
William-Cruz <chiefxcruz@gmail.com>
2018-06-13 11:47:18 -0500

    another day another dollar

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,16 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+  let tic = 'X';
+  $('div[data-cell]').click(function() {
+   $(this).prepend(tic);
+   if (tic === 'O') {
+     tic = 'X';
+    } else {
+      tic = 'O';
+     $( "#clear" ).click(function() {
+  $( 'div[data-cell]' ).empty();
+});
+    }
+  });
 });
--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,18 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
-});
+  let $block = null;
+
+  $('[data-stack]').click(function() {
+      let clickedOn = $(this).children().last().data('block');
+      let clickedTo = $(this).children().last().data('block');
+    if ($block) {
+      $(this).append($block);
+      $block = null;
+    } else {
+      $block = $(this).children().last().detach();
+      console.log(null);
+    }
+
+  })
+})
