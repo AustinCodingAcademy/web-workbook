0428bfb021e65e79f863ef9f046bfe7888b167f5
maryhepp <heppenstall.mary@gmail.com>
2018-06-14 17:54:53 -0500

    towers of hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,24 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  let $block = null;
+  let heightVar = $('[data-stack]').height;
+  $('[data-stack]').click(function() {
+    if ($block) {
+      $(this).append($block);
+      $block = null;
+    } else if ($(this).height() > heightVar) {
+    alert("Not Allowed");
+    } else {
+      $block = $(this).children().last().detach();
+    }
+    // click counter
+    // function countClicks() {
+  	// 	clicks++;
+  	// 	$clicksCount.html(clicks);
+  })
+  // testing console.logs
+  // console.log($('[data-block]').height());
+  // console.log($(this).height());
+  // console.log("heightVar");
 });
