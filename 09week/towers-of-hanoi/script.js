f892bed479bd9b5071d32d61c13d96a33238fc54
Ezequiel Gutierrez <ezequielgutierrez@Z-Dizzle.local>
2018-06-14 20:23:10 -0500

    updates

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,53 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  var $block = null;
+  var n = 4
+  var Beg = $('#poo').children().last().data('block');
+  var Aux = $('#p2').children().last().data('block');
+  var End = $('#p3').children().last().data('block');
+  var final = $('#p3').children().length();
+
+$('[data-stack]').click(function() {
+if ($block) {
+  $(this).append($block);
+  $block = null;
+ } else {
+   $block = $(this).children().last().detach();
+ })
+
+
+
+$('[data-stack]').click(function() {
+  if (Beg < Aux) {
+  $(this).children().last().detach();
+ })
+
+
+
+
+
+
+  console.log(Beg);
+  console.log(Aux);
+  console.log(End);
 });
+
+
+// T(N-1, Beg, End, Aux)
+// T(1, Beg, Aux, End)
+// T(N-1, Aux, Beg, End)
+
+// N = 3
+// Beg = A
+// Aux = B
+// End = C
+
+// T(3, A, B, C)
+
+
+// T(2, A, C, B)
+// T(1, A, B, C)
+// T(2, B, A, C)
+//$('[data-stack]').click(function() {
+//  var blockSize = $(this).childern().last().data('block');
