03f9da8bc05080a68c3e11dcd9b16723ab381f23
sarahcheatham <sarah.cheatham14@gmail.com>
2018-06-14 19:37:53 -0500

    scheatham0614

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,4 +2,34 @@
 
 $(document).ready(function() {
   // Put app logic here
+  //setting all the blocks to null
+  let $block = $('div[data-block]');
+  $block = null;
+  let blockData = $('[data-block]:lt(4)');
+  // console.log(blockData);
+  // var yellow = $('[data-block="100"]');
+  // var red = $('[data-block="75"]');
+  // var green = $('[data-block="50"]');
+  // var blue = $('[data-block="25"]');
+  // console.log(blockData);
+  // console.log($block.childNodes);
+
+  $('[data-stack]').click(function(e) {
+    let clickedOn = $('[data-block]').children().last().data('block');
+    console.log(clickedOn);
+    // let nextBlock = $('[data-stack]').;
+    // console.log(nextBlock);
+    if ($block) {
+      $(this).append($block);
+      $block = null;
+    } else {
+      $block = $(this).children().last().detach();
+    }
+  })
+
+  // function moveInOrder() {
+  //   if (blockData.data('block').lastChild > blockData.data('block').previousSibling) {
+  //
+  //   }
+  // }
 });
