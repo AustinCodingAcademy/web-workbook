2f350b47bce92f42f9eee911a67a1536b4ab8041
Lisa Umhey <lisa.umhey@gmail.com>
2018-06-19 18:20:08 -0500

    towers of hanoi ready for grading

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,32 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  let $block = null;
+  let $size = null;
+
+  $('[data-stack]').click(function() {
+  let $tower = parseInt($(this).attr('data-stack'));
+  let $previousBlock = parseInt($(this).children().first().attr('data-block'));
+  let $stack = parseInt($(this).children().length);
+  console.log("in the click function. $previousBlock: " + $previousBlock);
+  console.log("$tower: " + $tower + " stack: " + $stack);
+
+     if ($block && ($size > $previousBlock) && ($previousBlock !== null)) {
+         alert("That's against the rules, ya cheater!");
+       } else if ($block) {
+         $(this).prepend($block);
+         $size = parseInt($block.attr('data-block'));
+         console.log("elseif statement ran. $size: " + $size + " $tower: " + $tower + " $previousBlock: " + $previousBlock);
+         if ($size === 25 && $tower === 3 && $stack === 3) {
+           console.log("it's a win!")
+           $('#announce-game-won').add("<h1>YOU WON!</h1>").appendTo('#announce-game-won');
+         } else {
+           $block = null;
+         }
+       } else {
+           $block = $(this).children().first().detach();
+           $size = parseInt($block.attr('data-block'));
+           console.log("else statement ran. $size: " + $size + " $tower: " + $tower + " $previousBlock: " + $previousBlock);
+       }
+ })
 });
