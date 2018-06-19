88a33195df4ff471b5ee944136c28176058fe402
William-Cruz <chiefxcruz@gmail.com>
2018-06-19 16:00:53 -0500

    almost done

---
+++ 09week/customer-satisfaction/js/script.js
@@ -0,0 +1,40 @@
+// Wait for the DOM to be ready
+$(function() {
+  // Initialize form validation on the registration form.
+  // It has the name attribute "registration"
+  $("form[name='registration']").validate({
+    // Specify validation rules
+    rules: {
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+      firstname: "required",
+      lastname: "required",
+      email: {
+        required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 5
+      }
+    },
+    // Specify validation error messages
+    messages: {
+      firstname: "Please enter your firstname",
+      lastname: "Please enter your lastname",
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long"
+      },
+      email: "Please enter a valid email address"
+    },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
+    }
+  });
+});
--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -5,14 +5,18 @@ $(document).ready(function() {
 
   $('[data-stack]').click(function() {
       let clickedOn = $(this).children().last().data('block');
-      let clickedTo = $(this).children().last().data('block');
     if ($block) {
       $(this).append($block);
       $block = null;
     } else {
       $block = $(this).children().last().detach();
-      console.log(null);
+      console.log(clickedOn);
     }
 
   })
 })
+// $('[data-block]').click(function() {
+//     let clickedTo = $(this).children().data('block');
+//     console.log(clickedTo);
+//   }
+// )
