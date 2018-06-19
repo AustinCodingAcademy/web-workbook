23f12f231e0429f051404142d8ceafb96884ca4d
Sean Philippi <sean.philippi@gmail.com>
2018-06-19 15:37:02 -0500

    tic-tac-toe

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -3,49 +3,19 @@
 $(document).ready(function() {
 
   // var isEmpty = $('[data-cell]').length < 1;
-var last = 'X';
-// var value = $('[data-cell]').value();
-var value0 = $('[data-cell="0"]').text();
-var value1 = $('[data-cell="1"]').text();
-var value2 = $('[data-cell="2"]').text();
-var value3 = $('[data-cell="3"]').text();
-var value4 = $('[data-cell="4"]').text();
-var value5 = $('[data-cell="5"]').text();
-var value6 = $('[data-cell="6"]').text();
-var value7 = $('[data-cell="7"]').text();
-var value8 = $('[data-cell="8"]').text();
-var winPatternsX = [
-  //rows
-  (value0 = 'X') && (value1 = 'X') && (value2 = 'X'),
-  (value3 = 'X') && (value4 = 'X') && (value5 = 'X'),
-  (value6 = 'X') && (value7 = 'X') && (value8 = 'X'),
-  //columns
-  (value0 = 'X') && (value3 = 'X') && (value6 = 'X'),
-  (value1 = 'X') && (value4 = 'X') && (value7 = 'X'),
-  (value2 = 'X') && (value5 = 'X') && (value8 = 'X'),
-  //diagonals
-  (value0 = 'X') && (value4 = 'X') && (value8 = 'X'),
-  (value6 = 'X') && (value4 = 'X') && (value2 = 'X')
-];
-var winPatternsO = [
-  //rows
-  (value0 = 'O') && (value1 = 'O') && (value2 = 'O'),
-  (value3 = 'O') && (value4 = 'O') && (value5 = 'O'),
-  (value6 = 'O') && (value7 = 'O') && (value8 = 'O'),
-  //columns
-  (value0 = 'O') && (value3 = 'O') && (value6 = 'O'),
-  (value1 = 'O') && (value4 = 'O') && (value7 = 'O'),
-  (value2 = 'O') && (value5 = 'O') && (value8 = 'O'),
-  //diagonals
-  (value0 = 'O') && (value4 = 'O') && (value8 = 'O'),
-  (value6 = 'O') && (value4 = 'O') && (value2 = 'O')
-];
+  var last = 'X';
+  var board = ['']
+  // var value = $('[data-cell]').value();
+
+
 
 
   $('[data-cell]').click(function(){
+    // check if square is empty
     var isEmpty = $(this).text() === '';
     if(isEmpty){
       $(this).text('X');
+      // alternate between X and O
       if(last === 'O') {
         $(this).text('X');
         last = 'X';
@@ -55,24 +25,72 @@ var winPatternsO = [
       }
       //check if there is a win
       var i;
+      var value0 = $('[data-cell="0"]').text();
+      var value1 = $('[data-cell="1"]').text();
+      var value2 = $('[data-cell="2"]').text();
+      var value3 = $('[data-cell="3"]').text();
+      var value4 = $('[data-cell="4"]').text();
+      var value5 = $('[data-cell="5"]').text();
+      var value6 = $('[data-cell="6"]').text();
+      var value7 = $('[data-cell="7"]').text();
+      var value8 = $('[data-cell="8"]').text();
+      var winPatternsX = [
+        //rows
+        (value0 === 'X') && (value1 === 'X') && (value2 === 'X'),
+        (value3 === 'X') && (value4 === 'X') && (value5 === 'X'),
+        (value6 === 'X') && (value7 === 'X') && (value8 === 'X'),
+        //columns
+        (value0 === 'X') && (value3 === 'X') && (value6 === 'X'),
+        (value1 === 'X') && (value4 === 'X') && (value7 === 'X'),
+        (value2 === 'X') && (value5 === 'X') && (value8 === 'X'),
+        //diagonals
+        (value0 === 'X') && (value4 === 'X') && (value8 === 'X'),
+        (value6 === 'X') && (value4 === 'X') && (value2 === 'X')
+      ];
+      var winPatternsO = [
+        //rows
+        (value0 === 'O') && (value1 === 'O') && (value2 === 'O'),
+        (value3 === 'O') && (value4 === 'O') && (value5 === 'O'),
+        (value6 === 'O') && (value7 === 'O') && (value8 === 'O'),
+        //columns
+        (value0 === 'O') && (value3 === 'O') && (value6 === 'O'),
+        (value1 === 'O') && (value4 === 'O') && (value7 === 'O'),
+        (value2 === 'O') && (value5 === 'O') && (value8 === 'O'),
+        //diagonals
+        (value0 === 'O') && (value4 === 'O') && (value8 === 'O'),
+        (value6 === 'O') && (value4 === 'O') && (value2 === 'O')
+      ];
+      var foundaWin = false;
       for (i = 0; i < winPatternsX.length; i++) {
-        if (winPatternsX === true) {
-          $('#message').text = 'you won!';
-        } else if (winPatternsX != true) {
-          for (i = 0; i < winPatternsO.length; i++) {
-            if (winPatternsO === true) {
-              $('#message').text = 'you won!';
-            } else {
-              $('#message').text = "next person's turn!";
-            }
-          }
+        if (winPatternsX[i] === true) {
+          $('#message').text('you won!');
+          break;
+        } else if (winPatternsO[i] === true) {
+            $('#message').text('you won!');
+            break;
+        } else if ((winPatternsX[i] === false) && (winPatternsO[i] === false)) {
+            $('#message').text("next person's turn!");
+          //else if (winPatternsX[i] && winPatterns)[i]
         }
       }
     } else {
       alert('this square is already filled!');
     }
   });
+  $('#clear').click(function(){
+      $('[data-cell="0"]').empty();
+      $('[data-cell="1"]').empty();
+      $('[data-cell="2"]').empty();
+      $('[data-cell="3"]').empty();
+      $('[data-cell="4"]').empty();
+      $('[data-cell="5"]').empty();
+      $('[data-cell="6"]').empty();
+      $('[data-cell="7"]').empty();
+      $('[data-cell="8"]').empty();
+  })
 });
+
+
 
 // function isGameOver() {
 //   //check if gave is over
