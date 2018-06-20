e7f40e25d6b4468b719b13e9f949f502f0fc726d
Sean Philippi <sean.philippi@gmail.com>
2018-06-19 20:38:26 -0500

    towers 1

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,51 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  // draggable
+  // $('[data-stack="1"]').
+  // use draggable="true/false" to lock the blocks and make them draggable.
+  // if else statementn and .lastChild.  make the last child of data-stack have draggable="true".
+
 });
+
+// $('[data-block]').draggable()
+const blocks = document.querySelectorAll('[data-block]');
+const block = document.querySelector('[data-block]');
+const stack = document.querySelectorAll('[data-stack]');
+
+for (const block of blocks) {
+  block.addEventListener('dragstart', dragStart);
+  block.addEventListener('dragend', dragEnd);
+  block.addEventListener('dragover', dragOver);
+  block.addEventListener('dragenter', dragEnter);
+  block.addEventListener('dragleave', dragLeave);
+  block.addEventListener('drop', dragDrop);
+}
+
+function dragStart() {
+  setTimeout(() => (this.className = 'invisible'), 0)
+  console.log('start');
+}
+function dragEnd() {
+  this.className = 'block';
+  console.log('end');
+}
+function dragOver(e) {
+  e.preventDefault();
+  console.log('over');
+}
+function dragEnter(e) {
+  e.preventDefault();
+  console.log('enter');
+}
+function dragLeave() {
+  console.log('leave');
+}
+function dragDrop() {
+  this.append(block);
+  console.log('drop');
+}
+
+
+// block.addEventListener('dragstart', dragStart);
+// block.addEventListener('dragend', dragEnd);
