6c9e882e5d36e721affdf97c7a29a912a9cf3f31
Olivia Wilczek <liv.wilczek@gmail.com>
2018-06-20 20:40:42 -0500

    customer satisfaction form complete

---
+++ 09week/customer-satisfaction/js/script.js
@@ -0,0 +1,27 @@
+$(function() {
+
+  $("form[name='customer-satisfaction']").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 5
+      }
+    },
+
+    messages: {
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long"
+      },
+      email: "Please enter a valid email address"
+    },
+    
+    submitHandler: function(form) {
+      form.submit();
+    }
+  });
+});
