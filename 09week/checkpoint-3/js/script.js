be96d9821f63f7955925d09fbd7f7a747dd72a1b
sarahcheatham <sarah.cheatham14@gmail.com>
2018-06-22 17:49:13 -0500

    uploaded winner vid and audio

--- 09week/checkpoint-3/js/script.js
+++ 09week/checkpoint-3/js/script.js
@@ -1,17 +1,23 @@
 'use strict';
 
 $(document).ready(function()  {
+
   //making a function to hide the video on the load of the page
   function hideVideo() {
     $('.pukeVideo').hide();
+    $('#pukeAudio').hide();
   }
   hideVideo();
 
 //this is the function I am trying to play the video
-  // var vid = $('.pukeVideo');
-  // $('.container').click(function playVid() {
-  //   vid.play();
-  // });
+  var vid = document.getElementsByClassName("pukeVideo");
+  var hey = document.getElementById('hey');
+  var sound = document.getElementById('pukeAudio');
+
+  function playVideo() {
+    hey.play();
+    sound.play();
+  };
 
   //grabbing the picture of the llama
   let llamaContainer = $('div.llama-container');
@@ -41,16 +47,18 @@ $(document).ready(function()  {
   //creating a function to increase the bar height by 10px each click
   function move() {
     var bar = $('#bar').height(barHeight);
-    var maxBarHeight = document.getElementById('bar').style.maxHeight = 430;
+    var maxBarHeight = document.getElementById('bar').style.maxHeight = 630;
     var progress = document.getElementById('progress').childNodes[1];
-    progress.style.maxHeight = 430;
+    progress.style.maxHeight = 630;
     console.log(barHeight);
     var id = setInterval(progressBar, 10);
     function progressBar() {
-      if (barHeight >= 430) {
+      if (barHeight >= 630) {
         $('.pukeVideo').show();
+        $('#pukeAudio').show();
         $('.background').hide();
         $('.llama-container').hide();
+        audio.pause();
       }
     }
   }
@@ -63,7 +71,6 @@ $(document).ready(function()  {
     //change back to 10
     updatedNum.text(count);
     move();
-    // winner();
   });
   // console.log(barHeight);
   //end of function counter
