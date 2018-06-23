1ea3336ff469be4754460d463b01d0bf86bf7316
Christelle <hopeandash@gmail.com>
2018-06-23 14:12:06 -0400

    yes

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -6,14 +6,24 @@ $(document).ready(function() {
  });
  var count = 0;
  $('#dropArea, #dropArea, #dropArea').droppable({
-   drop: function (event, ui){
+   drop: function (event,ui){
      count++;
      var drag = $(ui.draggable).data('block');
      console.log(drag);
      var last = $(this).children().last().data('block');
      console.log(last);
-
+    if (drag>last){
+        $(ui.draggable).draggable("option","revert",true);
+    } else {
+      $(ui.draggable).appendTo(this).attr('style','position:relative;');
+      }
+      checkForWin()
   }
 })
+function checkForWin() {
+  if ($('[data-stack=3]').children().length===4 ){
+    $('#announce-game-won').text(`winner`)
+  }
 
+}
 });
