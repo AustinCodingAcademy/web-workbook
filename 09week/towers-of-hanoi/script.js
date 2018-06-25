de8cd9c835afec6afa2b0f1c4a11934a2994e41a
Sean Philippi <sean.philippi@gmail.com>
2018-06-24 19:47:20 -0500

    towers of hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,51 +1,74 @@
 'use strict';
 
 $(document).ready(function() {
-  // draggable
-  // $('[data-stack="1"]').
-  // use draggable="true/false" to lock the blocks and make them draggable.
-  // if else statementn and .lastChild.  make the last child of data-stack have draggable="true".
+
 
 });
 
 // $('[data-block]').draggable()
 const blocks = document.querySelectorAll('[data-block]');
-const block = document.querySelector('[data-block]');
-const stack = document.querySelectorAll('[data-stack]');
+var block = document.querySelector('[data-block]');
+const stacks = document.querySelectorAll('[data-stack]');
 
 for (const block of blocks) {
   block.addEventListener('dragstart', dragStart);
-  block.addEventListener('dragend', dragEnd);
-  block.addEventListener('dragover', dragOver);
-  block.addEventListener('dragenter', dragEnter);
-  block.addEventListener('dragleave', dragLeave);
-  block.addEventListener('drop', dragDrop);
 }
 
-function dragStart() {
-  setTimeout(() => (this.className = 'invisible'), 0)
-  console.log('start');
+for (const stack of stacks) {
+  stack.addEventListener('drop', dragDrop);
+  stack.addEventListener('dragover', dragOver);
+  stack.addEventListener('dragenter', dragEnter);
 }
-function dragEnd() {
-  this.className = 'block';
-  console.log('end');
+
+var draggable = document.querySelector(['block']).draggable('true');
+
+function dragStart(e) {
+  if (draggable) {
+    setTimeout(() => (this.className = 'invisible'), 0);
+    block = this;
+  } else {e.preventDefault();
 }
+
+  console.log('start', this);
+}
+
+function dragDrop() {
+  this.append(block);
+  // block.className = "block";
+  console.log('drop', this);
+}
+// function dragEnd() {
+//   this.className = 'block';
+//   console.log('end', this);
+// }
 function dragOver(e) {
   e.preventDefault();
-  console.log('over');
+  // console.log('over', this);
 }
 function dragEnter(e) {
   e.preventDefault();
-  console.log('enter');
 }
-function dragLeave() {
-  console.log('leave');
-}
-function dragDrop() {
-  this.append(block);
-  console.log('drop');
-}
+// function dragLeave() {
+//   console.log('leave', this);
+// }
 
+
+// block.addEventListener('dragend', dragEnd);
+// block.addEventListener('dragover', dragOver);
+// block.addEventListener('dragenter', dragEnter);
+// block.addEventListener('dragleave', dragLeave);
+// block.addEventListener('drop', dragDrop);
 
 // block.addEventListener('dragstart', dragStart);
 // block.addEventListener('dragend', dragEnd);
+// can i drag it?
+// draggable function
+//
+// can i drop it here?
+// drop place valid function
+
+
+// draggable
+// $('[data-stack="1"]').
+// use draggable="true/false" to lock the blocks and make them draggable.
+// if else statementn and .lastChild.  make the last child of data-stack have draggable="true".
