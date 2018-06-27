fa2df6f45fc2bd58848e29dcb7529c8d3c450c79
Olivia Wilczek <liv.wilczek@gmail.com>
2018-06-26 23:24:02 -0500

    game completed, working on additional logic for check win

--- checkpoints/checkpoint_3/js/script.js
+++ checkpoints/checkpoint_3/js/script.js
@@ -1,11 +1,13 @@
  'use strict';
-
+ //dieChar variable holds the array of dice unicodes
  let dieChar = ["&#9856;", "&#9857;", "&#9858;", "&#9859;", "&#9860;", "&#9861;"]
 
+ // function rolls the dice by generating a random number and places the rolled die into .die
  function roll() {
-   document.querySelector(".dice").innerHTML = "";
+   document.querySelector(".dice").innerHTML = ""; // clears .dice on roll
+   document.querySelector("#results").innerHTML = ""; // clears #results on roll
    for (let i = 0; i < 5; i++) {
-     let roll = Math.floor(Math.random() * 6) + 1;
+     let roll = Math.floor(Math.random() * 6) + 1; //math floor function returns the largest integer less than or equal to a given number. Math.random()*6 + 1 gives us a random number between 1 and 6.999999
      document.querySelector(".dice").innerHTML += `<span class="die" data-roll="${roll}">${dieChar[roll - 1]}</span>`;
    }
    report();
@@ -13,73 +15,123 @@
 
  roll(); //call function onclick in html
 
+
+
+// report function reports the results of each dice roll
  function report() {
-   document.querySelector("#results").innerHTML = "";
+// variables serve as counter
+   var num1 = 0;
+   var num2 = 0;
+   var num3 = 0;
+   var num4 = 0;
+   var num5 = 0;
+   var num6 = 0;
+
    let diceHtml = document.querySelectorAll('.die'); // puts individual die HTML elements in diceHtml
-   for (let dieVal = 1; dieVal < 7; dieVal++) { // Loop checks for all the 1s, then 2s, etc
-     let howManyDice = 0; // zeros out the counter
-     for (let i = 0; i < 5; i++) { // creates inner loop that cycles through the rolled dice
-       if (dieVal === Number(diceHtml[i].getAttribute('data-roll'))) { // Number - converts dieHtml to a numerical value. + This line checks if rolled die = one's we're counting
-         howManyDice++; // if so, we add one to the count
-       }
+   //loops through die to determine how many of each number are in roll
+   for (let i = 0; i < 5; i++) {
+     var value = Number(diceHtml[i].getAttribute('data-roll'));// converts die faces to numbers
+     if (value == 1) {
+       num1++; // if die has numerical value of 1, update num1 variable by 1.
+     } else if (value == 2) {
+       num2++;
+     } else if (value == 3) {
+       num3++;
+     } else if (value == 4) {
+       num4++;
+     } else if (value == 5) {
+       num5++;
+     } else if (value == 6) {
+       num6++;
      }
-     //if statements to check if goals have been met
-     if ((dieVal === 1) &&
-       (dieVal === 2) &&
-       (dieVal === 3) &&
-       (dieVal === 4))
-       ||
-       (dieVal == 2) &&
-       (dieVal == 3) &&
-       (dieVal == 4) &&
-       (dieVal == 5)) {
-     document.querySelector("#results").innerHTML += `Small Straight!<br>`;
-     $('#goal10').hide();
-     // } else if ((dieVal == 1) &&
-     //   (dieVal == 2) &&
-     //   (dieVal == 3) &&
-     //   (dieVal == 4) &&
-     //   (dieVal == 5) ||
-     //   ((dieVal == 2) &&
-     //   (dieVal == 3) &&
-     //   (dieVal == 4) &&
-     //   (dieVal == 5) &&
-     //   (dieVal == 6)) {
-     //   document.querySelector("#results").innerHTML += `Large Straight!<br>`;
-     //   $('#goal11').hide();
-   } else if (howManyDice === 2 && dieVal === 1) { // we check after the counting is done if it is equal to '2'
-     document.querySelector("#results").innerHTML += `There are a pair of ${dieVal}s<br>`; // if yes, we report a pair
-     $('#goal1').hide();
-   } else if (howManyDice === 2 && dieVal === 2) {
-     document.querySelector("#results").innerHTML += `There are a pair of ${dieVal}s<br>`;
-     $('#goal2').hide();
-   } else if (howManyDice === 2 && dieVal === 3) {
-     document.querySelector("#results").innerHTML += `There are a pair of ${dieVal}s<br>`;
-     $('#goal3').hide();
-   } else if (howManyDice === 2 && dieVal === 4) {
-     document.querySelector("#results").innerHTML += `There are a pair of ${dieVal}s<br>`;
-     $('#goal4').hide();
-   } else if (howManyDice === 2 && dieVal === 5) {
-     document.querySelector("#results").innerHTML += `There are a pair of ${dieVal}s<br>`;
-     $('#goal5').hide();
-   } else if (howManyDice === 2 && dieVal === 6) {
-     document.querySelector("#results").innerHTML += `There are a pair of ${dieVal}s<br>`;
-     $('#goal6').hide();
-   } else if ((howManyDice === 5) && (dieVal)) {
-     document.querySelector("#results").innerHTML += `Full House<br>`;
-   } else if (howManyDice === 3) {
-     document.querySelector("#results").innerHTML += `Three of a kind of ${dieVal}s<br>`;
-     $('#goal7').hide();
-   } else if (howManyDice === 4) {
-     document.querySelector("#results").innerHTML += `Four of a kind of ${dieVal}s<br>`;
-     $('#goal8').hide();
-   } else if (howManyDice === 5) {
+   }
+   var foundPair;
+   var threeOfKind
+   var fullHouse;
+   var smallStraight;
+   var largeStraight;
+   var fourOfKind;
+   var yahtzee;
+
+   var pairValue;
+
+
+   //determines if two dice share the same numerical value
+
+     if (num1 == 2) {
+        foundPair = true;
+        pairValue = 1; // this states that the pair is a pair of ones
+     }
+     if (num2 == 2) {
+       foundPair = true;
+       pairValue = 2;// this states that the pair is a pair of twos
+     }
+     if (num3 == 2) {
+       foundPair = true;
+       pairValue = 3;
+     }
+     if (num4 == 2) {
+       foundPair = true;
+       pairValue = 4;
+     }
+     if (num5 == 2) {
+       foundPair = true;
+       pairValue = 5;
+     }
+
+      if (num6 == 2) {
+       foundPair = true;
+       pairValue = 6;
+     }
+
+
+   // determines if three dice share a value
+   if (num1 == 3 || num2 == 3 || num3 == 3 || num4 == 3 || num5 == 3 || num6 == 3) {
+     threeOfKind = true;
+   }
+
+   // determines if four dice share a value
+   if (num1 == 4 || num2 == 4 || num3 == 4 || num4 == 4 || num5 == 4 || num6 == 4) {
+     fourOfKind = true;
+     document.querySelector("#results").innerHTML += `Four of a kind<br>`;
+     $('#goal8').appendTo('.matched');// if yes, move goal 8 to .matched
+   }
+   // determines if five dice share a value
+   if (num1 == 5 || num2 == 5 || num3 == 5 || num4 == 5 || num5 == 5 || num6 == 5) {
+     yahtzee = true;
      document.querySelector("#results").innerHTML += `Yahtzee!<br>`;
+     $('#goal12').appendTo('.matched');// if yahtzee = true, move goal 12 to .matched
+   }
+   // determines if a full house is present by checking if variable foundPair and threeofKind are both true.
+   if (foundPair == true && threeOfKind == true) {
+     fullHouse = true;
+     foundPair = false;// marked as false so program does not state that full house and pair are true at the same time
+     threeOfKind = false;// marked as false so program does not state that full house and three of kind are true at the same time
+     document.querySelector("#results").innerHTML += `Full House<br>`;
+     $('#goal9').appendTo('.matched');// if full house is true, move goal 9 to .matched
+   }
+// for foundPair - tells you what to put in the html if it's met earlier
+   if (foundPair == true) {
+     document.querySelector("#results").innerHTML += `There are a pair of ${pairValue}s<br>`;
+      $('#goal' +pairValue).appendTo('.matched');
+   }
+// for threeOfKind - tells you what to put in the html if it's met earlier 
+   if (threeOfKind == true) {
+     document.querySelector("#results").innerHTML += `Three of a kind<br>`;
+      $('#goal7').appendTo('.matched');
+   }
+
+   //determines if small straight is present by checking if num values contain the right numbers
+   if (num1 == 1 && num2 == 1 && num3 == 1 && num4 == 1 && num5 == 1 && num6 == 0) {
+     smallStraight = true;
+     document.querySelector("#results").innerHTML += `Small Straight<br>`;
+     $('#goal10').appendTo('.matched');// if small straight is true, move goal 10 to .matched
+   }
+   //determines if large straight is present by checking if num values contain the right numbers
+   if (num1 == 0 && num2 == 1 && num3 == 1 && num4 == 1 && num5 == 1 && num6 == 1) {
+     largeStraight = true;
+     document.querySelector("#results").innerHTML += `Large Straight<br>`;
+     $('#goal11').appendTo('.matched');// if large straight is true, move goal 11 to .matched
    }
 
  }
-
- }
-
- // reports results in div with  ID 'results'.
- report();
