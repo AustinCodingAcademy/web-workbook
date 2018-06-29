2ff4ab13cbefe7dbd3acffb46d2735fc8a294322
Matt Rogers <matthew.gary.rogers@gmail.com>
2018-06-29 14:34:36 -0500

    TicTacToe game

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -3,3 +3,71 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   // You code here
 });
+
+function sayHi(){
+    console.log("Hi!");
+}
+
+function addADiv(){
+    console.log("I am adding a div!")
+}
+
+var zero = 0;
+
+function addLi(){
+    console.log("I am in addLi");
+    var newLi = document.createElement("LI");
+    var text = document.createTextNode("Water");
+    newLi.appendChild(text);
+    
+    var theList = document.getElementById("myList");
+    theList.appendChild(newLi);
+    console.log("end of addLi");
+}
+
+/* function countList(){
+    console.log("I am in countlist");
+    var li_list = document.querySelectorAll("li");
+    for (var i = 0; i < li_list.length; i++){
+        console.log(li_list[i]);
+    }
+}
+
+countList(); */
+/*
+
+function increment(){
+    console.log("I am increment");
+    document.getElementById("total").innerHTML="NEW TEXT!";
+}
+
+increment();
+*/
+
+/* Below is some great starter code*/
+/*
+function userIncrement(){
+    console.log("inside user increment");
+    var userInput = parseInt(document.getElementById('userInput').valueOf);
+    increment(userInput);
+}
+
+function increment(someWeirdVariableName){
+    console.log('in increment');
+    var node = document.getElementById("myCounter");
+    console.log("the node I just fetched is ", node);
+    var oldValue = parseInt(node.innerHTML);
+    console.log("the old value is ", oldValue);
+    var newValue = oldValue+someWeirdVariableName;
+    console.log("the new value is ", newValue);
+    node.innerHTML = newValue;
+}
+*/
+
+function addItem(item){
+    var myDiv = document.createElement("div");
+    myDiv.innerHTML = "<h2>" +item+"</h2>";
+    var theBody = document.getElementById("body");
+    theBody.appendChild(myDiv);
+}
+
--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,85 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
-});
+  });
+
+function startGame() {
+    for (var i = 1; i <= 9; i++){
+        clearBox(i);
+    }
+      document.turn = "X";
+      document.winner = null;
+    setMessage(document.turn + " begins the game.")
+  }
+
+function setMessage(msg){
+    document.getElementById("message").innerText = msg;
+}
+
+function nextMove(square) {
+    if (document.winner != null) {
+        setMessage(document.turn + " has already won.")
+    } else if(square.innerText == ''){
+    square.innerText = document.turn;
+    switchTurn();      
+    } else {
+        setMessage("Pick another square.")
+    }
+}
+
+function switchTurn(){
+    if(checkForWinner(document.turn)) {
+        setMessage("Congrats " + document.turn + ", you won!")
+        document.winner = document.turn;
+        alert("Game over! " + document.turn + " wins!");
+        setTimeout(myFunction, 1000);
+        setTimeout(startGame, 1100);
+        
+    } else if (document.turn == "X") {
+        document.turn = "O"
+        setMessage("It's " + document.turn + "'s turn.")
+    } else {
+        document.turn = "X";
+        setMessage("It's " + document.turn + "'s turn.")
+    }
+}
+
+
+//Check for a winner...
+
+function checkForWinner(move){
+    var result = false;
+    if(checkRow(1, 2, 3, move) ||
+    checkRow(4, 5, 6, move) ||
+    checkRow(7, 8, 9, move) ||
+    checkRow(1, 4, 7, move) ||
+    checkRow(2, 5, 8, move) ||
+    checkRow(3, 6, 9, move) ||
+    checkRow(1, 5, 9, move) ||
+    checkRow(3, 5 ,7, move)) {
+        result = true;
+    }
+    return result;
+        
+}
+
+
+function checkRow(a, b, c, move) {
+    var result = false;
+    if (getBox(a) == move && getBox(b) == move && getBox(c) == move){
+        result = true;
+    }
+    return result;
+}
+
+function getBox(number){
+    return document.getElementById("s" + number).innerText;
+}
+
+function clearBox(number) {
+    document.getElementById("s" + number).innerText = "";
+}
+
+function myFunction() {
+    alert("Game over! " + document.turn + " wins!");
+}
