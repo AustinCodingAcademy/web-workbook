eda1b7864a30a5f5d757b1d5448905c857b531c1
Sean Philippi <sean.philippi@gmail.com>
2018-06-29 19:53:21 -0500

    beautiful towers of hanoi, timer needs some work

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
 $(document).ready(function() {
-  //targets last child of all the data blocks
-  let lastBlock = $('[data-block]:last-child');
-  // adds a class to moveable to all last child elements
-  lastBlock.addClass('moveAble');
+  //targets first child of all the data blocks
+  let topBlock = $('[data-block]:first-child');
+  // adds a class to moveable to all first child elements
+  topBlock.addClass('moveAble');
 
   $('.draggable').draggable({
       revert: true
@@ -19,183 +19,87 @@ $(document).ready(function() {
       console.log(drag);
       var last = $(this).children().last().data('block');
       console.log(last);
-      if(drag > last){
+      if (drag > last){
         $(ui.draggable).draggable( "option", "revert", true );
-      }else{
-        $(ui.draggable).appendTo(this).attr('style', 'position: relative');
+      } else {
+        $(ui.draggable).prependTo(this).attr('style', 'position: relative');
         //move block is set to the data-block as an array
-        //if there is more than one last child
-        lastBlock = $('[data-block]:last-child');
-        console.log(lastBlock)
+        //if there is more than one first child
+        topBlock = $('[data-block]:first-child');
+        console.log(topBlock)
         // removes all the moveable classes
         $('[data-block]').removeClass('moveAble');
-        // adds class move to child last data-blocks
-        lastBlock.addClass('moveAble');
+        // adds class move to first child data-blocks
+        topBlock.addClass('moveAble');
+        // add to moveCounter
+        count++;
+        moveCount();
+        console.log(count);
+      }
+    checkWin();
     }
-  checkWin();
+  })
+  function checkWin(){
+
+    if($('[data-stack=3]').children().length == 4){
+      $('#announce-game-won').text('Congratulations! You won!');
+      // stop time
+      stop();
+    }
+
   }
+
+  // move counter
+  function moveCount() {
+    $('#moveCounter').text('Moves: ' + count);
+  }
+
+  // timer
+  let timer;
+  let i = 0;
+  let divide = 1;
+  let min = 0;
+
+  const start = () => {
+    timer = self.setInterval(function() {
+      increment()
+    }, 1000);
+  }
+
+  const increment = () => {
+    i++;
+    document.getElementById('sec').innerHTML = (i / divide) + ' Sec';
+    minute();
+  }
+  // stop time upon win
+  const stop = () => {
+    clearTimeout(timer)
+  }
+
+  const minute = () => {
+    if (i == 60) {
+      min++;
+      document.getElementById('min').innerHTML = min + ' Min';
+      i = 0;
+    }
+  }
+  // start timer
+  $('#trigger').one('mousedown', function() {
+    start();
+  })
+  // reset game
+  $('#reset').click(function() {
+      location.reload();
+  })
+
+
 })
-function checkWin(){
-
-  if($('[data-stack=3]').children().length == 4){
-    $('#announce-game-won').text('Congratulations! You won!');
-    stop();
-  }
-}
-let timer
-let i = 0;
-let divide = 1;
-let min = 0;
-const start = () => {
-  timer = self.setInterval(function() {
-    increment()
-  }, 1000);
-}
-const increment = () => {
-  i++
-  document.getElementById('sec').innerHTML = (i / divide) + ' Sec'
-  minute();
-}
-
-const stop = () => {
-  clearTimeout(timer)
-}
-
-const minute = () => {
-  if (i == 60) {
-    min++
-    document.getElementById('min').innerHTML = min + ' Min';
-  }
-  if (i == 60) {
-    i = 0
-  }
-}
-
-$('#trigger').one('mousedown', function(){
-  start();
-})
 
 
 
-})
-// 'use strict';
-//
-// $(document).ready(function() {
-//
-//   let lastBlock = $('[data-block]:last-child');
-//
-//   lastBlock.addClass('moveAble');
-//
-//   $('.draggable').draggable({
-//     revert: true
-//   })
-//   var count = 0;
-//   $('#dropArea, #dropArea, #dropArea').droppable({
-//      //accepts only items with moveable class.
-//      accept: ".moveAble",
-//      drop: function (event, ui){
-//        console.log('counter is ' + count);
-//        var drag = $(ui.draggable).data('block');
-//        console.log(drag);
-//        var last = $(this).children().last().data('block');
-//        console.log(last);
-//        if(drag > last){
-//          $(ui.draggable).draggable( "option", "revert", true );
-//    }else{
-//      $(ui.draggable).appendTo(this).attr('style', 'position: relative');
-//      //move block is set to the data-block as an array
-//      //if there is more than one last child
-//      lastBlock = $('[data-block]:last-child');
-//      console.log(lastBlock)
-//      // removes all the moveable classes
-//      $('[data-block]').removeClass('moveAble');
-//      // adds class move to child last data-blocks
-//      lastBlock.addClass('moveAble');
-//    }
-//       checkWin();
-//     }
-//   })
-//
-//   function checkWin() {
-//     if($('[data-stack=3]').children().length === 4) {
-//       $('#announce-game-won').text('Winner');
-//     }
-//   }
-//
-// // move moveCounter
-//
-// // var move =
-//
-// //timer
-//
-// // const start = () => {
-// //   timer = self.setInterval(function () {
-// //     increment()
-// //   }, (1000/divide));
-// // }
-// // $('[data-block=25]').addEventListener('click', start)
-// //
-// //   let timer;
-// //   let i = 0;
-// //   let divide = 1;
-// //
-// //   const increment = () => {
-// //     i++;
-// //     document.getElementById('time').innerHTML = (i/divide);
-// //   }
-// //   start();
-// //
-// //   const stop = () => {
-// //     clearTimeout(timer);
-// //   }
-// //
-// //   document.getElementById('button').addEventListener('click', start);
-// //   document.getElementById('button2').addEventListener('click', stop);
-//
-//   // if(document.getElementById('time') == 60) {
-//   //   document.getElementById('time').innerHTML = += 1
-//   // }
-//
-//   // more timer code
-//
-//
-//   let timer;
-//   let i = 0;
-//   let divide = 1;
-//   let min = 0;
-//   const start = () =>{
-//     timer = self.setInterval(function() {
-//       increment()
-//       }, 1000);
-//     }
-//     const increment = () => {
-//       i++;
-//       document.getElementById('sec').innerHTML = (i/divide) + ' Sec';
-//       minute();
-//     }
-//
-//     const stop = () => {
-//       clearTimeout(timer);
-//     }
-//
-//     const minute = () => {
-//       if (i == 60) {
-//         min++
-//         document.getElementById('min').innerHTML = min + ' Min';
-//         }
-//         if (i == 60) {
-//           i = 0;
-//         }
-//       }
-//
-//   $('[data-block=25]').one('click', start)
 
 
 
-// draggable method
-
-
-// })
 
 
 //   var blocks = $('[data-block]');
@@ -331,62 +235,3 @@ $('#trigger').one('mousedown', function(){
  //     stop.addEventListener('click', stopTime);
  //   }); // stopwatch.js end
  //  }
-
-
-
-// $(document).ready(function(){
-//
-//   var blocks = $('[data-block]');
-//   var stacks = $('[data-stack]');
-//   // var movable = $('[data-block]:last-child');
-//   // movable.addClass('moveable');
-//
-//   var saved = null;
-//
-//   //function save
-//   $('[data-block]').click(function(){
-//     if ($(this).is(':last-child')) {
-//       $(this).detach();
-//       var saved = this;
-//       console.log(saved);
-//       //function place
-//       $('[data-stack]').click(function(){
-//         var value = $(this).attr('data-block')
-//         //if there is no last child, and there is a saved block, append
-//         if ($(this).children().length > 0) {
-//           //check if children are bigger than saved block
-//           parseInt(value)
-//         } else {
-//           $(this).append(saved);
-//           console.log(saved);
-//         }
-//       })
-//     }
-//   })
-// })
-
-
-
-// function dragLeave() {
-//   console.log('leave', this);
-// }
-
-// block.addEventListener('dragend', dragEnd);
-// block.addEventListener('dragover', dragOver);
-// block.addEventListener('dragenter', dragEnter);
-// block.addEventListener('dragleave', dragLeave);
-// block.addEventListener('drop', dragDrop);
-
-// block.addEventListener('dragstart', dragStart);
-// block.addEventListener('dragend', dragEnd);
-// can i drag it?
-// draggable function
-//
-// can i drop it here?
-// drop place valid function
-
-
-// draggable
-// $('[data-stack="1"]').
-// use draggable="true/false" to lock the blocks and make them draggable.
-// if else statementn and .lastChild.  make the last child of data-stack have draggable="true".
