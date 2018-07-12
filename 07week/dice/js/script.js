14e64d224853f179ffb0f5b6816826c1c27fdba3
Sean Philippi <sean.philippi@gmail.com>
2018-07-11 19:18:20 -0500

    dice, big straight and small straight improved, everything commented

--- 07week/dice/js/script.js
+++ 07week/dice/js/script.js
@@ -1,30 +1,23 @@
+// dice images
 const dieChar = ["&#9856;", "&#9857;", "&#9858;", "&#9859;", "&#9860;", "&#9861;"];
 
 function roll() {
-  document.querySelector("#dice").innerHTML = "";
-  for (let i = 0; i < 5; i++) {
-    const roll = Math.floor(Math.random() * 6) + 1;
+  document.querySelector("#dice").innerHTML = ""; // clear #dice div
+  for (let i = 0; i < 5; i++) { // go through all 5 dice
+    const roll = Math.floor(Math.random() * 6) + 1; // assign each die a random number between 1 and 6.
+    // insert span with correct image from dieChar array with class of "die" and correct data-roll value.
     document.querySelector("#dice").innerHTML += `<span class="die" data-roll="${roll}">${dieChar[roll - 1]}</span>`;
   }
-  report();
+  report(); // print report
 }
-
-
-//////////////////////////////////////////////////////
-// Add code here that uses loops and conditional statements
-// to determine if any of the following cases are true:
-//
 
 function report() {
   document.querySelector("#report").innerHTML = ""; // clear out the report box
 
   const diceHtml = document.querySelectorAll('.die'); // this puts all the individual die HTML elements in diceHtml as an array
-
-  const diceArray = [0,0,0,0,0,0];
+  const diceArray = [0,0,0,0,0,0]; // this is where counts of the rolls are stored
   const pairs = []; // array filled with die values of detected pairs
   const threes = []; // array filled with die values of detected triplets
-  let smallStraight = true;
-  let bigStraight = true;
   // loops through dice html to get data-roll attribute, then plugs the # of each possible roll into diceArray
   for (let i = 0; i < diceHtml.length; i++) {
     // num represents the number value within each individual die
@@ -32,32 +25,29 @@ function report() {
     // pushing the count of each possible roll number into diceArray
     diceArray[rollnum - 1]++;
   }
-
-  if (diceArray[0] != 0) bigStraight = false; // if the numbers of 1s rolled does not equal 0, bigStraight is false.
-  if (diceArray[5] != 0) smallStraight = false; // if the number of 6s rolled does not equal 0, smallStraight is false.
   // "i" here represents the position in the diceArray which is an Array full of counts for each possible dice roll
   for (let i = 0; i < diceArray.length; i++) {
-    // ignoring position 5 (represent # of 6s rolled), and all other positions in diceArray (represents # of 1s - 5s rolled)
-    // are not equal to 1, then smallStright is false
-  	if (i != 5 && diceArray[i] != 1) smallStraight = false;
-    // ignoring position 0 (representing # of 1s rolled), and all other positions in diceArray (representing # of 2s - 6s rolled)
-    // are not equal to 1, bigStraight must be false.
-    if (i != 0 && diceArray[i] != 1) bigStraight = false;
-    // if there is count of 2 for any possible roll in diceArray
-  	if (diceArray[i] === 2) {
+		// if diceArray has only 1 of each possible roll and no 1 value rolled, report big straight.
+  	if (diceArray.join('') === '011111') {
+      document.querySelector("#report").innerHTML += 'There is a big straight';
+      }
+		// if diceArray has only 1 of each possible roll and no 6 rolled, report small straight.
+    if (diceArray.join('') === '111110') {
+      document.querySelector("#report").innerHTML += 'There is a small straight';
+      }
+  	if (diceArray[i] === 2) { // if there is count of 2 for any possible roll in diceArray
       // print a message declaring a pair and plug in that value (i + 1).
     	document.querySelector("#report").innerHTML += `There are a pair of ${i + 1}s<br>`;
       pairs.push(i + 1); // push to the pairs array the # of the paired dice
-      // if there is three of any die
-    } else if (diceArray[i] === 3) {
+    } else if (diceArray[i] === 3) { // if there is three of any die
       // print a message declaring so, and plug in the value of the dice.
     	document.querySelector("#report").innerHTML += `There are a three of ${i + 1}s<br>`;
       threes.push(i + 1); // push to the threes array the # value of the detected triplet
-      // if there are 4 of any die
-    } else if (diceArray[i] === 4) {
+    } else if (diceArray[i] === 4) { // if there are 4 of any die
+      // print a message declaring so, and plug in the value of the dice.
     	document.querySelector("#report").innerHTML += `There are a four of ${i + 1}s<br>`;
-      // if there are 5 of any die
-    } else if (diceArray[i] === 5) {
+    } else if (diceArray[i] === 5) { // if there are 5 of any die
+      // print a message declaring so, and plug in the value of the dice.
     	document.querySelector("#report").innerHTML += `There are a yacht of ${i + 1}s<br>`;
     }
   }
@@ -68,25 +58,3 @@ function report() {
   } else if (pairs.length === 1 && threes.length === 1) {
   	document.querySelector("#report").innerHTML += `There is a full-house`;
   }
-  // report if smallStraight or bigStraight are true. 
-  if (smallStraight) document.querySelector("#report").innerHTML += 'There is a small straight';
-  if (bigStraight) document.querySelector("#report").innerHTML += 'There is a big straight';
-
-
-
-// two of dice have the same points, like 3 6 5 6 1 - called pair: example solved for you. Complete the other cases below:
-/*     if (howManyDice === 2) { // we check after the counting is done if it is equal to '2'
-      document.querySelector("#report").innerHTML += `There are a pair of ${dieVal}s<br>`;  // if yes, we report a pair
-    } */
-// three of dice have the same points, like 2 4 5 4 4 - called three;
-// four of dice have the same points, like 1 4 1 1 1 - called four;
-// all five dice have the same points, like 2 2 2 2 2 - called yacht;
-// two pairs at once, like 3 6 5 3 5 - called two-pairs;
-// pair and three at once, like 1 6 6 1 6 - called full-house;
-// sequence from 1 to 5, like 2 4 3 5 1 - called small-straight;
-// sequence from 2 to 6, like 6 3 4 2 5 - called big-straight.
-}
-
-//
-// report the results in the div with the ID 'report'.
-report();
