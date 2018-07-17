0b04d45d216cf93ba266d4ca59ec17538316fa12
Thobias Akwenye <thobias4@gmail.com>
2018-07-17 01:32:59 -0500

    assignments

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,47 +1,66 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(){
 
-  var listItems = document.querySelectorAll('li');
+  var shoppingItems = document.querySelectorAll('li')
+  console.log(shoppingItems)
 
-    console.log(listItems.length);
+  var h1 = document.querySelector('h1');
+  h1.insertAdjacentHTML('afterend','<h2>You have 4 items in your cart.</h2>');
+  //The Other way to append subheader...
+  // var subheader = document.createElement('h5');
+  // subheader.innerText = "You have 4 items in your cart";
+  //
+  // var h1 = document.querySelector('h1');
+  // h1.appendChild(subheader);
 
+  var h2 = document.querySelector('h2');
+  h2.insertAdjacentHTML('afterend','<input placeholder=\'Enter item\'/>')
 
+  var input = document.querySelector('input');
+  input.insertAdjacentHTML('afterend','<button>Add Item</button>')
 
-        var h = document.createElement("H2");
-        var t = document.createTextNode("You have _ items in your shopping cart");
-        h.appendChild(t);
-        document.body.appendChild(h);
-
-function addItem() {
-  var ul = document.targetElementById("dynamic-list");
-  var candidate = document.targetElementById("candidate");
-  var li = document.createElement("li");
-  li.setAttribute('id',candidate.value);
-  li.appendChild(document.createTextNode(candidate.value));
-  ul.appendChild(li);
-}
-
-function removeItem(){
-	var ul = document.getElementById("dynamic-list");
-  var candidate = document.getElementById("candidate");
-  var item = document.getElementById(candidate.value);
-  ul.removeChild(item);
-}
-
-window.onload = function() {
-   init();
-  
-}
-
-function init() {
-   document.getElementById("P1").onmouseover = function() {
-      this.className = "paragraph";
-   }
-   document.getElementById("P1").onmouseout = function() {
-      this.className = "";
-   }
-}
+  var button = document.querySelector('button')
+  button.addEventListener("click", addItemToList);
 
 
-});
+  function addItemToList() {
+    var inputValue = input.value;
+    console.log(inputValue)
+
+    var li = document.createElement('li')
+    li.innerText=inputValue;
+
+    var ul = document.querySelector('ul')
+    ul.appendChild(li)
+
+    updateSubheader();
+    addDeleteLink();
+
+  }
+    function updateSubheader(){
+      var listItems = document.querySelectorAll('li')
+      var listLength = listItems.length;
+      console.log(listLength)
+      h2.innerText = 'You have ' + listLength + ' items in your cart.'
+
+    }
+
+    function addDeleteLink(){
+      var listItems = document.querySelectorAll('li')
+      for (var i = 0; i < listItems.length; i++) {
+        if (listItems[i].children.length===0){
+
+        var deleteLink = document.createElement('a')
+        deleteLink.innerText = 'delete'
+        deleteLink.setAttribute('href','#')
+        deleteLink.addEventListener('click', removeItem)
+        listItems[i].appendChild(deleteLink)
+      }
+
+      }
+    }
+    function removeItem(){
+
+    }
+})
