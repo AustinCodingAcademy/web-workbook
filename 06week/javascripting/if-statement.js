5fff2faa408306a4ce8909a3240c2d7d4ba54236
Erlangga Ramzy <erlanggaramzy@gmail.com>
2018-08-23 12:43:34 -0500

    half of javascripting

---
+++ 06week/javascripting/if-statement.js
@@ -0,0 +1,7 @@
+var fruit = "orange";
+if (fruit.length > 5) {
+    console.log("The fruit name has more than five characters.");
+}
+if (fruit.length <= 5) {
+    console.log("The fruit name has five characters or less.");
+}
