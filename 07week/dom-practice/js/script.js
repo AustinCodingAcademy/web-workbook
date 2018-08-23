2e87d362758e423ded8d1db81d8746783b0b7712
dkroll713 <dkroll713@gmail.com>
2018-08-22 20:56:09 -0500

    dom-practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+window.onload = function(){
+
+  var count = document.querySelectorAll('li')
+
+
+  window.alert("There are " + count.length + " items on your list");
+
+  var newh2 = document.createElement('h2')
+  newh2.innerHTML = "You have " + count.length + " items in your shopping cart"
+  document.querySelector('h1').insertAdjacentHTML('afterEnd', '<h2>You have ' + count.length + ' items in your shopping cart.')
+
 });
