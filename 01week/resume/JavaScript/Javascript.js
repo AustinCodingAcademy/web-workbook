e687b3d74649fe9c167f5a7b34f55dd30974bdb9
Jackson <E208451@twcable.com>
2018-08-24 01:44:49 -0500

    created flow charts and added functioins to the JS file sectionSetup() and modifyArtical()

---
+++ 01week/resume/JavaScript/Javascript.js
@@ -0,0 +1,52 @@
+function sectionSetup(){
+
+}
+function modifyArtical(){
+    
+}
+
+
+
+//Creating elements
+//Example: make("input", { id: 'example', classList: ["button"], attr: {  "type": "button", "value": 'test', "onclick": "onclick();" }});
+function make(tag_name, opt) {
+    var ele = document.createElement(tag_name);
+    if (!opt) {
+      return ele;
+    }
+    opt = opt || {};
+    opt.classList = opt.classList || [];
+    opt.attr = opt.attr || {};
+  
+    opt.classList.forEach(function(v) {
+      ele.classList.add(v);
+    });
+    if (opt.text) {
+      ele.textContent = opt.text;
+    }
+    if (opt.html) {
+      ele.innerHTML = opt.html;
+    }
+    if (opt.id) {
+      ele.id = opt.id;
+    }
+    if (opt.value) {
+      ele.value = opt.value;
+    }
+    for (var k in opt.attr) {
+      if (opt.attr.hasOwnProperty(k)) {
+        ele.setAttribute(k, opt.attr[k]);
+      }
+    }
+    if (opt.append) {
+      ele.appendChild(opt.append);
+    }
+    if (opt.appendTo) {
+      if (typeof opt.appendTo === 'string') {
+        opt.appendTo = document.getElementById(opt.appendTo);
+      }
+      opt.appendTo.appendChild(ele);
+    }
+  
+    return ele;
+  }
