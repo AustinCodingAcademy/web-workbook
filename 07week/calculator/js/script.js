2b554d6e9d096e700219d89e7645bed8e4863a4f
Christopher Willis <willis.christopher.a@gmail.com>
2018-08-27 22:35:08 -0500

    Final Calculator

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,25 @@
-'use strict';
+function addNumber(input){
+  document.getElementById("results").value+=input;
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+function clearResults(){
+  document.getElementById("results").value = '';
+}
+
+function operation(operand){
+  document.getElementById("results").value += operand;
+}
+
+function equals(){
+  document.getElementById("results").value = eval(document.getElementById("results").value);
+}
+
+function deleteLast(){
+  let current = document.getElementById("results").value;
+  document.getElementById("results").value = current.substring(0,current.length-1);
+}
+
+function changeSign(){
+  document.getElementById("results").value = -1 * eval(document.getElementById("results").value);
+
+}
