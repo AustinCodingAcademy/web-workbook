8685dabe9022916afa1eec2e679e5a56bc01403a
Arthur Martinez <arthurm1981@gmail.com>
2018-08-28 00:55:59 -0500

    CP2 and some of the Javascripting

--- 06week/javascripting/if-statement.js
+++ 06week/javascripting/if-statement.js
@@ -1,6 +1,6 @@
 var fruit = 'orange';
 if (fruit.length > 5){
-  console.log('The variable n is greater than 1')
+  console.log('The fruit name has more than five characters.')
 } else {
   console.log('The variable n is less than or equal to 1')
 }
