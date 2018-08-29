0840d7e3f82d678d4062237aa5f40ce118243541
rebeccahopebishopp <rhbishopp@outlook.com>
2018-08-28 20:21:29 -0500

    calculator

---
+++ 06week/javascripting/if-statement.js
@@ -0,0 +1,4 @@
+var fruit = 'orange';{
+console.log("The fruit name has more than five characters.");
+console.log("The fruit name has five characters or less.");
+}
--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,38 @@
-'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
   // You code here
-});
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function division() {
+  document.querySelector("#results").value += "/";
+}
+
+function multiplication() {
+  document.querySelector("#results").value += "*";
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
+function plusMinus() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value)*-1;
+}
+
