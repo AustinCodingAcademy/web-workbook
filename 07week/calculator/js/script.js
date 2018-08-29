081a5cb222fcb2a6a50216cce9673eda97277175
Madeline Quitta-Carney <maddiequitta@yahoo.com>
2018-08-28 20:39:51 -0500

    07week

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,21 @@
-'use strict';
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+function addNumber(num) {
+  document.getElementById("results").value += num;
+}
+function clearResults() {
+  document.getElementById("results").value ="";
+}
+function addition() {
+  document.getElementById("results").value +="+";
+}
+function equals() {
+  document.getElementById("results").value = 
+  eval(document.getElementById("results").value);
+}
+function deleteLast() {
+  let current=document.getElementById("results").value; 
+  document.getElementById("results").value = current.substring(0,current.length-1);
+}
+function changesign() {
+  document.getElementById("results").value = 
+  eval(document.getElementById("results").value)*-1;
+}
