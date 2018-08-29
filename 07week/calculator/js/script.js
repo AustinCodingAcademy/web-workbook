a0f9f98261d2c716bc955f9bed74e78c00b32667
Arthur Martinez <arthurm1981@gmail.com>
2018-08-28 21:01:12 -0500

    Calculator assignment

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,38 @@
-'use strict';
+/* Not sure why this was here or if we'll need it. 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
   // You code here
-});
+}); */
+
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiply() {
+  document.querySelector("#results").value += "*";
+}
+
+function divide() {
+  document.querySelector("#results").value += "/";
+}
