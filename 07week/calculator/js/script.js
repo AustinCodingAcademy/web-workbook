202dd8a1d68cc5b79680e04889790f307f85d046
Dora Bojorquez <dorabojorquez@doras-mbp.attlocal.net>
2018-08-28 22:27:08 -0500

    homework

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,31 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+function multiply() {
+  document.querySelector("#results").value += "*";
+}
+function minus() {
+  document.querySelector("#results").value += "-";
+}
+function plusminus() {
+  document.querySelector("#results").value = -1 * eval(document.querySelector("#results").value);
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
