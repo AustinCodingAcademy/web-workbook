e659032628f9940f7be5113eabd82476be1c934b
Alan Houston <alanhouston5000@gmail.com>
2018-08-28 20:25:37 -0500

    calculator really complete now

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -13,6 +13,10 @@
       eval(document.getElementById("results").value);
   }
   
+  function posNeg() {
+    document.getElementById("results").value *= "-1";
+  }
+
   function deleteLast() {
     let current = document.getElementById("results").value;
     document.getElementById("results").value = current.substring(0,current.length-1);
