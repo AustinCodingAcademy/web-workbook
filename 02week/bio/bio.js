ab278f5cec20e31dd2f093e881f3b7e1382d9c26
Jackson <Brett.Jackson@charter.com>
2018-08-29 08:48:40 -0500

    Added Modal JS function and HTML

---
+++ 02week/bio/bio.js
@@ -0,0 +1,20 @@
+function launchModal(){
+    var linebreak = [];
+        for (i = 0; i <= 50; i++) {
+            linebreak.push(document.createElement("br"));
+    }
+    var modal = document.getElementById('bioModal');
+    var span = document.getElementsByClassName("close")[0];	
+        if (toBeDetermined){
+        modal.style.display = "block";
+        }else{
+    
+    }
+    span.onclick = function() {modal.style.display = "none";}
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    
+    }
