e80964e2981e3c979e5c592c2d570d03b06997b2
Christopher Willis <willis.christopher.a@gmail.com>
2018-08-28 20:24:42 -0500

    Dom start up

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+  let totalListItems = document.querySelectorAll('li');
+  let outputString = ''
+  outputString = "You have " + totalListItems.length + " items in your shopping cart";
+  document.querySelector('h1').insertAdjacentHTML('afterend', "<h2> "+ outputString + "</h2>")
 });
