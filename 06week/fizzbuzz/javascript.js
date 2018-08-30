644c185ab2b5de1509821fcf3b3633f2451e7ee0
Hanna Welborn <hannaw07@hotmail.com>
2018-08-29 22:47:22 -0500

    calculator

--- 06week/fizzbuzz/javascript.js
+++ 06week/fizzbuzz/javascript.js
@@ -1,4 +1,4 @@
-var text = "";
+
 var i;
 for (i = 0; i <= 1000; i++) {
 if (i % 3 == 0) {
--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,34 @@
-'use strict';
+// 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   // You code here
+// });
+function addNumber(num) {
+  document.getElementById("results").value += num;
+}
+function subtraction(){
+  document.getElementById("results").value += "-";
+}
+function division(){
+  document.getElementById("results").value += "/";
+}
+function addition(){
+  document.getElementById("results").value += "+";
+}
+function multiply(){
+  document.getElementById("results").value += "*";
+}
+function equals(){
+  document.getElementById("results").value = eval(document.getElementById("results").value);
+}
+function clr() {
+  document.getElementById("results").value = "";
+}
+function delet(){
+  let current = document.getElementById("results").value;
+  document.getElementById("results").value = current.substring(0,current.length-1);
+}
+function plusMinus() {
+let current = document.getElementById("results").value;
+current *= -1;
+document.getElementById("results").value = current;}
