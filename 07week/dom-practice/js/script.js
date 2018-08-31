fee1ecc9e4c9c69f57b495f96b446ab0a8dfc6cd
Wes Garbee <wgarbee@gmail.com>
2018-08-31 08:21:34 -0500

    shopping cart file

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+document.addEventListener("DOMContentLoaded", function() {  
+    let displayCartQty = document.querySelector('h1');
+    displayCartQty.insertAdjacentHTML('afterend', '<h2>You have ' + cartQty() + ' items in your shopping cart.</h2>');
 });
+
+let increaseCart = 0;
+
+function addToCart() {
+    if (document.getElementById("itemEntry").value !== "") {
+        let item = document.getElementById("itemEntry").value;
+        let product = document.querySelector('ul');
+        product.insertAdjacentHTML('beforeend', '<li>' + item + '</li>');
+        document.getElementById("itemEntry").value = "";
+        increaseCart++;
+    }
+}
+
+function cartQty() {
+    let listItems = document.getElementsByTagName('li').length;
+    return listItems;
+}
