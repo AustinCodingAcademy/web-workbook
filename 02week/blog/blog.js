cde5926d8d6cf4238a7e99c72c2ebd165f0588ca
brett <brettj85@gmail.com>
2018-09-01 21:09:28 -0500

    corrected CSS and JS lock is now functional

--- 02week/blog/blog.js
+++ 02week/blog/blog.js
@@ -12,27 +12,21 @@ function toTheTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-/*
-function showStatus(_passedValue) {
-    document.getElementById(_passedValue).style.display = "inline-block";
-}   
-
-function hideStatus(_passedValue) {
-    document.getElementById(_passedValue).style.display = "none";
-}  
-*/
 
 function lockUnlock(articleId){
     var getLockStatus = document.getElementById("lockStatus"+ articleId).innerHTML
 
     if(getLockStatus === "Click to lock")
   {
-
+    document.getElementById("lockStatus"+ articleId).innerHTML = "Click to unlock"
     document.getElementById(articleId).style.transition = "none";
-      document.getElementById(articleId).style.background = "(11, 227, 255, 0.9)";
-      document.getElementById(articleId).style.hight = "600px";
+    document.getElementById(articleId).style.backgroundColor = "rgba(11, 227, 255, 0.9)";
+    document.getElementById(articleId).style.height = "600px";
   }else{
-
+    document.getElementById("lockStatus"+ articleId).innerHTML = "Click to lock"
+    document.getElementById(articleId).style.transition = null;
+    document.getElementById(articleId).style.height = null;
+    document.getElementById(articleId).style.backgroundColor = null;
   }
 }
       /*document.getElementById("lockStatus"+articleId).innerHTML = "Click to unlock";
@@ -48,4 +42,12 @@ function lockUnlock(articleId){
     document.getElementById(articleId).addEventListener("mouseout", function(){ hideStatus('lockstatus'+articleId); }, true);
   }
 }*/
-
+/*
+width: 100%;
+transition: width 2s, height 2s, background-color 3s;
+transition-delay: .5s;
+height: 100px;
+margin-top:10px;
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+background-color: rgba(11, 227, 255,0.6);
+overflow: hidden;  */
