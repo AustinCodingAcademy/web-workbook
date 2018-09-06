73983456e44905e0483d251f1ad76bb2e215f857
unknown <erlanggaramzy@gmail.com>
2018-09-05 22:23:13 -0500

    dom stuff, unfinished

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,12 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+  let items = document.querySelectorAll(".paragraph");
+  let headingtwo = document.querySelector("h1");
+  headingtwo.insertAdjacentHTML('beforeend', `<h2 id="itemlist">There are ${items.length} items in the list!</h2>`);
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  let newitem = document.querySelector("#results").string;
+  let items = document.querySelectorAll(".paragraph");
 });
