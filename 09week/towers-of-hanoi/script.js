314e88aedefe6f6d9eddbb2b0f86b52c7313bee7
katiemacaulay <katiemmacaulay@gmail.com>
2018-09-06 10:43:59 -0500

    tower;if/elsestatement still needs help

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
+$(document).ready(function(){
+  $('[data-block]').draggable({
+    revert: 'invalid'
+  });
+  $('[data-stack]').droppable({
+    drop: function(event, ui){
+      let $last = ($(this).children().last()).data('block');
+      let $dragging = $(ui.draggable).data('block');
+        if($dragging > $last){
+          $(ui.draggable).draggable('option', 'revert', true);
+        }else{
+          $(ui.draggable).appendTo(this).attr('style', 'position: "realtive"');
+        }
+      checkWin()
+    }
+  });
+  if($('[data-stack="3"]').children().length === 4){
+      $("#announce-game-won").text(`You won!!`)
+  }
 });
