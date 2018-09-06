32962df9d2c56251959d265e2c9bcf5c726d39d7
Kellyn <bkellyn013@gmail.com>
2018-09-05 20:09:53 -0500

    shopping cart

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,49 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+
+ function addItems () { 
+  let moreItems = ["The Sparrow by Mary Russell", "Code by Charles Petzold", "ATP Luminometer"]; 
+  for (let i = 0; i < moreItems.length; i ++) {
+    let itemtext = moreItems[i];
+    let listItems = document.createElement("li");                 
+    listItems.innerHTML = itemtext;         
+    document.querySelector("ul").appendChild(listItems);
+  }
+ }
+
+ addItems();
+
+ 
+
+
+function deleteItem (){
+  let remove = document.querySelector('ul');
+  remove.removeChild(remove.querySelector('li'));
+  }
+
+deleteItem ();
+deleteItem ();
+
+
+let list = document.querySelectorAll('li').length; 
+alert("You have " + list + " items in your shopping cart");
+document.querySelector("h1").insertAdjacentHTML("afterend", "<h2>1</h2>");
+document.querySelector("h2").innerHTML = "You have "+ list + " items in your shopping cart";
+
+  
+
+
+
+    
+
+  
+
+
 });
+
+
+
+
+
+
