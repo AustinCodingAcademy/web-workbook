5266808a922fad5358caff752af3f02c80354295
dkroll713 <dkroll713@gmail.com>
2018-09-10 18:25:47 -0500

    towers-of-hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,28 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
-});
+$(document).ready(function(){
+  $('.draggable').draggable({
+   revert: 'invalid'
+ });
+ $('.dragArea').droppable({
+   drop: function(event, ui){
+     let $last = ($(this).children().last()).data('block');
+     let $dragging = $(ui.draggable).data('block');
+     if($dragging > $last){
+      $(ui.draggable).draggable('option','revert', true);
+    } else{
+         $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+    }
+    checkWin()
+   }
+ });
+ function checkWin(){
+   if($("[data-stack=3]").children().length === 4){
+     $("#announce-game-won").text('WINNER');
+   }
+ }
+ $('clear').click(function(){
+   $("#announce-game-won").empty();
+   location.reload();
+ })
+})
