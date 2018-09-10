eb2167fdd5c17ebe074f0efee3d1d21af77dcaec
katiemacaulay <katiemmacaulay@gmail.com>
2018-09-09 21:03:43 -0500

    finished:towers-of-hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,22 +1,35 @@
 'use strict';
 
+let counter = 0;
+
 $(document).ready(function(){
   $('[data-block]').draggable({
+    drag: function(event, ui) {
+      let last = ($(this).parent().children().last()).data('block');
+      let current = $(this).data('block');
+      if(current > last){
+        return false
+      }
+    },
     revert: 'invalid'
   });
   $('[data-stack]').droppable({
     drop: function(event, ui){
-      let $last = ($(this).children().last()).data('block');
-      let $dragging = $(ui.draggable).data('block');
-        if($dragging > $last){
-          $(ui.draggable).draggable('option', 'revert', true);
-        }else{
-          $(ui.draggable).appendTo(this).attr('style', 'position: "realtive"');
-        }
+      let lastValue = ($(this).children().last()).data('block');
+      let draggingValue = $(ui.draggable).data('block');
+      if(draggingValue > lastValue){
+        $(ui.draggable).draggable('option', 'revert', true);
+      }else{
+        $(ui.draggable).appendTo(this).attr('style', 'position: relative');
+        counter++;
+        $("#counter").text(`Number of moves: ${counter}`);
+      }
       checkWin()
     }
   });
-  if($('[data-stack="3"]').children().length === 4){
-      $("#announce-game-won").text(`You won!!`)
+  function checkWin(){
+    if($('[data-stack="3"]').children().length === 4){
+        $("#announce-game-won").text(`You won!!`)
+    }
   }
 });
