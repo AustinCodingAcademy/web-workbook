ce79ee735a9d31c1fe86eab220e165d478807181
Kelsey Boshears <kelseyboshears@yahoo.com>
2018-09-12 13:37:48 -0500

    DOM

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,25 @@
-'use strict';
+
+function addItem(){
+  var ul = document.getElementById("shopping-list");
+  var candidate = document.getElementById("candidate");
+  var li = document.createElement("li");
+  li.setAttribute('id',candidate.value);
+  li.appendChild(document.createTextNode(candidate.value));
+  ul.appendChild(li);
+}  
+function removeItem(){
+	var ul = document.getElementById("shopping-list");
+  var candidate = document.getElementById("candidate");
+  var item = document.getElementById(candidate.value);
+  ul.removeChild(item);
+}
 
 document.addEventListener("DOMContentLoaded", function(event) {
+
+ alert ("There are four list items on this page");
+
   // You code here
+
 });
+
+
