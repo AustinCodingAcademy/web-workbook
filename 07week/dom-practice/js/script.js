d57a3a6c55ffa16bae13d6829e1aefd1eb4c1785
rebeccahopebishopp <rhbishopp@outlook.com>
2018-09-13 20:12:28 -0500

    customer satisfaction

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,13 @@
 'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+alert (document.querySelectorAll ("li").length);
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+document.querySelector("h1").insertAdjacentHTML("afterend","<h2>You have " + document.querySelectorAll("li").length + " items in your shopping cart </h2>");
+
+document.querySelector("ul").insertAdjacentHTML("afterend", "<input id='enterText' type='text' value=''>" )
+document.querySelector("input").insertAdjacentHTML('afterend', "<input class='submitButton' type='submit' value='Add Item' onClick=addItem()>")
+reindexDeleteButtonsHack();
+
+
 });
+ 
