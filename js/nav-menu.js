d5fc7d1c29a71e07170923576a972cc3f75340d2
Andy Olson <andyolson09@gmail.com>
2018-09-13 23:45:10 -0500

    wrapped everything in a main and called it a day

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,3 +1,8 @@
+var link = document.querySelector('link[rel="import"]');
+var content = link.import;
+var el = content.querySelector('.nav-container');
+document.body.insertBefore(el.cloneNode(true), document.getElementsByTagName('main')[0]);
+
 const navToggle = document.querySelector('.nav-toggler')
 const navMenu   = document.querySelector('.nav-menu')
 
