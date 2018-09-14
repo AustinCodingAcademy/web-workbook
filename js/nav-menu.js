86f35292d17b2ac82e7de083c222460318874483
Andy Olson <andyolson09@gmail.com>
2018-09-14 14:34:14 -0500

    big design changes to global nav-menu

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -3,14 +3,16 @@ var content = link.import;
 var el = content.querySelector('.nav-container');
 document.body.insertBefore(el.cloneNode(true), document.getElementsByTagName('main')[0]);
 
-const navToggle = document.querySelector('.nav-toggler')
-const navMenu   = document.querySelector('.nav-menu')
-const currPage  = "../.." + window.location.pathname
+const pageContent = document.body.getElementsByTagName('main')[0]
+const navToggle   = document.querySelector('.nav-toggler')
+const navMenu     = document.querySelector('.nav-menu')
+const currPage    = "../.." + window.location.pathname
 
 navToggle.addEventListener('click', _ => {
-  navMenu.classList.toggle('active')
-  navToggle.classList.toggle('active')
+  pageContent.classList.toggle('body-open')
+  navToggle.classList.toggle('nav-open')
+  navMenu.classList.toggle('nav-open')
 })
 
 // add current-page class to current page
-document.querySelector('ul > a[href="' + currPage + '"]').classList.add('current-page')
+document.querySelector('a[href="' + currPage + '"]').classList.add('current-page')
