ee232177cdeaf2d30e18fff013f82944896e7ebb
Andy Olson <andyolson09@gmail.com>
2018-09-14 20:53:57 -0500

    commented nav-menu.js to better understand it

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,9 +1,14 @@
+
+// get current page directory path
 const currPagePath = "../.." + window.location.pathname
+
+// get current page HTML link (nav-menu.html), then insert it before current page <main>
 var link    = document.querySelector('link[rel="import"]');
 var content = link.import;
 var el      = content.querySelector('.nav-container');
 document.body.insertBefore(el.cloneNode(true), document.getElementsByTagName('main')[0]);
 
+// get necessary elements to animate opening and closing of nav-menu
 const pageContent = document.body.getElementsByTagName('main')[0]
 const navToggle   = document.querySelector('.nav-toggler')
 const navMenu     = document.querySelector('.nav-menu')
@@ -16,9 +21,10 @@ navToggle.addEventListener('click', _ => {
 // add current-page class to current page
 document.querySelector('a[href="' + currPagePath + '"]').classList.add('current-page')
 
+// traverse through each group of links to identify which group has the current page,
+// also handles opening and closing of groups
 let dropdown = document.querySelectorAll('.dropdown')
 let currPage = document.querySelector('.current-page')
-
 for(var i=0; i<dropdown.length; i++) {
   dropdown[i].addEventListener('click', open, false)
 
