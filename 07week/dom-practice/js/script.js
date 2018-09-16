9b6e98544a602be19be6d35ac0429cedc754b28a
dkroll713 <dkroll713@gmail.com>
2018-09-16 16:01:56 -0500

    dom-practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,36 +1,42 @@
-'use strict';
 
-document.getElementByID("")
 
-// document.getElementsByID("shoppingCart").innerhtml = "Please work";
-//
-// window.onload = function(){
-//
-//   var count = document.querySelectorAll('li')
-// 
-//
-//   window.alert("There are " + count.length + " items on your list");
-//
-//   var newh2 = document.createElement('h2')
-//   newh2.innerHTML = "You have " + count.length + " items in your shopping cart"
-//   document.querySelector('h1').insertAdjacentHTML('afterEnd', '<h2>You have ' + count.length + ' items in your shopping cart.')
-// }
-//
-//   function insertAddItem () {
-//     var newInput = document.createElement('input');
-//     newInput.setAttribute("placeholder", "Type to add");
-//     let cart = document.getElementsByTagName("ul");
-//     cart[0].after(newInput);
-//     newInput.setAttribute("id", "input-text");
-//
-//     let newButton = document.createElement('button');
-//     newButton.innerHTML = 'Add Items';
-//     newButton.setAttribute("id", "add-button")
-//     let nextButton = document.getElementsByTagName("input");
-//     nextButton[0].after(newButton);
-//
-//     newButton.onclick = addItem;
-//   }
-// ;
-//
-// insertAddItem();
+'use strict'
+
+window.onload = function() {
+
+  cartCount();
+  insertAddItem();
+  insertRemoveItem();
+
+  function cartCount() {
+    let list = document.getElementsByTagName('ul');
+    var count = document.querySelectorAll('li');
+    document.querySelector('h1').insertAdjacentHTML('afterEnd', '<h2>You have ' + count.length + ' items in your shopping cart.');
+  }
+
+  function insertAddItem() {
+    var newInput = document.createElement('input');
+    newInput.setAttribute("placeholder", "Type to add");
+    let cart = document.getElementsByTagName("ul");
+    cart[0].after(newInput);
+    newInput.setAttribute("id", "input-text");
+
+    let newButton = document.createElement('button');
+    newButton.innerHTML = 'Add Items';
+    newButton.setAttribute("id", "add-button")
+    let nextButton = document.getElementsByTagName("input");
+    nextButton[0].after(newButton);
+
+    newButton.onclick = addItem;
+  }
+
+  function addItem(event){
+    let newItemText = document.getElementByID("input-text");
+    let newItem = document.createElement("li");
+    newItem.innerHTML = newItemText.value;
+    document.getElementsByTagName("ul")[0].appendChild(newItem);
+    document.getElementByID('input-text').value='';
+    cartCount();
+  }
+
+}
