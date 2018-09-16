33142d61b5a5c4666889704bb64b51cd37895e75
dkroll713 <dkroll713@gmail.com>
2018-09-16 13:36:44 -0500

    dom-practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -12,4 +12,20 @@ window.onload = function(){
   newh2.innerHTML = "You have " + count.length + " items in your shopping cart"
   document.querySelector('h1').insertAdjacentHTML('afterEnd', '<h2>You have ' + count.length + ' items in your shopping cart.')
 
+  function insertAddItem () {
+    var newInput = document.createElement('input');
+    newInput.setAttribute("placeholder", "Type to add");
+    let cart = document.getElementsByTagName("ul");
+    cart[0].after(newInput);
+    newInput.setAttribute("id", "input-text");
+
+    let newButton = document.createElement('button');
+    newButton.innerHTML = 'Add Items';
+    newButton.setAttribute("id", "add-button")
+    let nextButton = document.getElementsByTagName("input");
+    nextButton[0].after(newButton);
+
+    newButton.onclick = addItem;
+  }
+
 });
