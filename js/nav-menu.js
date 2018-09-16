dcdeca60d513a317c9946337d5ace99f0f46c3aa
Andy Olson <andyolson09@gmail.com>
2018-09-16 17:26:24 -0500

    added more documentation structure to nav-menu.js

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,9 +1,19 @@
+// --------------------------------------------------------------
+// IMPORT HTML
+// --------------------------------------------------------------
 // get current page HTML link (nav-menu.html), then insert it before current page <main>
 var link    = document.querySelector('link[rel="import"]');
 var content = link.import;
 var el      = content.querySelector('.nav-container');
 
 document.body.insertBefore(el.cloneNode(true), document.getElementsByTagName('main')[0]);
+// --------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------
+// INITIALIZE PAGE
+// --------------------------------------------------------------
 
 // get necessary elements to animate opening and closing of nav-menu
 const pageContent = document.body.getElementsByTagName('main')[0]
@@ -14,6 +24,31 @@ const main        = document.getElementsByTagName('main')[0]
 // preset some classes to accomodate navbar being open
 pageContent.classList.add('body-open')
 pageContent.classList.add('transition-transform')
+
+// turn navMenu into a SimpleScrollbar element
+SimpleScrollbar.initEl(navMenu)
+// --------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------
+// SET CURRENT NAV-LINK
+// --------------------------------------------------------------
+
+// get current page index directory path
+const currPagePath = "../.." + window.location.pathname
+const currPageRef = document.querySelector('a[href="' + currPagePath + '"]')
+
+// add current-page class to current page and make it a variable
+currPageRef.classList.add('current-page')
+let currPage = document.querySelector('.current-page')
+// --------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------
+// OPEN / CLOSE ENTIRE NAV MENU
+// --------------------------------------------------------------
 
 navToggle.addEventListener('click', _ => { // toggle navbar when user clicks hamburger
   pageContent.classList.toggle('body-open')
@@ -27,15 +62,13 @@ main.addEventListener('click', _ => { // close navbar if user clicks off navbar 
     navMenu.classList.remove('nav-open')
   }
 })
+// --------------------------------------------------------------
 
-// get current page index directory path
-const currPagePath = "../.." + window.location.pathname
-const currPageRef = document.querySelector('a[href="' + currPagePath + '"]')
 
-// add current-page class to current page and make it a variable
-currPageRef.classList.add('current-page')
-let currPage = document.querySelector('.current-page')
 
+// --------------------------------------------------------------
+// OPEN / CLOSE FOLDERS
+// --------------------------------------------------------------
 // traverse through each group of links to identify which group has the current page,
 // also handles opening and closing of groups
 let dropdown = document.querySelectorAll('.dropdown')
@@ -53,6 +86,11 @@ for(var i=0; i<dropdown.length; i++) {
     dropdown[i].querySelector('.dropdown-content').classList.add('open')
   }
 }
+
+
+// --------------------------------------------------------------
+// FUNCTIONS
+// --------------------------------------------------------------
 function toggle() {
   this.classList.toggle('open')
   this.lastElementChild.classList.toggle('open')
@@ -78,3 +116,4 @@ function closeAll() {
     dropdown[i].querySelector('.dropdown-content').classList.remove('open')
   }
 }
+// --------------------------------------------------------------
