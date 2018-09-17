dae11ba3f25b6cf69ca08748c8fcca35a9ee5dc1
Andy Olson <andyolson09@gmail.com>
2018-09-16 21:17:05 -0500

    started working on making homepage responsive

--- js/home-nav-menu.js
+++ js/home-nav-menu.js
@@ -5,18 +5,71 @@ var el      = content.querySelector('.nav-container');
 
 document.body.insertBefore(el.cloneNode(true), document.getElementsByTagName('main')[0]);
 
-// traverse through each group of links to identify which group has the current page,
-// also handles opening and closing of groups
-let dropdown = document.querySelectorAll('.dropdown')
-let currPage = document.querySelector('.current-page')
+// --------------------------------------------------------------
+// INITIALIZE PAGE
+// --------------------------------------------------------------
+
+// get necessary elements to animate opening and closing of nav-menu
+const pageContent     = document.body.getElementsByTagName('main')[0]
+const homePageContent = document.querySelector('.content')
+const navToggle       = document.querySelector('.nav-toggler')
+const navMenu         = document.querySelector('.nav-menu')
+const main            = document.getElementsByTagName('main')[0]
+const dropdown        = document.querySelectorAll('.dropdown')
+const currPage        = document.querySelector('.current-page')
+
+pageContent.classList.add('transition-transform')
 
 for(var i=0; i<dropdown.length; i++) {
-  dropdown[i].addEventListener('mouseover', open, false)
-  dropdown[i].addEventListener('click', toggle, false)
-  dropdown[i].addEventListener('mouseout', close, false)
+  dropdown[i].addEventListener('click', toggle)
+}
 
-  if(dropdown[i].contains(currPage)) {
-    dropdown[i].classList.add('current-folder')
+// turn navMenu into a SimpleScrollbar element
+SimpleScrollbar.initEl(navMenu)
+// --------------------------------------------------------------
+
+// --------------------------------------------------------------
+// OPEN / CLOSE ENTIRE NAV MENU
+// --------------------------------------------------------------
+
+navToggle.addEventListener('click', _ => { // toggle navbar when user clicks hamburger
+  pageContent.classList.toggle('body-open')
+  homePageContent.classList.toggle('nav-open')
+  navToggle.classList.toggle('nav-open')
+  navMenu.classList.toggle('nav-open')
+})
+main.addEventListener('click', _ => { // close navbar if user clicks off navbar while it is open
+  if(navMenu.classList.contains('nav-open')) {
+    pageContent.classList.remove('body-open')
+    navToggle.classList.remove('nav-open')
+    navMenu.classList.remove('nav-open')
+  }
+})
+// --------------------------------------------------------------
+
+
+// traverse through each group of links to identify which group has the current page,
+// also handles opening and closing of groups
+var addedEventListener = false
+
+toggleHover()
+window.addEventListener("resize", toggleHover)
+
+function toggleHover() {
+  if (!window.matchMedia("(max-width: 768px)").matches && !addedEventListener){
+    for(var i=0; i<dropdown.length; i++) {
+      dropdown[i].addEventListener('mouseover', open)
+      dropdown[i].addEventListener('mouseout', close)
+    }
+    addedEventListener = true
+    closeAll()
+  }
+  else if (window.matchMedia("(max-width: 768px)").matches && addedEventListener) {
+    for(var i=0; i<dropdown.length; i++) {
+      dropdown[i].removeEventListener('mouseover', open)
+      dropdown[i].removeEventListener('mouseout', close)
+    }
+    addedEventListener = false
   }
 }
 
@@ -31,4 +84,10 @@ function toggle() {
 function close() {
   this.classList.remove('open')
   this.lastElementChild.classList.remove('open')
+}
+function closeAll() {
+  for(var i=0; i<dropdown.length; i++) {
+    dropdown[i].classList.remove('open')
+    dropdown[i].lastElementChild.classList.remove('open')
+  }
 }
