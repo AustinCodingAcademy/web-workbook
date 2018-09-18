f1270a062a7fc1911656a39ad88c2b33e2db3156
Andy Olson <andyolson09@gmail.com>
2018-09-18 16:20:36 -0500

    same as before

--- js/home-nav-menu.js
+++ js/home-nav-menu.js
@@ -1,78 +1,78 @@
-
 /** ==============================================
-*  IMPORTS AND INSERTS HTML link (nav-menu.html) 
-*  ==============================================
-* */
+ *  IMPORTS AND INSERTS HTML link (nav-menu.html)
+ *  ==============================================
+ * */
 
 /** ______________________________________________________________________________________
-*  get current page HTML link (nav-menu.html), then insert it before current page <main>
-*  */
-var link    = document.querySelector('link[rel="import"]');
+ *  get current page HTML link (nav-menu.html), then insert it before current page <main>
+ *  */
+var link = document.querySelector('link[rel="import"]');
 var content = link.import;
 var el = content.querySelector('.nav-container');
-document.body.insertBefore(el.cloneNode(true), document.querySelector('.wrapper'));
-
+document.body.insertBefore(
+  el.cloneNode(true),
+  document.getElementsByTagName('main')[0]
+);
 
 /** ==============================
  *  INIT PAGE VARIABLES / CLASSES
  *  ==============================
  * */
-const pageContent     = document.body.getElementsByTagName('main')[0]
-const homePageContent = document.querySelector('.content')
-const navToggle       = document.querySelector('.nav-toggler')
-const navMenu         = document.querySelector('.nav-menu')
-const openCloseAll    = document.querySelector('.nav-open-close-all')
-const main            = document.getElementsByTagName('main')[0]
-const dropdown        = document.querySelectorAll('.dropdown')
-const currPage        = document.querySelector('.current-page')
-pageContent.classList.add('transition-transform')
-SimpleScrollbar.initEl(navMenu)
-
+const pageContent = document.body.getElementsByTagName('main')[0];
+const homePageContent = document.querySelector('.content');
+const navToggle = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.nav-menu');
+const openCloseAll = document.querySelector('.nav-open-close-all');
+const main = document.getElementsByTagName('main')[0];
+const dropdown = document.querySelectorAll('.dropdown');
+const currPage = document.querySelector('.current-page');
+pageContent.classList.add('transition-transform');
+SimpleScrollbar.initEl(navMenu);
 
 /** =======================
  *  OPEN / CLOSE NAV-MENU
  *  =======================
  * */
-navToggle.addEventListener('click', _ => { // toggle navbar when user clicks navToggle button
-  pageContent.classList.toggle('body-open')
-  homePageContent.classList.toggle('nav-open')
-  navToggle.classList.toggle('nav-open')
-  navMenu.classList.toggle('nav-open')
-})
-main.addEventListener('click', _ => { // close navbar if user clicks main content of page
-  if(navMenu.classList.contains('nav-open')) {
-    pageContent.classList.remove('body-open')
-    navToggle.classList.remove('nav-open')
-    navMenu.classList.remove('nav-open')
+navToggle.addEventListener('click', _ => {
+  // toggle navbar when user clicks navToggle button
+  pageContent.classList.toggle('body-open');
+  homePageContent.classList.toggle('nav-open');
+  navToggle.classList.toggle('nav-open');
+  navMenu.classList.toggle('nav-open');
+});
+main.addEventListener('click', _ => {
+  // close navbar if user clicks main content of page
+  if (navMenu.classList.contains('nav-open')) {
+    pageContent.classList.remove('body-open');
+    navToggle.classList.remove('nav-open');
+    navMenu.classList.remove('nav-open');
   }
-})
-
+});
 
 /** =================================================
  *  DROPDOWN MOUSOVER / CLICK EVENT LISTENER CONTROL
  *  =================================================
  * */
-for(var i=0; i<dropdown.length; i++) { // always load page with click listeners
-  dropdown[i].addEventListener('click', toggle)
+for (var i = 0; i < dropdown.length; i++) {
+  // always load page with click listeners
+  dropdown[i].addEventListener('click', toggle);
 }
 
 /** _______________________________________________
  *  initialize window size variables on page load
  */
-var addedHover = false
-const widthThreshold = "(max-width: 768px)"
-var windowSmall = window.matchMedia(widthThreshold)
-toggleMouseoverListener() // check window size once on page load
-
+var addedHover = false;
+const widthThreshold = '(max-width: 768px)';
+var windowSmall = window.matchMedia(widthThreshold);
+toggleMouseoverListener(); // check window size once on page load
 
 // listen for any window resizing and add/remove mouseover listeners accordingly
-window.addEventListener("resize", toggleMouseoverListener)
-openCloseAll.classList.toggle('closed')
-openCloseAll.addEventListener('click', openClose)
-
+window.addEventListener('resize', toggleMouseoverListener);
+openCloseAll.classList.toggle('closed');
+openCloseAll.addEventListener('click', openClose);
 
 /** ______________________________________________________________________________
- *  toggleMouseoverListener dynamically adds/removes mouseover event listeners to 
+ *  toggleMouseoverListener dynamically adds/removes mouseover event listeners to
  *  nav-menu dropdown elements depending on window size (const widthThreshold)
  *  */
 function toggleMouseoverListener() {
@@ -80,24 +80,23 @@ function toggleMouseoverListener() {
    *  when window is resized from <widthThreshold to >widthThreshold
    *  or if window loads at >widthThreshold
    *  */
-  if (!windowSmall.matches && !addedHover){ 
-    for(var i=0; i<dropdown.length; i++) {
-      dropdown[i].addEventListener('mouseover', open)
-      dropdown[i].addEventListener('mouseout', close)
+  if (!windowSmall.matches && !addedHover) {
+    for (var i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener('mouseover', open);
+      dropdown[i].addEventListener('mouseout', close);
     }
-    addedHover = true
-    closeAll()
-  }
+    addedHover = true;
+    closeAll();
+  } else if (windowSmall.matches && addedHover) {
   /** ____________________________________________________________________________
-   *  when window is resized to <widthThreshold and mouseover listeners have already 
+   *  when window is resized to <widthThreshold and mouseover listeners have already
    *  been added
    *  */
-  else if (windowSmall.matches && addedHover) {
-    for(var i=0; i<dropdown.length; i++) {
-      dropdown[i].removeEventListener('mouseover', open)
-      dropdown[i].removeEventListener('mouseout', close)
+    for (var i = 0; i < dropdown.length; i++) {
+      dropdown[i].removeEventListener('mouseover', open);
+      dropdown[i].removeEventListener('mouseout', close);
     }
-    addedHover = false
+    addedHover = false;
   }
 }
 
@@ -105,35 +104,34 @@ function toggleMouseoverListener() {
  *  functions to be called by mouseover / click event listeners
  */
 function open() {
-  this.classList.add('open')
-  this.lastElementChild.classList.add('open')
+  this.classList.add('open');
+  this.lastElementChild.classList.add('open');
 }
 function toggle() {
-  this.classList.toggle('open')
-  this.lastElementChild.classList.toggle('open')
+  this.classList.toggle('open');
+  this.lastElementChild.classList.toggle('open');
 }
 function close() {
-  this.classList.remove('open')
-  this.lastElementChild.classList.remove('open')
+  this.classList.remove('open');
+  this.lastElementChild.classList.remove('open');
 }
 function openClose() {
-  if(this.classList.contains('closed')) {
-    openAll()
+  if (this.classList.contains('closed')) {
+    openAll();
+  } else {
+    closeAll();
   }
-  else {
-    closeAll()
-  }
-  this.classList.toggle('closed')
+  this.classList.toggle('closed');
 }
 function openAll() {
-  for(var i=0; i<dropdown.length; i++) {
-    dropdown[i].classList.add('open')
-    dropdown[i].lastElementChild.classList.add('open')
+  for (var i = 0; i < dropdown.length; i++) {
+    dropdown[i].classList.add('open');
+    dropdown[i].lastElementChild.classList.add('open');
   }
 }
 function closeAll() {
-  for(var i=0; i<dropdown.length; i++) {
-    dropdown[i].classList.remove('open')
-    dropdown[i].lastElementChild.classList.remove('open')
+  for (var i = 0; i < dropdown.length; i++) {
+    dropdown[i].classList.remove('open');
+    dropdown[i].lastElementChild.classList.remove('open');
   }
 }
