5d0bb9cd67ae3faa8cc894cbb37289b62e1eb7c1
Arthur Martinez <arthurm1981@gmail.com>
2018-09-17 22:37:32 -0500

    CP3

--- CP3/script.js
+++ CP3/script.js
@@ -1,34 +1,9 @@
-var data = {
-  totalRevs:360, 
-  totalCurrent:0, 
-  totalRPS: 0
-};
 
-setInterval(goGo,1000);
+$(document).ready(function(){
 
-function goGo() {
-  data.totalRevs += data.totalRPS;
-  data.totalCurrent += data.totalRPS;
-  $("#wheel").css({ 'transform': 'rotate(' + data.totalRevs + 'deg)'});
-  updateReport();
-}
+  $('#robotCounter').click(function() { //couldn't get my original idea to work. This is just a numeric counter that displays some text.
+    $('.count').html(function(i, val) { return val*1+1 });
+    Materialize.toast('You punched a robot in the face!', 2000)
+    });
 
-function updateReport() {
-  $("#currentTotal").text(Math.floor(data.totalCurrent));
-  $("#rps").text((data.totalRPS/70.4).toFixed(3));
-}
-
-
-$("#wheel").click(function (){
-  data.totalRevs ++;
-  data.totalCurrent ++;
-  updateReport();
-})
-
-$(".button").click(function (){ 
-  var addVal = $(this).data( "cost" );
-  if ($(this).data( "cost" ) < data.totalCurrent ) {
-    data.totalCurrent -=  parseFloat($(this).data( "cost" ).toPrecision(2));
-    data.totalRPS += parseFloat($(this).data( "val" ));
-    $( this ).children("span").html( parseInt($( this ).children("span").html()*1.15)); 
-    $( this ).data( "cost", parseInt($(this).data( "cost" ) * 1.15) ); 
+  });
