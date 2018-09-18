e5c3cc043031ce19ca244899c4bd53ecd2e6e9c7
Arthur Martinez <arthurm1981@gmail.com>
2018-09-18 14:57:27 -0500

    CP3 edits

--- CP3/script.js
+++ CP3/script.js
@@ -1,9 +1,9 @@
-
 $(document).ready(function(){
 
-  $('#robotCounter').click(function() { //couldn't get my original idea to work. This is just a numeric counter that displays some text.
+  $('.fist').click(function() {
+    /*$('.count').html(function(i, val) {return val++});*/
     $('.count').html(function(i, val) { return val*1+1 });
-    Materialize.toast('You punched a robot in the face!', 2000)
+    Materialize.toast('Ouuu, you hit me!', 2000)
     });
 
   });
