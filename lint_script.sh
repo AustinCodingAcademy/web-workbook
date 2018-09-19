eb36be44f1c0d05940dedb335ce31e398e7d2379
Tosin Awofeso <midastouchproductions@gmail.com>
2018-09-18 20:30:07 -0500

    edit script so exit with non zero so circle ci reads it

--- lint_script.sh
+++ lint_script.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
-echo ===================CSS ERRORS START======================
-echo ===================CSS ERRORS START======================
+echo ==============CSS ERRORS===============
+echo ==============CSS ERRORS===============
+echo 
 stylelint ./**/*.css 
-echo ===================CSS ERRORS END======================
-echo ===================CSS ERRORS END======================
+CSS="$?"
+echo 
+echo ==============CSS ERRORS===============
+echo ==============CSS ERRORS===============
 echo 
 echo 
-echo ===================JS ERRORS START======================
-echo ===================JS ERRORS START======================
-eslint . 
-echo ===================JS ERRORS END======================
-echo ===================JS ERRORS END======================
+echo ==============JS ERRORS===============
+echo ==============JS ERRORS===============
+echo 
+eslint .
+JS="$?"
+echo 
+echo ==============JS ERRORS===============
+echo ==============JS ERRORS===============
 echo 
 echo 
-echo ===================HTML ERRORS START======================
-echo ===================HTML ERRORS START======================
+echo ==============HTML ERRORS===============
+echo ==============HTML ERRORS===============
+echo 
 htmllint {*.html,./**/*.html}
-echo ===================HTML ERRORS END======================
-echo ===================HTML ERRORS END======================
+HTML="$?"
+echo 
+echo ==============HTML ERRORS===============
+echo ==============HTML ERRORS===============
+echo 
+
+if [ $CSS -eq 0 ] && [ $JS -eq 0 ] && [ $HTML -eq 0 ]
+then
+echo "YAY WE DID IT"
+else
+echo "FAILURES"
+exit 2
+fi
+
 
