cf70d6c8b40cd95532a41ee34bc0589974207d47
Taylor Colten <taylorwcolten@gmail.com>
2018-09-27 21:53:31 -0500

    javascript'

---
+++ 06week/javascripting/fizzbuzz.js
@@ -0,0 +1,16 @@
+
+for (let i =1; i <= 1000; i++){
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzbuzz");
+
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+
+    }else if (i % 3 === 0) {
+        console.log("fizz");
+    
+    }else {
+        console.log(i);
+    }
+}
