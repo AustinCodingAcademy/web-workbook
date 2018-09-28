3791455289216f089740a579aaac54cedd137a9c
Laura Colten <lauracolten5@gmail.com>
2018-09-27 19:49:38 -0500

    formatting

--- 06week/FizzBuzz/fizzbuzz.js
+++ 06week/FizzBuzz/fizzbuzz.js
@@ -1,14 +1,14 @@
 for (index = 0; index < 1001; index++) {
   if (index % 5 === 0 && index % 3 === 0) {
-      console.log('FizzBuzz');
-  }   
+    console.log('FizzBuzz');
+  }
   else if (index % 3 === 0) {
-      console.log('Fizz');
+    console.log('Fizz');
   }
   else if (index % 5 === 0) {
-      console.log('Buzz');
+    console.log('Buzz');
   }
   else {
-      console.log (index);
+    console.log(index);
   }
 }  
