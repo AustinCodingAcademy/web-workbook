4f69b9be20f229d7e235ac3c4fc488b98bdf5ba6
Andy Olson <andyolson09@gmail.com>
2018-10-02 11:54:49 -0500

    documented js

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,10 +1,16 @@
+
+// call navbarScroll when user scrolls
 window.onscroll = function() {
   navbarScroll();
 };
 
+// get navbar container
 const navbar = document.querySelector(".nav-container");
+
+// get an original Y offset value for page
 var originScrollPos = window.pageYOffset;
 
+// This function adds/removes a hidden class depending on page Y offset value
 function navbarScroll() {
   let currentScrollPos = window.pageYOffset;
 
@@ -17,6 +23,7 @@ function navbarScroll() {
   originScrollPos = currentScrollPos;
 }
 
+// This function is called on resize of body tag to recalculate height vars if user resizes window
 function recalcHeights() {
   heroHeight = document.querySelector(".hero").clientHeight;
   navbarHeght = navbar.clientHeight;
