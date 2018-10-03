51ccd5286b19b4be72662d58cf80f2a850264eb4
Andy Olson <andyolson09@gmail.com>
2018-10-03 01:32:33 -0500

    created errorHandler function for +/- button

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// You code here
 function addNumber(num) {
   document.querySelector("#results").value += num;
 }
@@ -46,23 +45,34 @@ function deleteLast() {
 
 function toggleSign() {
   let current = document.querySelector("#results").value;
-  let currentFirstChar = current.slice(0, 1);
-  let currentAfterFirstChar = current.slice(1);
+  let currentFirstChar = current.slice(0, 1); // first char of results
+  let currentAfterFirstChar = current.slice(1); // everything after first char of results
 
-  if (
-    current === "" ||
-    currentAfterFirstChar.includes("+") ||
-    currentAfterFirstChar.includes("-") ||
-    currentAfterFirstChar.includes("*") ||
-    currentAfterFirstChar.includes("/")
-  ) {
-    alert("invalid input")
-    return;
-  }
+  // check for cases where adding/removing a negative sign wouldn't make sense
+  if (errorHandler(current, currentAfterFirstChar)) return;
 
   if (currentFirstChar === "-") {
+    // remove "-" from current results
     document.querySelector("#results").value = current.substr(1);
-  } else {
+  } 
+  else {
+    // add "-" to current results
     document.querySelector("#results").value = "-" + current;
   }
+}
+
+// Function that checks if str1 is empty or str2 contains a "+", "-", "*", or "/"
+function errorHandler(str1, str2) {
+  if (
+    str1 === "" ||
+    str1 === "0" ||
+    str2.includes("+") ||
+    str2.includes("-") ||
+    str2.includes("*") ||
+    str2.includes("/")
+  )
+    return true;
+  
+    else 
+    return false;
 }
