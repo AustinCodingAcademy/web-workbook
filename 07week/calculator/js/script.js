392de46c43150a320da0bd01c10ddee518fabfdb
Joanna Filipowska <j.filipowska@gmail.com>
2018-10-02 20:00:55 -0500

    07 week js

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -1,5 +1,35 @@
 'use strict';
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiple() {
+  document.querySelector("#results").value += "*";
+}
+function devide() {
+  document.querySelector("#results").value += "/";
+}
+function plusMinus() {
+  document.querySelector("#results").value = document.querySelector("#results").value * -1;
+}
