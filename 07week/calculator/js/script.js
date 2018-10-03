2eb70d072d8b1b2fc634d804244ab511d3332687
oaljaber <osama.aljabr@gmail.com>
2018-10-02 20:32:34 -0500

    all calculator functions is working

--- 07week/calculator/js/script.js
+++ 07week/calculator/js/script.js
@@ -2,6 +2,27 @@ function addNumber(num) {
   document.querySelector("#results").value += num;
 }
 
+function subNumber(num) {
+  document.querySelector("#results").value += "-";
+}
+
+
+function multiNumber(num) {
+  document.querySelector("#results").value += "*";
+}
+
+
+function divNumber(num) {
+  document.querySelector("#results").value += "/";
+}
+
+
+function swapNumber(num) {
+  document.querySelector("#results").value += "* -1";
+  equals();
+}
+
+
 function clearResults() {
   document.querySelector("#results").value = "";
 }
