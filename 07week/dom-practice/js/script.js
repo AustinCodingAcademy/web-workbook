703db04f49fd4d53ea2c1464a1e74323b1b97a40
Andy Olson <andyolson09@gmail.com>
2018-10-04 23:38:52 -0500

    got all features working except challenge feature

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -2,6 +2,36 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   // You code here
-  console.log("hello!");
+  const list = document.getElementsByTagName("ul")[0];
+  const items = list.getElementsByTagName("li");
   
+  var itemCount = getItemCount();
+  // alert("You have " + itemCount + " items");
+  
+  insertItemCountMessage();
+  
+  var buttonAddItem = document.getElementById('btn-add-item');
+  buttonAddItem.addEventListener("click", () => { 
+    let item = prompt("What would you like to add?"); 
+    list.insertAdjacentHTML("beforeend", '<li>'+item+'</li>');
+  });
+  
+  var buttonRemoveItem = document.getElementById('btn-remove-item');
+  buttonRemoveItem.addEventListener("click", () => {  
+    let lastItem = items[items.length - 1];
+    if(lastItem) list.removeChild(lastItem);
+  });
+
+
+  function getItemCount() {
+    return list.children.length;
+  }
+
+  function insertItemCountMessage() {
+    let itemCount = getItemCount();
+    let mainTitle = document.getElementById('main-title');
+    mainTitle.insertAdjacentHTML("afterend", '<h2 id="itemCount">You have ' + itemCount + ' items in your shopping cart.');
+  }
+    
+
 });
