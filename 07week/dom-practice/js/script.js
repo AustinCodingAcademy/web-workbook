358db28b4410ff84f5726db25e762e136cb1459f
Andy Olson <andyolson09@gmail.com>
2018-10-05 12:59:53 -0500

    restructured code into more functions / added img display hover feature

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,37 +1,105 @@
-'use strict';
+"use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
   // You code here
   const list = document.getElementsByTagName("ul")[0];
-  const items = list.getElementsByTagName("li");
-  
-  var itemCount = getItemCount();
-  // alert("You have " + itemCount + " items");
-  
-  insertItemCountMessage();
-  
-  var buttonAddItem = document.getElementById('btn-add-item');
-  buttonAddItem.addEventListener("click", () => { 
-    let item = prompt("What would you like to add?"); 
-    list.insertAdjacentHTML("beforeend", '<li>'+item+'</li>');
-  });
-  
-  var buttonRemoveItem = document.getElementById('btn-remove-item');
-  buttonRemoveItem.addEventListener("click", () => {  
-    let lastItem = items[items.length - 1];
-    if(lastItem) list.removeChild(lastItem);
-  });
+  const listItems = list.getElementsByTagName("li");
 
+  var listItemCount = getListItemCount();
+  alert("You have " + listItemCount + " items");
+  initItems();
+  initButtons();
+  insertListItemCountMessage();
 
-  function getItemCount() {
+  /**
+   * ========================
+   * FUNCTIONS
+   * ========================
+   */
+
+  /**
+   * Function: insertListItemCountMessage()
+   * Description: Inserts an H2 directly under page's #main-title displaying
+   * a message that tells the user how many items are in shopping cart
+   */
+  function insertListItemCountMessage() {
+    let itemCount = getListItemCount();
+    let mainTitle = document.getElementById("main-title");
+    mainTitle.insertAdjacentHTML(
+      "afterend",
+      '<h2 id="itemCount">You have ' +
+        itemCount +
+        " items in your shopping cart."
+    );
+  }
+  /**
+   * Function: getListItemCount()
+   * Description: Helper function for inertListItemCountMessage()
+   * Returns: number of chidren nodes belonging to shopping cart
+   */
+  function getListItemCount() {
     return list.children.length;
   }
 
-  function insertItemCountMessage() {
-    let itemCount = getItemCount();
-    let mainTitle = document.getElementById('main-title');
-    mainTitle.insertAdjacentHTML("afterend", '<h2 id="itemCount">You have ' + itemCount + ' items in your shopping cart.');
-  }
-    
+  /**
+   * Function: initItems()
+   * Description: Adds mouseover/mouseout event listeners to .item-name elements
+   * that will add a class to each item's img that will display the img
+   */
+  function initItems() {
+    let items = getItems();
 
+    for (i = 0; i < items.length; i++) {
+      let itemImg = items[i].getElementsByTagName("img")[0];
+      let currentItemName = items[i].getElementsByClassName("item-name");
+
+      currentItemName[0].addEventListener("mouseover", () => {
+        itemImg.classList.add("show-item");
+      });
+      currentItemName[0].addEventListener("mouseout", () => {
+        itemImg.classList.remove("show-item");
+      });
+    }
+  }
+  /**
+   * Function: getItems()
+   * Description: Helper funciton for initItems()
+   * Returns: an HTML collection have .item class divs
+   */
+  function getItems() {
+    let items = document.getElementsByClassName("item");
+    return items;
+  }
+
+  /**
+   * Function: initButtons()
+   * Description: Adds click event listeners to #btn-add-item/#btn-remove-item
+   * elements that will insert a new <li></li> to shopping cart or remove the
+   * most recently added <li></li>(last item in shopping cart) in shopping cart
+   * respectively
+   */
+  function initButtons() {
+    let buttonAddItem = getButton("btn-add-item");
+    let buttonRemoveItem = getButton("btn-remove-item");
+
+    buttonAddItem.addEventListener("click", () => {
+      let item = prompt("What would you like to add?");
+      list.insertAdjacentHTML("beforeend", "<li>" + item + "</li>");
+    });
+    buttonRemoveItem.addEventListener("click", () => {
+      let lastItem = listItems[listItems.length - 1];
+      if (lastItem) list.removeChild(lastItem);
+    });
+  }
+
+  /**
+   * Function: getButton()
+   * Description: Helper function for initButtons()
+   * Params: name: the name of the id of the button we want to get
+   * Returns: an the HTML element of given id
+   */
+  function getButton(name) {
+    let button = document.getElementById(name);
+    return button;
+  }
 });
