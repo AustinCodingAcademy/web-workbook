5f70af99f7b40150fe910f7ae6370c37c0c9984d
Ronald Hajewski <ronald.hajewski@gmail.com>
2018-10-06 15:31:19 -0500

    back to getting the alert to work

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // You code here
 
   // Alert the user to how many list items ther are on the page
+  alert( "Size: " + $( "li" ).length );
 
   // Add a <h2> to the page under the title that says "You have_items in your shopping cart'. Do not use html to do this. Use JavaScript. 
 
@@ -12,6 +13,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   // Extension Chanllenge: Create the ability to display a picture of the product when the mouse hovers over the name of the project. 
 
-  
+
 
 });
