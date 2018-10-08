c1b49eacd177f0e7ffe0bb5350fb5780ace17650
Oscar Garcia <oscar.gch@gmail.com>
2018-10-08 16:57:48 -0500

    found the codepen with the solution. I still don't understand all of the code.

--- 06week/dice/js/script.js
+++ 06week/dice/js/script.js
@@ -7,7 +7,6 @@ function roll() {
     document.querySelector("#dice").innerHTML += `<span class="die" data-roll="${roll}">${dieChar[roll - 1]}</span>`;
   }
   report();
-  
 }
 
 roll();
@@ -22,59 +21,59 @@ roll();
 function report() {
   document.querySelector("#report").innerHTML = ""; // clear out the report box
   let diceHtml = document.querySelectorAll('.die'); // this puts all the individual die HTML elements in diceHtml
-  for (let dieVal = 1; dieVal < 7; dieVal++) {  // we set up a nested loop, first we check for all the 1s, then 2s, etc
+  
+//* This is the new code that was added to try to understand it: *//
+  let pair = 0; 
+  let house = 0;
+  let straight = [];
+  for (var dieVal = 1; dieVal < 7; dieVal++) {  // we set up a nested loop, first we check for all the 1s, then 2s, etc
     let howManyDice = 0; // we zero out our counter
-    for (let i = 0; i < 5; i++) {  // we create the inner loop that cycles through the rolled dice
+
+
+    for (var i = 0; i < 5; i++) {  // we create the inner loop that cycles through the rolled dice
+      
       if (dieVal === Number(diceHtml[i].getAttribute('data-roll'))) {  // we check if the rolled die is equal to the one's we're counting
         howManyDice++; // if so, we add one to the count
       }
-
     }
+
     if (howManyDice === 2) { // we check after the counting is done if it is equal to '2'
       document.querySelector("#report").innerHTML += "There is a pair. ";  // if yes, we report a pair
-    }
-    if (howManyDice === 3) { // we check after the counting is done if it is equal to '3'
+      pair++;
+    } else if (howManyDice === 3) { // we check after the counting is done if it is equal to '3'
     document.querySelector("#report").innerHTML += "There is a three. ";  // if yes, we report a three
-    }
-    if (howManyDice === 4) { // we check after the counting is done if it is equal to '4'
+    house++;
+    } else if (howManyDice === 4) { // we check after the counting is done if it is equal to '4'
     document.querySelector("#report").innerHTML += "There is a four. ";  // if yes, we report a four
-    }
-    if (howManyDice === 5) { // we check after the counting is done if it is equal to '5'
+    } else if (howManyDice === 5) { // we check after the counting is done if it is equal to '5'
     document.querySelector("#report").innerHTML += "There is a yacht. ";  // if yes, we report a yacht
     }
-} 
+  } 
+  if (pair === 2) {
+    document.getElementById("report").innerHTML += "This is two-pairs.";
+  }
+  if ((pair === 1) && (house === 1)) {
+    document.getElementById("report").innerHTML += "This is full-house.";
+  }
+  straight.sort();
+  straight.toString();
+  if (straight == "1,2,3,4,5") {
+    document.getElementById("report").innerHTML += "This is a rare small-straight.";
+  }
+  if (straight == "2,3,4,5,6") {
+    document.getElementById("report").innerHTML += "This is a rare big-straight.";
+  }
+  // two of dice have the same points, like 3 6 5 6 1 - called pair: example solved for you. Complete the other cases below:
 
-  function twoPairs() {
-
+  // three of dice have the same points, like 2 4 5 4 4 - called three;
+  // four of dice have the same points, like 1 4 1 1 1 - called four;
+  // all five dice have the same points, like 2 2 2 2 2 - called yacht;
+  // two pairs at once, like 3 6 5 3 5 - called two-pairs;
+  // pair and three at once, like 1 6 6 1 6 - called full-house;
+  // sequence from 1 to 5, like 2 4 3 5 1 - called small-straight;
+  // sequence from 2 to 6, like 6 3 4 2 5 - called big-straight.
   }
 
-
-  function fullHouse() {
-    
-  }
-
-  function smallStraight() {
-  document.querySelector("#report").innerHTML = "";
-  let diceHtml = document.querySelectorAll('.die');
-} 
-
-function bigStraight() {
-  document.querySelector("#report").innerHTML = "";
-  let diceHtml = document.querySelectorAll('.die');
-} 
-
-
-// two of dice have the same points, like 3 6 5 6 1 - called pair: example solved for you. Complete the other cases below:
-
-// three of dice have the same points, like 2 4 5 4 4 - called three;
-// four of dice have the same points, like 1 4 1 1 1 - called four;
-// all five dice have the same points, like 2 2 2 2 2 - called yacht;
-// two pairs at once, like 3 6 5 3 5 - called two-pairs;
-// pair and three at once, like 1 6 6 1 6 - called full-house;
-// sequence from 1 to 5, like 2 4 3 5 1 - called small-straight;
-// sequence from 2 to 6, like 6 3 4 2 5 - called big-straight.
-}
-
-//
-// report the results in the div with the ID 'report'.
-report();
+  //
+  // report the results in the div with the ID 'report'.
+  report();
