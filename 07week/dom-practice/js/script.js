b2999391a95ac792d22dfd9275a257993cc894a8
Ronald Hajewski <ronald.hajewski@gmail.com>
2018-10-08 18:40:35 -0500

    still working on list items

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -3,7 +3,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // You code here
 
   // Alert the user to how many list items ther are on the page
-  alert("this is an alert!");
+  // first i want to select all of the "li" on the page
+  function myFunction() {
+    var listItems = document.querySelector("ul").childElementCount;
+    // next i want to alert the user with a popup that says "there are X number of items on the page"
+    document.getElementById("demo").innerHTML = c;
+
+  }
   
 
   // Add a <h2> to the page under the title that says "You have_items in your shopping cart'. Do not use html to do this. Use JavaScript. 
