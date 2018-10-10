cf850f766b15b57aef0def6fbac445886c572f07
Andy Olson <andyolson09@gmail.com>
2018-10-09 19:59:04 -0500

    done with initial setup

--- 08week/dom-events/js/main.js
+++ 08week/dom-events/js/main.js
@@ -1,4 +1,9 @@
 'use strict';
-$(document).ready(function(){
-
+// when the document is done loading, run the script inside
+$(document).ready(function() {
+  var num = 0;
+  $('.box').click(function() {
+    num++;
+    $(this).text(num);
+  })
 });
