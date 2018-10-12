6d9515e669648cae5ee430200c1d319300632e3c
Andy Olson <andyolson09@gmail.com>
2018-10-11 21:06:56 -0500

    finished insert x and o function, setup other functions for logic

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,55 @@
-'use strict';
+"use strict";
 
 $(document).ready(function() {
   // Put app logic in here
+  var dataCells = $(".data-cell");
+  var playerTurn = "X";
+  var clearBtn = $("#clear");
+  
+  // function: insert "X" or "O" in cells
+  dataCells.click(function() {
+    
+    if(playerTurn === "X") {
+      this.innerHTML = playerTurn;
+      playerTurn = "O";
+    }
+    
+    else if(playerTurn === "O") {
+      this.innerHTML = playerTurn;
+      playerTurn = "X";
+    }
+
+    // Check if someone has won
+    checkRowWin();
+    checkColWin();
+    checkDiagWin();
+    
+  });
+  
+  // function: clear board
+  clearBtn.click(function() {
+    dataCells.text("");
+  });
+
+
+  // function: check row win
+  function checkRowWin() {
+    console.log("checking row win")
+  }
+
+  // function: check col win
+  function checkColWin() {
+    console.log("checking col win")
+  }
+
+  // function: check diag win
+  function checkDiagWin() {
+    console.log("checking diag win")
+  }
+
+  // function: check for draw
+  function checkForDraw() {
+    console.log("checking for draw")
+  }
+
 });
