ffd915926e9b84f1729b22a0651a3dde07f27e55
Brandon Turner <brandontheturner@gmail.com>
2018-10-11 21:05:49 -0500

    initial script for tic tac toe

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,56 @@
-'use strict';
+"use strict";
 
 $(document).ready(function() {
   // Put app logic in here
+
+  var dataCells = $(".datacell");
+  var playerTurn = "X";
+  var clearBtn = $("#clear");
+  console.log(clearBtn);
+  // Function that adds an X or O to clicked box
+
+  dataCells.click(function() {
+    if (playerTurn === "X") {
+      this.innerHTML = playerTurn;
+      playerTurn = "O";
+    } else if (playerTurn === "O") {
+      this.innerHTML = playerTurn;
+      playerTurn = "X";
+    }
+
+
+
+    //check if someone has won
+    checkRowWin();
+    checkColWin();
+    checkDiagWin();
+
+  });
+
+  clearBtn.click(function() {
+    dataCells.text("");
+  });
+
+function checkRowWin () {
+  console.log('checking for row win');
+}
+
+function checkColWin () {
+  console.log('checking for col win');
+}
+
+function checkDiagWin () {
+  console.log('checking for diag win');
+}
+
+
+
 });
+
+//clear button
+
+// select all data cells by data cell attribute
+// use .text()  function
+// global variable
+// toggle player state or toggle input string on click
+// c
