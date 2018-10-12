66a152c7f3b4c3cd8d9c962af2fd1f9d7a872397
Andy Olson <andyolson09@gmail.com>
2018-10-12 08:05:22 -0500

    created boardInit fn that loads data cells into 2d array

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -5,16 +5,15 @@ $(document).ready(function() {
   var dataCells = $(".data-cell");
   var playerTurn = "X";
   var clearBtn = $("#clear");
-  
+
+  const board = boardInit(3,3);
+
   // function: insert "X" or "O" in cells
   dataCells.click(function() {
-    
-    if(playerTurn === "X") {
+    if (playerTurn === "X") {
       this.innerHTML = playerTurn;
       playerTurn = "O";
-    }
-    
-    else if(playerTurn === "O") {
+    } else if (playerTurn === "O") {
       this.innerHTML = playerTurn;
       playerTurn = "X";
     }
@@ -23,33 +22,41 @@ $(document).ready(function() {
     checkRowWin();
     checkColWin();
     checkDiagWin();
-    
   });
-  
+
   // function: clear board
   clearBtn.click(function() {
     dataCells.text("");
   });
 
+  // function: load board cells into 2d array
+  function boardInit(rows, cols) {
+    let board = new Array(rows);
+    let cell = 0;
+    
+    for (var i = 0; i < cols; i++) {
+      board[i] = new Array(cols);
+    }
+
+    for (var i = 0; i < rows; i++) {
+      for (var j = 0; j < cols; j++) {
+        board[i][j] = dataCells[cell];
+        cell++;
+      }
+    }
+
+    return board;
+  }
 
   // function: check row win
-  function checkRowWin() {
-    console.log("checking row win")
-  }
+  function checkRowWin() {}
 
   // function: check col win
-  function checkColWin() {
-    console.log("checking col win")
-  }
+  function checkColWin() {}
 
   // function: check diag win
-  function checkDiagWin() {
-    console.log("checking diag win")
-  }
+  function checkDiagWin() {}
 
   // function: check for draw
-  function checkForDraw() {
-    console.log("checking for draw")
-  }
-
+  function checkForDraw() {}
 });
