b4ff8d1fc65414899156d748655c2ac5bfbcef36
Ronald Hajewski <ronald.hajewski@gmail.com>
2018-10-16 17:01:21 -0500

    this is gonna have to work for now (still need to get reset and draw working)

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -11,17 +11,30 @@ $(document).ready(function(){
 
   $("#gameBoard tr td").click(function() {
     if ($(this).text()=="" && play) {
-      if ((move%2)==1) { $(this).append("X").css({fontSize: '50px', color: 'red'}); } 
-      else { $(this).append("O").css({fontSize: '50px', color: 'blue'}); }
+      if ((move%2)==1) { $(this).append("X").css({fontSize: '50px', color: 'pink'}); } 
+      else { $(this).append("O").css({fontSize: '50px', color: '#00ffff6f'}); }
       move++; 
       if (checkForWin()!=-1 && checkForWin()!="") { 
+        // Write function: winner text
         if (checkForWin()=="X") { alert("Player 1 Winner!"); }
+        // Write function: winner text
         else { alert("Player 2 Winner!"); }
         play = false; 
       }
     }
   });
+  // Write function: clear board
+  
+  /* $('.reset').click (function () {
+    let clearBoard = $("#gameBoard");
+    let i = 0;
+    for (i = 0; i < clearBoard.length; i++) {
+      $(clearBoard[i]).text('');
+    }
+  } */
 
+
+  // Write function: check for win 
   function checkForWin() {
     var box1 = $("#gameBoard tr:nth-child(1) td:nth-child(1)").text();
     var box2 = $("#gameBoard tr:nth-child(1) td:nth-child(2)").text();
@@ -32,15 +45,15 @@ $(document).ready(function(){
     var box7 = $("#gameBoard tr:nth-child(3) td:nth-child(1)").text();
     var box8 = $("#gameBoard tr:nth-child(3) td:nth-child(2)").text();
     var box9 = $("#gameBoard tr:nth-child(3) td:nth-child(3)").text();
-    // check rows
+    // check all rows
     if      ((box1==box2) && (box2==box3)) { return box3; }
     else if ((box4==box5) && (box5==box6)) { return box6; }	
     else if ((box7==box8) && (box8==box9)) { return box9; }
-    // check columns
+    // check all columns
     else if ((box1==box4) && (box4==box7)) { return box7; }
     else if ((box2==box5) && (box5==box8)) { return box8; }
     else if ((box3==box6) && (box6==box9)) { return box9; }
-    // check diagonals
+    // check both diagonals
     else if ((box1==box5) && (box5==box9)) { return box9; }
     else if ((box3==box5) && (box5==box7)) { return box7; }
     // no winner
@@ -50,13 +63,7 @@ $(document).ready(function(){
 });
 
 
-
-// Write function: clear board
     
-// Write function: check for win 
-
-// Write function: winner text
-
 // Write function: check for draw
 
 // Write function: draw text
