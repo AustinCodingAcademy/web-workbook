18158ad7aa413b8d064cae19cd8e5c54f1e98d73
Andy Olson <andyolson09@gmail.com>
2018-10-17 10:16:45 -0500

    improved modularity, basic logic in place

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,32 +1,97 @@
-'use strict';
+"use strict";
 
-// what we know...
+// to do...
 // ✔ can only move one piece at a time
-// - can only put smaller blocks on larger blocks
-// - must move all blocks to last column in same order
-// - 3+ columns
-// - start with 3 blocks, blocks++ each level
+// ✔ can only put smaller blocks on larger blocks
+// - check for win
 
 $(document).ready(function() {
-  let $block = null;
-  var $blockSize = null;
-  
-  $('[data-stack]').click(function() {
-    if ($block) {
-      var $prevBlock = $(this).children().last();
-      var $prevBlockSize = parseInt($prevBlock.attr("data-block"));
-      console.log("$prevBlockSize: ", $prevBlockSize);
-      if($prevBlockSize != null && $prevBlockSize < $blockSize) {
-        console.log($prevBlockSize, "<", $blockSize);
-      } else {
-        $(this).append($block);
+  let currentBlock = null;
+
+  $("[data-stack]").click(function() {
+    let blockCount = $(this).children().length;
+
+    // if we have a block, look to append it
+    if (currentBlock) {
+      // if row is not empty ensure valid move
+      if (blockCount > 0) {
+        var prevBlock = getPrevBlock($(this));
+        var isValidMove = compareBlocks(prevBlock, currentBlock);
+
+        // valid move
+        if (isValidMove) {
+          appendBlock($(this), currentBlock);
+        }
+        // invalid move
+        else {
+          console.log("Invalid Move!");
+          return;
+        }
       }
-      $block = null;
-      $blockSize = null;
-    } else {
-      $block = $(this).children().last().detach();
-      $blockSize = parseInt($block.attr("data-block"));
-      console.log("$blockSize: ", $blockSize)
+
+      // otherwise, row is empty so just append cuurentBlock
+      else {
+        appendBlock($(this), currentBlock);
+      }
+
+      // reset currentBlock to null once we have appended it
+      currentBlock = null;
     }
-  })
-})
+
+    // otherwise, we do not have a block so get one
+    else {
+      currentBlock = removeBlock($(this));
+    }
+  });
+});
+
+/**
+ * Function: getPrevBlock()
+ * Description:
+ */
+function getPrevBlock(stack) {
+  let prevBlock = stack.children().last();
+  return prevBlock;
+}
+
+/**
+ * Function: appendBlock()
+ * Description:
+ */
+function appendBlock(stack, block) {
+  stack.append(block);
+  return stack;
+}
+
+/**
+ * Function: removeBlock()
+ * Description:
+ */
+function removeBlock(stack) {
+  let block = stack
+    .children()
+    .last()
+    .detach();
+  return block;
+}
+
+/**
+ * Function: getPrevBlockSize()
+ * Description:
+ */
+function getBlockSize(block) {
+  let blockSize = parseInt(block.attr("data-block"));
+  return blockSize;
+}
+
+/**
+ * Function: compareBlocks()
+ * Description:
+ */
+function compareBlocks(block1, block2) {
+  if (getBlockSize(block1) > getBlockSize(block2)) {
+    return true;
+  } else {
+    return false;
+  }
+}
