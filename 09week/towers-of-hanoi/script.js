ffe75627d4d8ebe21ce9e35d23181891d5831168
Brandon Turner <brandontheturner@gmail.com>
2018-10-17 10:19:43 -0500

    initial javascript files

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,36 @@
 'use strict';
-
+// what we know
+// move one piece at a time
+// can't put larger blocks on top of smaller blocks
+// to wim, move all blocks in order to last column
+// 3 columns
+// start with 3 blocks
+// every level goes up by one block
 $(document).ready(function() {
+  let $block = null;
+  let $blockSize = null;
+  let $currentBlockInRow = null;
+
+  $('[data-stack]').click(function() {
+
+    if ($block) {
+
+      var $currentBlockInRow =$(this).children().last();
+      console.log("$currentBlockInRow", $currentBlockInRow.attr('data-block'));
+      
+      if ($(this)children().length()!=0){
+
+      $(this).append($block);
+
+    } else {
+
+      $block = $(this).children().last().detach();
+
+      var $blockSize = parseInt($block.attr('data-block'))
+    }
+
+  })
+})
+
   // Put app logic here
-});
+
