ba758880a578e44865acb78587f3f69876efc4b9
oaljaber <osama.aljabr@gmail.com>
2018-10-16 19:28:38 -0500

    towers of hanoi added

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,23 @@
 'use strict';
+//what we know
+//*move one piece at a time
+//* to win move all items in same order to last column
+//* 3 columns
+//* start with 3 block
+//* every level goes up one block
+//* blocks can move any where
 
 $(document).ready(function() {
   // Put app logic here
+  let $block = null;
+  
+  $('[data-stack]').click(function() {
+    if ($block) {
+      $(this).append($block);
+      $block = null;
+    } else {
+      $block = $(this).children().last().detach();
+    }
+  })
+
 });
