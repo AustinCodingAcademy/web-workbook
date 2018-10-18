2898d22d2c27f82f1984a30ab0f06db33b11ae77
Andy Olson <andyolson09@gmail.com>
2018-10-17 21:58:07 -0500

    created checkForWin fn / made code more readable

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -7,20 +7,31 @@
 
 $(document).ready(function() {
   let currentBlock = null;
+  var stacks = $("[data-stack]");
+  const blockTotal = getBlockTotal(stacks.first());
 
-  $("[data-stack]").click(function() {
+  // click event for each stack
+  stacks.click(function() {
+    let currentStack = $(this);
     let blockCount = $(this).children().length;
+
+    // check if player has won on each click
+    let playerHasWon = checkForWin(stacks, blockTotal);
+    if (playerHasWon) {
+      console.log("Winner winner, chicken dinner!");
+      return;
+    }
 
     // if we have a block, look to append it
     if (currentBlock) {
       // if row is not empty ensure valid move
       if (blockCount > 0) {
-        var prevBlock = getPrevBlock($(this));
+        var prevBlock = getPrevBlock(currentStack);
         var isValidMove = compareBlocks(prevBlock, currentBlock);
 
         // valid move
         if (isValidMove) {
-          appendBlock($(this), currentBlock);
+          appendBlock(currentStack, currentBlock);
         }
         // invalid move
         else {
@@ -28,22 +39,30 @@ $(document).ready(function() {
           return;
         }
       }
-
+      
       // otherwise, row is empty so just append cuurentBlock
       else {
-        appendBlock($(this), currentBlock);
+        appendBlock(currentStack, currentBlock);
       }
-
+      
       // reset currentBlock to null once we have appended it
       currentBlock = null;
     }
 
     // otherwise, we do not have a block so get one
     else {
-      currentBlock = removeBlock($(this));
+      currentBlock = removeBlock(currentStack);
     }
   });
 });
+
+/**
+ * Function: getBlockTotal()
+ * Description:  
+ */
+function getBlockTotal(stack) {
+  return stack.children().length;
+}
 
 /**
  * Function: getPrevBlock()
@@ -92,6 +111,22 @@ function compareBlocks(block1, block2) {
   if (getBlockSize(block1) > getBlockSize(block2)) {
     return true;
   } else {
+    return false;
+  }
+}
+
+/**
+ * Function: checkForWin()
+ * Description:
+ */
+function checkForWin(stacks, blockTotal) {
+  let lastStack = stacks.last();
+  let blockCount = lastStack.children().length; 
+
+  if (blockCount === blockTotal) {
+    return true;
+  }
+  else {
     return false;
   }
 }
