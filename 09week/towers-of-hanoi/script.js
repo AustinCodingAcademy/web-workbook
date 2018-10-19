fbf0b4e552b77028baad30ab401640b344ae1176
Brandon Turner <brandontheturner@gmail.com>
2018-10-18 23:36:10 -0500

    towers working on win condition

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 // what we know
 // move one piece at a time
 // can't put larger blocks on top of smaller blocks
@@ -6,31 +6,63 @@
 // 3 columns
 // start with 3 blocks
 // every level goes up by one block
-$(document).ready(function() {
-  let $block = null;
-  let $blockSize = null;
-  let $currentBlockInRow = null;
+$(document).ready(function () {
+  let currentBlock = null;
+  let blockSize = null;
+  let prevBlock = null;
 
-  $('[data-stack]').click(function() {
+  $("[data-stack]").click(function () {
+    let blockCount = $(this).children().length;
+    console.log(blockCount);
+    // If we have a currentBlock attached to cursor 
+    if (currentBlock) {
+      var prevBlock = $(this)
+        .children()
+        .last();
+      // Empty stack
+      if (blockCount === 0) {
+        $(this).append(currentBlock);
+      } else {
+        // 
+        let isValid = blockCompare(currentBlock, prevBlock);
+        if (isValid) {
+          $(this).append(currentBlock);
+        } else {
+          return;
+        }
+      }
 
-    if ($block) {
-
-      var $currentBlockInRow =$(this).children().last();
-      console.log("$currentBlockInRow", $currentBlockInRow.attr('data-block'));
-      
-      if ($(this)children().length()!=0){
-
-      $(this).append($block);
-
+      currentBlock = null;
+      // we dont have a block selected
     } else {
-
-      $block = $(this).children().last().detach();
-
-      var $blockSize = parseInt($block.attr('data-block'))
+      currentBlock = $(this)
+        .children()
+        .last()
+        .detach();
     }
+  });
+});
 
-  })
-})
+function blockCompare(block1, block2) {
+  // data value of block one > value of data value block2
+  let block1Size = getBlockSize(block1);
+  let block2Size = getBlockSize(block2);
+  if (block1Size < block2Size) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-  // Put app logic here
-
+function getBlockSize(block1) {
+  var blockSize = parseInt(block1.attr("data-block"));
+  return blockSize;
+}
+// winning condition function
+function winCond(blockCount) {
+  if (blockCount === 3);
+  console.log(blockCount);
+  return true;
+} else {
+  return false;
+}
