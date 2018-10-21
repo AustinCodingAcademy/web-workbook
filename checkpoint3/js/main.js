309fd899fe04c47cf2282125fdcd8399db21248b
Andy Olson <andyolson09@gmail.com>
2018-10-20 21:34:24 -0500

    prettier

--- checkpoint3/js/main.js
+++ checkpoint3/js/main.js
@@ -4,25 +4,23 @@ $(document).ready(function() {
   const hammer = $("#hammer");
   const anvil = $("#anvil");
   const sparks = $(".spark");
-  const counter= $("#counter");
+  const counter = $("#counter");
   var count = 0;
 
   anvil.mousedown(function() {
     sparks.toggleClass("animate");
     hammer.toggleClass("down");
     anvil.toggleClass("shake");
-    
+
     count++;
     counter.text(count);
   });
-  
 
   anvil.mouseup(function() {
     sparks.removeClass("animate");
     hammer.toggleClass("down");
     anvil.toggleClass("shake");
-  })
-
+  });
 
   main.mouseup(function() {
     hammer.removeClass("down");
