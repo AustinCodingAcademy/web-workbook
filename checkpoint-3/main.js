1c69a4340e75468c33594b5587249bedb0abf93f
Taylor Colten <taylorwcolten@gmail.com>
2018-10-22 22:54:53 -0500

    put the button in the center of the page

--- checkpoint-3/main.js
+++ checkpoint-3/main.js
@@ -1,6 +1,17 @@
-//click counter 
+//variable
 var clicks = 0; 
-        function countClicks() {
-            clicks += 1;
-            document.getElementById("clicks").innerHTML = clicks;
-        }
+//click function
+function countClicks() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+var timer = new Timer();
+timer.start({countdown: true, startValues: {seconds: 30}});
+$('#countdown .values').html(timer.getTimeValues().toString());
+timer.addEventListener('secondsUpdated', function (e) {
+    $('#countdown .values').html(timer.getTimeValues().toString());
+});
+timer.addEventListener('targetAchieved', function (e) {
+    $('#countdown .values').html('KABOOM!!');
+});
