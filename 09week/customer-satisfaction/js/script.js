e717bd768e7164d0f1a8e3d67d6995808b99fc7b
Ronald Hajewski <ronald.hajewski@gmail.com>
2018-10-23 16:31:50 -0500

    form looks pretty good - still want to style the check boxes and radio buttons

---
+++ 09week/customer-satisfaction/js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(function() {
+
+  var slider = document.getElementById("myRange");
+  var output = document.getElementById("demo");
+  output.innerHTML = slider.value; // Display the default slider value
+
+  // Update the current slider value (each time you drag the slider handle)
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
+
+});
