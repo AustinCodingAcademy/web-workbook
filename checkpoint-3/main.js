5b3954cf05d17c74a4269ce60c6e1eb86359a4cc
Taylor Colten <taylorwcolten@gmail.com>
2018-10-23 01:03:28 -0500

    created timer styled the font

--- checkpoint-3/main.js
+++ checkpoint-3/main.js
@@ -1,17 +1,23 @@
-//variable
+//variable starting value of clicks
 var clicks = 0; 
 //click function
 function countClicks() {
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
 };
+//timer variable how long the timer is set
+var count=30;
+ //1000 will  run it every 1 second
+var counter=setInterval(timer, 1000);
+//timer function
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     return;
+  }
 
-var timer = new Timer();
-timer.start({countdown: true, startValues: {seconds: 30}});
-$('#countdown .values').html(timer.getTimeValues().toString());
-timer.addEventListener('secondsUpdated', function (e) {
-    $('#countdown .values').html(timer.getTimeValues().toString());
-});
-timer.addEventListener('targetAchieved', function (e) {
-    $('#countdown .values').html('KABOOM!!');
-});
+  document.getElementById("timer").innerHTML=count + " secs"; 
+}
