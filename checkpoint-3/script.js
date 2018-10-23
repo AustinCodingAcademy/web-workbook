606ede83a07ac55d2ab0b95483b7b9a395003794
Ronald Hajewski <ronald.hajewski@gmail.com>
2018-10-22 20:22:14 -0500

    html was giving lint error based on class value (added dash 1)

--- checkpoint-3/script.js
+++ checkpoint-3/script.js
@@ -1,23 +1,22 @@
 'use strict';
 $(document).ready(function(){
 
+  // counter goes up every time you click on the bad guy
   var num1 = 0;  
-  // var num2 = 0;
-  // var num3 = 0; 
   $('#screen').click(function() {
     num1++;
     $('#points').text(num1);
   });
 
+  // bleep sound on-click
   var obj = document.createElement("audio");
-  obj.src="https://github.com/konarie1990/web-workbook/blob/gh-pages/checkpoint-3/bleep3.mp3";
+  obj.src="https://github.com/konarie1990/web-workbook/blob/gh-pages/checkpoint-3/bleep3.mp3?raw=true";
   obj.volume=0.70;
   obj.autoPlay=false;
   obj.preLoad=true;       
 
-  $(".playSound").click(function() {
+  $("#screen-1").click(function() {
     obj.play();
   });
-
 
 });
