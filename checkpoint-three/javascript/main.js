62d3482e8b71eec385a3b380b8e4ef8e05dec73f
brett <brettj85@gmail.com>
2018-10-23 18:09:28 -0500

    finished the advanced inventory shell and js to open and close it

---
+++ checkpoint-three/javascript/main.js
@@ -0,0 +1,24 @@
+
+
+var openInventoryButton = document.querySelector(".inventoryOpenButton");
+openInventoryButton.addEventListener("click", function (e) {
+openInventory();
+e.preventDefault();
+});
+
+var closeInventoryButton = document.getElementById("exitInventory");
+closeInventoryButton.addEventListener("click", function (e) {
+closeInventory();
+e.preventDefault();
+});
+
+
+function openInventory(){
+    var invContainer = document.getElementById("fullInventory");
+    invContainer.style.display = "flex";
+}
+
+function closeInventory(){
+    var invContainer = document.getElementById("fullInventory");
+    invContainer.style.display = "none";
+}
