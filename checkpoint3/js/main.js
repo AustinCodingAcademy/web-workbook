c339f08f81184511dd44c0f6d0a255151abdac3c
Andy Olson <andyolson09@gmail.com>
2018-10-23 12:26:53 -0500

    set up basic progression system

--- checkpoint3/js/main.js
+++ checkpoint3/js/main.js
@@ -1,11 +1,29 @@
 "use strict";
 $(document).ready(function() {
   const main = $("main");
+  
+  // hammer/anvil elements
   const hammer = $("#hammer");
   const anvil = $("#anvil");
   const sparks = $(".spark");
+  
+  // weapon elements
+  const weaponImg = $("#weapon-img");
+  const weaponName = $("#weapon-name");
+  
+  // ui elements
   const counter = $("#counter");
+  const nextWeapon = $("#next-weapon");
+  const announceWin = $("#max-level");
+  
+  // counter/level variables
+  var maxLevel = 15;
   var count = 0;
+  var currentLevel = 1;
+  var nextLevel = 2;
+  var levelIncrement = nextLevel;
+  var playerHasReachedMaxLevel = false;
+  nextWeapon.text("Next Weapon: " + nextLevel);  
 
   anvil.mousedown(function() {
     sparks.toggleClass("animate");
@@ -14,12 +32,47 @@ $(document).ready(function() {
 
     count++;
     counter.text(count);
-  });
+    
+    // exit if player have reached highest level
+    if(playerHasReachedMaxLevel) {
+      return;
+    }
+    
+    // level Up!
+    if(count === nextLevel) {
+      
+      currentLevel++;
+      
+      // this if/else just handles file names
+      if (currentLevel < 10) {
+        weaponName.text("sword_0" + currentLevel);
+        weaponImg.attr("src", "img/sword_0" + currentLevel + ".png");
+      }
+      else {
+        weaponName.text("sword_" + currentLevel);
+        weaponImg.attr("src", "img/sword_" + currentLevel + ".png");
+      }
+      
+      // annouce max level
+      if(currentLevel === maxLevel && !playerHasReachedMaxLevel) {
+        console.log("hi");
+        announceWin.addClass("show");
+        playerHasReachedMaxLevel = true;
+        nextWeapon.text("Next Weapon: ???");
+        return;
+      }
 
+      nextLevel += levelIncrement;
+      nextWeapon.text("Next Weapon: " + nextLevel);
+      
+    }
+  });
+  
   anvil.mouseup(function() {
     sparks.removeClass("animate");
     hammer.toggleClass("down");
     anvil.toggleClass("shake");
+    
   });
 
   main.mouseup(function() {
