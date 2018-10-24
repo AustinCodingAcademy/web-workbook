493d115247edd283521dc888b8bb9bd4e4ab2983
brett <brettj85@gmail.com>
2018-10-23 20:44:24 -0500

    finished buildings shell

--- checkpoint-three/javascript/main.js
+++ checkpoint-three/javascript/main.js
@@ -1,24 +1,22 @@
-
-
 var openInventoryButton = document.querySelector(".inventoryOpenButton");
-openInventoryButton.addEventListener("click", function (e) {
-openInventory();
-e.preventDefault();
+openInventoryButton.addEventListener("click", function(e) {
+  openInventory();
+  e.preventDefault();
 });
 
 var closeInventoryButton = document.getElementById("exitInventory");
-closeInventoryButton.addEventListener("click", function (e) {
-closeInventory();
-e.preventDefault();
+closeInventoryButton.addEventListener("click", function(e) {
+  closeInventory();
+  e.preventDefault();
 });
 
 
-function openInventory(){
-    var invContainer = document.getElementById("fullInventory");
-    invContainer.style.display = "flex";
+function openInventory() {
+  var invContainer = document.getElementById("fullInventory");
+  invContainer.style.display = "flex";
 }
 
-function closeInventory(){
-    var invContainer = document.getElementById("fullInventory");
-    invContainer.style.display = "none";
+function closeInventory() {
+  var invContainer = document.getElementById("fullInventory");
+  invContainer.style.display = "none";
 }
