b8f17ae9aa832994d8ec35105e4a78e6d360fc1c
Andy Olson <andyolson09@gmail.com>
2018-10-24 15:48:26 -0500

    tons of new features! true shop item progression. more modularity!

--- checkpoint3/js/main.js
+++ checkpoint3/js/main.js
@@ -32,19 +32,18 @@ $(document).ready(function() {
   const counter = $("#counter");
   const nextWeapon = $("#next-weapon-level");
   const announceWin = $("#max-level");
-
+  
   // upgrade costs
-  const cost_HireApprentice = 1;
-  const cost_ApprenUpgrade1 = 1;
-  const cost_ApprenUpgrade2 = 1;
-  const cost_ApprenUpgrade3 = 1;
+  const cost_HireApprentice = 5;
+  const cost_ApprenUpgrade1 = 10;
+  const cost_ApprenUpgrade2 = 20;
+  const cost_ApprenUpgrade3 = 30;
   const cost_HammerUpgrade1 = 1;
-  const cost_HammerUpgrade2 = 1;
-  const cost_HammerUpgrade3 = 1;
-  const cost_HammerUpgrade4 = 1;
+  const cost_HammerUpgrade2 = 2;
+  const cost_HammerUpgrade3 = 3;
+  const cost_HammerUpgrade4 = 4;
 
-  // upgrade elements
-  const shop = $("#shop");
+  // shop elements
   const hireApprentice = $("#hire-apprentice");
   const apprenticeUpgrade1 = $("#apprentice-upgrade-1");
   const apprenticeUpgrade2 = $("#apprentice-upgrade-2");
@@ -53,8 +52,8 @@ $(document).ready(function() {
   const hammerUpgrade2 = $("#hammer-upgrade-2");
   const hammerUpgrade3 = $("#hammer-upgrade-3");
   const hammerUpgrade4 = $("#hammer-upgrade-4");
-  updateUpgradeCost(hireApprentice, cost_HireApprentice);
-  updateUpgradeCost(hammerUpgrade1, cost_HammerUpgrade1);
+  const shop = $("#shop");
+  initShop();
 
   // counter/level variables
   var maxLevel = 15;
@@ -64,16 +63,20 @@ $(document).ready(function() {
   var currentLevel = 1;
   var numberOfClicksToNextLevel = 100;
   var levelIncrement = numberOfClicksToNextLevel;
-  const timeout = 3000;
+  var timeout = 3000;
   var currentMultiplier = 1;
-  nextWeapon.text("Next Weapon: " + numberOfClicksToNextLevel);
+  nextWeapon.text("Next Weapon: " + numberOfClicksToNextLevel + " clicks");
 
   // game state variables
-  var playerHasReachedMaxLevel = false;
   var playerHasApprentice = false;
-  var playerHasPlayerHammerUpgrade = false;
-  var apprenticeHasHammerEfficiency = false;
-
+  var apprenticeHasUpgrade1 = false;
+  var apprenticeHasUpgrade2 = false;
+  var apprenticeHasUpgrade3 = false;
+  var playerHasHammerUpgrade1 = false;
+  var playerHasHammerUpgrade2 = false;
+  var playerHasHammerUpgrade3 = false;
+  var playerHasHammerUpgrade4 = false;
+  var playerHasReachedMaxLevel = false;
 
   // MASTER CLICK FUNCTIONS
   // ====================================================================
@@ -93,86 +96,314 @@ $(document).ready(function() {
   // SHOP FUNCTIONS
   // ====================================================================
   hireApprentice.click(function() {
+    
+    let unlocked = shop_checkLockStatus($(this));
+
+    if (!unlocked) {
+      console.log("you haven't unlocked that item yet");
+      return;
+    }
 
     if (playerHasApprentice) {
       return;
     }
 
-
-    if (count >= cost_HireApprentice && !playerHasApprentice) {
+    if (count >= cost_HireApprentice) {
       shop_hireApprentice();
+    } else return;
+  });
+  apprenticeUpgrade1.click(function() {
+    
+    let unlocked = shop_checkLockStatus($(this));
+
+    if (!unlocked) {
+      console.log("you haven't unlocked that item yet");
+      return;
+    }
+
+    shop_checkLockStatus($(this));
+    if (apprenticeHasUpgrade1) {
+      return;
+    }
+
+    if (count >= cost_ApprenUpgrade1) {
+      shop_buyApprenticeUpgrade1();
+    } else return;
+  });
+  apprenticeUpgrade2.click(function() {
+    
+    let unlocked = shop_checkLockStatus($(this));
+
+    if (!unlocked) {
+      console.log("you haven't unlocked that item yet");
+      return;
+    }
+
+    shop_checkLockStatus($(this));
+    if (apprenticeHasUpgrade2) {
+      return;
+    }
+
+    if (count >= cost_ApprenUpgrade2) {
+      shop_buyApprenticeUpgrade2();
+    } else return;
+  });
+  apprenticeUpgrade3.click(function() {
+    
+    let unlocked = shop_checkLockStatus($(this));
+
+    if (!unlocked) {
+      console.log("you haven't unlocked that item yet");
+      return;
+    }
+    
+    shop_checkLockStatus($(this));
+    if (apprenticeHasUpgrade3) {
+      return;
+    }
+
+    if (count >= cost_ApprenUpgrade3) {
+      shop_buyApprenticeUpgrade3();
+    } else return;
+  });
+
+  hammerUpgrade1.click(function() {
+    
+    let unlocked = shop_checkLockStatus($(this));
+
+    if (!unlocked) {
+      console.log("you haven't unlocked that item yet");
+      return;
+    }
+    
+    shop_checkLockStatus($(this));
+    if (playerHasHammerUpgrade1) {
+      return;
+    }
+
+    if (count >= cost_HammerUpgrade1) {
+      shop_buyHammerUpgrade1();
     } else {
       return;
     }
   });
+  hammerUpgrade2.click(function() {
+    
+    let unlocked = shop_checkLockStatus($(this));
 
-  hammerUpgrade1.click(function() {
-
-    if (playerHasPlayerHammerUpgrade) {
+    if (!unlocked) {
+      console.log("you haven't unlocked that item yet");
+      return;
+    }
+    
+    shop_checkLockStatus($(this));
+    if (playerHasHammerUpgrade2) {
       return;
     }
 
-    if (count >= cost_HammerUpgrade1 && !playerHasPlayerHammerUpgrade) {
-      shop_buyHammerUpgrade();
+    if (count >= cost_HammerUpgrade2) {
+      shop_buyHammerUpgrade2();
+    } else {
+      return;
+    }
+  });
+  hammerUpgrade3.click(function() {
+    
+    let unlocked = shop_checkLockStatus($(this));
+
+    if (!unlocked) {
+      console.log("you haven't unlocked that item yet");
+      return;
+    }
+    
+    shop_checkLockStatus($(this));
+    if (playerHasHammerUpgrade3) {
+      return;
+    }
+
+    if (count >= cost_HammerUpgrade3) {
+      shop_buyHammerUpgrade3();
+    } else {
+      return;
+    }
+  });
+  hammerUpgrade4.click(function() {
+   
+    let unlocked = shop_checkLockStatus($(this));
+
+    if (!unlocked) {
+      console.log("you haven't unlocked that item yet");
+      return;
+    }
+    
+    shop_checkLockStatus($(this));
+    if (playerHasHammerUpgrade4) {
+      return;
+    }
+
+    if (count >= cost_HammerUpgrade4) {
+      shop_buyHammerUpgrade4();
     } else {
       return;
     }
   });
 
   function shop_hireApprentice() {
-    playerHasApprentice = true;
     count -= cost_HireApprentice;
+    playerHasApprentice = true;
     updateCounter();
     apprenticeStart();
   }
 
-  function shop_buyApprenticeUpgrade() {
+  function shop_buyApprenticeUpgrade1() {
     apprenticeHammerEfficiency = 2;
-    apprenticeHasHammerEfficiency = true;
-    count -= cost_HireApprentice;
+    apprenticeHasUpgrade1 = true;
+    count -= cost_ApprenUpgrade1;
+    updateCounter();
+  }
+  function shop_buyApprenticeUpgrade2() {
+    apprenticeHammerEfficiency = 4;
+    apprenticeHasUpgrade2 = true;
+    count -= cost_ApprenUpgrade2;
+    updateCounter();
+  }
+  function shop_buyApprenticeUpgrade3() {
+    apprenticeHammerEfficiency = 8;
+    apprenticeHasUpgrade3 = true;
+    count -= cost_ApprenUpgrade3;
     updateCounter();
   }
 
-  function shop_buyHammerUpgrade() {
+  function shop_buyHammerUpgrade1() {
     playerHammerEfficiency = 2;
-    playerHasPlayerHammerUpgrade = true;
     count -= cost_HammerUpgrade1;
+    playerHasHammerUpgrade1 = true;
+    $(hammerUpgrade2).addClass("unlocked");
     updateCounter();
+  }
+  function shop_buyHammerUpgrade2() {
+    playerHammerEfficiency = 4;
+    count -= cost_HammerUpgrade2;
+    playerHasHammerUpgrade2 = true;
+    $(hammerUpgrade3).addClass("unlocked");
+    updateCounter();
+  }
+  function shop_buyHammerUpgrade3() {
+    playerHammerEfficiency = 8;
+    count -= cost_HammerUpgrade3;
+    playerHasHammerUpgrade3 = true;
+    $(hammerUpgrade4).addClass("unlocked");
+    updateCounter();
+  }
+  function shop_buyHammerUpgrade4() {
+    playerHammerEfficiency *= 2;
+    count -= cost_HammerUpgrade4;
+    playerHasHammerUpgrade4 = true;
+    updateCounter();
+  }
+
+  function shop_checkLockStatus(el) {
+    if ($(el).hasClass("unlocked")) {
+      console.log("unlocked");
+      return true;
+    }
+    else {
+      console.log("locked");
+      return false;
+    } 
   }
 
   /**
    * function: updateShop()
-   * desc: checks if player can afford items in the shop (red text if cannot, white if can)
+   * desc: changes shop item text to purchased if player has purchased that item,
+   * also checks if player can afford items in the shop (red text if cannot, white if can)
    */
   function updateShop() {
     let costs = shop.find(".shop-cost");
 
-    if (playerHasPlayerHammerUpgrade) {
+    // changes hammer upgrade text to purchased if player has purchased
+    if (playerHasHammerUpgrade1) {
       let cost = hammerUpgrade1.find(".shop-cost");
       cost.text("Purchased");
-
-      return;
     }
+    if (playerHasHammerUpgrade2) {
+      let cost = hammerUpgrade2.find(".shop-cost");
+      cost.text("Purchased");
+    }
+    if (playerHasHammerUpgrade3) {
+      let cost = hammerUpgrade3.find(".shop-cost");
+      cost.text("Purchased");
+    }
+    if (playerHasHammerUpgrade4) {
+      let cost = hammerUpgrade4.find(".shop-cost");
+      cost.text("Purchased");
+    }
+
+    // changes apprentice upgrade text to purchased if player has purchased
     if (playerHasApprentice) {
       let cost = hireApprentice.find(".shop-cost");
       cost.text("Purchased");
-      
-      return;
+    }
+    if (apprenticeHasUpgrade1) {
+      let cost = apprenticeUpgrade1.find(".shop-cost");
+      cost.text("Purchased");
+    }
+    if (apprenticeHasUpgrade2) {
+      let cost = apprenticeUpgrade2.find(".shop-cost");
+      cost.text("Purchased");
+    }
+    if (apprenticeHasUpgrade3) {
+      let cost = apprenticeUpgrade3.find(".shop-cost");
+      cost.text("Purchased");
     }
 
+    // dynamic cost color control if player can purchase or not
     for (var i = 0; i < costs.length; i++) {
-      let cost = costs[i].innerHTML;
+      let currentItemCost = $(costs[i]);
+      let currentItem = $(currentItemCost).parent();
+      
+      let cost = currentItemCost.text();
 
-      if (count >= cost) {
-        costs[i].classList.add("purchasable");
-      } else {
-        costs[i].classList.remove("purchasable");
+      if (count >= cost && shop_checkLockStatus(currentItem)) {
+        currentItemCost.addClass("green");
+      } 
+      else if (currentItemCost.text() === "Purchased") {
+        currentItemCost.addClass("green");
+      } 
+      else {
+        currentItemCost.removeClass("green");
+        currentItemCost.addClass("red");
       }
     }
   }
 
+  /**
+   * function: initShop
+   * desc: sets up preliminary shop status
+   */
+  function initShop() {
+    updateUpgradeCost(hireApprentice, cost_HireApprentice);
+    updateUpgradeCost(apprenticeUpgrade1, cost_ApprenUpgrade1);
+    updateUpgradeCost(apprenticeUpgrade2, cost_ApprenUpgrade2);
+    updateUpgradeCost(apprenticeUpgrade3, cost_ApprenUpgrade3);
+    updateUpgradeCost(hammerUpgrade1, cost_HammerUpgrade1);
+    updateUpgradeCost(hammerUpgrade2, cost_HammerUpgrade2);
+    updateUpgradeCost(hammerUpgrade3, cost_HammerUpgrade3);
+    updateUpgradeCost(hammerUpgrade4, cost_HammerUpgrade4);
+    updateShop();
+  }
+
   // HELPER/OTHER FUNCTIONS
   // ======================================================================
+
+  /**
+   * function: updateCounter()
+   * desc: helper function that updates UI when needed
+   */
+  function updateCounter() {
+    counter.text(count);
+    updateShop();
+  }
 
   /**
    * fucntion: updateUpgradeCosts()
@@ -266,14 +497,5 @@ $(document).ready(function() {
 
   function shakeAnvil() {
     anvil.toggleClass("shake");
-  }
-
-  /**
-   * function: updateCounter()
-   * desc: simple helper funtion to update counter when needed
-   */
-  function updateCounter() {
-    counter.text(count);
-    updateShop();
   }
 });
