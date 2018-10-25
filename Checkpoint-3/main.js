532f6372f23b52a36fca6e0ce83a8a08f1085122
Brandon Turner <brandontheturner@gmail.com>
2018-10-25 17:25:03 -0500

    checkpoint 3 work

--- Checkpoint-3/main.js
+++ Checkpoint-3/main.js
@@ -1,36 +1,23 @@
-// make a giant hand icon that clicks on different colored boxes that do different counts.
-// use the counter code and divide it up and apply it to this game
 $(document).ready(function () {
 
-  // When the user clicks on the '10' box, a separate counter counts up by 10.
-  // When the user clicks on the '100' box, a separate counter counts up by 100.
-  // Create a new box that says click all which triggers the effect of all boxes being clicked at the same time.
-  // When the page loads, a pop up appears saying 'Are you ready to count by ones, tens, and hundred?'
-  // Extra Credit: allow the user to drag and drop the shapes.
-
-  // when the document is done loading, run the script inside
   let num = 0;
   let num2 = 10;
   let num3 = 100;
-  $('#counter-1').click(function () {
+  $('#gun').click(function () {
     num++;
     $(this).text(num);
   })
-  $('#counter-10').click(function () {
-    num2 = num2 + 10;
-    $(this).text(num2);
-  })
-  $('#counter-100').click(function () {
-    num3 = num3 + 100;
-    $(this).text(num3);
-  })
-  $('#click-all').click(function () {
+  $('#click-box').click(function () {
     num++;
-    num2 = num2 + 10;
-    num3 = num3 + 100;
-    $('#counter-1').text(num);
-    $('#counter-10').text(num2);
-    $('#counter-100').text(num3);
+    $('#gun').text(num);
+    // on fire click CSS animation effect to shake gun
   })
-  window.alert('Are you ready to count by ones, tens, and hundreds?');
+  
+  window.alert('Pew Pew!!!');
 });
+
+// 3 boxes in a row containing different fire arms in a line across the bottom
+// ability to select different firearms at intervals of 10, 100, 1000 points
+// attach a sound effect to the fire button, that changes with the selection of different firearms
+// on click have a flash effect in the background
+// 
