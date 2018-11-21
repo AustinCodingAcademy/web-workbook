b391879608fa7f0b780e22a3590c3307ff192715
Collin Betori <cbetori@gmail.com>
2018-11-21 13:35:10 -0600

    Update

--- checkpoint-two/js/function.js
+++ checkpoint-two/js/function.js
@@ -1,12 +1,10 @@
 function navBar() {
-    var containerName = document.getElementById("container").className;
-    // document.getElementById("container").style.transition = "all 2s"
-    // document.getElementById("container").style.gridTemplateColumns = "150px auto auto auto auto";
-    
+    let containerName = document.getElementById("container").className;
+
   switch (containerName) {
     case "containerstyle":
       document.getElementById("container").className = "navstyle";
-      document.getElementById("navcol").style.display = "flex";
+      document.getElementById("navcol").style.display = "block";
       document.getElementById("container").style.transition = "all 2s";  
       break;
     case "navstyle":
@@ -15,4 +13,41 @@ function navBar() {
       document.getElementById("container").style.transition = "all 2s";
       break;
   }
+  let sales = ["Leads", "Vendors", "Statistics"]
+document.getElementsByClassName("section1").value = sales;
 }
+
+window.onload = function loadsales(){
+
+  let sales = ["Leads", "Vendors", "Statistics"];
+  let account = ["Information", "Details", "Statistics"];
+  let vendors = ["Recent", "List", "Pending"];
+  let help = ["Contact", "Questions"];
+
+  let result = "";
+
+  for(var i=0; i< sales.length; i++){
+    result += "<div class='navlist'>"+ sales[i] +'</div>';
+  }
+    document.getElementById("saleslist").innerHTML = result;
+    result = "";
+
+  for(var i=0; i< account.length; i++){
+    result += "<div class='navlist'>"+ account[i] +'</div>';
+  }
+    document.getElementById("accountlist").innerHTML = result;
+    result = "";
+
+  for(var i=0; i< vendors.length; i++){
+    result += "<div class='navlist'>"+ vendors[i] +'</div>';
+  }
+    document.getElementById("vendorlist").innerHTML = result;
+    result = "";
+
+  for(var i=0; i< help.length; i++){
+    result += "<div class='navlist'>"+ help[i] +'</div>';
+  }
+    document.getElementById("helplist").innerHTML = result;
+    result = "";
+
+  }
