148712494343f3439eaf5c180ff297879c288497
Aaron Altounian <aaron.altounian@gmail.com>
2018-11-30 04:16:01 -0600

    add clear list function, beat head against wall trying to remove individual li's in ul with span onclick, unsuccessful

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,20 +1,52 @@
 'use strict';
 document.addEventListener("DOMContentLoaded", function(event){
-  //You code here
+
   let num = document.querySelectorAll('li').length;
   let title = document.querySelector('h1');
-  
+
+  //Alert with number of items in cart on page load
   alert("There are" + " " + num + " " + "items in your cart.");
 
+  //Insert html h2 with number of items in cart after h1
   title.insertAdjacentHTML('afterend', '<h2>There are' + ' ' + num + ' ' + 'items in your cart</h2>');
 
-});
+  //Function to update cart count:
+  function updateCount() {
+    document.querySelector('h2').innerHTML = 'There are' + ' ' + document.querySelectorAll('li').length + ' ' + 'items in your cart.';
+  }
 
-function addListItem() {
-  let text = document.querySelector('#addToCart').value;
-  let li = '<li>' + text + '</li>';
-  let ul = document.querySelector('#cart');
-  ul.insertAdjacentHTML('beforeend', li);
-  console.log(text);
-  console.log(li);
-}
+  //Add new items to cart from input field, clear field and update cart count
+  document.querySelector('#add').onclick = function() {
+    // Get text from input field:
+    let text = document.querySelector('#addToCart').value;
+    // Create new li string with text from input field:
+    let li = '<li>' + text + '<span class="delete">&times;</span></li>';
+    // Shorten query selector for ul so it's easier to read:
+    let ul = document.querySelector('#cart');
+    // Insert new html li string at the end of the ul, but not if text form is blank:
+    if(text == '') {
+    } else {
+      ul.insertAdjacentHTML('beforeend', li);
+      // Clear input form:
+      document.querySelector('#addToCart').value = '';
+      // Update h2 with new cart count:
+      updateCount();
+    }
+  }
+
+  //Clear entire cart function
+  document.querySelector('#clear').onclick = function() {
+    document.querySelector('ul').innerHTML = '';
+    updateCount();
+  }
+
+/*
+  //Trying to delete individual lines from list
+  let del = document.querySelectorAll('#delete');
+  let line = document.querySelectorAll('li');
+
+  for(var i = 0; i < num; i++){
+    console.log(i);
+  }
+*/
+});
