4235e895913cac298f2680703d15c6c5523f2dd8
Aaron Altounian <aaron.altounian@gmail.com>
2018-11-29 21:04:31 -0600

    started work

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,20 @@
 'use strict';
+document.addEventListener("DOMContentLoaded", function(event){
+  //You code here
+  let num = document.querySelectorAll('li').length;
+  let title = document.querySelector('h1');
+  
+  alert("There are" + " " + num + " " + "items in your cart.");
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+  title.insertAdjacentHTML('afterend', '<h2>There are' + ' ' + num + ' ' + 'items in your cart</h2>');
+
 });
+
+function addListItem() {
+  let text = document.querySelector('#addToCart').value;
+  let li = '<li>' + text + '</li>';
+  let ul = document.querySelector('#cart');
+  ul.insertAdjacentHTML('beforeend', li);
+  console.log(text);
+  console.log(li);
+}
