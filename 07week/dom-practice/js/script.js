6821152ed2b41531a3e4a549578c7c9b5db39eae
Andrew Sommere <andrew.conrad.sommer@gmail.com>
2018-12-04 16:58:22 -0600

    dom pratice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,24 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+
+  let num = document.querySelectorAll("li").length;
+
+  alert ("There are" + " " + num  + " " + "items in your cart");
+
+  let title = document.querySelector("h1");
+
+  title.insertAdjacentHTML("afterend", "<h2> You have" + " " + num + " " + "items in your cart. </h2>");;
+
+  function myFunction() {
+
+    var x = document.getElementById("listItem");
+    var node = document.createElement("li");
+    var textnode = document.createTextNode("x")
+    document.getElementById(myList).innerHTML = x;
+  }
+
+  function deleteItemOne() {
+    document.getElementById("itemOne").innerHTML = " "
+  }
 });
