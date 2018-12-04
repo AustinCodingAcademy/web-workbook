9d9fb281e776fad36689220da140123a32545188
samozen <samanthaozen@hotmail.com>
2018-12-04 16:56:12 -0600

    dom practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,29 @@
-'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+var numberofitems = (dcoument.querySelectorAll('#shoppinglist li').length);
+
+alert(numberofitems);
+
+var newh2 = document.createElement("h2");
+var node = document.createTextNode("You have "+ numberofitems + " items in your shopping cart");
+newh2.appendChild(node);
+var element = document.getElementById("theshoppinglist");
+element.appendChild(newh2);
+
+function addItemFunction() {
+
+  var newitem = document.createElement("li");
+  var node = document.createTextNode("test")
+  newitem.appendChild(node);
+
+  var element = document.getElementById("shoppinglist");
+  element.appendChild(newitem);
+}
+
+function deleteItemFunction() {
+
+  var shoppinglist = document.getElementById("shoppinglist");
+  shoppinglist.removeChild(shoppinglist.childNodes[0]); 
+
+}
+
+
