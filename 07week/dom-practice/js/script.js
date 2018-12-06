d9ba4123fcd55b62cfa885e8eebe555051f393c3
Andrew Sommere <andrew.conrad.sommer@gmail.com>
2018-12-05 22:53:49 -0600

    new dom pratice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,24 +1,33 @@
 'use strict';
+ document.addEventListener("DOMContentLoaded", function(event) {
+  // You code here
+let num = document.querySelectorAll("li").length;
+   alert ("There are" + " " + num  + " " + "items in your cart");
+   let title = document.querySelector("h1");
+   title.insertAdjacentHTML("afterend", "<h2> You have" + " " + num + " " + "items in your cart. </h2>");
 
-document.addEventListener("DOMContentLoaded", function(event) {
-
-  let num = document.querySelectorAll("li").length;
-
-  alert ("There are" + " " + num  + " " + "items in your cart");
-
-  let title = document.querySelector("h1");
-
-  title.insertAdjacentHTML("afterend", "<h2> You have" + " " + num + " " + "items in your cart. </h2>");;
-
-  function myFunction() {
-
-    var x = document.getElementById("listItem");
-    var node = document.createElement("li");
-    var textnode = document.createTextNode("x")
-    document.getElementById(myList).innerHTML = x;
+   function deleteItem(id) {
+    id.style.display = "none";
   }
 
-  function deleteItemOne() {
-    document.getElementById("itemOne").innerHTML = " "
+  function deleteItem(id) {
+    id.style.display = "none";
+}
+
+  function addItem() {
+    var ul = document.getElementById("myList");
+    var listItem = document.getElementById('listItem');
+    var li = document.createElement("li");
+    li.setAttribute('id'.listItem.value);
+    li.appendChild(document.createTextNode(listItem.value));
+    ul.appendChild(li);
+}
+
+
+  function deleteItem(id) {
+    id.style.display = "none";
   }
-});
+
+
+  
+})
