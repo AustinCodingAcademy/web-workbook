ba5c71641130515b6228568c8e1738ca9aaa4171
Collin Betori <cbetori@gmail.com>
2018-12-11 12:57:40 -0600

    Final tic tac toe

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//All possible winning combinations
 const winState = [
   //horizontal
   [0, 1, 2],
@@ -14,71 +15,72 @@ const winState = [
   [2, 4, 6]
 ]
 
-function testWinner() {
-  for (let i = 0; i < winState.length; i++) {
-    let idNumber = '_' + i;
-    let final = document.getElementById(idNumber).innerHTML;
-    for (let x = 0; x < winState[i].length; x++) {
-      // if (winState[i][x] = )
-      if (final == 'x') {
-        let text =+ 1;
-        console.log(text);
-      }
-
-      //if (document.querySelectorAll('square')[i]= winState[i]){
-
-    }
-  }
-  
-}
-
-
+//On start 
 $(document).ready(function () {
-  testWinner();
-  //Every other click (either X or O)
   let click = 0;
   $('.square').click(function () {
-    if ($(this).text() == "") {
+    //If the square is already filled in or a winner has been announced this will prevent additional clicks
+    if ($(this).text() == "" && $('#announce-winner').text() == '' ) {
+      //Every other click is an X or O based on even or odd number of clicks
       if (click % 2 == 0) {
         $(this).text('x');
       } else {
         $(this).text('o');
       }
+      //Counts click to determine even or odd click 
       click = click + 1;
-      //winnerCheck();
+      //After every click it checks for a winner
       testWinner();
     }
+    //If every square has been clicked on but no winner declared it will through a draw
     if (click == 9) {
-
-      conclusion();
-      testWinner();
+      draw();
     }
   });
 
-  //Check for winner
-  function winnerCheck() {
-    for (let i; i > 3; i++)
-      $('.win' + i).each(function () {
-        let currentValue = $(this).text();
-        console.log("true")
-        if (currentValue == previousValue) {
+  //Determines if anyone has won the game
+  function testWinner() {
+    //Loops 8 times times to check each individual array
+    for (let i = 0; i < winState.length; i++) {
+      //Variables counts the number of times X or O is present in each indivial array if the number = 3 then a winner is chosen
+      let xCount = 0;
+      let oCount = 0;
+      //Loops 3 times to review values of each individual array
+      for (let x = 0; x < winState[i].length; x++) {
+        //Variable is used to concatonate id name in order to check potential winning cells
+        let posisition = winState[i][x];
+  
+        //Check if xCount is equal to three if so then throws winner alert
+        if ($('#_' + posisition).text() == 'x') {
+          xCount = xCount + 1;
+          if (xCount == 3) {
+            $('#announce-winner').text('X IS THE WINNER');
+            click = 0;
+          }
 
+         //Check if oCount is equal to three if so then throws winner alert
+        } else if ($('#_' + posisition).text() == 'o'){
+          oCount = oCount+1;
+          if (oCount == 3) {
+            $('#announce-winner').text('O IS THE WINNER');
+            click = 0;
+          }
         }
-      });
-    let previousValue = $(this).text();
-  };
+      }
+    }
+  }
 
-  //Announce winner or draw
-  function conclusion() {
+  //Announce draw if necassary
+  function draw() {
     $('#announce-winner').text('DRAW');
   };
-
+  
   //Clear everything from board
   $('#clear').click(function () {
     $('.square').each(function () {
       $(this).text('');
     });
     $('#announce-winner').text('');
+    click = 0;
   });
-
 });
