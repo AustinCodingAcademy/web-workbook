dfe703c4c8924c6b4ed11b27b7dde9842afcfb3c
Jim Schaefer <jschaef80@gmail.com>
2018-12-13 12:49:58 -0600

    towers-of-hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,17 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
+$(document).ready(function () {
+
+  var $block = null;
+  $('[data-stack]').click(function () {
+    if ($block && ($block.data("block") < $(this).children().last().data("block") || $(this).children().last().data() == undefined)) {
+      $(this).append($block);
+      $block = null;
+    } else if ($block && ($block.data("block") > $(this).children().last().data("block"))) {
+      return;
+    } else {
+      $block = $(this).children().last().detach();
+    }
+  });
+
 });
