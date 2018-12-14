98245d8f8893f8a9c1ef0d8a1c4adea5aa417650
Collin Betori <cbetori@gmail.com>
2018-12-13 18:29:16 -0600

    Final Towers of Hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -11,11 +11,20 @@ $(document).ready(function () {
     if (block && (currentValue > oldvalue || currentValue === undefined)) {
       $(this).append(block);
       block = null;
-    } else if (block == null){
+      checkWin();
+    } else if (block == null) {
       oldvalue = $(this).children().last().width();
       block = $(this).children().last().detach();
 
     }
-    console.log(currentValue)
+    console.log($('#stack3').children().length)
+    
   })
+
+  function checkWin() {
+    if ($('#stack3').children().length == 4){
+      $('#announce-game-won').text('YOU WON');
+    }
+  }
+
 });
