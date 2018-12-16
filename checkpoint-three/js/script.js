3e09327750f52d26e582d0ae3e94ac86dc52d5c1
Aaron Altounian <aaron.altounian@gmail.com>
2018-12-16 03:10:45 -0600

    trying to figure out how to get data-val multiplier to work

--- checkpoint-three/js/script.js
+++ checkpoint-three/js/script.js
@@ -1,36 +1,38 @@
 $(document).ready(function(){
-
+    // define variable attributes for holding data:
     var data = {
         totalCurrent: 0,
         totalSHU: 0
     }
-
+    // define pepper array for changing pepper type:
     var pepperArray = [
         "Bell", "Melrose", "Italian Sweet", "Paprika Supreme", "Trinidad", "Pimento", "Banana", "Santa Fe", "Anaheim", "Shishito", "Italian Long Hot", "Beaver Dam", "Aji Panca", "Poblano", "Numex Big Jim", "Mulato Chile", "Numex Heritage 6-4", "Mirasol Chile", "Guajillo Chile", "Cacho de Cabra", "Chimayo Chile", "Rezha Macedonian", "Kung Pao", "Jalapeno", "Aleppo", "Ring-O-Fire Cayenne", "Serrano", "Jwala Finger Hot", "Bolivian Rainbow", "Hot Golden Nugget", "Guntur Sannam Chile", "Tabasco", "Super Chili", "Santaka", "Manzano", "Pequin", "Prairie Fire", "Tianjin", "Thai Super Chile", "Tabiche", "Carolina Cayenne", "Rocoto", "Datil", "Habanero", "Tiger Paw Habanero", "Trinidad 7", "Bhut Jolokia", "Super Naga", "Komodo Dragon", "Trinidad Scorpion", "Carolina Reaper", "Dragon's Breath", "Pepper X"
     ]
-   
+    // set pepper to the first one
     $('#type').children('span').text(pepperArray[0]);
 
+    //make function to update the shu's per second and total shu count
     function updateReport() {
         $('#scovilles').text(data.totalCurrent);
         $('#perSecond').text(data.totalSHU);
+
     }
 
+    // set interval for function to execute once every second
     setInterval(heat,1000);
 
+    // add shu's per second number to total shu's
     function heat() {
         data.totalCurrent += data.totalSHU;
         updateReport();
     }
 
+    // add 1 to total scoville's when pepper is clicked:
     $('#pepper').click(function(){
         data.totalCurrent++;
         updateReport();
     });
 
-    function purchase() {
-        
-    }
     $('.button').click(function(){
         let cost = $(this).data('cost');
         let val = $(this).data('val');
@@ -49,18 +51,27 @@ $(document).ready(function(){
     let i = 0;
     $('.unlock').click(function(){
         let cost = $(this).data('cost');
-        let val = $(this).data('val');
         let span = $(this).children('span');
         let type = $('#type').children('span');
         if(cost <= data.totalCurrent) {
             data.totalCurrent -= parseInt(cost);
-            data.totalSHU += parseInt(val);
-            span.html(parseInt(span.html() * 2));
-            $(this).data('cost', parseInt(cost * 2));
+            data.totalSHU = Math.floor(data.totalSHU * 1.15);
+            span.html(parseInt(span.html() * 1.25));
+            $(this).data('cost', parseInt(cost * 1.25));
             i++;
         }
         updateReport();
         type.text(pepperArray[i]);
     })
-        
+    
+    $('#research').click(function(){
+        let button = $('.button');
+        let span = $('label').children('span');
+        //if(button.data('cost') <= data.totalCurrent){
+            console.log('works');
+            button.data('val', button.data('val') * 2);
+            console.log(button.data('val'));
+            span.html(span.html() * 2);
+        //}
+    })
 })
