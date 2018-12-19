5fdc1bc7e85ff826e0611a5b7382ceeadd6610f2
Aaron Altounian <aaron.altounian@gmail.com>
2018-12-18 19:04:33 -0600

    commented js and did a few minor tweaks

--- checkpoint-three/js/script.js
+++ checkpoint-three/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     // define variable attributes for holding data:
     var data = {
         totalCurrent: 0,
@@ -19,7 +20,7 @@ $(document).ready(function(){
     }
 
     // set interval for function to execute once every second
-    setInterval(heat,3000);
+    setInterval(heat,2000);
 
     // add shu's per second number to total shu's
     function heat() {
@@ -33,51 +34,64 @@ $(document).ready(function(){
         updateReport();
     });
 
+    // clicking one of the upgrades
     $('.button').click(function(){
         let cost = $(this).data('cost');
         let val = $(this).data('val');
         let span = $(this).children('span');
 
         if(cost <= data.totalCurrent) {
+            // subtract cost of button from total:
             data.totalCurrent -= parseInt(cost);
+            // add the per 3 seconds value to the SHUs per 3 seconds:
             data.totalSHU += parseInt(val);
+            // make the button 20% more expensive:
             span.html(parseInt(span.html() * 1.2));
             $(this).data('cost', parseInt(cost * 1.2));
         }
         updateReport();
-        console.log(data.totalSHU);
     });
 
     let i = 0;
 
+    // unlock new pepper button:
     $('.unlock').click(function(){
         let cost = $(this).data('cost');
         let span = $(this).children('span');
         let type = $('#type').children('span');
         if(cost <= data.totalCurrent) {
+            // subtract cost of button from total:
             data.totalCurrent -= parseInt(cost);
+            // add 15% more units per 3s:
             data.totalSHU = Math.floor(data.totalSHU * 1.15);
+            // make next pepper upgrade more expensive:
             span.html(parseInt(Math.floor(span.html() * 1.4)));
             $(this).data('cost', parseInt(Math.floor(cost * 1.4)));
+            // advance pepper array variable by 1:
             i++;
         }
         updateReport();
+        // change pepper type using array:
         type.text(pepperArray[i]);
     })
-    
+
+    // research better techniques button:
     $('#research').click(function(){
         let button = $('.button');
         let span = $('label').children('span');
         let cost = $(this).data('cost');
         
         if(button.data('cost') <= data.totalCurrent){
+            // subtract cost from total:
             data.totalCurrent -= parseInt(cost);
+            // go thru each upgrae and double its per 3s value:
             button.each(function(){
                 $(this).data('val', $(this).data('val') *2);
             })
             span.each(function(){
                 $(this).html($(this).html() * 2);
             })
+            // increase cost of research by 80%:
             $(this).data('cost', Math.floor(cost * 1.8));
             $('#researchCost').html(Math.floor($('#researchCost').html() * 1.8));
         }
