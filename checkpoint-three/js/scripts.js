c752ca454eab9f5d2515b4ccd03d79a48727f5c1
Collin Betori <cbetori@gmail.com>
2018-12-19 17:39:03 -0600

    Updated checkpoint three

--- checkpoint-three/js/scripts.js
+++ checkpoint-three/js/scripts.js
@@ -1,33 +1,57 @@
 // 'use strict';
 
+let damage = 0;
+
+let charCount = 0;
+let charCost = 10;
+let charNum = 79;
+let charArray = Array.apply(null, {length: charNum}).map(Number.call, Number);
+
+
 $(document).ready(function () {
 
-    let x = 0;
+    function updateCounter(){
+        $('#count').text(damage);
+    }
+
+    function updateCharacterCost(){
+        $('#cost_1').text(charCost);
+    }
 
     $('#symbol').click(function () {
-        x += 1;
-        $('#count').text(x);
-        console.log(x);
+        damage += 1;
+        updateCounter();
     });
 
+    //Adds additional character
     $('#btn_1').click(function () {
-        x += 1;
-        console.log(x);
+        if ($('#count').text()>=charCost && charCount<=79){
+        $('<img class="characters" src="pics/char_'+charCount+'.png">').insertAfter('#symbol')
+        updateCounter();
+        damage -= charCost;
+        charCount += 1;
+        charCost += Math.round(charCost*.2);
+        $('#counter_1').text(charCount+"/80")
+        animateDiv();
+        console.log(w);      
+        console.log(h);
+        updateCharacterCost();
+        }
     });
 
     $('#btn_2').click(function () {
-        x += 1;
-        console.log(x);
+        damage += 1;
+        updateCounter();
     });
 
     $('#btn_3').click(function () {
         x += 1;
-        console.log(x);
+        updateCounter();
     });
 
     $('#btn_4').click(function () {
-        x += 1;
-        console.log(x);
+        damage += 1;
+        updateCounter();
     });
 
     $('#reset').click(function () {
@@ -41,11 +65,34 @@ $(document).ready(function () {
         $(this).addClass('shake-hard shake-constant');
         
         setTimeout(function(){
-            $('#symbol').removeClass('shake-hard shake-constant');
-        console.log("true")},100);
+            $('#symbol').removeClass('shake-hard shake-constant')},100);
     });
 
     function timeOut() {
         setTimeout(shake, 3);
     }
+
+    console.log($('#wrapper').height())
 });
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    let h = $('#wrapper').height() - 250;
+    let w = $('#wrapper').width() - 50;
+    
+    let nh = Math.floor(Math.random() * h);
+    let nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(){
+    let newq = makeNewPosition();
+    $('.characters').animate({ top: newq[0], left: newq[1] }, function(){
+      animateDiv();  
+  
+    });
+    
+};
