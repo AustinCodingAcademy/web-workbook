d4347f783d805cb8a3b9eb14f297e4518712f5a1
Amber Jones <amberlovescats14@gmail.com>
2019-01-27 21:17:28 -0600

    pushing up my new javascript and jquery projects

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,42 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
-});
+  let player1 = 'x'
+  let player2 = 'o'
+  let play = 'x'
+  let dataCell = $('div[data-cell]')
+  let playArray = []
+
+  dataCell.click(function(){
+    if (play === 'x') {
+      $(this).append('o')
+      play = 'o'
+      playArray[attrVal] = play
+
+    } else {
+      $(this).append('x')
+      play = 'x'
+      playArray[attrVal] = play
+    }
+    readDataCellValue($this)
+  })
+  function readDataCellValue(cell){
+    cell
+  }
+  $('button').on('click', function(){
+    let attrVal = cell.attr('data-cell')
+
+    // console.log(cell.attr('data-cell'))
+
+  })
+  dataCell.draggable()
+  })
+  // determine a winner by datacell [0,1,2], [3,4,5], [6, 7, 8]
+  //diagonal
+  //var player1 = and player2
+  // clear board!!!
+  // append an x or o 
+  //***player can add to many to one square */
+
+  //practice drage and drop
+
--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,81 @@
 'use strict';
+$(document).ready(function(){
 
-$(document).ready(function() {
-  // Put app logic here
-});
+  let $block = null;
+  let $pickUp = null;
+  let $dropOff = null;
+  
+// this picks up the block number
+$('[data-stack]').click(function(){
+  if($block) {
+  
+    $dropOff=$(this).children().last().data("block");
+    console.log("I am dropping off this here" + $dropOff);
+  } else {
+  $pickUp= $(this).children().last().data("block");
+  console.log($pickUp)
+  }
+  //this prevents moves that aren't allowed and actually moves the blocks
+  if ((($block) && ($pickUp > $dropOff)) || (($block) && ($dropOff === undefined))) {
+    $(this).append($block);
+    $block = null;
+  } else if ($block === null) {
+    $block = $(this).children().last().detach();
+  } else {
+    alert("You can't put this here. Try again!");
+  }
+  setTimeout(checkWin, 300)
+  })
+  function checkWin() {
+    if ($('[data-stack=3]').children().length === 3) {
+      alert("You Win!")
+    }
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const block = document.querySelectorAll('.block');
+
+  // function columnDrop() {
+  //   const column = this.column.add;
+  //   document.documentElement.addDiv(`${this.block}`, this.value + column)
+    
+
+  // }
+  
+  // inputs.forEach(input => input.eventTarget('mouseleave', columnDrop));
+
+  // inputs.forEach(input => input.addEventListener('mousemove', columnDrop))
+ 
+
+
+  // $('.post').droppable()
+  // $('.post').droppable({
+  //   function(){
+  //     let column = $('this')
+  //     let [red = 1, green = 2, blue = 3 ]
+  //     red.append.append('this')
+  //   }
+
+  //   })
+  
