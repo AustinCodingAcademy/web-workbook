395b7646d66863a543a77f2df83998c21c2271ae
Emilio Sanchez <sanchez.emilio483@gmail.com>
2019-01-29 18:20:53 -0600

    made the boxes draggable

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
     play = 'X'
     $('#turn').text("X's Turn")
   })
-  // dataCell.draggable();
+  dataCell.draggable();
   function victory(){
   //determine a winner by data-cell {0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, 
   //diagnols{0,4,,8}, {2,4,6}
