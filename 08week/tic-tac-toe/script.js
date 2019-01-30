c4cf1563148f4b5c81dc1409069bc01ba68c4555
Jessica Pena <penajessica123@yahoo.com>
2019-01-30 13:36:06 -0600

    added content to projects

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,49 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+  let player1 = 'x'
+  let player2 = 'o'
+  let play = 'x'
+  let dataCell = $('div[data-cell]')
+  let playArray = []
+ 
+  dataCell.click(function() {
+    if (play === 'x') {
+      $(this).append('o')
+      play = 'o'
+    } else {
+      $(this).append('x')
+      play = 'x'
+    }
+    readDataCellValue(cell)
+  })
+
+
+  function readDataCellValue(cell) {
+    let attrVal = cell.attr('data-cell')
+    console.log(cell.attr('data-cell'))
+  }
+  $('#clear').on('click', function(){
+    dataCell.empty()
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
