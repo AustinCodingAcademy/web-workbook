dfe6b50fd9afd57ca42306576fd4579712f5c5a9
Jessica Pena <penajessica123@yahoo.com>
2019-01-31 17:41:47 -0600

    updated towers of hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,24 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+$('.draggable').draggable({
+revert: true
 });
+
+    $('#dropArea, #dropArea, #dropArea').droppable({
+      //accepts only items with moveable class.
+      accept: ".draggable",
+      drop: function (event, ui){
+      $(ui.draggable).appendTo(this).attr('style', 'position: relative');
+
+      checkWin()
+  }
+});
+})
+
+function checkWin(){
+  if ($('[data-stack="3"]').children().length ==4){
+  $('#announce-game-won').text('Winner');
+  stop();
+  }
+}
