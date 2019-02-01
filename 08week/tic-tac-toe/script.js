b7f072d43079e94a480d4272523f172928615d11
erickaherod <erickaherod@gmail.com>
2019-01-31 18:16:38 -0600

    tic-tac-toe

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -2,4 +2,63 @@
 
 $(document).ready(function() {
   // Put app logic in here
-});
+  /// determine winner by (0,1,2)
+  /// var player1, player 2, count
+  /// function for clear board
+  /// 
+
+
+let player1 = 'x'
+let player2 = 'o'
+let play = 'x'
+let dataCell = $('div[data-cell]')
+
+dataCell.click(function(){
+if (play === 'x') {
+  $(this).append('o')  
+  play = '0'
+  } else {
+    $(this).append('x')
+    play = 'x'
+  }
+  function readDataCellValue(cell){
+    let attrVal = cell.attr('data-cell')
+
+  }
+
+})
+
+$('#clear').on('click', function() {
+  dataCell.empty()
+})
+
+
+///drag and drop practice
+dataCell.draggable()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+///drag and drop practice
