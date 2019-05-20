a4afea1b89e2a2d28ceed6dd5ec8694f4ce68911
Amber Jones <amberlovescats14@gmail.com>
2019-05-20 10:05:53 -0500

    button

--- 01week/helloworld/script.js
+++ 01week/helloworld/script.js
@@ -1,10 +1,15 @@
-
- const eventbrite = () => {
+const eventBrite = () => {
     fetch('https://www.eventbrite.com/oauth/authorize?response_type=KXBIEIL3SS3GWKDXSU7F&client_id=XMP73B2OF2SBKFJZNX&redirect_uri=https://amberlovescats14.github.io/web-workbook/01week/helloworld/')
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data)
-       
-      })
+        .then((res) => res.json())
+        .then((data) => {
+          // document.getElementsByClassName('get').innerHTML = data;
+          console.log(data)
+        
+        })
   }
-  eventbrite()
+  eventBrite()
+  
+  // This is the link for the documentation
+  //https://www.eventbrite.com/platform/api#/introduction/quick-start
+  
+  // I put a screen shot of my keys in the repository as an image
